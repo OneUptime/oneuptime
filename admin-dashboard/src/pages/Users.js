e3f6b4fc9fec243bd7ddaebf2c3be95853c70794
ioedeveloper <daviddisu8@gmail.com>
2019-08-05 15:10:39 +0100

    Users List UI

--- admin-dashboard/src/pages/Users.js
+++ admin-dashboard/src/pages/Users.js
@@ -26,7 +26,7 @@ class Users extends Component {
 								>
                                     <div className="bs-BIM">
                                         <div className="Box-root Margin-bottom--12">
-                                            <div className="bs-ContentSection Card-root Card-shadow--medium">
+                                        <div className="bs-ContentSection Card-root Card-shadow--medium">
                                             <div className="Box-root">
                                                 <div className="ContentHeader Box-root Box-background--white Box-divider--surface-bottom-1 Flex-flex Flex-direction--column Padding-horizontal--20 Padding-vertical--16">
                                                     <div className="Box-root Flex-flex Flex-direction--row Flex-justifyContent--spaceBetween">
@@ -99,9 +99,47 @@ class Users extends Component {
                                                         </div>
                                                     </div>
                                                 </div>
+                                                <div className="bs-Tail bs-Tail--separated bs-Tail--short">
+
+                                            <div className="bs-Tail-copy">
+                                                <span>
+                                                    1 Team Member
+                                                </span></div>
+                                                    <div className="bs-Tail-actions">
+                                                        <div className="ButtonGroup Box-root Flex-flex Flex-alignItems--center Flex-direction--row Flex-justifyContent--flexStart">
+                                                            <div className="Box-root Margin-right--8">
+                                                                <button
+                                                                    data-test="TeamSettings-paginationButton"
+                                                                    className={`Button bs-ButtonLegacy`}
+                                                                    type="button"
+                                                                >
+                                                                    <div className="Button-fill bs-ButtonLegacy-fill Box-root Box-background--white Flex-inlineFlex Flex-alignItems--center Flex-direction--row Padding-horizontal--8 Padding-vertical--4">
+                                                                        <span className="Button-label Text-color--default Text-display--inline Text-fontSize--14 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--base Text-wrap--noWrap">
+                                                                            <span>Previous</span>
+                                                                        </span>
+                                                                    </div>
+                                                                </button>
+                                                            </div>
+                                                            <div className="Box-root">
+                                                                <button
+                                                                    data-test="TeamSettings-paginationButton"
+                                                                    className={`Button bs-ButtonLegacy`}
+                                                                    type="button"
+                                                                >
+                                                                    <div className="Button-fill bs-ButtonLegacy-fill Box-root Box-background--white Flex-inlineFlex Flex-alignItems--center Flex-direction--row Padding-horizontal--8 Padding-vertical--4">
+                                                                        <span className="Button-label Text-color--default Text-display--inline Text-fontSize--14 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--base Text-wrap--noWrap">
+                                                                            <span>Next</span>
+                                                                        </span>
+                                                                    </div>
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    </div>
+                                            </div>
                                             </div>
                                         </div>
-                                    </div>
+                                        
                                 </div>  
                             </div>
                         </div>
