3aad3ca0ea8d4eff8c1fbc4363e3fb183cd42427
Nilanshu <nilanshu.kumar1994@gmail.com>
2019-08-08 15:07:16 +0530

    completed probe ui and services

--- backend/backend/middlewares/probeAuthorization.js
+++ backend/backend/middlewares/probeAuthorization.js
@@ -5,55 +5,61 @@
  */
 var ProbeService = require('../services/probeService');
 var sendErrorResponse = require('../middlewares/response').sendErrorResponse;
+var ErrorService = require('../services/errorService');
 
 module.exports = {
     isAuthorizedProbe: async function (req, res, next) {
-        let probeKey,probeName;
+        let probeKey, probeName;
 
-        if(req.params.probeKey){
+        if (req.params.probeKey) {
             probeKey = req.params.probeKey;
-        }else if(req.query.probeKey){
+        } else if (req.query.probeKey) {
             probeKey = req.query.probeKey;
-        }else if(req.headers['probeKey']){
+        } else if (req.headers['probeKey']) {
             probeKey = req.headers['probeKey'];
-        }else if(req.headers['probekey']){
+        } else if (req.headers['probekey']) {
             probeKey = req.headers['probekey'];
-        }else if(req.body.probeKey){
+        } else if (req.body.probeKey) {
             probeKey = req.body.probeKey;
-        }else{
-            return sendErrorResponse( req, res, {
+        } else {
+            return sendErrorResponse(req, res, {
                 code: 400,
                 message: 'Probe Key not found.'
             });
         }
 
-        if(req.params.probeName){
+        if (req.params.probeName) {
             probeName = req.params.probeName;
-        }else if(req.query.probeName){
+        } else if (req.query.probeName) {
             probeName = req.query.probeName;
-        }else if(req.headers['probeName']){
+        } else if (req.headers['probeName']) {
             probeName = req.headers['probeName'];
-        }else if(req.headers['probename']){
+        } else if (req.headers['probename']) {
             probeName = req.headers['probename'];
-        }else if(req.body.probeName){
+        } else if (req.body.probeName) {
             probeName = req.body.probeName;
-        }else{
-            return sendErrorResponse( req, res, {
+        } else {
+            return sendErrorResponse(req, res, {
                 code: 400,
                 message: 'Probe Name not found.'
             });
         }
-
-        var probe = await ProbeService.findOneBy({probeKey});
-        if( probe && probe.probeName && probe.probeName === probeName){
-            req.probe = {};
-            req.probe.id = probe._id;
-            next();
-        }else{
-            return sendErrorResponse( req, res, {
-                code: 400,
-                message: 'Probe key and probe name do not match.'
-            });
+        try {
+            var probe = await ProbeService.findOneBy({ probeKey });
+            if (probe && probe.probeName && probe.probeName === probeName) {
+                req.probe = {};
+                req.probe.id = probe._id;
+                await ProbeService.updateProbeStatus(probe._id);
+                next();
+            } else {
+                return sendErrorResponse(req, res, {
+                    code: 400,
+                    message: 'Probe key and probe name do not match.'
+                });
+            }
+        } catch (error) {
+            ErrorService.log('isAuthorizedProbe', error);
+            throw error;
         }
     }
 };
---
+++ dashboard/src/reducers/probe.js
@@ -0,0 +1,74 @@
+import * as types from '../constants/probe'
+
+
+const initialState = {
+    probes: {
+        requesting: false,
+        error: null,
+        success: false,
+        data: [],
+        count: null,
+        limit: null,
+        skip: null
+    }
+};
+
+
+export default function probes(state = initialState, action) {
+    switch (action.type) {
+        case types.PROBE_SUCCESS:
+            return Object.assign({}, state, {
+                probes: {
+                    requesting: false,
+                    error: null,
+                    success: true,
+                    data: action.payload.data,
+                    count: action.payload.count,
+                    limit: action.payload.limit,
+                    skip: action.payload.skip
+                },
+            });
+
+        case types.PROBE_REQUEST:
+            return Object.assign({}, state, {
+                probes: {
+                    requesting: true,
+                    error: null,
+                    success: false,
+                    data: state.probes.data,
+                    count: state.probes.count,
+                    limit: state.probes.limit,
+                    skip: state.probes.skip
+                }
+            });
+
+
+        case types.PROBE_FAILED:
+            return Object.assign({}, state, {
+                probes: {
+                    requesting: false,
+                    error: action.payload,
+                    success: false,
+                    data: state.probes.data,
+                    count: state.probes.count,
+                    limit: state.probes.limit,
+                    skip: state.probes.skip
+                }
+            });
+
+        case types.PROBE_RESET:
+            return Object.assign({}, state, {
+                probes: {
+                    requesting: false,
+                    error: null,
+                    success: false,
+                    data: [],
+                    count: null,
+                    limit: null,
+                    skip: null
+                }
+            });
+
+        default: return state;
+    }
+}
