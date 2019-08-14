a71c1a4fabfbdeba9e68b974c15895068c6cdfe8
Rajat Soni <rajat.tcp@gmail.com>
2019-08-14 13:02:34 +0530

    fixed all relative path

--- kubernetes/ci/setup-cluster.sh
+++ kubernetes/ci/setup-cluster.sh
@@ -14,7 +14,7 @@ echo "RUNNING COMMAND: sudo kubectl create secret docker-registry gitlabcredv2 -
 sudo kubectl create secret docker-registry gitlabcredv2 --docker-server=$DOCKER_REGISTRY_SERVER --docker-username=$DOCKER_USER --docker-password=$DOCKER_PASSWORD --docker-email=$DOCKER_EMAIL
 # Create the entire cluster.
 echo "RUNNING COMMAND: sudo kubectl create -f test-server.yaml"
-sudo kubectl create -f ./ci/ci-server.yaml
+sudo kubectl create -f ./kubernetes/ci/ci-server.yaml
 # Wait for all the services to come online.
 echo "RUNNING COMMAND: echo 'Wait for 5 mins....'"
 echo 'Wait for 5 mins....'
