c98a6b93c38618eaba2c1dd1f7bf018ba53ab8dd
Rajat Soni <rajat.tcp@gmail.com>
2019-08-16 16:31:53 +0530

    refactor tests to run in pipeline

--- dashboard/src/test/puppeteer/MonitorSubProject.test.js
+++ dashboard/src/test/puppeteer/MonitorSubProject.test.js
@@ -25,92 +25,94 @@ const newUser = {
 
 let subProjectName = utils.generateRandomString();
 
-beforeAll(async (done) => {
-    jest.setTimeout(200000);
-    // browser for parent user
-    browser1 = await puppeteer.launch();
-    page = await browser1.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
 
-    // intercept request and mock response for login
-    await page.setRequestInterception(true);
-    await page.on('request', async (request)=>{
-        if((await request.url()).match(/user\/login/)){
-            request.respond({
-                status: 200,
-                contentType: 'application/json',
-                body: JSON.stringify(userCredentials)
-            });
-        }else{
-            request.continue();
-        }
-    });
-    await page.on('response', async (response)=>{
-        try{
-            var res = await response.json();
-            if(res && res.tokens){
-                userCredentials = res;
-            }
-        }catch(error){}
-    });
-
-    // browser sub-project user
-    browser2 = await puppeteer.launch();
-    newPage = await browser2.newPage();
-    await newPage.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
-
-    // intercept request and mock response for login
-    await newPage.setRequestInterception(true);
-    await newPage.on('request', async (request)=>{
-        if((await request.url()).match(/user\/login/)){
-            request.respond({
-                status: 200,
-                contentType: 'application/json',
-                body: JSON.stringify(userCredentials)
-            });
-        }else{
-            request.continue();
-        }
-    });
-    await newPage.on('response', async (response)=>{
-        try{
-            var res = await response.json();
-            if(res && res.tokens){
-                userCredentials = res;
-            }
-        }catch(error){}
-    });
-
-    // parent user
-    await init.registerUser(user, page);
-    await init.loginUser(user, page);
-
-    // rename default project
-    await init.renameProject(projectName, page);
-
-    // add sub-project
-    await init.addSubProject(subProjectName, page);
-
-    // new user (sub-project user)
-    await init.registerUser(newUser, newPage);
-    await init.loginUser(newUser, newPage);
-
-    // add new user to sub-project
-    await init.addUserToProject({email: newUser.email, role: 'Member', subProjectName}, page);
-    
-    // switch to invited project for new user
-    await init.switchProject(projectName, newPage);
-    done();
-});
-
-afterAll(async (done) => {
-    await browser1.close();
-    await browser2.close();
-    done();
-});
 
 describe('Monitor API With SubProjects', () => {
     const operationTimeOut = 25000;
+
+    beforeAll(async (done) => {
+        jest.setTimeout(200000);
+        // browser for parent user
+        browser1 = await puppeteer.launch();
+        page = await browser1.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+    
+        // intercept request and mock response for login
+        await page.setRequestInterception(true);
+        await page.on('request', async (request)=>{
+            if((await request.url()).match(/user\/login/)){
+                request.respond({
+                    status: 200,
+                    contentType: 'application/json',
+                    body: JSON.stringify(userCredentials)
+                });
+            }else{
+                request.continue();
+            }
+        });
+        await page.on('response', async (response)=>{
+            try{
+                var res = await response.json();
+                if(res && res.tokens){
+                    userCredentials = res;
+                }
+            }catch(error){}
+        });
+    
+        // browser sub-project user
+        browser2 = await puppeteer.launch();
+        newPage = await browser2.newPage();
+        await newPage.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+    
+        // intercept request and mock response for login
+        await newPage.setRequestInterception(true);
+        await newPage.on('request', async (request)=>{
+            if((await request.url()).match(/user\/login/)){
+                request.respond({
+                    status: 200,
+                    contentType: 'application/json',
+                    body: JSON.stringify(userCredentials)
+                });
+            }else{
+                request.continue();
+            }
+        });
+        await newPage.on('response', async (response)=>{
+            try{
+                var res = await response.json();
+                if(res && res.tokens){
+                    userCredentials = res;
+                }
+            }catch(error){}
+        });
+    
+        // parent user
+        await init.registerUser(user, page);
+        await init.loginUser(user, page);
+    
+        // rename default project
+        await init.renameProject(projectName, page);
+    
+        // add sub-project
+        await init.addSubProject(subProjectName, page);
+    
+        // new user (sub-project user)
+        await init.registerUser(newUser, newPage);
+        await init.loginUser(newUser, newPage);
+    
+        // add new user to sub-project
+        await init.addUserToProject({email: newUser.email, role: 'Member', subProjectName}, page);
+        
+        // switch to invited project for new user
+        await init.switchProject(projectName, newPage);
+        done();
+    });
+    
+    afterAll(async (done) => {
+        await browser1.close();
+        await browser2.close();
+        done();
+    });
 
     it('should not display new monitor form for user that is not `admin` in sub-project.', async (done) => {
         await newPage.waitForSelector('#monitors');
--- dashboard/src/test/puppeteer/UserFeedback.test.js
+++ dashboard/src/test/puppeteer/UserFeedback.test.js
@@ -12,53 +12,55 @@ let email = utils.generateRandomBusinessEmail();
 
 let testFeedback = 'test feedback';
 
-beforeAll(async () => {
-    jest.setTimeout(100000);
-    projectId = await localStorage.getItem('id');
-    browser = await puppeteer.launch();
-    page = await browser.newPage();
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
 
-    // intercept request and mock response for login
-    await page.setRequestInterception(true);
-    await page.on('request', async (request)=>{
-        if((await request.url()).match(/user\/login/)){
-            request.respond({
-                status: 200,
-                contentType: 'application/json',
-                body: JSON.stringify(userCredentials)
-            });
-        }else{
-            request.continue();
-        }
-    });
-    await page.on('response', async (response)=>{
-        try{
-            var res = await response.json();
-            if(res && res.tokens){
-                userCredentials = res;
-            }
-        }catch(error){}
-    });
-
-    // user credentials
-    let email = utils.generateRandomBusinessEmail();
-    let password = utils.generateRandomString();
-    const user = {
-        email,
-        password
-    };
-
-    // register and signin user
-    await init.registerUser(user, page);
-    await init.loginUser(user, page);
-});
-
-afterAll(async () => {
-    await browser.close();
-});
 
 describe('User Feedback', () => {
+    
+    beforeAll(async () => {
+        jest.setTimeout(100000);
+        projectId = await localStorage.getItem('id');
+        browser = await puppeteer.launch({headless:utils.headlessMode});
+        page = await browser.newPage();
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+    
+        // intercept request and mock response for login
+        await page.setRequestInterception(true);
+        await page.on('request', async (request)=>{
+            if((await request.url()).match(/user\/login/)){
+                request.respond({
+                    status: 200,
+                    contentType: 'application/json',
+                    body: JSON.stringify(userCredentials)
+                });
+            }else{
+                request.continue();
+            }
+        });
+        await page.on('response', async (response)=>{
+            try{
+                var res = await response.json();
+                if(res && res.tokens){
+                    userCredentials = res;
+                }
+            }catch(error){}
+        });
+    
+        // user credentials
+        let email = utils.generateRandomBusinessEmail();
+        let password = utils.generateRandomString();
+        const user = {
+            email,
+            password
+        };
+    
+        // register and signin user
+        await init.registerUser(user, page);
+        await init.loginUser(user, page);
+    });
+    
+    afterAll(async () => {
+        await browser.close();
+    });
 
     it('should send feedback from the dashboard', async (done) => {
         await page.reload({ waitUntil: 'networkidle2' });
