601d0495e1c23734a239b1e0cb12e3ee4befadfc
Rajat Soni <rajat.tcp@gmail.com>
2019-08-29 15:28:58 +0530

    added dashboard host

--- accounts/index.js
+++ accounts/index.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 var env = {
   REACT_APP_FYIPE_HOSTED: process.env.FYIPE_HOSTED,
   REACT_APP_HOST: process.env.HOST,
-  REACT_APP_ACCOUNTS_HOST: process.env.ACCOUNTS_HOST,
+  REACT_APP_DASHBOARD_HOST: process.env.DASHBOARD_HOST,
   REACT_APP_BACKEND_HOST: process.env.BACKEND_HOST
 }
 
