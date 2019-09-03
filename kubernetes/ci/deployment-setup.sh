186707a8394dce80df118e7fac0200901e7c410d
Rajat Soni <rajat.tcp@gmail.com>
2019-09-03 09:25:35 +0530

    fix path errors in staging step

--- kubernetes/ci/deployment-setup.sh
+++ kubernetes/ci/deployment-setup.sh
@@ -1,6 +1,7 @@
-cd credentials
+cd ./kubernetes/credentials
 openssl enc -in encrypted-credentials.enc -out encrypted-credentials.tar -d -aes256 -pbkdf2 -k $KUBE_ENC
 tar -xvf encrypted-credentials.tar
+cd ..
 cd ..
 curl -sSL https://sdk.cloud.google.com | bash > /dev/null;
 source $HOME/google-cloud-sdk/path.bash.inc
