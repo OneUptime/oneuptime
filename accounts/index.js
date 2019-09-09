2b5d07c3c2b376156ef1efe9af841695b36c7b9e
Rajat Soni <rajat.tcp@gmail.com>
2019-09-09 19:00:52 +0530

    add route for env.js

--- accounts/index.js
+++ accounts/index.js
@@ -20,8 +20,13 @@ child_process.execSync('react-env', {
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/env.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'public', 'env.js'));
+});
+
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
 
 app.listen(3003);
