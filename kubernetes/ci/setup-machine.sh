834af947006d37691bc7ad9b61451682d0e66a5d
Rajat Soni <rajat.tcp@gmail.com>
2019-09-15 00:28:02 +0530

    CI/setup-machine.sh: Sleep 2m after setup-machine is done.

--- kubernetes/ci/setup-machine.sh
+++ kubernetes/ci/setup-machine.sh
@@ -119,3 +119,12 @@ libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 li
 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 \
 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 \
 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
+
+echo "RUNNING COMMAND: curl localhost:32000/v2"
+curl localhost:32000/v2
+
+echo "RUNNING COMMAND: sleep 2m"
+sleep 2m
+
+echo "RUNNING COMMAND: curl localhost:32000/v2"
+curl localhost:32000/v2
