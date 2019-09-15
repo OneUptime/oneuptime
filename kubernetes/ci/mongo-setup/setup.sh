10fcdbd7f4237231ea9162314a552faa948256e8
Rajat Soni <rajat.tcp@gmail.com>
2019-09-16 00:12:37 +0530

    CI/mongo-setup: Change from kubectl to k

--- kubernetes/ci/mongo-setup/setup.sh
+++ kubernetes/ci/mongo-setup/setup.sh
@@ -1,7 +1,7 @@
 sudo k create -f ./kubernetes/ci/mongo-setup/unsecureMongo.yaml
 echo "Wait for 2 mins...."
 sleep 2m
-MONGO_IP=`sudo kubectl get --all-namespaces --output json pods | jq '.items[] | select(.metadata.name=="mongo-0")' | jq .status.podIP`
+MONGO_IP=`sudo k get --all-namespaces --output json pods | jq '.items[] | select(.metadata.name=="mongo-0")' | jq .status.podIP`
 MONGO_IP=`echo "$MONGO_IP" | tr -d '"'`
 mongo $MONGO_IP:27017/admin ./kubernetes/ci/mongo-setup/initialReplicaSetSetup.js
 echo "Wait for 1 min...."
