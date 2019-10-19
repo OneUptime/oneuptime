b2f7a031231accc1baeb91f1d7a5a05552a1232d
Ibukun Dairo <ibukundairo@hackerbay.io>
2019-10-19 22:54:11 +0100

    Move probe decision making to backend for api monitor

--- probe/utils/apiService.js
+++ probe/utils/apiService.js
@@ -1,10 +1,13 @@
 const postApi = require('./api').postApi;
 
 module.exports = {
-    setMonitorTime: async function (monitorId,responseTime,responseStatus,status) {
-        return await postApi(`probe/setTime/${monitorId}`,{responseTime,responseStatus,status});
+    setMonitorTime: async function (monitorId, responseTime, responseStatus, status) {
+        return await postApi(`probe/setTime/${monitorId}`, { responseTime, responseStatus, status });
     },
-    getMonitorTime: async function (monitorId,date) {
-        return await postApi(`probe/getTime/${monitorId}`,date);
+    getMonitorTime: async function (monitorId, date) {
+        return await postApi(`probe/getTime/${monitorId}`, date);
     },
+    ping: async function (monitorId, data) {
+        return await postApi(`probe/ping/${monitorId}`, data);
+    }
 };
