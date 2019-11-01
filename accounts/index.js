a957dd9926304ac3a59b71c13d68b9c40bc278dd
Rajat Soni <rajat.tcp@gmail.com>
2019-11-01 16:49:00 +0530

    Fix typo

--- accounts/index.js
+++ accounts/index.js
@@ -11,7 +11,7 @@ var env = {
   REACT_APP_DASHBOARD_HOST: process.env.DASHBOARD_HOST,
   REACT_APP_BACKEND_HOST: process.env.BACKEND_HOST,
   REACT_APP_DOMAIN: process.env.DOMAIN,
-  REACT_APP_PUBLIC_STRIPE_KEY = process.env.PUBLIC_STRIPE_KEY
+  REACT_APP_PUBLIC_STRIPE_KEY: process.env.PUBLIC_STRIPE_KEY
 }
 
 fs.writeFileSync('.env', envfile.stringifySync(env));
