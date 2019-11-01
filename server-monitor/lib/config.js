93b84273769a15f7186ccccbcbc47d358be63f15
Ibukun Dairo <ibukundairo@hackerbay.io>
2019-11-01 13:37:32 +0100

    Update API URL

--- server-monitor/lib/config.js
+++ server-monitor/lib/config.js
@@ -8,6 +8,6 @@
 
 /** The api url to send server information. */
 const API_URL = process.env.API_URL ||
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : 'https://api.fyipe.com';
+  (process.env.NODE_ENV === 'development' ? 'http://localhost:3002' : 'https://api.fyipe.com');
 
 module.exports = { API_URL };
