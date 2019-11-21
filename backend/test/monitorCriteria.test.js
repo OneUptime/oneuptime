04967b7dbc942b2e7acc98c4942325c3083cd773
Ibukun Dairo <ibukundairo@hackerbay.io>
2019-11-21 22:26:08 +0100

    Add monitor criteria test

---
+++ backend/test/monitorCriteria.test.js
@@ -0,0 +1,57 @@
+/* eslint-disable linebreak-style */
+process.env.PORT = 3020;
+var expect = require('chai').expect;
+var userData = require('./data/user');
+var chai = require('chai');
+chai.use(require('chai-http'));
+var app = require('../server');
+
+var request = chai.request.agent(app);
+var { createUser } = require('./utils/userSignUp');
+var UserService = require('../backend/services/userService');
+var ProjectService = require('../backend/services/projectService');
+var VerificationTokenModel = require('../backend/models/verificationToken');
+var AirtableService = require('../backend/services/airtableService');
+
+var token, projectId, userId, airtableId;
+
+describe('Monitor Criteria API', function () {
+    this.timeout(20000);
+
+    before(function (done) {
+        this.timeout(40000);
+        createUser(request, userData.user, function (err, res) {
+            let project = res.body.project;
+            projectId = project._id;
+            userId = res.body.id;
+            airtableId = res.body.airtableId;
+
+            VerificationTokenModel.findOne({ userId }, function (err, verificationToken) {
+                request.get(`/user/confirmation/${verificationToken.token}`).redirects(0).end(function () {
+                    request.post('/user/login').send({
+                        email: userData.user.email,
+                        password: userData.user.password
+                    }).end(function () {
+                        token = res.body.tokens.jwtAccessToken;
+                        done();
+                    });
+                });
+            });
+        });
+    });
+
+    after(async function () {
+        await UserService.hardDeleteBy({ email: { $in: [userData.user.email, userData.newUser.email, userData.anotherUser.email] } });
+        await ProjectService.hardDeleteBy({ _id: projectId }, userId);
+        await AirtableService.deleteUser(airtableId);
+    });
+
+    it('should get the monitor criteria', function (done) {
+        var authorization = `Basic ${token}`;
+        request.get('/monitorCriteria').set('Authorization', authorization).end(function (err, res) {
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('object');
+            done();
+        });
+    });
+});
