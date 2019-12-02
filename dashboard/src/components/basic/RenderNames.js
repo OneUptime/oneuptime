3d4f125d5b51981bf505d9551f1c7eb3dadb83c6
Nnanyielugo <kz.onyegbule@gmail.com>
2019-12-02 16:47:19 +0100

    collapse time and timezone by default, and make expandable when text is clicked

--- dashboard/src/components/basic/RenderNames.js
+++ dashboard/src/components/basic/RenderNames.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types'
 import { Field } from 'redux-form';
 import ShouldRender from '../basic/ShouldRender';
@@ -7,6 +7,7 @@ import TeamSelector from './TeamSelector';
 import TimeSelector from './TimeSelector';
 
 const RenderNames = ({ fields, meta: { error, submitFailed }, subProjectId }) => {
+    const [timeVisible, setTimeVisible] = useState(false);
     return (
         <ul>
             {
@@ -127,7 +128,10 @@ const RenderNames = ({ fields, meta: { error, submitFailed }, subProjectId }) =>
 
                             <div className="bs-Fieldset-row">
                                 <label className="bs-Fieldset-label">Time : </label>
-                                <div className="bs-Fieldset-fields">
+                                {!timeVisible ? (
+                                    <text className="Text-color--blue Text-display--inline Text-fontSize--14 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--base" style={{ marginTop: '5px', cursor: 'pointer' }} onClick={(() => setTimeVisible(true))}>Add on-call duty times</text>
+                                ) : (
+                                  <div className="bs-Fieldset-fields">
                                     <div className="bs-Fieldset-row" style={{paddingLeft:'0px',paddingTop:'0px'}}>
                                         <label className="bs-Fieldset-label" style={{maxWidth:'40px'}}>From</label>
                                         <div className="bs-Fieldset-fields">
@@ -168,6 +172,7 @@ const RenderNames = ({ fields, meta: { error, submitFailed }, subProjectId }) =>
                                       </div>
                                   </div>
                                 </div>
+                                )}
                             </div>
                             <ShouldRender if={fields.length > 1}>
                             <div className="bs-Fieldset-row">
