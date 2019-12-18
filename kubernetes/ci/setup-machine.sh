61cb985883429eaa4723064be0c4a96a7130d206
Ibukun Dairo <ibukundairo@hackerbay.io>
2019-12-19 00:33:44 +0100

    Audit backend and dashboard Add redis-server

--- kubernetes/ci/setup-machine.sh
+++ kubernetes/ci/setup-machine.sh
@@ -96,6 +96,8 @@ echo "RUNNING COMMAND: sudo apt-get update"
 sudo apt-get update
 echo "RUNNING COMMAND: sudo apt-get install -y mongodb-org"
 sudo apt-get install -y mongodb-org
+echo "RUNNING COMMAND: sudo apt-get install -y redis-server"
+sudo apt-get install -y redis-server
 # Install JQ, a way for bash to interact with JSON
 echo "RUNNING COMMAND: sudo apt-get install -y jq"
 sudo apt-get install -y jq
