f8dab5ec94a0c8782ef28a16dc555dafca08c573
Rajat <rajat.tcp@gmail.com>
2019-12-19 13:39:53 +0530

    Status-page: remove query after tokens are saved

--- status-page/src/App.js
+++ status-page/src/App.js
@@ -5,13 +5,15 @@ import Login from './components/Login';
 import { BrowserRouter as Router, Route, Switch,Redirect } from 'react-router-dom';
 import { User } from './config';
 import queryString from 'query-string';
+import { removeQuery } from './store/store';
 
 const userId = queryString.parse(window.location.search).userId;
 const accessToken = queryString.parse(window.location.search).accessToken;
 
 if (userId && accessToken){
 	User.setUserId(userId);
-	User.setAccessToken(accessToken);
+    User.setAccessToken(accessToken);
+    removeQuery();
 }
 
 const App = () => (
