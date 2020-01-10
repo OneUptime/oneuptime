dd8db1615889e59e85d195be27d2ed780dec0d8b
Rajat <rajat.tcp@gmail.com>
2020-01-10 13:15:35 +0530

    Backend: Remove accesstokens from signup response

--- backend/backend/services/userService.js
+++ backend/backend/services/userService.js
@@ -324,7 +324,7 @@ module.exports = {
                         ErrorService.log('userService.login', error);
                         throw error;
                     }
-                    if (!user.isVerified) {
+                    if (!user.isVerified && NODE_ENV !== 'development') {
                         let error = new Error('Verify your email first.');
                         error.code = 401;
                         ErrorService.log('userService.login', error);
@@ -611,7 +611,7 @@ var ErrorService = require('./errorService');
 var jwt = require('jsonwebtoken');
 var iplocation = require('iplocation').default;
 var jwtKey = require('../config/keys');
-var { BACKEND_HOST } = process.env;
+var { BACKEND_HOST, NODE_ENV } = process.env;
 var VerificationTokenModel = require('../models/verificationToken');
 var MailService = require('../services/mailService');
 var AirtableService = require('./airtableService');
