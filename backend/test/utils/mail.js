aa02d80eb7640dd4504acf743e1d006c93597358
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-01-10 23:56:21 +0100

    fix tests.

--- backend/test/utils/mail.js
+++ backend/test/utils/mail.js
@@ -12,7 +12,7 @@ function openBox(cb) {
     imap.openBox('INBOX', true, cb);
 }
 
-var signUpEmailContent = 'WELCOME TO FYIPE\nHiJohn,\n\n I\'m Nawaz and I\'m the founder of Fyipe. I can\'t thank you enough for signing\nup. \n\nIf you need any help using Fyipe, Please Send us an email at support@fyipe.com\nand let me know.\n\nThanks, have a great day.\n\nFyipe Team';
+var signUpEmailContent = 'WELCOME TO FYIPE\nHi John,\n\n I\'m Nawaz and I\'m the founder of Fyipe. I can\'t thank you enough for signing\nup. \n\nIf you need any help using Fyipe, Please Send us an email at support@fyipe.com\nand let me know.\n\nThanks, have a great day.\n\nFyipe Team';
 
 var feedbackEmailContent = ' THANK YOU FOR YOUR FEEDBACK\nHi John, \n\nThank you for your feedback. We’ll get back to you as soon as we can. Have a\ngreat day. \n\nFyipe Team';
 
