40e72532581593f3e818100d7c59747b8ffa0e83
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-01-23 16:38:24 +0100

    Setup monitor log models

---
+++ backend/backend/models/monitorLogByHour.js
@@ -0,0 +1,23 @@
+var mongoose = require('../config/db');
+
+var Schema = mongoose.Schema;
+var monitorLogByHourSchema = new Schema({
+    monitorId: { type: String, ref: 'Monitor' }, //which monitor does this belong to.
+    probeId: { type: String, ref: 'Probe' }, //which probe does this belong to.
+    responseTime: Number, // Time taken for ping
+    responseStatus: Number,
+    status: String,
+    data: Object,
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    intervalDate: Date,
+    avgResponseTime: Number,
+    avgCpuLoad: Number,
+    avgMemoryUsed: Number,
+    avgStorageUsed: Number,
+    avgMainTemp: Number,
+    count: Number
+});
+module.exports = mongoose.model('MonitorLog', monitorLogByHourSchema);
