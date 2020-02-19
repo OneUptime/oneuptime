8ea6e3cb547ba47a5505193cc4bb407b0c6d3e6b
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-02-19 22:33:43 +0100

    change env var

--- backend/backend/middlewares/rateLimit.js
+++ backend/backend/middlewares/rateLimit.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
-const { RATE_LIMIT_TIME_PERIOD_IN_MS, RATE_LIMIT_REQUEST_LIMIT } = process.env;
+const { RATE_LIMITTER_TIME_PERIOD_IN_MS, RATE_LIMITTER_REQUEST_LIMIT } = process.env;
 const limiter = rateLimit({
-    windowMs: Number(RATE_LIMIT_TIME_PERIOD_IN_MS), 
-    max: Number(RATE_LIMIT_REQUEST_LIMIT),
+    windowMs: Number(RATE_LIMITTER_TIME_PERIOD_IN_MS), 
+    max: Number(RATE_LIMITTER_REQUEST_LIMIT),
     keyGenerator: function (req) {
         const accessToken = req.headers.authorization || req.query.accessToken;
         if (accessToken){
