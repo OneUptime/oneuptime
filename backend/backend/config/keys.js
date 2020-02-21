81c0969c340dd9e4299095490eda4dc34f379289
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-02-21 18:05:10 +0100

    Update mongoose key.

--- backend/backend/config/keys.js
+++ backend/backend/config/keys.js
@@ -6,7 +6,7 @@
 
 module.exports = {
     jwtSecretKey: process.env['JWT_SECRET'],
-    dbURL: process.env['MONGO_URL'],
+    dbURL: process.env['MONGO_URL'] || "mongodb://localhost/fyipedb",
     redisURL: process.env['REDIS_HOST'],
     airtableApiKey: process.env['AIRTABLE_API_KEY'],
     airtableBaseId: process.env['AIRTABLE_BASE_ID']
