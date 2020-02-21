c971c67bfdf7515cda420e20dbc8968ab48316c6
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-02-21 20:04:19 +0100

    Comment services.

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,20 @@ try {
         require('custom-env').env(process.env.NODE_ENV || 'development');
     }
 
-   
-
     const path = require('path');
     const http = require('http').createServer(app);
-    const io = require('socket.io')(http);
-    const redisAdapter = require('socket.io-redis');
-    const keys = require('./backend/config/keys.js');
+    // const io = require('socket.io')(http);
+    // const redisAdapter = require('socket.io-redis');
+    // const keys = require('./backend/config/keys.js');
     const bodyParser = require('body-parser');
     const cors = require('cors');
 
-    io.adapter(redisAdapter({
-        host: keys.redisURL || 'localhost',
-        port: process.env.REDIS_PORT || 6379
-    }));
+    // io.adapter(redisAdapter({
+    //     host: keys.redisURL || 'localhost',
+    //     port: process.env.REDIS_PORT || 6379
+    // }));
 
-    global.io = io;
+    // global.io = io;
     
     app.use(cors());
 
@@ -42,55 +40,55 @@ try {
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
-    const { RATE_LIMITTER_ENABLED } = process.env;
-    if (RATE_LIMITTER_ENABLED === 'true') {
-        const rateLimiter = require('./backend/middlewares/rateLimit');
-        app.use(rateLimiter);
-    }
-    //View engine setup
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'ejs');
+    // const { RATE_LIMITTER_ENABLED } = process.env;
+    // if (RATE_LIMITTER_ENABLED === 'true') {
+    //     const rateLimiter = require('./backend/middlewares/rateLimit');
+    //     app.use(rateLimiter);
+    // }
+    // //View engine setup
+    // app.set('views', path.join(__dirname, 'views'));
+    // app.set('view engine', 'ejs');
 
-    app.use(express.static(path.join(__dirname, 'views')));
+    // app.use(express.static(path.join(__dirname, 'views')));
 
-    app.use(require('./backend/middlewares/auditLogs').log);
+    // app.use(require('./backend/middlewares/auditLogs').log);
     
     
     // Routes(API)
-    app.use('/server', require('./backend/api/server'));
-    app.use('/alert', require('./backend/api/alert'));
-    app.use('/user', require('./backend/api/user'));
-    app.use('/token', require('./backend/api/token'));
-    app.use('/team', require('./backend/api/team'));
-    app.use('/project', require('./backend/api/project'));
-    app.use('/invoice', require('./backend/api/invoice'));
-    app.use('/schedule', require('./backend/api/schedule'));
-    app.use('/monitor', require('./backend/api/monitor'));
-    app.use('/statusPage', require('./backend/api/statusPage'));
-    app.use('/file', require('./backend/api/file'));
-    app.use('/incident', require('./backend/api/incident'));
-    app.use('/reports', require('./backend/api/report'));
-    app.use('/lead', require('./backend/api/lead'));
-    app.use('/feedback', require('./backend/api/feedback'));
-    app.use('/twilio', require('./backend/api/twilio'));
-    app.use('/zapier', require('./backend/api/zapier'));
-    app.use('/slack', require('./backend/api/slack'));
-    app.use('/webhook', require('./backend/api/webHook'));
-    app.use('/notification', require('./backend/api/notification'));
-    app.use('/stripe', require('./backend/api/stripe'));
-    app.use('/subscriber', require('./backend/api/subscriber'));
-    app.use('/subscriberAlert', require('./backend/api/subscriberAlert'));
-    app.use('/emailTemplate', require('./backend/api/emailTemplate'));
-    app.use('/emailSmtp', require('./backend/api/emailSmtp'));
-    app.use('/smsTemplate', require('./backend/api/smsTemplate'));
-    app.use('/smsSmtp', require('./backend/api/smsSmtp'));
-    app.use('/monitorCategory', require('./backend/api/monitorCategory'));
-    app.use('/monitorCriteria', require('./backend/api/monitorCriteria'));
-    app.use('/scheduledEvent', require('./backend/api/scheduledEvent'));
-    app.use('/probe', require('./backend/api/probe'));
-    app.use('/version', require('./backend/api/version'));
-    app.use('/tutorial', require('./backend/api/tutorial'));
-    app.use('/audit-logs', require('./backend/api/auditLogs'));
+    // app.use('/server', require('./backend/api/server'));
+    // app.use('/alert', require('./backend/api/alert'));
+    // app.use('/user', require('./backend/api/user'));
+    // app.use('/token', require('./backend/api/token'));
+    // app.use('/team', require('./backend/api/team'));
+    // app.use('/project', require('./backend/api/project'));
+    // app.use('/invoice', require('./backend/api/invoice'));
+    // app.use('/schedule', require('./backend/api/schedule'));
+    // app.use('/monitor', require('./backend/api/monitor'));
+    // app.use('/statusPage', require('./backend/api/statusPage'));
+    // app.use('/file', require('./backend/api/file'));
+    // app.use('/incident', require('./backend/api/incident'));
+    // app.use('/reports', require('./backend/api/report'));
+    // app.use('/lead', require('./backend/api/lead'));
+    // app.use('/feedback', require('./backend/api/feedback'));
+    // app.use('/twilio', require('./backend/api/twilio'));
+    // app.use('/zapier', require('./backend/api/zapier'));
+    // app.use('/slack', require('./backend/api/slack'));
+    // app.use('/webhook', require('./backend/api/webHook'));
+    // app.use('/notification', require('./backend/api/notification'));
+    // app.use('/stripe', require('./backend/api/stripe'));
+    // app.use('/subscriber', require('./backend/api/subscriber'));
+    // app.use('/subscriberAlert', require('./backend/api/subscriberAlert'));
+    // app.use('/emailTemplate', require('./backend/api/emailTemplate'));
+    // app.use('/emailSmtp', require('./backend/api/emailSmtp'));
+    // app.use('/smsTemplate', require('./backend/api/smsTemplate'));
+    // app.use('/smsSmtp', require('./backend/api/smsSmtp'));
+    // app.use('/monitorCategory', require('./backend/api/monitorCategory'));
+    // app.use('/monitorCriteria', require('./backend/api/monitorCriteria'));
+    // app.use('/scheduledEvent', require('./backend/api/scheduledEvent'));
+    // app.use('/probe', require('./backend/api/probe'));
+    // app.use('/version', require('./backend/api/version'));
+    // app.use('/tutorial', require('./backend/api/tutorial'));
+    // app.use('/audit-logs', require('./backend/api/auditLogs'));
     app.set('port', process.env.PORT || 3002);
     
     const server = http.listen(app.get('port'), function () {
