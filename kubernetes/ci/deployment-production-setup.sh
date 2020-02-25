0f4778f45e717b23d37a59713f5154f7670d9e95
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-02-25 19:35:14 +0100

    Rerun the build.

--- kubernetes/ci/deployment-production-setup.sh
+++ kubernetes/ci/deployment-production-setup.sh
@@ -1,5 +1,6 @@
 [ ! -d "$HOME/.kube" ] && sudo mkdir "$HOME/.kube"
 ls
+echo "---------------------"
 cd kubernetes/credentials
 ls
 sudo mv kubernetes/credentials/encrypted-credentials/production/config $HOME/.kube/config
