d2c28d8c8a2d44648bbf72d2db656aaadb7c8caf
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-02-25 19:02:28 +0100

    Change encrypted file.

--- kubernetes/ci/deployment-production-setup.sh
+++ kubernetes/ci/deployment-production-setup.sh
@@ -1,3 +1,2 @@
-sudo $HOME/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file credentials/encrypted-credentials/production/fyipe-production.json
-sudo $HOME/google-cloud-sdk/bin/gcloud container clusters get-credentials fyipe --zone us-central1-c --project fyipe-production
-sudo $HOME/google-cloud-sdk/bin/kubectl config use-context gke_fyipe-production_us-central1-c_fyipe
+sudo mkdir $HOME/.kube || "Directory already exists"
+sudo mv credentials/encrypted-credentials/production/config $HOME/.kube/config
