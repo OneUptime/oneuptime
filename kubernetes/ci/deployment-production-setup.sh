13a114da901ec77c5e84070fe5855e492100d140
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-02-25 19:27:33 +0100

    Update command.

--- kubernetes/ci/deployment-production-setup.sh
+++ kubernetes/ci/deployment-production-setup.sh
@@ -1,3 +1,5 @@
 [ ! -d "$HOME/.kube" ] && sudo mkdir "$HOME/.kube"
 ls
+cd kubernetes/credentials/encrypted-credentials
+ls
 sudo mv kubernetes/credentials/encrypted-credentials/production/config $HOME/.kube/config
