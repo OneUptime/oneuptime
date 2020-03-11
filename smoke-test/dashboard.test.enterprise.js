bf7846d44d345a50e2945cfa2b42b0784cf23f31
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-03-11 21:01:40 +0100

    Setup enterprise e2e

---
+++ smoke-test/dashboard.test.enterprise.js
@@ -0,0 +1,151 @@
+const puppeteer = require('puppeteer');
+const utils = require('./test-utils');
+const init = require('./test-init');
+const { Cluster } = require('puppeteer-cluster');
+
+require('should');
+
+// user credentials
+const email = utils.generateRandomBusinessEmail();
+const password = '1234567890';
+
+describe('Enterprise Dashboard API', () => {
+    const operationTimeOut = 100000;
+
+    beforeAll(async done => {
+        jest.setTimeout(200000);
+
+        const cluster = await Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_PAGE,
+            puppeteerOptions: utils.puppeteerLaunchConfig,
+            puppeteer,
+            timeout: 120000,
+        });
+
+        cluster.on('taskerror', err => {
+            throw err;
+        });
+
+        // Register user
+        await cluster.task(async ({ page, data }) => {
+            const user = {
+                email: data.email,
+                password: data.password,
+            };
+            // user
+            await init.registerEnterpriseUser(user, page);
+        });
+
+        await cluster.queue({ email, password });
+
+        await cluster.idle();
+        await cluster.close();
+        done();
+    });
+
+    afterAll(async done => {
+        done();
+    });
+
+    it(
+        'Should create new monitor with correct details',
+        async done => {
+            const cluster = await Cluster.launch({
+                concurrency: Cluster.CONCURRENCY_PAGE,
+                puppeteerOptions: utils.puppeteerLaunchConfig,
+                puppeteer,
+                timeout: 100000,
+            });
+
+            cluster.on('taskerror', err => {
+                throw err;
+            });
+
+            const monitorName = utils.generateRandomString();
+
+            await cluster.task(async ({ page, data }) => {
+                const user = {
+                    email: data.email,
+                    password: data.password,
+                };
+
+                await init.loginUser(user, page);
+
+                await page.waitFor(10000);
+                await page.waitForSelector('#monitors');
+                await page.click('#monitors');
+                await page.waitForSelector('#frmNewMonitor');
+                await page.click('input[id=name]');
+                await page.type('input[id=name]', data.monitorName);
+                await init.selectByText('#type', 'url', page);
+                await page.waitForSelector('#url');
+                await page.click('#url');
+                await page.type('#url', 'https://google.com');
+                await page.click('button[type=submit]');
+                await page.waitFor(10000);
+
+                let spanElement;
+                spanElement = await page.$(
+                    `#monitor_title_${data.monitorName}`
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+                spanElement.should.be.exactly(data.monitorName);
+            });
+
+            cluster.queue({ email, password, monitorName });
+            await cluster.idle();
+            await cluster.close();
+            done();
+        },
+        operationTimeOut
+    );
+
+    it(
+        'Should not create new monitor when details are incorrect',
+        async done => {
+            const cluster = await Cluster.launch({
+                concurrency: Cluster.CONCURRENCY_PAGE,
+                puppeteerOptions: utils.puppeteerLaunchConfig,
+                puppeteer,
+                timeout: 100000,
+            });
+
+            cluster.on('taskerror', err => {
+                throw err;
+            });
+
+            await cluster.task(async ({ page, data }) => {
+                const user = {
+                    email: data.email,
+                    password: data.password,
+                };
+
+                await init.loginUser(user, page);
+
+                await page.waitFor(10000);
+                await page.waitForSelector('#name');
+                await init.selectByText('#type', 'url', page);
+                await page.waitForSelector('#url');
+                await page.type('#url', 'https://google.com');
+                await page.click('button[type=submit]');
+                await page.waitFor(5000);
+                let spanElement;
+                spanElement = await page.$(
+                    '#frmNewMonitor > div > div > div > fieldset > div > div > div > span >  div > div > span'
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+                spanElement.should.be.exactly(
+                    'This field cannot be left blank'
+                );
+            });
+
+            cluster.queue({ email, password });
+            await cluster.idle();
+            await cluster.close();
+            done();
+        },
+        operationTimeOut
+    );
+});
