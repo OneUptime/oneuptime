46e3107e90bc08d1df4db76c2c48bfafbb04aba7
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-03-22 11:50:48 +0100

    Refactor API docs.

--- api-docs/server.js
+++ api-docs/server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: 2592000 }));
 app.use('/docs', express.static(path.join(__dirname, 'public'), { maxAge: 2592000 }));
 
 // index page
-app.get('/', function(req, res) {
+app.get(['/','/docs'], function(req, res) {
     res.render('pages/index');
 });
 
