3f471abe160b22219fb96a1c617ee6f414553fec
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-04-06 20:58:15 +0200

    Fix region

--- ci/scripts/deployment-staging-setup.sh
+++ ci/scripts/deployment-staging-setup.sh
@@ -1,2 +1,2 @@
 sudo $HOME/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ./ci/credentials/encrypted-credentials/staging/fyipe-staging.json
-sudo $HOME/google-cloud-sdk/bin/gcloud container clusters get-credentials fyipe-helm --zone us-central1-a --project fyipe-staging
+sudo $HOME/google-cloud-sdk/bin/gcloud container clusters get-credentials fyipe-helm --zone us-central1-c --project fyipe-staging
