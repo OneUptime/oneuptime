a9989f168781658c30f7c9f1dd4e42570cec0097
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-04-06 19:05:48 +0200

    Update GCP billing agent and GCR integrtion.

---
+++ gcp-deployer/README.md
@@ -0,0 +1,8 @@
+# Deploy the deployer to Google Cloud docker Registry (GCR)
+
+Run: 
+
+`gcloud auth configure-docker` to auth GCR registry and then, 
+
+`bash gcp-deployer/install.sh` from `app` (root) directory.
+
--- gcp-deployer/install.sh
+++ gcp-deployer/install.sh
@@ -5,9 +5,10 @@ rm -rf ./gcp-deployer/chart
 mkdir ./gcp-deployer/chart
 cp -R ./helm-chart/public/fyipe ./gcp-deployer/chart
 cp ./gcp-deployer/application.yaml ./gcp-deployer/chart/fyipe/templates
+cp ./gcp-deployer/billing-agent.yaml ./gcp-deployer/chart/fyipe/templates
 sudo cat ./gcp-deployer/values.yaml >> ./gcp-deployer/chart/fyipe/values.yaml
 
 # Build and push docker container.
-sudo docker build --tag gcr.io/fyipe-public/deployer .
-sudo docker push gcr.io/fyipe-public/deployer
+sudo docker build --tag gcr.io/fyipe-public/fyipe/deployer ./gcp-deployer
+sudo docker push gcr.io/fyipe-public/fyipe/deployer/
 
