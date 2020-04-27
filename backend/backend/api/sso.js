c1da1f53ddc7bcfa5288c7620003b1293340af11
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-27 16:20:16 +0100

    implement sso/ssos (sso index) endpoint

--- backend/backend/api/sso.js
+++ backend/backend/api/sso.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const sendListResponse = require('../middlewares/response').sendListResponse;
+const sendErrorResponse = require('../middlewares/response').sendErrorResponse;
+const SsoService = require('../services/ssoService')
+
+router.get('/ssos', async function (req, res) {
+  try {
+    const ssos = await SsoService.getAllSsos()
+    return sendListResponse(req, res, ssos)
+  } catch (error) {
+    return sendErrorResponse(req, res, error)
+  }
+})
+
+module.exports = router;
