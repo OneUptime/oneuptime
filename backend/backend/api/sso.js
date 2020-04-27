2a6f51ba5e89d7ed0d164109b6bdd511de915371
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-27 17:26:54 +0100

    Implement DELETE /sso/:ssoId endpoint

--- backend/backend/api/sso.js
+++ backend/backend/api/sso.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const sendListResponse = require('../middlewares/response').sendListResponse;
+const sendItemResponse = require('../middlewares/response').sendItemResponse;
 const sendErrorResponse = require('../middlewares/response').sendErrorResponse;
 const SsoService = require('../services/ssoService')
 
@@ -11,6 +12,23 @@ router.get('/ssos', async function (req, res) {
   } catch (error) {
     return sendErrorResponse(req, res, error)
   }
+})
+
+router.delete('/:ssoId', async function (req, res) {
+  const ssoId = req.params.ssoId
+  if (!ssoId) {
+    return sendErrorResponse(req, res, {
+      code: 400,
+      message: 'SsoId must be present.'
+    })
+  }
+  try {
+    const message = await SsoService.deleteSso(ssoId);
+    return sendItemResponse(req, res, message);
+  } catch (error) {
+    return sendErrorResponse(req, res, error)
+  }
+
 })
 
 module.exports = router;
