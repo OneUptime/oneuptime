38463e25690338fb6c117a16973b0b5ad43d889a
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-27 16:48:27 +0100

    Implement deleteSso action

--- admin-dashboard/src/actions/sso.js
+++ admin-dashboard/src/actions/sso.js
@@ -1,6 +1,6 @@
 import * as types from "../constants/sso"
 import errors from '../errors';
-import { getApi } from "../api";
+import { getApi, deleteApi } from "../api";
 
 export const fetchSsosRequest = () => {
   return {
@@ -17,11 +17,10 @@ export const fetchSsosSuccess = (payload) => {
 
 export const fetchSsosError = payload => {
   return {
-      type: types.FETCH_SSOS_FAILURE,
-      payload,
+    type: types.FETCH_SSOS_FAILURE,
+    payload,
   };
 };
-
 
 export const fetchSsos = () => async dispatch => {
   dispatch(fetchSsosRequest);
@@ -41,5 +40,47 @@ export const fetchSsos = () => async dispatch => {
       errorMsg = 'Network Error';
     }
     dispatch(fetchSsosError(errors(errorMsg)));
+  }
+}
+
+export const deleteSsoRequest = () => async dispatch => {
+  return {
+    type: types.DELETE_SSO_REQUEST,
+  };
+}
+
+export const deleteSsoSuccess = payload => async dispatch => {
+  return {
+    type: types.DELETE_SSO_SUCCESS,
+    payload
+  };
+}
+
+export const deleteSsoError = payload => async dispatch => {
+  return {
+    type: types.DELETE_SSO_FAILED,
+    payload,
+  };
+}
+
+export const deleteSso = ssoId => async dispatch => {
+  dispatch(deleteSsoRequest());
+  try {
+    const response = await deleteApi(`sso/${ssoId}`)
+    const { data } = response
+    dispatch(deleteSsoSuccess(data))
+  } catch (error) {
+    let errorMsg;
+    if (error && error.response && error.response.data)
+      errorMsg = error.response.data;
+    if (error && error.data) {
+      errorMsg = error.data;
+    }
+    if (error && error.message) {
+      errorMsg = error.message;
+    } else {
+      errorMsg = 'Network Error';
+    }
+    dispatch(deleteSsoError(errorMsg));
   }
 }
