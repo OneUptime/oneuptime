a2dea77638fc2e39ff1f25afda0cfa1aca668d89
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-27 21:14:47 +0100

    Extract add sso modal into a separated file

--- admin-dashboard/src/components/settings/sso.js
+++ admin-dashboard/src/components/settings/sso.js
@@ -1,102 +1,17 @@
 import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { reduxForm, Field } from 'redux-form';
-import { RenderField } from '../basic/RenderField';
-import { Validate } from '../../config';
-import { FormLoader } from '../basic/Loader';
 import PropTypes from 'prop-types';
+import uuid from 'uuid';
 import { fetchSettings, saveSettings } from '../../actions/settings';
 import { fetchSsos, deleteSso } from '../../actions/sso';
 import moment from 'moment';
 import { openModal, closeModal } from '../../actions/modal';
 import SsoDeleteModal from './sso/SsoDeleteModal';
+import SsoAddModal from './sso/SsoAddModal';
 
-// Client side validation
-function validate(values) {
-    const errors = {};
-
-    if (!Validate.text(values.samlSsoUrl)) {
-        errors.samlSsoUrl = 'SSO URL is not valid.';
-    }
-
-    if (!Validate.text(values.certificateFingerprint)) {
-        errors.certificateFingerprint = 'Certificate Fingerprint is not valid.';
-    }
-
-    if (!Validate.text(values.remoteLogoutUrl)) {
-        errors.remoteLogoutUrl = 'Remote logout URL is not valid.';
-    }
-
-    if (!Validate.text(values.ipRanges)) {
-        errors.ipRanges = 'Ip range is not valid.';
-    }
-
-    return errors;
-}
 
 const settingsType = 'sso';
-
-const fields = [
-    {
-        key: 'saml-enabled',
-        label: 'Enable SAML',
-        description:
-            'SAML is an industry standard SSO framework typically used by large enterprises for communicating identities across the internet.',
-        // eslint-disable-next-line react/display-name, react/prop-types
-        component: ({ input: { value, onChange } }) => (
-            <label className="Toggler-wrap">
-                <input
-                    className="btn-toggler"
-                    checked={value}
-                    onChange={onChange}
-                    type="checkbox"
-                    name="saml-enabled"
-                    id="saml-enabled"
-                />
-                <span className="TogglerBtn-slider round"></span>
-            </label>
-        ),
-    },
-    {
-        key: 'samlSsoUrl',
-        label: 'SAML SSO URL',
-        placeholder:
-            'https://login.microsoftonline.com/a4e1d-1s4f-965f-ert452136',
-        description:
-            'This is the Url that fyipe will invoke to redirect users to your identity provider.',
-        type: 'text',
-        component: RenderField,
-    },
-    {
-        key: 'certificateFingerprint',
-        label: 'Certificate Fingerprint',
-        placeholder: 'ASFD254689CSVDS45DS5S4DGV6SD4V',
-        description:
-            'The SHA256 or SHA1 fingerprint of the SAML certificate obtain this from your Saml service provider.',
-        type: 'text',
-        component: RenderField,
-    },
-    {
-        key: 'remoteLogoutUrl',
-        label: 'Remote Logout URL',
-        placeholder:
-            'https://login.microsoftonline.com/common/wsfederation?wa=wsignout1.0',
-        description:
-            'This is the Url that fyipe will direct your users to after they sign out.',
-        type: 'text',
-        component: RenderField,
-    },
-    {
-        key: 'ipRanges',
-        label: 'IP Ranges',
-        placeholder: '10.0. 0.0 – 10.255. 255.255.',
-        description:
-            'Request from these IP ranges will always be routed via remote authentication. Requests from IP adresses outside these range will be routed to the normal sign in form.',
-        type: 'text',
-        component: RenderField,
-    },
-];
 
 export class Component extends React.Component {
     async componentDidMount() {
@@ -118,8 +33,17 @@ export class Component extends React.Component {
             content: SsoDeleteModal,
         })
     }
+
+    addSso = async () => {
+        this.props.openModal({
+            id: uuid.v4(),
+            onConfirm: (e) => {e.preventDefault() },
+            content: SsoAddModal,
+        })
+    }
+
     render() {
-        const { settings, handleSubmit, ssos } = this.props;
+        const { ssos } = this.props;
         return (
             <div className="bs-ContentSection Card-root Card-shadow--medium">
                 <div className="Box-root">
@@ -136,6 +60,11 @@ export class Component extends React.Component {
                                     sign in to Fyipe. Requires configuration.
                                 </span>
                             </p>
+                            <button
+                                onClick={() => this.addSso()}
+                            >
+                                Add
+                            </button>
                         </div>
                     </div>
                     <div style={{ overflow: "auto hidden" }}>
@@ -279,83 +208,6 @@ export class Component extends React.Component {
                             </tbody>
                         </table>
                     </div>
-                    <form
-                        id="sso-form"
-                        onSubmit={handleSubmit(this.submitForm)}
-                    >
-                        <div className="bs-ContentSection-content Box-root Box-background--offset Box-divider--surface-bottom-1 Padding-horizontal--8 Padding-vertical--2">
-                            <div>
-                                <div className="bs-Fieldset-wrapper Box-root Margin-bottom--2">
-                                    <fieldset className="bs-Fieldset">
-                                        <div className="bs-Fieldset-rows">
-                                            {fields.map(field => (
-                                                <div
-                                                    key={field.key}
-                                                    className="bs-Fieldset-row"
-                                                >
-                                                    <label className="bs-Fieldset-label">
-                                                        {field.label}
-                                                    </label>
-                                                    <div
-                                                        className="bs-Fieldset-fields"
-                                                        style={{
-                                                            paddingTop: 3,
-                                                        }}
-                                                    >
-                                                        <Field
-                                                            className="db-BusinessSettings-input TextInput bs-TextInput"
-                                                            type={field.type}
-                                                            name={field.key}
-                                                            id={field.key}
-                                                            placeholder={
-                                                                field.placeholder ||
-                                                                field.label
-                                                            }
-                                                            component={
-                                                                field.component
-                                                            }
-                                                            disabled={
-                                                                settings &&
-                                                                settings.requesting
-                                                            }
-                                                            style={{
-                                                                width: '350px',
-                                                            }}
-                                                        />
-                                                        <span
-                                                            style={{
-                                                                marginTop:
-                                                                    '10px',
-                                                            }}
-                                                        >
-                                                            {field.description}
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            ))}
-                                        </div>
-                                    </fieldset>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="bs-ContentSection-footer bs-ContentSection-content Box-root Box-background--white Flex-flex Flex-alignItems--center Flex-justifyContent--spaceBetween Padding-horizontal--20 Padding-vertical--12">
-                            <span className="db-SettingsForm-footerMessage"></span>
-                            <div>
-                                <button
-                                    className="bs-Button bs-Button--blue"
-                                    disabled={settings && settings.requesting}
-                                    type="submit"
-                                >
-                                    {settings.requesting ? (
-                                        <FormLoader />
-                                    ) : (
-                                            <span>Save Settings</span>
-                                        )}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
                 </div>
             </div>
         );
@@ -371,7 +223,6 @@ Component.propTypes = {
     saveSettings: PropTypes.func.isRequired,
     fetchSettings: PropTypes.func.isRequired,
     initialValues: PropTypes.object,
-    ssoForm: PropTypes.object,
 };
 
 const mapDispatchToProps = dispatch => {
@@ -397,10 +248,4 @@ function mapStateToProps(state) {
     };
 }
 
-const ReduxFormComponent = reduxForm({
-    form: 'sso-form',
-    enableReinitialize: true,
-    validate,
-})(Component);
-
-export default connect(mapStateToProps, mapDispatchToProps)(ReduxFormComponent);
+export default connect(mapStateToProps, mapDispatchToProps)(Component);
