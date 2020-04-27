f72574cd343341b77e29e808178069b12d5892c1
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-27 14:39:16 +0100

    Defines fetchSsos action

---
+++ admin-dashboard/src/actions/sso.js
@@ -0,0 +1,47 @@
+import * as types from "../constants/sso"
+import errors from '../errors';
+import { getApi } from "../api";
+
+export const fetchSsosRequest = () => {
+  return {
+    type: types.FETCH_SSOS_REQUEST,
+  }
+}
+
+export const fetchSsosSuccess = (ssos) => {
+  return {
+    type: types.FETCH_SSOS_SUCCESS,
+    payload:ssos,
+  }
+}
+
+export const fetchSsosError = payload => {
+  return {
+      type: types.FETCH_SSOS_FAILURE,
+      payload,
+  };
+};
+
+
+const fetchSsos = () => async dispatch => {
+  dispatch(fetchSsosRequest);
+  try {
+    const response = await getApi(
+      `/sso/ssos`
+    );
+    dispatch(fetchSsosSuccess(response.data))
+  } catch (error) {
+    let errorMsg;
+    if (error && error.response && error.response.data)
+      errorMsg = error.response.data;
+    if (error && error.data) {
+      errorMsg = error.data;
+    }
+    if (error && error.message) {
+      errorMsg = error.message;
+    } else {
+      errorMsg = 'Network Error';
+    }
+    dispatch(fetchSsosError(errors(errorMsg)));
+  }
+}
