9c22bd705033a2d76334a7b538a2aef534ad951c
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-27 17:26:12 +0100

    Implements deleteSso in ssoService

--- backend/backend/services/ssoService.js
+++ backend/backend/services/ssoService.js
@@ -1,8 +1,24 @@
 module.exports = {
-    getAllSsos:async function(){
-        const ssos=await SsoModel.find({});
+    getAllSsos: async function () {
+        const ssos = await SsoModel.find({});
         return ssos;
+    },
+    deleteSso: async function (ssoId) {
+        try {
+            const result = await SsoModel.deleteOne({ _id: ssoId });
+            const { ok, deletedCount } = result;
+            if (!ok || !deletedCount)
+                throw {
+                    code: 400,
+                    message: 'Failed to remove the sso.'
+                }
+            return 'Sso remove successfully!';
+        } catch (error) {
+            ErrorService.log('ssoService.deleteSso', error);
+            throw error;
+        }
     }
 }
 
 const SsoModel = require('../models/sso');
+const ErrorService = require('./errorService');
