3abd087a24ae0e52956bb63ca4cb644b9c8bef50
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-28 16:08:23 +0100

    Add limit/skip parameters in getAllSsos query

--- backend/backend/services/ssoService.js
+++ backend/backend/services/ssoService.js
@@ -1,10 +1,25 @@
 module.exports = {
-    getAllSsos: async function () {
-        const ssos = await SsoModel.find(
-            {},
-            { _id: 1, domain: 1, createdAt: 1 }
-        );
-        return ssos;
+    getAllSsos: async function (skip, limit) {
+        if (!skip) skip = 0;
+
+        if (!limit) limit = 0;
+
+        if (typeof skip === 'string') skip = parseInt(skip);
+
+        if (typeof limit === 'string') limit = parseInt(limit);
+
+        try {
+            const ssos = await SsoModel.find(
+                {},
+                { _id: 1, domain: 1, createdAt: 1 })
+                .sort([['createdAt', -1]])
+                .skip(skip)
+                .limit(limit);
+            return ssos;
+        } catch (error) {
+            ErrorService.log('ssoService.deleteSso', error);
+            throw error;
+        }
     },
     deleteSso: async function (ssoId) {
         try {
