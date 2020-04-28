958b4baf41e33ac75b1b323e80c4a1f87ff744af
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-04-28 13:25:43 +0100

    Update hosts test

--- smoke-test/server.test.js
+++ smoke-test/server.test.js
@@ -20,9 +20,26 @@ describe('Check Server', () => {
         const response = await page.$eval('body > pre', e => {
             return e.innerHTML;
         });
-        expect(response).toBe(
-            '{"api":"https://fyipe.com/api","home":"https://fyipe.com","accounts":"https://fyipe.com/accounts","dashboard":"https://fyipe.com/dashboard"}'
-        );
+
+        let expectedResponse;
+        if (utils.BACKEND_URL.includes('localhost')) {
+            if (utils.BACKEND_URL.includes('localhost:')) {
+                expectedResponse =
+                    '{"api":"http://localhost:3002/api","home":"http://localhost:1444","accounts":"http://localhost:3003/accounts","dashboard":"http://localhost:3000/dashboard"}';
+            } else {
+                expectedResponse =
+                    '{"api":"http://localhost/api","home":"http://localhost","accounts":"http://localhost/accounts","dashboard":"http://localhost/dashboard"}';
+            }
+        } else {
+            if (utils.BACKEND_URL.includes('staging.')) {
+                expectedResponse =
+                    '{"api":"https://staging.fyipe.com/api","home":"https://staging.fyipe.com","accounts":"https://staging.fyipe.com/accounts","dashboard":"https://staging.fyipe.com/dashboard"}';
+            } else {
+                expectedResponse =
+                    '{"api":"https://fyipe.com/api","home":"https://fyipe.com","accounts":"https://fyipe.com/accounts","dashboard":"https://fyipe.com/dashboard"}';
+            }
+        }
+        expect(response).toBe(expectedResponse);
     });
 
     test('should get saas status true from server', async () => {
