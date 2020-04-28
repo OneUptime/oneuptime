2c6f1800990b88b20739755ea2f682bd44af0861
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-28 17:14:15 +0100

    Protecting all sso endpoints by checking if the user is a master admin

--- backend/backend/api/sso.js
+++ backend/backend/api/sso.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const getUser = require('../middlewares/user').getUser;
+const isUserMasterAdmin = require('../middlewares/user').isUserMasterAdmin;
 const sendListResponse = require('../middlewares/response').sendListResponse;
 const sendItemResponse = require('../middlewares/response').sendItemResponse;
 const sendErrorResponse = require('../middlewares/response').sendErrorResponse;
 const SsoService = require('../services/ssoService')
 
-router.get('/ssos', async function (req, res) {
+router.get('/ssos', getUser, isUserMasterAdmin, async function (req, res) {
   const skip = req.query.skip || 0;
   const limit = req.query.limit || 10;
   try {
     const ssos = await SsoService.getAllSsos(skip, limit)
-    const count= await SsoService.getCount()
-    return sendListResponse(req, res, ssos,count)
+    const count = await SsoService.getCount()
+    return sendListResponse(req, res, ssos, count)
   } catch (error) {
     return sendErrorResponse(req, res, error)
   }
 })
 
-router.delete('/:ssoId', async function (req, res) {
+router.delete('/:ssoId', getUser, isUserMasterAdmin, async function (req, res) {
   const ssoId = req.params.ssoId
   if (!ssoId) {
     return sendErrorResponse(req, res, {
@@ -33,7 +35,7 @@ router.delete('/:ssoId', async function (req, res) {
   }
 })
 
-router.post('/create', async function (req, res) {
+router.post('/create', getUser, isUserMasterAdmin, async function (req, res) {
   const data = req.body
   try {
     await SsoService.createSso(data)
@@ -43,7 +45,7 @@ router.post('/create', async function (req, res) {
   }
 })
 
-router.get('/:ssoId', async function (req, res) {
+router.get('/:ssoId', getUser, isUserMasterAdmin, async function (req, res) {
   const ssoId = req.params.ssoId
   if (!ssoId) {
     return sendErrorResponse(req, res, {
@@ -59,7 +61,7 @@ router.get('/:ssoId', async function (req, res) {
   }
 })
 
-router.put('/update', async function (req, res) {
+router.put('/update', getUser, isUserMasterAdmin, async function (req, res) {
   const data = req.body
   const { _id: ssoId } = data
   if (!ssoId) {
