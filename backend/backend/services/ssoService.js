fe49a5b63c46b20f4497f9218fca2460933e62c9
Mohamed ABDELLANI <abdellani@gmail.com>
2020-04-28 18:47:14 +0100

    Replaces getAllSsos with findBy in ssoService

--- backend/backend/services/ssoService.js
+++ backend/backend/services/ssoService.js
@@ -1,16 +1,19 @@
 module.exports = {
-    getAllSsos: async function (skip, limit) {
-        if (!skip) skip = 0;
-
-        if (!limit) limit = 0;
-
-        if (typeof skip === 'string') skip = parseInt(skip);
-
-        if (typeof limit === 'string') limit = parseInt(limit);
-
+    findBy: async function (query, limit, skip) {
         try {
+            if (!skip) skip = 0;
+
+            if (!limit) limit = 0;
+
+            if (typeof skip === 'string') skip = parseInt(skip);
+
+            if (typeof limit === 'string') limit = parseInt(limit);
+
+            if (!query) query = {};
+
+            // if (!query.deleted) query.deleted = false;
             const ssos = await SsoModel.find(
-                {},
+                query,
                 { _id: 1, domain: 1, createdAt: 1 })
                 .sort([['createdAt', -1]])
                 .skip(skip)
@@ -21,6 +24,7 @@ module.exports = {
             throw error;
         }
     },
+
     deleteSso: async function (ssoId) {
         try {
             const result = await SsoModel.deleteOne({ _id: ssoId });
