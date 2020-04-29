7a3645511a4561e745f870a0890f817db864f34a
Ojini Chizoba Jude <judoc96@gmail.com>
2020-04-29 21:30:33 +0100

    modal for confirming deleting domain

---
+++ dashboard/src/components/statusPage/DeleteDomainModal.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import ShouldRender from '../basic/ShouldRender';
+
+const DeleteDomainModal = ({
+    confirmThisDialog,
+    closeThisDialog,
+    deleteDomain,
+}) => {
+    return (
+        <div
+            onKeyDown={e => e.key === 'Escape' && closeThisDialog()}
+            className="ModalLayer-wash Box-root Flex-flex Flex-alignItems--flexStart Flex-justifyContent--center"
+        >
+            <div
+                className="ModalLayer-contents"
+                tabIndex={-1}
+                style={{ marginTop: 40 }}
+            >
+                <div className="bs-BIM">
+                    <div className="bs-Modal bs-Modal--medium">
+                        <div className="bs-Modal-header">
+                            <div className="bs-Modal-header-copy">
+                                <span className="Text-color--inherit Text-display--inline Text-fontSize--20 Text-fontWeight--regular Text-lineHeight--24 Text-typeface--base Text-wrap--wrap">
+                                    <span>Delete Domain</span>
+                                </span>
+                            </div>
+                        </div>
+                        <div className="bs-Modal-content">
+                            <span className="Text-color--inherit Text-display--inline Text-fontSize--14 Text-fontWeight--regular Text-lineHeight--24 Text-typeface--base Text-wrap--wrap">
+                                Do you want to delete this domain?
+                            </span>
+                        </div>
+                        <div className="bs-Modal-footer">
+                            <div className="bs-Modal-footer-actions">
+                                <ShouldRender if={deleteDomain.error}>
+                                    <div className="bs-Tail-copy">
+                                        <div
+                                            className="Box-root Flex-flex Flex-alignItems--stretch Flex-direction--row Flex-justifyContent--flexStart"
+                                            style={{ marginTop: '10px' }}
+                                        >
+                                            <div className="Box-root Margin-right--8">
+                                                <div
+                                                    className="Icon Icon--info Icon--color--red Icon--size--14 Box-root Flex-flex"
+                                                    style={{ marginTop: '2px' }}
+                                                ></div>
+                                            </div>
+                                            <div className="Box-root">
+                                                <span style={{ color: 'red' }}>
+                                                    {deleteDomain.error}
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </ShouldRender>
+                                {/* TODO: HANDLE DISABLING THE BUTTON WHEN PROCESSING */}
+                                <button
+                                    id="cancelAuditDelete"
+                                    className={`bs-Button ${deleteDomain.requesting &&
+                                        'bs-is-disabled'}`}
+                                    type="button"
+                                    onClick={closeThisDialog}
+                                    disabled={deleteDomain.requesting}
+                                >
+                                    <span>Cancel</span>
+                                </button>
+                                <button
+                                    id="confirmDelete"
+                                    className={`bs-Button bs-Button--red Box-background--red ${deleteDomain.requesting &&
+                                        'bs-is-disabled'}`}
+                                    onClick={confirmThisDialog}
+                                    disabled={deleteDomain.requesting}
+                                >
+                                    <span>Delete Domain</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const mapStateToProps = state => ({
+    deleteDomain: state.statusPage.deleteDomain,
+});
+
+DeleteDomainModal.displayName = 'Delete Domain Modal';
+
+DeleteDomainModal.propTypes = {
+    confirmThisDialog: PropTypes.func.isRequired,
+    closeThisDialog: PropTypes.func,
+    deleteDomain: PropTypes.object,
+};
+
+export default connect(mapStateToProps)(DeleteDomainModal);
