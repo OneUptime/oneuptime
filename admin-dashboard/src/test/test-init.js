9fb2db2493cbc58ea7929c7ef28fda13ea178196
Ojini Chizoba Jude <judoc96@gmail.com>
2020-05-08 22:29:57 +0100

    register a user for puppeteer test

--- admin-dashboard/src/test/test-init.js
+++ admin-dashboard/src/test/test-init.js
@@ -45,4 +45,75 @@ module.exports = {
             ]);
         }
     },
+    registerUser: async function(user, page, checkCard = true) {
+        const { email, password } = user;
+        let frame, elementHandle;
+        await page.goto(utils.ACCOUNTS_URL + '/register', {
+            waitUntil: 'networkidle2',
+        });
+        await page.waitForSelector('#email');
+        await page.click('input[name=email]');
+        await page.type('input[name=email]', email);
+        await page.click('input[name=name]');
+        await page.type('input[name=name]', 'Test Name');
+        await page.click('input[name=companyName]');
+        await page.type('input[name=companyName]', 'Test Name');
+        await page.click('input[name=companyPhoneNumber]');
+        await page.type('input[name=companyPhoneNumber]', '99105688');
+        await page.click('input[name=password]');
+        await page.type('input[name=password]', password);
+        await page.click('input[name=confirmPassword]');
+        await page.type('input[name=confirmPassword]', password);
+
+        if (checkCard) {
+            await Promise.all([
+                page.waitForSelector(`form#card-form`),
+                page.click('button[type=submit]'),
+            ]);
+            await page.waitForSelector('iframe[name=__privateStripeFrame5]');
+            await page.waitForSelector('iframe[name=__privateStripeFrame6]');
+            await page.waitForSelector('iframe[name=__privateStripeFrame7]');
+
+            await page.click('input[name=cardName]');
+            await page.type('input[name=cardName]', 'Test name');
+
+            elementHandle = await page.$('iframe[name=__privateStripeFrame5]');
+            frame = await elementHandle.contentFrame();
+            await frame.waitForSelector('input[name=cardnumber]');
+            await frame.type('input[name=cardnumber]', '42424242424242424242', {
+                delay: 50,
+            });
+
+            elementHandle = await page.$('iframe[name=__privateStripeFrame6]');
+            frame = await elementHandle.contentFrame();
+            await frame.waitForSelector('input[name=cvc]');
+            await frame.type('input[name=cvc]', '123', {
+                delay: 50,
+            });
+
+            elementHandle = await page.$('iframe[name=__privateStripeFrame7]');
+            frame = await elementHandle.contentFrame();
+            await frame.waitForSelector('input[name=exp-date]');
+            await frame.type('input[name=exp-date]', '11/23', {
+                delay: 50,
+            });
+
+            await page.click('input[name=address1]');
+            await page.type('input[name=address1]', "Enugu, Nigerian");
+            await page.click('input[name=address2]');
+            await page.type('input[name=address2]', "Lagos, Nigeria");
+            await page.click('input[name=city]');
+            await page.type('input[name=city]', "Awka");
+            await page.click('input[name=state]');
+            await page.type('input[name=state]', "Enugu");
+            await page.click('input[name=zipCode]');
+            await page.type('input[name=zipCode]', "420110");
+            await page.select('#country', 'India');
+        }
+
+        await Promise.all([
+            page.waitForSelector('div#success-step'),
+            page.click('button[type=submit]'),
+        ]);
+    },
 };
