c92ab3ac698a51475c2c5da45e10c3a931a8fec7
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-05-21 17:57:18 +0200

    Remove caching.

--- home/server.js
+++ home/server.js
@@ -9,13 +9,9 @@ const productCompare = require('./config/product-compare');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(compression());
 
 app.use('*', function(req, res, next) {
-    if (process.env && process.env.PRODUCTION) {
-        res.set('Cache-Control', 'public, max-age=86400');
-    } else res.set('Cache-Control', 'no-cache');
     next();
 });
 
