f84d2f820264aacb017291bb428f9e8f4a1fe2ab
Ojini Chizoba Jude <judoc96@gmail.com>
2020-05-31 00:02:14 +0100

    label indicator for various issue level

---
+++ dashboard/src/components/security/IssueLabel.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const IssueLabel = ({ level }) => {
+    let color;
+    switch (level) {
+        case 'critical':
+            color = 'red';
+            break;
+        case 'high':
+            color = 'yellow';
+            break;
+        case 'moderate':
+            color = 'blue';
+            break;
+        case 'low':
+            color = 'green';
+            break;
+        default:
+            color = 'green';
+    }
+
+    const colorStyle = {
+        // overide the default blue color
+        color: 'rgba(0, 0, 255, 0.8)',
+    };
+
+    return (
+        <div
+            className={`Badge Badge--color--${color} Box-root Flex-inlineFlex Flex-alignItems--center Padding-horizontal--8 Padding-vertical--2`}
+        >
+            <span
+                className={`Badge-text Text-color--${color} Text-display--inline Text-fontSize--12 Text-fontWeight--bold Text-lineHeight--16 Text-typeface--upper Text-wrap--noWrap`}
+                style={level === 'moderate' ? colorStyle : {}}
+            >
+                <span>{level}</span>
+            </span>
+        </div>
+    );
+};
+
+IssueLabel.displayName = 'IssueLabel';
+
+IssueLabel.propTypes = {
+    level: PropTypes.string,
+};
+
+export default IssueLabel;
