160c2123f64f9f6a128570c5764f5627e7f0ba22
Ojini Chizoba Jude <judoc96@gmail.com>
2020-05-30 23:46:01 +0100

    application and security model

---
+++ backend/backend/models/applicationSecurity.js
@@ -0,0 +1,26 @@
+const mongoose = require('../config/db');
+
+const Schema = mongoose.Schema;
+
+const applicationSecuritySchema = new Schema(
+    {
+        name: String,
+        gitRepositoryUrl: String,
+        gitCredential: { type: Schema.Types.ObjectId, ref: 'GitCredential' },
+        componentId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Component',
+        },
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: Date,
+    },
+    { timestamps: true } //automatically adds createdAt and updatedAt to the schema
+);
+
+module.exports = mongoose.model(
+    'ApplicationSecurity',
+    applicationSecuritySchema
+);
---
+++ backend/backend/models/containerSecurity.js
@@ -0,0 +1,27 @@
+const mongoose = require('../config/db');
+
+const Schema = mongoose.Schema;
+
+const containerSecuritySchema = new Schema(
+    {
+        name: String,
+        dockerCredential: {
+            type: Schema.Types.ObjectId,
+            ref: 'DockerCredential',
+        },
+        imagePath: String,
+        imageTags: String,
+        componentId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Component',
+        },
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+        deleteAt: Date,
+    },
+    { timestamps: true } //automatically adds createdAt and updatedAt to the schema
+);
+
+module.exports = mongoose.model('ContainerSecurity', containerSecuritySchema);
