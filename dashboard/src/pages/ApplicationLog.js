a46f80894912ce235081a7f43fe9388d0284522c
Zadat Olayinka <bundayy@outlook.com>
2020-06-02 20:51:12 +0100

    application log page

--- dashboard/src/pages/ApplicationLog.js
+++ dashboard/src/pages/ApplicationLog.js
@@ -5,22 +5,37 @@ import ShouldRender from '../components/basic/ShouldRender';
 import TutorialBox from '../components/tutorial/TutorialBox';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import NewApplicationLog from '../components/application/NewApplicationLog';
+import getParentRoute from '../utils/getParentRoute';
 
 
 
 class ApplicationLog extends Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
     render() {
+        const {
+            location: { pathname },
+            component,
+        } = this.props;
+
+        const componentName = component.length > 0 ? component[0].name : null;
         return (
             <Dashboard>
-                <BreadCrumbItem route="#" name="Test"/>
-                <BreadCrumbItem route="#" name="Application Log" />
+                <BreadCrumbItem
+                    route={getParentRoute(pathname)}
+                    name={componentName}
+                />
+                <BreadCrumbItem route={pathname} name="Application Log" />
                 <div>
                     <div>
                         <div className="db-RadarRulesLists-page">
                             <ShouldRender if={this.props.applicationLogTutorial.show}>
                                 <TutorialBox type="applicationLog" />
                             </ShouldRender>
-
+                            <NewApplicationLog/>
                         </div>
                     </div>
                 </div>
@@ -28,12 +43,27 @@ class ApplicationLog extends Component {
         )
     }
 }
-const mapStateToProps = (state) => {
+const mapStateToProps = (state,props) => {
+    const { componentId } = props.match.params;
+    
+    const component = state.component.componentList.components.map(item => {
+        return item.components.find(component => component._id === componentId);
+    });
     return {
         applicationLogTutorial: state.tutorial.incident,
+        componentId,
+        component,
     };
 };
 ApplicationLog.propTypes = {
     applicationLogTutorial: PropTypes.object,
+    location: PropTypes.shape({
+        pathname: PropTypes.string,
+    }),
+    component: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string,
+        })
+    ),
 };
 export default connect(mapStateToProps)(ApplicationLog);
