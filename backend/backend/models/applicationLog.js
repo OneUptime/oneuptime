51a12eeaf8e0117966559fa062eeb01ec159f748
Zadat Olayinka <bundayy@outlook.com>
2020-06-03 11:42:50 +0100

    application log model

---
+++ backend/backend/models/applicationLog.js
@@ -0,0 +1,32 @@
+const mongoose = require('../config/db');
+
+const Schema = mongoose.Schema;
+const applicationLogSchema = new Schema({
+    componentId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Component',
+        alias: 'component',
+    }, //which component this application log belongs to.
+    name: String,
+    createdById: { type: String, ref: 'User' }, //userId.
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    deleted: { type: Boolean, default: false },
+
+    deletedAt: {
+        type: Date,
+    },
+
+    deletedById: { type: String, ref: 'User' },
+});
+
+applicationLogSchema.virtual('project', {
+    localField: '_id',
+    foreignField: 'componentId',
+    ref: 'Component',
+    justOne: true,
+});
+
+module.exports = mongoose.model('ApplicationLog', applicationLogSchema);
