dbcd333977ac314e9fe5f844c936bae98909f1c0
Zadat Olayinka <bundayy@outlook.com>
2020-06-04 09:21:19 +0100

    removed select + add button

--- dashboard/src/components/application/NewApplicationLog.js
+++ dashboard/src/components/application/NewApplicationLog.js
@@ -3,9 +3,12 @@ import { RenderField } from '../basic/RenderField';
 import { ValidateField } from '../../config';
 import { Field, reduxForm } from 'redux-form';
 import { RenderSelect } from '../basic/RenderSelect';
+import { FormLoader } from '../basic/Loader';
+import ShouldRender from '../basic/ShouldRender';
 
 class NewApplicationLog extends Component {
     render() {
+        const requesting = false;
         return (
             <div className="Box-root Margin-bottom--12">
                 <div className="bs-ContentSection Card-root Card-shadow--medium">
@@ -24,79 +27,81 @@ class NewApplicationLog extends Component {
                             </div>
                         </div>
                         <form id="form-new-application-log">
-                                    <div
-                                        className="bs-ContentSection-content Box-root Box-background--offset Box-divider--surface-bottom-1 Padding-vertical--2"
-                                        style={{ boxShadow: 'none' }}
-                                    >
-                                        <div>
-                                            <div className="bs-Fieldset-wrapper Box-root Margin-bottom--2">
-                                                <fieldset className="bs-Fieldset">
-                                                    <div className="bs-Fieldset-rows">
-                                                        <div className="bs-Fieldset-row">
-                                                            <label className="bs-Fieldset-label">
-                                                                Name
-                                                            </label>
-                                                            <div className="bs-Fieldset-fields">
-                                                                <Field
-                                                                    className="db-BusinessSettings-input TextInput bs-TextInput"
-                                                                    component={
-                                                                        RenderField
-                                                                    }
-                                                                    type="text"
-                                                                    name={`name_${this.props.index}`}
-                                                                    id="name"
-                                                                    placeholder="Application Name"
-                                                                    validate={
-                                                                        ValidateField.text
-                                                                    }
-                                                                />
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </fieldset>
-                                            </div>
-                                            <div className="bs-Fieldset-row">
-                                                <label className="bs-Fieldset-label">
-                                                    Application Type
-                                                </label>
-
-                                                <div className="bs-Fieldset-fields">
-                                                    <span className="flex">
+                            <div
+                                className="bs-ContentSection-content Box-root Box-background--offset Box-divider--surface-bottom-1 Padding-vertical--2"
+                                style={{ boxShadow: 'none' }}
+                            >
+                                <div>
+                                    <div className="bs-Fieldset-wrapper Box-root Margin-bottom--2">
+                                        <fieldset className="bs-Fieldset">
+                                            <div className="bs-Fieldset-rows">
+                                                <div className="bs-Fieldset-row">
+                                                    <label className="bs-Fieldset-label">
+                                                        Name
+                                                    </label>
+                                                    <div className="bs-Fieldset-fields">
                                                         <Field
-                                                            className="db-select-nw"
+                                                            className="db-BusinessSettings-input TextInput bs-TextInput"
                                                             component={
-                                                                RenderSelect
+                                                                RenderField
                                                             }
-                                                            name={`type_${this.props.index}`}
-                                                            id="type"
-                                                            placeholder="Application Type"
+                                                            type="text"
+                                                            name={`name_${this.props.index}`}
+                                                            id="name"
+                                                            placeholder="Application Name"
                                                             validate={
-                                                                ValidateField.select
+                                                                ValidateField.text
                                                             }
-                                                            options={[
-                                                                {
-                                                                    value: '',
-                                                                    label:
-                                                                        'Select application type',
-                                                                },
-                                                                {
-                                                                    value: 'a',
-                                                                    label:
-                                                                        'Option A',
-                                                                },
-                                                                {
-                                                                    value: 'b',
-                                                                    label:
-                                                                        'Option B',
-                                                                },
-                                                            ]}
                                                         />
-                                                    </span>
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
+                                        </fieldset>
                                     </div>
-                                </form>
+                                </div>
+                            </div>
+                            <div className="bs-ContentSection-footer bs-ContentSection-content Box-root Box-background--white Flex-flex Flex-alignItems--center Flex-justifyContent--spaceBetween Padding-horizontal--20 Padding-vertical--12">
+                                <div className="bs-Tail-copy">
+                                    <div className="Box-root Flex-flex Flex-alignItems--stretch Flex-direction--row Flex-justifyContent--flexStart">
+                                        {/* <ShouldRender
+                                            if={
+                                                this.props.applicationLog
+                                                    .newApplicationLog.error
+                                            }
+                                        >
+                                            <div className="Box-root Margin-right--8">
+                                                <div className="Icon Icon--info Icon--color--red Icon--size--14 Box-root Flex-flex"></div>
+                                            </div>
+                                            <div className="Box-root">
+                                                <span style={{ color: 'red' }}>
+                                                    {
+                                                        this.props
+                                                            .applicationLog
+                                                            .newApplicationLog
+                                                            .error
+                                                    }
+                                                </span>
+                                            </div>
+                                        </ShouldRender> */}
+                                    </div>
+                                </div>
+                                <div>
+                                    <button
+                                        id="addApplicationLogButton"
+                                        className="bs-Button bs-Button--blue"
+                                        type="submit"
+                                    >
+                                        <ShouldRender if={!requesting}>
+                                            <span>Add Application </span>
+                                        </ShouldRender>
+
+                                        <ShouldRender if={requesting}>
+                                            <FormLoader />
+                                        </ShouldRender>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
