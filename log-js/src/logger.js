466f1f21077ab4518d8878084df5b7b5db946bef
Zadat Olayinka <bundayy@outlook.com>
2020-06-08 14:38:51 +0100

    clean up on library

--- log-js/src/logger.js
+++ log-js/src/logger.js
@@ -2,7 +2,23 @@ class Logger {
     constructor(applicationLogId, applicationLogKey) {
         this.applicationLogId = applicationLogId;
         this.applicationLogKey = applicationLogKey;
-        this.apiUrl = '';
+        this.apiUrl = '/applicationLogId/create-log';
+
+        const isLocalhost =
+            window &&
+            window.location &&
+            window.location.host &&
+            (window.location.host.includes('localhost:') ||
+                window.location.host.includes('0.0.0.0:') ||
+                window.location.host.includes('127.0.0.1:'));
+
+        if (isLocalhost || isLocalhost === '') {
+            let protocol = 'http:';
+            if(window.location.protocol != 'file:'){
+                protocol = window.location.protocol
+            }
+            this.apiUrl = `${protocol}//localhost:3002/api/application-log/${this.applicationLogId}/create-log`;
+        }
     }
 
     log(data) {
@@ -16,11 +32,13 @@ class Logger {
             // api request
             var xmlHttp = new XMLHttpRequest();
             xmlHttp.open('POST', this.apiUrl, true);
-            xmlHttp.setRequestHeader('Content-Type', 'text/plain');
-            xmlHttp.send(JSON.stringify(data));
+            xmlHttp.setRequestHeader('Content-Type', 'application/json');
+            xmlHttp.send(JSON.stringify({"data":data}));
         } catch (error) {
-            console.log("Failed to log to loggly because of this exception:\n" + ex);
-            console.log("Failed log data:", data);
+            console.log(
+                'Failed to log to server because of this exception:\n' + error
+            );
+            console.log('Failed log data:', data);
         }
     }
 }
