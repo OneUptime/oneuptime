9424a90775bbd06a082c4d9ef445c8ddd5dd3f22
Zadat Olayinka <bundayy@outlook.com>
2020-06-08 21:58:07 +0100

    api url

---
+++ log-js/src/config.js
@@ -0,0 +1,9 @@
+/** The api url to send server information. */
+const API_URL =
+    // process.env.API_URL ||
+    // (process.env.NODE_ENV === 'development'
+    //     ? 
+        'http://localhost:3002/api/application-log'
+        // : 'https://fyipe.com/api/application-log');
+
+module.exports = { API_URL };
