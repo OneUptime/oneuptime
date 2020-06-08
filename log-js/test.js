0579a77757c82565cb4d38dba806453c09309595
Zadat Olayinka <bundayy@outlook.com>
2020-06-08 22:14:06 +0100

    object logged in demo

--- log-js/test.js
+++ log-js/test.js
@@ -2,7 +2,7 @@ const Logger = require('./src/index');
 
 async function logApiRequest() {
     Logger('id', 'key') // set application log id and key
-        .log('test') // set any data type to log
+        .log({content: 'this is a test log'}) // set any data type to log
         .then(res => console.log(res.data)) // get response
         .catch(err => console.log(err.message)); // get error
 }
