e339c7c3538607bdebff231cebfb8ffacba26e79
Zadat Olayinka <bundayy@outlook.com>
2020-06-09 16:15:12 +0100

    preset bug

--- log-js/webpack.config.js
+++ log-js/webpack.config.js
@@ -1,23 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve('dist'),
-    filename: 'logger.js',
-    libraryTarget: 'commonjs2',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /(node_modules)/,
-        use: 'babel-loader',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js'],
-  },
+    mode: 'production',
+    entry: './src/logger.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'logger.js',
+        libraryTarget: 'commonjs2',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js'],
+    },
 };
