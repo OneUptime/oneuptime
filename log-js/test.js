4f0df9f3ed713291e3b3cfcfab38bd7f47f5b784
Zadat Olayinka <bundayy@outlook.com>
2020-06-09 10:40:36 +0100

    test library

--- log-js/test.js
+++ log-js/test.js
@@ -1,9 +1,15 @@
 const Logger = require('./src/index');
 
 async function logApiRequest() {
-    Logger('id', 'key') // set application log id and key
+    Logger('5edf4d94a68af8016be57931', '2003e4d7-ebe9-4e16-b254-b3ed4b75575a') // set application log id and key
         .log({content: 'this is a test log'}) // set any data type to log
-        .then(res => console.log(res.data)) // get response
-        .catch(err => console.log(err.message)); // get error
+        .then(res => {
+            if(res.status === 200) { // request was successfully logged
+                console.log(res.data)
+            } else { // request failed 
+                console.log(res.response.data)
+            }
+        }) // get response
+        .catch(err => console.log(err.data)); // get error
 }
 logApiRequest();
