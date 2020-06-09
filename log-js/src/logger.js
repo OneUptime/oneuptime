5f3432e8689d4b00926e00dfac73e2049ca50802
Zadat Olayinka <bundayy@outlook.com>
2020-06-09 16:15:27 +0100

    api url

--- log-js/src/logger.js
+++ log-js/src/logger.js
@@ -1,44 +1,32 @@
+import axios from '../node_modules/axios';
 class Logger {
     constructor(applicationLogId, applicationLogKey) {
         this.applicationLogId = applicationLogId;
         this.applicationLogKey = applicationLogKey;
-        this.apiUrl = '/applicationLogId/create-log';
-
-        const isLocalhost =
-            window &&
-            window.location &&
-            window.location.host &&
-            (window.location.host.includes('localhost:') ||
-                window.location.host.includes('0.0.0.0:') ||
-                window.location.host.includes('127.0.0.1:'));
-
-        if (isLocalhost || isLocalhost === '') {
-            let protocol = 'http:';
-            if(window.location.protocol != 'file:'){
-                protocol = window.location.protocol
-            }
-            this.apiUrl = `${protocol}//localhost:3002/api/application-log/${this.applicationLogId}/create-log`;
-        }
+        this.apiUrl = `http://localhost:3002/api/application-log/${this.applicationLogId}/log-content`;
     }
 
-    log(data) {
+    async log(data) {
         var type = typeof data;
 
         if (!data || !(type === 'object' || type === 'string')) {
             return;
         }
         // make api requeest to the server to save a log with the key, id and content
-        try {
-            // api request
-            var xmlHttp = new XMLHttpRequest();
-            xmlHttp.open('POST', this.apiUrl, true);
-            xmlHttp.setRequestHeader('Content-Type', 'application/json');
-            xmlHttp.send(JSON.stringify({"data":data}));
-        } catch (error) {
-            console.log(
-                'Failed to log to server because of this exception:\n' + error
-            );
-            console.log('Failed log data:', data);
-        }
+        return await this.makeApiRequest(data);
+    }
+
+    makeApiRequest(data) {
+        return new Promise((resolve, reject) => {
+            axios
+                .post(this.apiUrl, { content: data, applicationLogKey: this.applicationLogKey })
+                .then(res => {
+                    resolve(res);
+                })
+                .catch(err => {
+                    reject(err);
+                });
+        });
     }
 }
+export default Logger;
