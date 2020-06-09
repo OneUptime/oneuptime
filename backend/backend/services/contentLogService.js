fdb8ae8d6a320500d8493118eb64b3cf52391365
Zadat Olayinka <bundayy@outlook.com>
2020-06-09 10:25:16 +0100

    find by query on content log

--- backend/backend/services/contentLogService.js
+++ backend/backend/services/contentLogService.js
@@ -34,6 +34,71 @@ module.exports = {
             throw error;
         }
     },
+    async findBy(query, limit, skip) {
+        try {
+            if (!skip) skip = 0;
+
+            if (!limit) limit = 0;
+
+            if (typeof skip === 'string') {
+                skip = parseInt(skip);
+            }
+
+            if (typeof limit === 'string') {
+                limit = parseInt(limit);
+            }
+
+            if (!query) {
+                query = {};
+            }
+
+            if (!query.deleted) query.deleted = false;
+            const contentLogs = await ContentLogModel.find(query)
+                .sort([['createdAt', -1]])
+                .limit(limit)
+                .skip(skip)
+                .populate('applicationLogId', 'name');
+            return contentLogs;
+        } catch (error) {
+            ErrorService.log('contentLogService.findBy', error);
+            throw error;
+        }
+    },
+    async getContentLogsApplicationLogId(applicationLogId, limit, skip) {
+        // try to get the application log by the ID
+        let applicationLog = await ApplicationLogService.findOneBy({
+            _id: applicationLogId,
+        });
+        // send an error if the component doesnt exist
+        if (!applicationLog) {
+            const error = new Error('Application Log does not exist.');
+            error.code = 400;
+            ErrorService.log(
+                'contentLogService.getContentLogsApplicationLogId',
+                error
+            );
+            throw error;
+        }
+
+        try {
+            if (typeof limit === 'string') limit = parseInt(limit);
+            if (typeof skip === 'string') skip = parseInt(skip);
+            const _this = this;
+
+            const contentLogs = await _this.findBy(
+                { applicationLogId: applicationLogId },
+                limit,
+                skip
+            );
+            return contentLogs;
+        } catch (error) {
+            ErrorService.log(
+                'contentLogService.getContentLogsApplicationLogId',
+                error
+            );
+            throw error;
+        }
+    },
 };
 
 const ContentLogModel = require('../models/contentLog');
