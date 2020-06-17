dd25fc667d10aca8c04f6a1557b61de2d556efff
Zadat Olayinka <bundayy@outlook.com>
2020-06-17 11:00:28 +0100

    added fil to beign application log test

---
+++ dashboard/src/test/puppeteer/ApplicationLog.test.js
@@ -0,0 +1,78 @@
+const puppeteer = require('puppeteer');
+const utils = require('./test-utils');
+const init = require('./test-init');
+const { Cluster } = require('puppeteer-cluster');
+
+require('should');
+
+// user credentials
+const user = {
+    email: utils.generateRandomBusinessEmail(),
+    password: '1234567890',
+};
+const componentName = utils.generateRandomString();
+const newComponentName = utils.generateRandomString();
+const applicationLogName = utils.generateRandomString();
+const newApplicationLogName = utils.generateRandomString();
+
+describe('Components', () => {
+    const operationTimeOut = 200000;
+
+    let cluster;
+
+    beforeAll(async () => {
+        jest.setTimeout(200000);
+
+        cluster = await Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_PAGE,
+            puppeteerOptions: utils.puppeteerLaunchConfig,
+            puppeteer,
+            timeout: 120000,
+        });
+
+        cluster.on('taskerror', err => {
+            throw err;
+        });
+
+        return await cluster.execute(null, async ({ page }) => {
+            await init.registerUser(user, page);
+            await init.loginUser(user, page);
+        });
+    });
+
+    afterAll(async () => {
+        await cluster.idle();
+        await cluster.close();
+    });
+
+    test(
+        'Should create new component',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                // Navigate to Components page
+                await page.goto(utils.DASHBOARD_URL, {
+                    waitUntil: 'networkidle0',
+                });
+                await page.waitForSelector('#components', { timeout:120000});
+                await page.click('#components');
+
+                // Fill and submit New Component form
+                await page.waitForSelector('#form-new-component');
+                await page.click('input[id=name]');
+                await page.type('input[id=name]', componentName);
+                await page.click('button[type=submit]');
+                await page.goto(utils.DASHBOARD_URL);
+
+                let spanElement;
+                spanElement = await page.waitForSelector(
+                    `span#component-title-${componentName}`
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+                spanElement.should.be.exactly(componentName);
+            });
+        },
+        operationTimeOut
+    );
+
+});
