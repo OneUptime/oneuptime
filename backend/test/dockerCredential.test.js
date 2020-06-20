0bf8eb176323371a92dede973fbb6b63a5034b96
Ojini Chizoba Jude <judoc96@gmail.com>
2020-06-20 18:25:41 +0100

    backend test for error scenerios for docker credential

--- backend/test/dockerCredential.test.js
+++ backend/test/dockerCredential.test.js
@@ -126,4 +126,111 @@ describe('Docker Credential API', function() {
                     });
             });
     });
+
+    it('should not create docker credential with an existing docker registry url in a project', function(done) {
+        const authorization = `Basic ${token}`;
+        const dockerRegistryUrl = 'https://dockerhub.com/reactjs';
+        const dockerUsername = 'username';
+        const dockerPassword = 'password';
+
+        request
+            .post(`/credential/${projectId}/dockerCredential`)
+            .set('Authorization', authorization)
+            .send({
+                dockerRegistryUrl,
+                dockerUsername,
+                dockerPassword,
+            })
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Docker Credential already exist in this project'
+                );
+                done();
+            });
+    });
+
+    it('should not create docker credential if docker registry url is missing', function(done) {
+        const authorization = `Basic ${token}`;
+        const dockerRegistryUrl = ''; // empty docker registry url
+        const dockerUsername = 'username';
+        const dockerPassword = 'password';
+
+        request
+            .post(`/credential/${projectId}/dockerCredential`)
+            .set('Authorization', authorization)
+            .send({
+                dockerRegistryUrl,
+                dockerUsername,
+                dockerPassword,
+            })
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Docker Registry URL is required'
+                );
+                done();
+            });
+    });
+
+    it('should not create docker credential if docker username is missing', function(done) {
+        const authorization = `Basic ${token}`;
+        const dockerRegistryUrl = 'https://dockerhub.com/nodejs';
+        const dockerUsername = '';
+        const dockerPassword = 'password';
+
+        request
+            .post(`/credential/${projectId}/dockerCredential`)
+            .set('Authorization', authorization)
+            .send({
+                dockerRegistryUrl,
+                dockerUsername,
+                dockerPassword,
+            })
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Docker Username is required'
+                );
+                done();
+            });
+    });
+
+    it('should not create docker credential if docker password is missing', function(done) {
+        const authorization = `Basic ${token}`;
+        const dockerRegistryUrl = 'https://dockerhub.com/twitter';
+        const dockerUsername = 'username';
+
+        request
+            .post(`/credential/${projectId}/dockerCredential`)
+            .set('Authorization', authorization)
+            .send({
+                dockerRegistryUrl,
+                dockerUsername,
+            })
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Docker Password is required'
+                );
+                done();
+            });
+    });
+
+    it('should not remove a non-existing docker credential', function(done) {
+        const authorization = `Basic ${token}`;
+        const newCredentialId = '5e8db97b2cc46e3a229ebc62'; // non-existing credential id
+
+        request
+            .delete(`/credential/${projectId}/dockerCredential/${newCredentialId}`)
+            .set('Authorization', authorization)
+            .end(function(err, res) {
+                console.log(res.body);
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Docker Credential not found or does not exist'
+                );
+                done();
+            });
+    });
 });
