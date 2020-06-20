2ab34f518623cd45e671d2a053324cc8f856791f
Ojini Chizoba Jude <judoc96@gmail.com>
2020-06-20 19:52:29 +0100

    backend test for error scenerios in git credential

--- backend/test/gitCredential.test.js
+++ backend/test/gitCredential.test.js
@@ -15,7 +15,7 @@ const UserService = require('../backend/services/userService');
 const ProjectService = require('../backend/services/projectService');
 const GitCredentialService = require('../backend/services/gitCredentialService');
 
-describe('Docker Credential API', function() {
+describe('Git Credential API', function() {
     const timeout = 30000;
     let projectId, userId, token, credentialId;
 
@@ -116,6 +116,83 @@ describe('Docker Credential API', function() {
                 expect(res).to.have.status(200);
                 expect(res.body._id).to.be.equal(credentialId);
                 expect(res.body.deleted).to.be.true;
+                done();
+            });
+    });
+
+    it('should not create git credential with an existing git user in a project', function(done) {
+        const authorization = `Basic ${token}`;
+        const gitUsername = 'anotherUsername'; // an existing username
+        const gitPassword = 'password';
+
+        request
+            .post(`/credential/${projectId}/gitCredential`)
+            .set('Authorization', authorization)
+            .send({
+                gitUsername,
+                gitPassword,
+            })
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Git Credential already exist in this project'
+                );
+                done();
+            });
+    });
+
+    it('should not create git credential if git username is missing', function(done) {
+        const authorization = `Basic ${token}`;
+        const gitUsername = '';
+        const gitPassword = 'password';
+
+        request
+            .post(`/credential/${projectId}/gitCredential`)
+            .set('Authorization', authorization)
+            .send({
+                gitUsername,
+                gitPassword,
+            })
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Git Username is required'
+                );
+                done();
+            });
+    });
+
+    it('should not create git credential if git password is missing', function(done) {
+        const authorization = `Basic ${token}`;
+        const gitUsername = 'username';
+
+        request
+            .post(`/credential/${projectId}/gitCredential`)
+            .set('Authorization', authorization)
+            .send({
+                gitUsername,
+            })
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Please provide a password'
+                );
+                done();
+            });
+    });
+
+    it('should not remove a non-existing git credential', function(done) {
+        const authorization = `Basic ${token}`;
+        const credentialId = '5e8db97b2cc46e3a229ebc62'; // non-existing credential id
+
+        request
+            .delete(`/credential/${projectId}/gitCredential/${credentialId}`)
+            .set('Authorization', authorization)
+            .end(function(err, res) {
+                expect(res).to.have.status(400);
+                expect(res.body.message).to.be.equal(
+                    'Git Credential not found or does not exist'
+                );
                 done();
             });
     });
