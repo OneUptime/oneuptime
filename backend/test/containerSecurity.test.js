879d05eff08a5aa8cfce55ceef7eed2e5153e1e6
Ojini Chizoba Jude <judoc96@gmail.com>
2020-06-22 09:40:29 +0100

    backend test for creating a container security

---
+++ backend/test/containerSecurity.test.js
@@ -0,0 +1,121 @@
+/* eslint-disable no-undef */
+
+process.env.PORT = 3020;
+process.env.IS_SAAS_SERVICE = true;
+const chai = require('chai');
+const expect = require('chai').expect;
+const userData = require('./data/user');
+const dockerCredential = require('./data/dockerCredential');
+const app = require('../server');
+chai.use(require('chai-http'));
+const request = chai.request.agent(app);
+const GlobalConfig = require('./utils/globalConfig');
+const { createUser } = require('./utils/userSignUp');
+const VerificationTokenModel = require('../backend/models/verificationToken');
+const UserService = require('../backend/services/userService');
+const ProjectService = require('../backend/services/projectService');
+const ComponentService = require('../backend/services/componentService');
+const DockerCredentialService = require('../backend/services/dockerCredentialService');
+const ContainerSecurityService = require('../backend/services/containerSecurityService');
+
+describe('Container Security API', function() {
+    const timeout = 30000;
+    let projectId,
+        componentId,
+        userId,
+        token,
+        containerSecurityId,
+        credentialId;
+
+    this.timeout(timeout);
+    before(function(done) {
+        GlobalConfig.initTestConfig().then(function() {
+            createUser(request, userData.user, function(err, res) {
+                const project = res.body.project;
+                projectId = project._id;
+                userId = res.body.id;
+
+                UserService.updateOneBy(
+                    { _id: userId },
+                    { role: 'master-admin' }
+                ).then(function() {
+                    VerificationTokenModel.findOne({ userId }, function(
+                        err,
+                        verificationToken
+                    ) {
+                        request
+                            .get(
+                                `/user/confirmation/${verificationToken.token}`
+                            )
+                            .redirects(0)
+                            .end(function() {
+                                request
+                                    .post('/user/login')
+                                    .send({
+                                        email: userData.user.email,
+                                        password: userData.user.password,
+                                    })
+                                    .end(function(err, res) {
+                                        token = res.body.tokens.jwtAccessToken;
+                                        const authorization = `Basic ${token}`;
+
+                                        request
+                                            .post(`/component/${projectId}`)
+                                            .set('Authorization', authorization)
+                                            .send({ name: 'Test Component' })
+                                            .end(function(err, res) {
+                                                componentId = res.body._id;
+                                                done();
+                                            });
+                                    });
+                            });
+                    });
+                });
+            });
+        });
+    });
+
+    after(async function() {
+        await GlobalConfig.removeTestConfig();
+        await ProjectService.hardDeleteBy({ _id: projectId });
+        await UserService.hardDeleteBy({
+            email: userData.user.email,
+        });
+        await ComponentService.hardDeleteBy({ projectId });
+        await DockerCredentialService.hardDeleteBy({ projectId });
+        await ContainerSecurityService.hardDelete({ componentId });
+    });
+
+    it('should create a container security', function(done) {
+        const authorization = `Basic ${token}`;
+
+        DockerCredentialService.create({
+            dockerUsername: dockerCredential.dockerUsername,
+            dockerPassword: dockerCredential.dockerPassword,
+            dockerRegistryUrl: dockerCredential.dockerRegistryUrl,
+            projectId,
+        }).then(function(credential) {
+            credentialId = credential._id;
+            const data = {
+                name: 'Test Container',
+                dockerCredential: credential._id,
+                imagePath: dockerCredential.imagePath,
+                imageTags: dockerCredential.imageTags,
+            };
+
+            request
+                .post(`/security/${projectId}/${componentId}/container`)
+                .set('Authorization', authorization)
+                .send(data)
+                .end(function(err, res) {
+                    containerSecurityId = res.body._id;
+                    expect(res).to.have.status(200);
+                    expect(res.body.componentId).to.be.equal(componentId);
+                    expect(res.body.name).to.be.equal(data.name);
+                    expect(res.body.imagePath).to.be.equal(data.imagePath);
+                    expect(res.body.imageTags).to.be.equal(data.imageTags);
+                    done();
+                });
+        });
+    });
+});
