cfd43d9d611be24e5810d7d18d96b179639e4574
Ojini Chizoba Jude <judoc96@gmail.com>
2020-06-22 18:41:21 +0100

    utility function that returns the max vulnerability level

---
+++ dashboard/src/utils/threatLevel.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} vulnerability : An object containing the issue level
+ * @description Analyse vulnerability object and returns the maximum threat level
+ * @returns {string} critical, high, moderate or low
+ */
+
+const threatLevel = vulnerability => {
+    if (vulnerability.critical > 0) {
+        return 'critical';
+    }
+    if (vulnerability.high > 0) {
+        return 'high';
+    }
+    if (vulnerability.moderate > 0) {
+        return 'moderate';
+    }
+    if (vulnerability.low > 0) {
+        return 'low';
+    }
+};
+
+export default threatLevel;
