34f1036e9a5bf4a06b5ff966313ed9757a3a86c2
Ojini Chizoba Jude <judoc96@gmail.com>
2020-06-22 09:39:25 +0100

    setup the env needed for the test

---
+++ backend/test/data/dockerCredential.js
@@ -0,0 +1,7 @@
+module.exports = {
+    dockerUsername: process.env.DOCKER_USERNAME,
+    dockerPassword: process.env.DOCKER_PASSWORD,
+    dockerRegistryUrl: process.env.DOCKER_REGISTRY_URL,
+    imagePath: process.env.IMAGE_PATH,
+    imageTags: process.env.IMAGE_TAGS,
+}
