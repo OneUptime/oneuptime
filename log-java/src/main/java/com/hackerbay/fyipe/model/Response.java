3b8729addccf66b3b1f3a99f0b9707647ff0b5ac
Zadat Olayinka <bundayy@outlook.com>
2020-06-26 21:05:13 +0100

    logger class + request string builder + response builder

--- log-java/src/main/java/com/hackerbay/fyipe/model/Response.java
+++ log-java/src/main/java/com/hackerbay/fyipe/model/Response.java
@@ -1,4 +1,40 @@
 package main.java.com.hackerbay.fyipe.model;
 
 public class Response {
+    private String _id;
+    private String type;
+    private String createdAt;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Response{" +
+                "_id='" + _id + '\'' +
+                ", type='" + type + '\'' +
+                ", createdAt='" + createdAt + '\'' +
+                '}';
+    }
 }
