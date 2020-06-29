565dead5a43a69245fbfb36542c4c2ed453e3210
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-06-29 18:51:53 +0100

    Update incident sub project test

--- dashboard/src/test/puppeteer/IncidentSubProject.test.js
+++ dashboard/src/test/puppeteer/IncidentSubProject.test.js
@@ -66,6 +66,7 @@ describe('Incident API With SubProjects', () => {
                 await page.click('#create-project');
                 await page.waitForSelector('#name');
                 await page.type('#name', projectName);
+                await page.waitFor(2000);
                 await page.$$eval(
                     'input[name="planId"]',
                     inputs => inputs[2].click() // select Growth plan
