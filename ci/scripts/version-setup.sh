7e4e64a8c730aea3e59c683492cd50f52334ca81
Zadat Olayinka <bundayy@outlook.com>
2020-07-02 15:57:39 +0100

    all deployment restored

--- ci/scripts/version-setup.sh
+++ ci/scripts/version-setup.sh
@@ -3,7 +3,6 @@ echo "
 This script changes version of every project
 "
 function version {
-  ls 
   cd $1
   npm version "3.0.$CI_PIPELINE_IID"
   cd ..
@@ -12,18 +11,18 @@ function version {
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-# version dashboard
-# version accounts 
-# version backend
-# version home 
-# version status-page 
-# version api-docs
-# version server-monitor
-# version probe
-# version admin-dashboard
-# version slack
-# version init-script
-# version licensing
-# version helm-chart
+version dashboard
+version accounts 
+version backend
+version home 
+version status-page 
+version api-docs
+version server-monitor
+version probe
+version admin-dashboard
+version slack
+version init-script
+version licensing
+version helm-chart
 version log-js
-# version .
+version .
