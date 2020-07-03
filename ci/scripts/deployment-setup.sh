e60339f123176ceb3a7da4fca111f2a5a98e9582
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-07-03 14:50:33 +0200

    Remove logs.

--- ci/scripts/deployment-setup.sh
+++ ci/scripts/deployment-setup.sh
@@ -5,6 +5,8 @@ cd ..
 cd ..
 echo "Remove Google Cloud SDK"
 sudo rm -rf /home/gitlab-runner/google-cloud-sdk
+echo "Remove Google Cloud Logs"
+sudo rm -rf /home/gitlab-runner/.config/gcloud
 curl -sSL https://sdk.cloud.google.com | bash > /dev/null;
 source $HOME/google-cloud-sdk/path.bash.inc
 $HOME/google-cloud-sdk/bin/gcloud components update kubectl
