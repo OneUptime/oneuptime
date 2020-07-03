f565580e11e22bb0058deb6f0e72ef7c9cd30679
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-07-03 14:21:07 +0200

    Remove SDK before installing a new one.

--- ci/scripts/deployment-setup.sh
+++ ci/scripts/deployment-setup.sh
@@ -3,6 +3,7 @@ openssl enc -in encrypted-credentials.enc -out encrypted-credentials.tar -d -aes
 tar -xvf encrypted-credentials.tar
 cd ..
 cd ..
+sudo rm -rf /home/gitlab-runner/google-cloud-sdk
 curl -sSL https://sdk.cloud.google.com | bash > /dev/null;
 source $HOME/google-cloud-sdk/path.bash.inc
 $HOME/google-cloud-sdk/bin/gcloud components update kubectl
