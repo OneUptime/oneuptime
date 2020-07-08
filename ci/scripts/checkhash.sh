14d0023ded9d92b03f928fed604793faa44d8159
deityhub <judoc96@gmail.com>
2020-07-08 20:19:57 +0100

    debugging

--- ci/scripts/checkhash.sh
+++ ci/scripts/checkhash.sh
@@ -4,7 +4,8 @@ sudo apt-get install -y jq
 
 function checkHash {
     # $1 is the project
-    HASH_VALUE=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
+    PROJECT_DIR=`./$1`
+    HASH_VALUE=`find $PROJECT_DIR -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
     RESPONSE=`curl -H "Content-Type: application/json" -d "{\"structuredQuery\": {\"from\": {\"collectionId\": \"builds\"},\"where\": {\"compositeFilter\": {\"op\": \"AND\",\"filters\": [{\"fieldFilter\": {\"field\": {\"fieldPath\": \"project\"},\"op\": \"EQUAL\",\"value\": {\"stringValue\": '$1'}}},{\"fieldFilter\": {\"field\": {\"fieldPath\": \"hash\"},\"op\": \"EQUAL\",\"value\": {\"stringValue\": '$HASH_VALUE'}}}]}}}}" -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents:runQuery"`
     
     # if response contains an array with document key, then the hash already exist in db
