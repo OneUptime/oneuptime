e63be1b6063e4edba6d5ab6902fdad20b7ef6db2
deityhub <judoc96@gmail.com>
2020-07-08 19:17:16 +0100

    store hash on build pass

--- ci/scripts/storehash.sh
+++ ci/scripts/storehash.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-# check the status of build process
-# if successful, then store hash in firestore
-# if not skip
-function storeHash {
-    # store hash in firestore
-    # $1 is project name $2 is hash value
-    curl -H "Content-Type: application/json" -d "{\"fields\": {\"project\": {\"stringValue\": '$1'},\"hash\": {\"stringValue\": '$2'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
-}
+# store hash in firestore
+# $1 is project name $2 is hash value
+
+HASH_VALUE=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
+
+curl -H "Content-Type: application/json" -d "{\"fields\": {\"project\": {\"stringValue\": '$1'},\"hash\": {\"stringValue\": '$HASH_VALUE'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
