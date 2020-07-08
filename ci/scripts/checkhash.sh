5384099d1f508f74d94375d61fd0d2c5f16e2432
Ojini Chizoba Jude <deityhub@deityhub.local>
2020-07-08 10:38:20 +0100

    debugging

--- ci/scripts/checkhash.sh
+++ ci/scripts/checkhash.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-echo "
-This script does the following.
-- Checks if hash of project exists in firestore db
-- If hash does not exist, add hash to firestore db
-- returns true or false based on whether the hash exists on firestore db
-"
+# echo "
+# This script does the following.
+# - Checks if hash of project exists in firestore db
+# - If hash does not exist, add hash to firestore db
+# - returns true or false based on whether the hash exists on firestore db
+# "
+
+echo "value of '$1': $1"
+echo "value of '$2': $2"
 
 sudo apt-get install -y jq
 
@@ -13,13 +16,13 @@ function storeHash {
     # $1 is project name $2 is hash value
     echo "Store hash on db"
     
-    curl -d '{"fields": {"project": {"stringValue": "$1"},"hash": {"stringValue": "$2"}}}' -H "Content-Type: application/json" -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
+    curl -d '{"fields": {"project": {"stringValue": $1},"hash": {"stringValue": $2}}}' -H "Content-Type: application/json" -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
 }
 
 # $1 is the project
 # $2 is the hash
 
-RESPONSE=`curl -d '{"structuredQuery": {"from": {"collectionId": "builds"},"where": {"compositeFilter": {"op": "AND","filters": [{"fieldFilter": {"field": {"fieldPath": "project"},"op": "EQUAL","value": {"stringValue": "$1"}}},{"fieldFilter": {"field": {"fieldPath": "hash"},"op": "EQUAL","value": {"stringValue": "$2"}}}]}}}}' -H "Content-Type: application/json" -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents:runQuery"`
+RESPONSE=`curl -d '{"structuredQuery": {"from": {"collectionId": "builds"},"where": {"compositeFilter": {"op": "AND","filters": [{"fieldFilter": {"field": {"fieldPath": "project"},"op": "EQUAL","value": {"stringValue": $1}}},{"fieldFilter": {"field": {"fieldPath": "hash"},"op": "EQUAL","value": {"stringValue": $2}}}]}}}}' -H "Content-Type: application/json" -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents:runQuery"`
 # if response contains an array with document key, then the hash already exist in db
 # if response does not contain an array with document key, then the hash does not exist in db, create the document
 document=`jq '.[0].document' <<< "$RESPONSE"`
