b32f5af0b9b3abe3f4a5df3a34dddb4aa71d8ae4
deityhub <judoc96@gmail.com>
2020-07-08 23:25:15 +0100

    refactor to check hash and store hash

--- ci/scripts/storehash.sh
+++ ci/scripts/storehash.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # store hash in firestore
-# $1 is project name $2 is hash value
+# $1 is project name
 HASH_VALUE=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
 
 curl -H "Content-Type: application/json" -d "{\"fields\": {\"project\": {\"stringValue\": '$1'},\"hash\": {\"stringValue\": '$HASH_VALUE'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
