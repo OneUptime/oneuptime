61172195845a547ede3b24522478325ffee40d18
Mohamed ABDELLANI <abdellani@gmail.com>
2020-07-08 22:23:39 +0100

    [skip ci] Add new test : 'should create msteams webhook.'

--- backend/test/webhook.test.js
+++ backend/test/webhook.test.js
@@ -153,4 +153,23 @@ describe('Webhook API', function () {
                 done();
             });
     });
+
+    it('should create msteams webhook.', function (done) {
+        const authorization = `Basic ${token}`;
+        request
+            .post(`/webhook/${projectId}/create`)
+            .set('Authorization', authorization)
+            .send(msTeamsPayload)
+            .end(function (err, res) {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('object');
+                expect(res.body).to.have.property('data');
+                expect(res.body).to.have.property('projectId');
+                expect(res.body).to.have.property('monitorId');
+                expect(res.body).to.have.property('notificationOptions');
+                done();
+            });
+    });
+
+
 });
