1c53ec93b1eda9a7e0fffedd138cb45e1510fc02
Ojini Chizoba Jude <deityhub@deityhub.local>
2020-07-08 12:23:00 +0100

    debugging

--- ci/scripts/hash.sh
+++ ci/scripts/hash.sh
@@ -6,5 +6,5 @@ echo "The project being hashed is $1"
 
 # shasum --> sha256sum or sha1sum
 # use head to only get the first 64 characters, removing the trailing dash
-HASH=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
-echo $HASH
+export HASH=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
+# echo $HASH
