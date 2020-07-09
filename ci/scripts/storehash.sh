6a5a1ab221505f31c7a817011b7f90c0c85aad7e
deityhub <judoc96@gmail.com>
2020-07-09 01:59:18 +0100

    debugging

--- ci/scripts/storehash.sh
+++ ci/scripts/storehash.sh
@@ -4,7 +4,8 @@ chmod +x ./ci/scripts/checkhash.sh
 
 # make sure hash don't already exist in db
 export hash_exist=`./ci/scripts/checkhash.sh $1`
-echo $hash_exist
+echo "Does hash exist: '$hash_exist'"
+echo "Value of parameter: '$1"
 if [[ $hash_exist == *"false"* ]]
 then
     HASH_VALUE=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
