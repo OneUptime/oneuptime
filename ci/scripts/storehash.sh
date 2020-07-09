77d7ca7c955a15ac54adb64477315d77beaf19a2
deityhub <judoc96@gmail.com>
2020-07-09 02:08:58 +0100

    refactor storehash script

--- ci/scripts/storehash.sh
+++ ci/scripts/storehash.sh
@@ -4,8 +4,6 @@ chmod +x ./ci/scripts/checkhash.sh
 
 # make sure hash don't already exist in db
 export hash_exist=`./ci/scripts/checkhash.sh $1`
-echo "Does hash exist: '$hash_exist'"
-echo "Value of parameter: '$1"
 if [[ $hash_exist == *"false"* ]]
 then
     HASH_VALUE=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
