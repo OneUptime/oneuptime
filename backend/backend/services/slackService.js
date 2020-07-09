68b332c2170ed8331cff19f65e2879c8401f5623
Mohamed ABDELLANI <abdellani@gmail.com>
2020-07-09 23:14:38 +0100

    [skip ci] Update the slackService to run as the msTeamsService

--- backend/backend/services/slackService.js
+++ backend/backend/services/slackService.js
@@ -2,77 +2,131 @@ module.exports = {
     // process messages to be sent to slack workspace channels
     sendNotification: async function(
         projectId,
-        incidentId,
-        userId,
-        text,
-        incident
+        incident,
+        monitor,
+        incidentStatus,
+        component
     ) {
         try {
             const self = this;
+            let response;
             const project = await ProjectService.findOneBy({ _id: projectId });
-            const integrations = await IntegrationModel.find({
-                projectId,
+            if (project && project.parentProjectId) {
+                projectId = project.parentProjectId._id;
+            }
+            let query = {
+                projectId: projectId,
                 integrationType: 'slack',
+                monitorId: monitor._id,
+            };
+            if (incidentStatus === 'resolved') {
+                query = {
+                    ...query,
+                    'notificationOptions.incidentResolved': true,
+                };
+            } else if (incidentStatus === 'created') {
+                query = {
+                    ...query,
+                    'notificationOptions.incidentCreated': true,
+                };
+            } else if (incidentStatus === 'acknowledged') {
+                query = {
+                    ...query,
+                    'notificationOptions.incidentAcknowledged': true,
+                };
+            } else {
+                return;
+            }
+            const integrations = await IntegrationService.findBy(query);
+            const monitorStatus = await MonitorStatusService.findOneBy({
+                monitorId: monitor._id,
             });
 
-            if (integrations.length === 0)
-                return 'no connected slack workspace to notify';
             for (const integration of integrations) {
-                // call the notify function that just sends the slack notification
-                const response = await self.notify(
-                    integration,
+                response = await self.notify(
                     project,
-                    text,
+                    monitor,
                     incident,
-                    incidentId
+                    integration,
+                    monitorStatus ? monitorStatus.status : null,
+                    component
                 );
-                return response;
             }
+            return response;
         } catch (error) {
-            ErrorService.log('slackService.sendNotification', error);
+            ErrorService.log('msTeamsService.sendNotification', error);
             throw error;
         }
     },
 
     // send notification to slack workspace channels
-    async notify(team, project, text, incident, incidentId) {
+    async notify(
+        project,
+        monitor,
+        incident,
+        integration,
+        monitorStatus,
+        component
+    ) {
         try {
-            const token = team.data.accessToken;
-            const web = new WebClient(token);
-            const channelId = team.data.channelId; //project.channelId;
-            const color = incident ? 'danger' : 'good';
+            const uri = `${global.dashboardHost}/project/${component.projectId._id}/${component._id}/incidents/${incident._id}`;
 
-            const slackText = `${project.name}: ${text}`;
-            if (incident) {
-                const incAttachment = {
-                    text: slackText,
-                    fallback: slackText,
-                    callback_id: incidentId,
-                    color: color,
-                    attachment_type: 'default',
-                };
-                const response = await web.chat.postMessage({
-                    channel: channelId,
-                    text: slackText,
-                    attachments: [incAttachment],
-                });
+            const payloadText = incident.resolved
+                ? `Incident on *${component.name} / ${
+                      monitor.name
+                  }* is resolved by ${
+                      incident.resolvedBy ? incident.resolvedBy.name : 'Fyipe'
+                  } at ${incident.resolvedAt}\n <${uri}|More details>`
+                : incident.acknowledged
+                ? `Incident on *${component.name} / ${
+                      monitor.name
+                  }* is acknowledge by ${
+                      incident.acknowledgedBy
+                          ? incident.acknowledgedBy.name
+                          : 'Fyipe'
+                  } at ${incident.acknowledgedAt} \n <${uri}|More details>`
+                : `
+*New incident:*
 
-                return response;
-            } else {
-                const res = await web.chat.postMessage({
-                    channel: channelId,
-                    text: slackText,
-                });
-                return `Message sent: ${res.ts}`;
-            }
+*Project name:* ${project.name}
+
+*Monitor name:* ${component.name} / ${monitor.name}
+
+*Created at:* ${incident.createdAt}
+
+*Created by:* ${incident.createdById ? incident.createdById.name : 'Fyipe'}
+
+*Incident status:* ${incident.incidentType}
+
+*Monitor status:* ${monitorStatus}
+
+<${uri}|Acknowledge>
+`;
+            const payload = {
+                'text': payloadText
+            };
+            await axios.post(
+                integration.data.endpoint,
+                {
+                    ...payload,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            return 'Webhook successfully pinged';
         } catch (error) {
-            ErrorService.log('slackService.notify', error);
+            ErrorService.log('WebHookService.notify', error);
             throw error;
         }
     },
 };
 
-const { WebClient } = require('@slack/client');
-const IntegrationModel = require('../models/integration');
+const IntegrationService = require('./integrationService');
+const axios = require('axios');
 const ProjectService = require('./projectService');
+const MonitorStatusService = require('./monitorStatusService');
 const ErrorService = require('./errorService');
