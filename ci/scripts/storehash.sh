4e72397a75a7450ee35460c5d77770160d10f9b0
deityhub <judoc96@gmail.com>
2020-07-09 01:27:48 +0100

    refactor storehash script

--- ci/scripts/storehash.sh
+++ ci/scripts/storehash.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# store hash in firestore
-# $1 is project name
-HASH_VALUE=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
+chmod +x ./ci/scripts/checkhash.sh
 
-curl -H "Content-Type: application/json" -d "{\"fields\": {\"project\": {\"stringValue\": '$1'},\"hash\": {\"stringValue\": '$HASH_VALUE'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
+# make sure hash don't already exist in db
+export hash_exist=`./ci/scripts/checkhash.sh $1`
+if [[ $hash_exist == *"false"* ]]
+then
+    HASH_VALUE=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
+    curl -H "Content-Type: application/json" -d "{\"fields\": {\"project\": {\"stringValue\": '$1'},\"hash\": {\"stringValue\": '$HASH_VALUE'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
+fi
