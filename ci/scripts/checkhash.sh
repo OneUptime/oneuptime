7aaff540d47f8d33ee93064662006d0c4c44a1e1
deityhub <judoc96@gmail.com>
2020-07-09 01:28:54 +0100

    update comment text

--- ci/scripts/checkhash.sh
+++ ci/scripts/checkhash.sh
@@ -7,8 +7,7 @@ function checkHash {
     HASH_VALUE=`find $1 -type f ! -path "*node_modules*" -print0 | sort -z | xargs -0 sha256sum | sha256sum | head -c 64`
     RESPONSE=`curl -H "Content-Type: application/json" -d "{\"structuredQuery\": {\"from\": {\"collectionId\": \"builds\"},\"where\": {\"compositeFilter\": {\"op\": \"AND\",\"filters\": [{\"fieldFilter\": {\"field\": {\"fieldPath\": \"project\"},\"op\": \"EQUAL\",\"value\": {\"stringValue\": '$1'}}},{\"fieldFilter\": {\"field\": {\"fieldPath\": \"hash\"},\"op\": \"EQUAL\",\"value\": {\"stringValue\": '$HASH_VALUE'}}}]}}}}" -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents:runQuery"`
     
-    # if response contains an array with document key, then the hash already exist in db
-    # if response does not contain an array with document key, then the hash does not exist in db, create the document
+    # if response contains an array of object with document key, then the hash already exist in db
     document=`jq '.[0].document' <<< "$RESPONSE"`
     if [[ $document = null ]]
     then
