7f7144f9345ee87720c3e317db9a30a36dc21830
deityhub <judoc96@gmail.com>
2020-07-14 16:58:16 +0100

    debugging document returned

--- ci/scripts/checkhash.sh
+++ ci/scripts/checkhash.sh
@@ -10,6 +10,9 @@ function checkHash {
     
     # if response contains an array of object with document key, then the hash already exist in db
     document=`jq '.[0].document' <<< "$RESPONSE"`
+    echo "document we have is: $document"
+    echo "response we have is: $RESPONSE"
+    
     if [[ $document != null ]]
     then
         exit ${CI_JOB_SKIP_EXIT_CODE:-0}
