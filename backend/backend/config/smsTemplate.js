ab5a9bc25832db31616d5cacb471a84d0525c9a2
Zadat Olayinka <bundayy@outlook.com>
2020-07-15 22:33:26 +0100

    componentName added to sms on frontend

--- backend/backend/config/smsTemplate.js
+++ backend/backend/config/smsTemplate.js
@@ -5,10 +5,11 @@ module.exports = [
             '{{monitorName}} : Name of the monitor on which incident has occured.',
             '{{projectName}} : Name of the project on which the incident has occured.',
             '{{incidentType}} : Type of incident.',
+            '{{componentName}} : Name of the component the monitor belongs to',
         ],
         smsType: 'Subscriber Incident Created',
         body:
-            '{{projectName}}/{{monitorName}} is {{incidentType}} at {{incidentTime}}. You are receiving this message because you subscribed to this monitor.',
+            '{{projectName}} - {{componentName}}/{{monitorName}} is {{incidentType}} at {{incidentTime}}. You are receiving this message because you subscribed to this monitor.',
     },
     {
         allowedVariables: [
@@ -16,10 +17,11 @@ module.exports = [
             '{{monitorName}} : Name of the monitor on which incident has occured.',
             '{{projectName}} : Name of the project on which the incident has occured.',
             '{{incidentType}} : Type of incident.',
+            '{{componentName}} : Name of the component the monitor belongs to',
         ],
         smsType: 'Subscriber Incident Acknowldeged',
         body:
-            '{{projectName}}/{{monitorName}} is {{incidentType}} at {{incidentTime}}. You are receiving this message because you subscribed to this monitor.',
+            '{{projectName}} - {{componentName}}/{{monitorName}} is {{incidentType}} at {{incidentTime}}. You are receiving this message because you subscribed to this monitor.',
     },
     {
         allowedVariables: [
@@ -27,9 +29,10 @@ module.exports = [
             '{{monitorName}} : Name of the monitor on which incident has occured.',
             '{{projectName}} : Name of the project on which the incident has occured.',
             '{{incidentType}} : Type of incident.',
+            '{{componentName}} : Name of the component the monitor belongs to',
         ],
         smsType: 'Subscriber Incident Resolved',
         body:
-            '{{projectName}}/{{monitorName}} is {{incidentType}} at {{incidentTime}}. You are receiving this message because you subscribed to this monitor.',
+            '{{projectName}} - {{componentName}}/{{monitorName}} is {{incidentType}} at {{incidentTime}}. You are receiving this message because you subscribed to this monitor.',
     },
 ];
--- backend/backend/config/smsTemplateVariables.js
+++ backend/backend/config/smsTemplateVariables.js
@@ -4,17 +4,20 @@ module.exports = {
         '{{monitorName}} : Name of the monitor on which incident has occured.',
         '{{projectName}} : Name of the project on which the incident has occured.',
         '{{incidentType}} : Type of incident.',
+        '{{componentName}} : Name of the component the monitor belongs to',
     ],
     'Subscriber Incident Acknowldeged': [
         '{{incidentTime}} : Time at which this incident occured.',
         '{{monitorName}} : Name of the monitor on which incident has occured.',
         '{{projectName}} : Name of the project on which the incident has occured.',
         '{{incidentType}} : Type of incident.',
+        '{{componentName}} : Name of the component the monitor belongs to',
     ],
     'Subscriber Incident Resolved': [
         '{{incidentTime}} : Time at which this incident occured.',
         '{{monitorName}} : Name of the monitor on which incident has occured.',
         '{{projectName}} : Name of the project on which the incident has occured.',
         '{{incidentType}} : Type of incident.',
+        '{{componentName}} : Name of the component the monitor belongs to',
     ],
 };
