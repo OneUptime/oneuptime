8f142aae7a178abf5470c5af5622caf5b5429790
deityhub <judoc96@gmail.com>
2020-07-15 16:25:08 +0100

    remove firestore auth

--- ci/scripts/storehash.sh
+++ ci/scripts/storehash.sh
@@ -6,10 +6,7 @@ chmod +x ./ci/scripts/hashexist.sh
 function storeHash {
     # $1 -> Job Name; $2 -> Project
     HASH_VALUE=`./ci/scripts/gethash.sh $1 $2`
-    # signin with valid api key, email and password
-    RESPONSE=`curl -X POST "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$FIRESTORE_API_KEY" -H 'Content-Type: application/json' --data-binary "{\"email\": $FIRESTORE_EMAIL,\"password\": $FIRESTORE_PASSWORD,\"returnSecureToken\":true}"`
-    TOKEN=`jq '.idToken' <<< "$RESPONSE"`
-    curl -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" -d "{\"fields\": {\"project\": {\"stringValue\": '$2'},\"hash\": {\"stringValue\": '$HASH_VALUE'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
+    curl -H "Content-Type: application/json" -d "{\"fields\": {\"project\": {\"stringValue\": '$2'},\"hash\": {\"stringValue\": '$HASH_VALUE'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
 }
 
 if [[ $CI_COMMIT_BRANCH != "master" ]] && [[ $CI_COMMIT_BRANCH != "release" ]]
