79d403c2a8591b42007314a9a27cb2ddb6d4ee5b
deityhub <judoc96@gmail.com>
2020-07-17 22:15:34 +0100

    debugging

--- ci/scripts/hashexist.sh
+++ ci/scripts/hashexist.sh
@@ -12,11 +12,13 @@ fi
 function hashExist {
     # $1 is the job name
     # $2 is the project
-    HASH_VALUE=`./ci/scripts/gethash.sh $1 $2`
+    HASH_VALUE=`find $2 -type f -print0 | sort -z | xargs -0 sha256sum | sha256sum | read projecthash; echo $projecthash$1 | sha256sum | head -c 64`
     RESPONSE=`curl -H "Content-Type: application/json" -d "{\"structuredQuery\": {\"from\": {\"collectionId\": \"builds\"},\"where\": {\"compositeFilter\": {\"op\": \"AND\",\"filters\": [{\"fieldFilter\": {\"field\": {\"fieldPath\": \"project\"},\"op\": \"EQUAL\",\"value\": {\"stringValue\": '$2'}}},{\"fieldFilter\": {\"field\": {\"fieldPath\": \"hash\"},\"op\": \"EQUAL\",\"value\": {\"stringValue\": '$HASH_VALUE'}}}]}}}}" -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents:runQuery"`
     
     echo "hash value is; $HASH_VALUE"
     echo "response is; $RESPONSE"
+    echo "first variable is $1"
+    echo "second variable is $2"
     # if response contains an array of object with document key, then the hash already exist in db
     document=`jq '.[0].document' <<< "$RESPONSE"`
     if [[ $document == null ]]
