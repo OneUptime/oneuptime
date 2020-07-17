7d5f90f44a383f2253d20f5b008fd21b6c2f6730
deityhub <judoc96@gmail.com>
2020-07-17 22:07:50 +0100

    refactor hashing

--- ci/scripts/gethash.sh
+++ ci/scripts/gethash.sh
@@ -2,4 +2,4 @@
 
 # $1 is the job name
 # $2 is the project
-find $2 -type f -print0 | sort -z | xargs -0 sha256sum | sha256sum | read projecthash; echo "$projecthash $1" | sha256sum | head -c 64
+find $2 -type f -print0 | sort -z | xargs -0 sha256sum | sha256sum | read projecthash; echo $projecthash$1 | sha256sum | head -c 64
--- ci/scripts/storehash.sh
+++ ci/scripts/storehash.sh
@@ -6,6 +6,7 @@ chmod +x ./ci/scripts/hashexist.sh
 function storeHash {
     # $1 -> Job Name; $2 -> Project
     HASH_VALUE=`./ci/scripts/gethash.sh $1 $2`
+    echo "hash value before store is $HASH_VALUE"
     curl -H "Content-Type: application/json" -d "{\"fields\": {\"project\": {\"stringValue\": '$2'},\"hash\": {\"stringValue\": '$HASH_VALUE'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
 }
 
@@ -15,6 +16,7 @@ then
     for ((i = 2; i <= $#; i++ ))
     do
         hash_exist=`./ci/scripts/hashexist.sh $1 ${!i}`
+        echo "before store hash_exist $hash_exist"
         if [[ $hash_exist == *"false"* ]]
         then
             storeHash $1 ${!i}
