07404d530612fb2f97a6357cf19d2d3b07960d21
deityhub <judoc96@gmail.com>
2020-07-17 23:16:20 +0100

    fix bug with hashcheck always creating the same hash

--- ci/scripts/hashexist.sh
+++ ci/scripts/hashexist.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-chmod +x ./ci/scripts/gethash.sh
-
-
 # install jq only when it does not exist
 if [[ ! $(which jq) ]]
 then
@@ -16,11 +13,6 @@ function hashExist {
     HASH_VALUE=`echo $1$PROJECT_HASH | sha256sum | head -c 64`
     RESPONSE=`curl -H "Content-Type: application/json" -d "{\"structuredQuery\": {\"from\": {\"collectionId\": \"builds\"},\"where\": {\"compositeFilter\": {\"op\": \"AND\",\"filters\": [{\"fieldFilter\": {\"field\": {\"fieldPath\": \"project\"},\"op\": \"EQUAL\",\"value\": {\"stringValue\": '$2'}}},{\"fieldFilter\": {\"field\": {\"fieldPath\": \"hash\"},\"op\": \"EQUAL\",\"value\": {\"stringValue\": '$HASH_VALUE'}}}]}}}}" -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents:runQuery"`
     
-    echo "initial project hash is $PROJECT_HASH"
-    echo "hash value is; $HASH_VALUE"
-    echo "response is; $RESPONSE"
-    echo "first variable is $1"
-    echo "second variable is $2"
     # if response contains an array of object with document key, then the hash already exist in db
     document=`jq '.[0].document' <<< "$RESPONSE"`
     if [[ $document == null ]]
--- ci/scripts/storehash.sh
+++ ci/scripts/storehash.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-chmod +x ./ci/scripts/gethash.sh
 chmod +x ./ci/scripts/hashexist.sh
 
 function storeHash {
     # $1 -> Job Name; $2 -> Project
-    HASH_VALUE=`./ci/scripts/gethash.sh $1 $2`
-    echo "hash value before store is $HASH_VALUE"
+    PROJECT_HASH=`find ./$2 -type f -print0 | sort -z | xargs -0 sha256sum | sha256sum`
+    HASH_VALUE=`echo $1$PROJECT_HASH | sha256sum | head -c 64`
     curl -H "Content-Type: application/json" -d "{\"fields\": {\"project\": {\"stringValue\": '$2'},\"hash\": {\"stringValue\": '$HASH_VALUE'}}}"  -X POST "https://firestore.googleapis.com/v1/projects/fyipe-devops/databases/(default)/documents/builds"
 }
 
@@ -16,7 +15,6 @@ then
     for ((i = 2; i <= $#; i++ ))
     do
         hash_exist=`./ci/scripts/hashexist.sh $1 ${!i}`
-        echo "before store hash_exist $hash_exist"
         if [[ $hash_exist == *"false"* ]]
         then
             storeHash $1 ${!i}
