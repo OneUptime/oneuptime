57d29030d0bded9cbf9fb6442099e83e9309747e
deityhub <judoc96@gmail.com>
2020-07-28 20:16:02 +0100

    backend test for restoring a project

--- backend/test/enterpriseProject.test.js
+++ backend/test/enterpriseProject.test.js
@@ -74,4 +74,15 @@ describe('Enterprise Project API', function() {
                 done();
             });
     });
+
+    it('should restore a deleted project', done => {
+        const authorization = `Basic ${token}`;
+        request
+            .put(`/project/${projectId}/restoreProject`)
+            .set('Authorization', authorization)
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
 });
