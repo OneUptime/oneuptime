cd5305fde6658c837f8be7fd3fd38a642036149f
Zadat Olayinka <bundayy@outlook.com>
2020-07-28 15:45:19 +0100

    incident timelinee test merged

--- dashboard/src/test/puppeteer/IncidentTimeline.test.js
+++ dashboard/src/test/puppeteer/IncidentTimeline.test.js
@@ -11,7 +11,7 @@ const projectName = utils.generateRandomString();
 const projectMonitorName = utils.generateRandomString();
 const componentName = utils.generateRandomString();
 
-// const bodyText = utils.generateRandomString();
+const bodyText = utils.generateRandomString();
 const message = utils.generateRandomString();
 
 describe('Incident Timeline API', () => {
@@ -71,21 +71,41 @@ describe('Incident Timeline API', () => {
     });
 
     test(
-        'should create incident in project and add to message to the incident message thread',
+        'should create incident in project with multi-probes and add to incident timeline',
         async () => {
+            expect.assertions(2);
+            const testServer = async ({ page }) => {
+                await page.goto(utils.HTTP_TEST_SERVER_URL + '/settings');
+                await page.evaluate(
+                    () => (document.getElementById('responseTime').value = '')
+                );
+                await page.evaluate(
+                    () => (document.getElementById('statusCode').value = '')
+                );
+                await page.evaluate(
+                    () => (document.getElementById('body').value = '')
+                );
+                await page.waitForSelector('#responseTime');
+                await page.click('input[name=responseTime]');
+                await page.type('input[name=responseTime]', '0');
+                await page.waitForSelector('#statusCode');
+                await page.click('input[name=statusCode]');
+                await page.type('input[name=statusCode]', '400');
+                await page.select('#responseType', 'html');
+                await page.waitForSelector('#body');
+                await page.click('textarea[name=body]');
+                await page.type(
+                    'textarea[name=body]',
+                    `<h1 id="html"><span>${bodyText}</span></h1>`
+                );
+                await page.click('button[type=submit]');
+                await page.waitForSelector('#save-btn');
+            };
+
             const dashboard = async ({ page }) => {
-                await page.waitFor(5000);
-                const type = 'investigation';
+                await page.waitFor(350000);
                 // Navigate to Component details
                 await init.navigateToComponentDetails(componentName, page);
-                // create incident
-                await page.waitForSelector(
-                    `#create_incident_${projectMonitorName}`
-                );
-                await page.click(`#create_incident_${projectMonitorName}`);
-                await page.waitForSelector('#createIncident');
-                await init.selectByText('#incidentType', 'Offline', page);
-                await page.click('#createIncident');
 
                 await page.waitForSelector('#incident_span_0');
 
@@ -98,10 +118,97 @@ describe('Incident Timeline API', () => {
                 expect(textContent.toLowerCase()).toEqual(
                     `${projectMonitorName}'s Incident Status`.toLowerCase()
                 );
-                await page.waitFor(2000);
-                await page.click(`#more-details-${projectMonitorName}`);
+
                 await page.waitForSelector(`#incident_${projectMonitorName}_0`);
                 await page.click(`#incident_${projectMonitorName}_0`);
+                await page.waitFor(5000);
+
+                const incidentTimelineRows = await page.$$(
+                    'tr.incidentListItem'
+                );
+                const countIncidentTimelines = incidentTimelineRows.length;
+                expect(countIncidentTimelines).toEqual(2);
+            };
+
+            await cluster.execute(null, testServer);
+            await cluster.execute(null, dashboard);
+        },
+        operationTimeOut
+    );
+
+    test(
+        'should auto-resolve incident in project with multi-probes and add to incident timeline',
+        async () => {
+            expect.assertions(2);
+            const testServer = async ({ page }) => {
+                await page.goto(utils.HTTP_TEST_SERVER_URL + '/settings', {
+                    waitUntil: 'networkidle2',
+                });
+                await page.evaluate(
+                    () => (document.getElementById('responseTime').value = '')
+                );
+                await page.evaluate(
+                    () => (document.getElementById('statusCode').value = '')
+                );
+                await page.evaluate(
+                    () => (document.getElementById('body').value = '')
+                );
+                await page.waitForSelector('#responseTime');
+                await page.click('input[name=responseTime]');
+                await page.type('input[name=responseTime]', '0');
+                await page.waitForSelector('#statusCode');
+                await page.click('input[name=statusCode]');
+                await page.type('input[name=statusCode]', '200');
+                await page.select('#responseType', 'html');
+                await page.waitForSelector('#body');
+                await page.click('textarea[name=body]');
+                await page.type(
+                    'textarea[name=body]',
+                    `<h1 id="html"><span>${bodyText}</span></h1>`
+                );
+                await page.click('button[type=submit]');
+                await page.waitForSelector('#save-btn');
+            };
+
+            const dashboard = async ({ page }) => {
+                await page.waitFor(350000);
+                // Navigate to Component details
+                await init.navigateToComponentDetails(componentName, page);
+
+                await page.waitForSelector('#ResolveText_0');
+
+                const resolveTextSelector = await page.$('#ResolveText_0');
+                expect(resolveTextSelector).not.toBeNull();
+
+                await page.waitForSelector(`#incident_${projectMonitorName}_0`);
+                await page.click(`#incident_${projectMonitorName}_0`);
+                await page.waitFor(5000);
+
+                const incidentTimelineRows = await page.$$(
+                    'tr.incidentListItem'
+                );
+                const countIncidentTimelines = incidentTimelineRows.length;
+                expect(countIncidentTimelines).toEqual(6);
+            };
+
+            await cluster.execute(null, testServer);
+            await cluster.execute(null, dashboard);
+        },
+        operationTimeOut
+    );
+    test(
+        'should open an existing incident in project and add to message to the incident message thread',
+        async () => {
+            const dashboard = async ({ page }) => {
+                await page.waitFor(5000);
+                const type = 'investigation';
+                // Navigate to Component details
+                await init.navigateToComponentDetails(componentName, page);
+
+                await page.waitFor(2000);
+                await page.waitForSelector(`#incident_${projectMonitorName}_0`);
+                await page.click(`#incident_${projectMonitorName}_0`);
+                await page.waitFor(2000);
 
                 // fill investigation message thread form
                 await page.waitFor(2000);
@@ -137,9 +244,9 @@ describe('Incident Timeline API', () => {
                 await init.navigateToComponentDetails(componentName, page);
 
                 await page.waitFor(2000);
-                await page.click(`#more-details-${projectMonitorName}`);
                 await page.waitForSelector(`#incident_${projectMonitorName}_0`);
                 await page.click(`#incident_${projectMonitorName}_0`);
+                await page.waitFor(2000);
 
                 await page.waitFor(2000);
                 await page.waitForSelector(`#edit_${type}_incident_message_0`);
@@ -175,21 +282,10 @@ describe('Incident Timeline API', () => {
                 // Navigate to Component details
                 await init.navigateToComponentDetails(componentName, page);
 
-                await page.waitForSelector('#incident_span_0');
-
-                const incidentTitleSelector = await page.$('#incident_span_0');
-
-                let textContent = await incidentTitleSelector.getProperty(
-                    'innerText'
-                );
-                textContent = await textContent.jsonValue();
-                expect(textContent.toLowerCase()).toEqual(
-                    `${projectMonitorName}'s Incident Status`.toLowerCase()
-                );
                 await page.waitFor(2000);
-                await page.click(`#more-details-${projectMonitorName}`);
                 await page.waitForSelector(`#incident_${projectMonitorName}_0`);
                 await page.click(`#incident_${projectMonitorName}_0`);
+                await page.waitFor(2000);
 
                 // fill internal message thread form
                 await page.waitFor(2000);
@@ -225,8 +321,8 @@ describe('Incident Timeline API', () => {
                 await init.navigateToComponentDetails(componentName, page);
 
                 await page.waitFor(2000);
-                await page.click(`#more-details-${projectMonitorName}`);
                 await page.waitForSelector(`#incident_${projectMonitorName}_0`);
+                await page.click(`#incident_${projectMonitorName}_0`);
                 await page.click(`#incident_${projectMonitorName}_0`);
 
                 await page.waitFor(2000);
@@ -255,172 +351,49 @@ describe('Incident Timeline API', () => {
         operationTimeOut
     );
 
-    // test(
-    //     'should create incident in project with multi-probes and add to incident timeline',
-    //     async () => {
-    //         expect.assertions(2);
-    //         const testServer = async ({ page }) => {
-    //             await page.goto(utils.HTTP_TEST_SERVER_URL + '/settings');
-    //             await page.evaluate(
-    //                 () => (document.getElementById('responseTime').value = '')
-    //             );
-    //             await page.evaluate(
-    //                 () => (document.getElementById('statusCode').value = '')
-    //             );
-    //             await page.evaluate(
-    //                 () => (document.getElementById('body').value = '')
-    //             );
-    //             await page.waitForSelector('#responseTime');
-    //             await page.click('input[name=responseTime]');
-    //             await page.type('input[name=responseTime]', '0');
-    //             await page.waitForSelector('#statusCode');
-    //             await page.click('input[name=statusCode]');
-    //             await page.type('input[name=statusCode]', '400');
-    //             await page.select('#responseType', 'html');
-    //             await page.waitForSelector('#body');
-    //             await page.click('textarea[name=body]');
-    //             await page.type(
-    //                 'textarea[name=body]',
-    //                 `<h1 id="html"><span>${bodyText}</span></h1>`
-    //             );
-    //             await page.click('button[type=submit]');
-    //             await page.waitForSelector('#save-btn');
-    //         };
+    test('should get incident timeline and paginate for incident timeline in project', async () => {
+        expect.assertions(3);
+        const internalNote = utils.generateRandomString();
+        const type = 'internal';
+        return await cluster.execute(null, async ({ page }) => {
+            // Navigate to Component details
+            await init.navigateToComponentDetails(componentName, page);
 
-    //         const dashboard = async ({ page }) => {
-    //             await page.waitFor(350000);
-    //             // Navigate to Component details
-    //             await init.navigateToComponentDetails(componentName, page);
+            await page.waitForSelector(`#incident_${projectMonitorName}_0`);
+            await page.click(`#incident_${projectMonitorName}_0`);
 
-    //             await page.waitForSelector('#incident_span_0');
+            for (let i = 0; i < 10; i++) {
+                // update internal note
+                await page.waitForSelector(
+                    `#form-new-incident-${type}-message`
+                );
+                await page.click(`textarea[id=new-${type}]`);
+                await page.type(`textarea[id=new-${type}]`, `${internalNote}`);
+                await page.click(`#${type}-addButton`);
+                await page.waitFor(1000);
+            }
 
-    //             const incidentTitleSelector = await page.$('#incident_span_0');
+            await page.waitForSelector('tr.incidentListItem');
+            let incidentTimelineRows = await page.$$('tr.incidentListItem');
+            let countIncidentTimelines = incidentTimelineRows.length;
 
-    //             let textContent = await incidentTitleSelector.getProperty(
-    //                 'innerText'
-    //             );
-    //             textContent = await textContent.jsonValue();
-    //             expect(textContent.toLowerCase()).toEqual(
-    //                 `${projectMonitorName}'s Incident Status`.toLowerCase()
-    //             );
+            expect(countIncidentTimelines).toEqual(10);
 
-    //             await page.waitForSelector(`#incident_${projectMonitorName}_0`);
-    //             await page.click(`#incident_${projectMonitorName}_0`);
-    //             await page.waitFor(5000);
+            const nextSelector = await page.$('#btnTimelineNext');
 
-    //             const incidentTimelineRows = await page.$$(
-    //                 'tr.incidentListItem'
-    //             );
-    //             const countIncidentTimelines = incidentTimelineRows.length;
-    //             expect(countIncidentTimelines).toEqual(2);
-    //         };
+            await nextSelector.click();
+            await page.waitFor(7000);
+            incidentTimelineRows = await page.$$('tr.incidentListItem');
+            countIncidentTimelines = incidentTimelineRows.length;
+            expect(countIncidentTimelines).toEqual(10);
 
-    //         await cluster.execute(null, testServer);
-    //         await cluster.execute(null, dashboard);
-    //     },
-    //     operationTimeOut
-    // );
+            const prevSelector = await page.$('#btnTimelinePrev');
 
-    // test(
-    //     'should auto-resolve incident in project with multi-probes and add to incident timeline',
-    //     async () => {
-    //         expect.assertions(2);
-    //         const testServer = async ({ page }) => {
-    //             await page.goto(utils.HTTP_TEST_SERVER_URL + '/settings', {
-    //                 waitUntil: 'networkidle2',
-    //             });
-    //             await page.evaluate(
-    //                 () => (document.getElementById('responseTime').value = '')
-    //             );
-    //             await page.evaluate(
-    //                 () => (document.getElementById('statusCode').value = '')
-    //             );
-    //             await page.evaluate(
-    //                 () => (document.getElementById('body').value = '')
-    //             );
-    //             await page.waitForSelector('#responseTime');
-    //             await page.click('input[name=responseTime]');
-    //             await page.type('input[name=responseTime]', '0');
-    //             await page.waitForSelector('#statusCode');
-    //             await page.click('input[name=statusCode]');
-    //             await page.type('input[name=statusCode]', '200');
-    //             await page.select('#responseType', 'html');
-    //             await page.waitForSelector('#body');
-    //             await page.click('textarea[name=body]');
-    //             await page.type(
-    //                 'textarea[name=body]',
-    //                 `<h1 id="html"><span>${bodyText}</span></h1>`
-    //             );
-    //             await page.click('button[type=submit]');
-    //             await page.waitForSelector('#save-btn');
-    //         };
-
-    //         const dashboard = async ({ page }) => {
-    //             await page.waitFor(350000);
-    //             // Navigate to Component details
-    //             await init.navigateToComponentDetails(componentName, page);
-
-    //             await page.waitForSelector('#ResolveText_0');
-
-    //             const resolveTextSelector = await page.$('#ResolveText_0');
-    //             expect(resolveTextSelector).not.toBeNull();
-
-    //             await page.waitForSelector(`#incident_${projectMonitorName}_0`);
-    //             await page.click(`#incident_${projectMonitorName}_0`);
-    //             await page.waitFor(5000);
-
-    //             const incidentTimelineRows = await page.$$(
-    //                 'tr.incidentListItem'
-    //             );
-    //             const countIncidentTimelines = incidentTimelineRows.length;
-    //             expect(countIncidentTimelines).toEqual(6);
-    //         };
-
-    //         await cluster.execute(null, testServer);
-    //         await cluster.execute(null, dashboard);
-    //     },
-    //     operationTimeOut
-    // );
-
-    // test('should get incident timeline and paginate for incident timeline in project', async () => {
-    //     expect.assertions(3);
-    //     const internalNote = utils.generateRandomString();
-    //     return await cluster.execute(null, async ({ page }) => {
-    //         // Navigate to Component details
-    //         await init.navigateToComponentDetails(componentName, page);
-
-    //         await page.waitForSelector(`#incident_${projectMonitorName}_0`);
-    //         await page.click(`#incident_${projectMonitorName}_0`);
-
-    //         for (let i = 0; i < 10; i++) {
-    //             // update internal note
-    //             await page.waitForSelector('#txtInternalNote');
-    //             await page.type('#txtInternalNote', internalNote);
-    //             await page.click('#btnUpdateInternalNote');
-    //             await page.waitFor(1000);
-    //         }
-
-    //         await page.waitForSelector('tr.incidentListItem');
-    //         let incidentTimelineRows = await page.$$('tr.incidentListItem');
-    //         let countIncidentTimelines = incidentTimelineRows.length;
-
-    //         expect(countIncidentTimelines).toEqual(10);
-
-    //         const nextSelector = await page.$('#btnTimelineNext');
-
-    //         await nextSelector.click();
-    //         await page.waitFor(7000);
-    //         incidentTimelineRows = await page.$$('tr.incidentListItem');
-    //         countIncidentTimelines = incidentTimelineRows.length;
-    //         expect(countIncidentTimelines).toEqual(6);
-
-    //         const prevSelector = await page.$('#btnTimelinePrev');
-
-    //         await prevSelector.click();
-    //         await page.waitFor(7000);
-    //         incidentTimelineRows = await page.$$('tr.incidentListItem');
-    //         countIncidentTimelines = incidentTimelineRows.length;
-    //         expect(countIncidentTimelines).toEqual(10);
-    //     });
-    // }, 300000);
+            await prevSelector.click();
+            await page.waitFor(7000);
+            incidentTimelineRows = await page.$$('tr.incidentListItem');
+            countIncidentTimelines = incidentTimelineRows.length;
+            expect(countIncidentTimelines).toEqual(10);
+        });
+    }, 300000);
 });
