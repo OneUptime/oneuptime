679dfb389ecd40b9446cb1c14b76ed145fe47cdb
Mohamed ABDELLANI <abdellani@gmail.com>
2020-07-29 22:58:08 +0100

    Update the alert message in the slackService

--- backend/backend/services/slackService.js
+++ backend/backend/services/slackService.js
@@ -79,9 +79,9 @@ module.exports = {
                       monitor.name
                   }* is resolved by ${
                       incident.resolvedBy ? incident.resolvedBy.name : 'Fyipe'
-                  } at ${
-                      incident.resolvedAt
-                  } after being down for ${duration}\n <${uri}|More details>`
+                  } at ${incident.resolvedAt} after being ${
+                      incident.incidentType === 'degraded' ? 'degraded' : 'down'
+                  } for ${duration}\n <${uri}|More details>`
                 : incident.acknowledged
                 ? `Incident on *${component.name} / ${
                       monitor.name
