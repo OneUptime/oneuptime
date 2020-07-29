0819f2e17d9a9143858c6413b77ccf1312b04758
deityhub <judoc96@gmail.com>
2020-07-29 15:34:48 +0100

    refactor keybinding for side nav

--- dashboard/src/utils/keybinding.js
+++ dashboard/src/utils/keybinding.js
@@ -1,49 +1,52 @@
 import { history } from '../store';
 
+// This is an array of the available first key characters for the shortcut
+const initKeys = ['c', 'p', 's', 'u', 't', 'm', 'a', 'o'];
+
 /**
- * @description creates keybinding for fyipe side nav
+ * @description creates keybinding for side nav
+ * @param {object} event keyboard event
  * @param {array} route individual route in the application
  * @param {object} path actual path to navigate to
+ * @param {array} keys array of keys typed in
+ * @param {function} resetKeys function to reset the keys array
  */
 
-export const navKeyBind = (route, path) => {
+const keyBind = (event, route, path, keys, resetKeys) => {
     let shortcut = [];
     if (route.shortcut) {
         shortcut = route.shortcut.split('+');
-        // remember all the keys and ensure it's in sequence as the key binding
-        let keys = [];
+
         // reasons to use keydown
         // 1 --> gives the user impression that they can press and hold two keys simultaneously
         // 2 --> accommodate users that don't like pressing and holding two keys simultaneously (which is the actual behaviour, (^-^))
-        const func = e => {
-            keys.push(e.key);
-            if (keys.length === 2) {
-                if (keys[0] === shortcut[0] && keys[1] === shortcut[1]) {
-                    keys = [];
-                    return history.push(path);
-                }
-                keys = [];
+        keys.push(event.key);
+        if (keys.length === 1 && !initKeys.includes(event.key)) {
+            resetKeys();
+        }
+
+        if (keys.length === 2) {
+            if (keys[0] === shortcut[0] && keys[1] === shortcut[1]) {
+                resetKeys();
+                return history.push(path);
             }
-        };
-        window.addEventListener('keydown', func);
+            resetKeys();
+        }
     }
 };
 
+export const navKeyBind = (route, path) => {
+    let keys = [];
+    const resetKeys = () => (keys = []);
+    window.addEventListener('keydown', e =>
+        keyBind(e, route, path, keys, resetKeys)
+    );
+};
+
 export const cleanBind = (route, path) => {
-    let shortcut = [];
-    if (route.shortcut) {
-        shortcut = route.shortcut.split('+');
-        let keys = [];
-        const func = e => {
-            keys.push(e.key);
-            if (keys.length === 2) {
-                if (keys[0] === shortcut[0] && keys[1] === shortcut[1]) {
-                    keys = [];
-                    return history.push(path);
-                }
-                keys = [];
-            }
-        };
-        window.removeEventListener('keydown', func);
-    }
+    let keys = [];
+    const resetKeys = () => (keys = []);
+    window.removeEventListener('keydown', e =>
+        keyBind(e, route, path, keys, resetKeys)
+    );
 };
