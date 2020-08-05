2b5d2faac628dada15daef12de9e67e5bc5ff858
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-08-05 22:58:09 +0100

    Get active status

--- dashboard/src/pages/Home.js
+++ dashboard/src/pages/Home.js
@@ -15,6 +15,8 @@ import { LoadingState } from '../components/basic/Loader';
 import _ from 'lodash';
 import moment from 'moment-timezone';
 import OnCallSchedule from '../components/onCall/OnCallSchedule';
+import RenderIfUserInSubProject from '../components/basic/RenderIfUserInSubProject';
+import IncidentStatus from '../components/incident/IncidentStatus';
 
 class Home extends Component {
     componentDidMount() {
@@ -54,14 +56,18 @@ class Home extends Component {
 
         const userSchedules = _.flattenDeep(
             escalations.map(escalation => {
-                return escalation.teams.map(team => {
-                    const schedule = team.teamMembers
-                        .map(teamMember => teamMember)
-                        .filter(user => user.userId === this.props.user.id)
-                        .pop();
-                    schedule.scheduleId = escalation.scheduleId;
-                    return schedule;
-                });
+                return escalation.teams
+                    .map(team => {
+                        const schedule = team.teamMembers
+                            .map(teamMember => teamMember)
+                            .filter(user => user.userId === this.props.user.id)
+                            .pop();
+                        if (schedule) {
+                            schedule.scheduleId = escalation.scheduleId;
+                        }
+                        return schedule;
+                    })
+                    .filter(escalation => escalation);
             })
         );
 
@@ -69,23 +75,66 @@ class Home extends Component {
         const upcomingSchedules = [];
         const inactiveSchedules = [];
 
-        if (userSchedules) {
+        if (userSchedules && userSchedules.length > 0) {
             userSchedules.forEach(userSchedule => {
-                const startTime = moment(userSchedule.startTime)
-                    .tz(userSchedule.timezone)
-                    .format('HH:mm');
-                const endTime = moment(userSchedule.endTime)
-                    .tz(userSchedule.timezone)
-                    .format('HH:mm');
-                const now = moment()
-                    .tz(userSchedule.timezone)
-                    .format('HH:mm');
+                const now = (userSchedule && userSchedule.timezone
+                    ? moment().tz(userSchedule.timezone)
+                    : moment()
+                ).format('HH:mm');
+                const dayStart = moment().startOf('day');
+                const dayEnd = moment().endOf('day');
+
+                const startTime = (userSchedule && userSchedule.timezone
+                    ? moment(userSchedule.startTime || dayStart).tz(
+                          userSchedule.timezone
+                      )
+                    : moment(userSchedule.startTime || dayStart)
+                ).format('HH:mm');
+                const endTime = (userSchedule && userSchedule.timezone
+                    ? moment(userSchedule.endTime || dayEnd).tz(
+                          userSchedule.timezone
+                      )
+                    : moment(userSchedule.endTime || dayEnd)
+                ).format('HH:mm');
+
+                let hours = Math.ceil(
+                    moment(endTime, 'HH:mm').diff(
+                        moment(startTime, 'HH:mm'),
+                        'minutes'
+                    ) / 60
+                );
+                hours = hours < 0 ? hours + 24 : hours;
+
+                let hoursToStart = Math.ceil(
+                    moment(startTime, 'HH:mm').diff(
+                        moment(now, 'HH:mm'),
+                        'minutes'
+                    ) / 60
+                );
+                hoursToStart =
+                    hoursToStart < 0 ? hoursToStart + 24 : hoursToStart;
+
+                let hoursToEnd = hours + hoursToStart;
+
+                let nowToEnd = Math.ceil(
+                    moment(endTime, 'HH:mm').diff(
+                        moment(now, 'HH:mm'),
+                        'minutes'
+                    ) / 60
+                );
+                nowToEnd = nowToEnd < 0 ? nowToEnd + 24 : nowToEnd;
+
                 const isUserActive =
-                    moment(now, 'HH:mm').isAfter(moment(startTime, 'HH:mm')) &&
-                    moment(now, 'HH:mm').isBefore(moment(endTime, 'HH:mm'));
-                const timezone = moment(userSchedule.startTime)
-                    .tz(userSchedule.timezone)
-                    .zoneAbbr();
+                    (hoursToEnd !== nowToEnd || hoursToStart <= 0) &&
+                    nowToEnd > 0;
+
+                const timezone = (userSchedule && userSchedule.timezone
+                    ? moment(userSchedule.startTime || dayStart).tz(
+                          userSchedule.timezone
+                      )
+                    : moment(userSchedule.startTime || dayStart)
+                ).zoneAbbr();
+
                 const tempObj = { ...userSchedule };
                 tempObj.startTime = startTime;
                 tempObj.endTime = endTime;
@@ -94,8 +143,34 @@ class Home extends Component {
                 if (isUserActive) {
                     activeSchedules.push(tempObj);
                 } else {
-                    inactiveSchedules.push(tempObj);
+                    hoursToStart =
+                        hoursToStart <= 0 ? hoursToStart + 24 : hoursToStart;
+                    if (hoursToStart < 24) {
+                        upcomingSchedules.push(tempObj);
+                    } else {
+                        inactiveSchedules.push(tempObj);
+                    }
                 }
+            });
+        }
+
+        let incidentslist;
+        if (this.props.incidents) {
+            incidentslist = this.props.incidents.map((incident, i) => {
+                return (
+                    <RenderIfUserInSubProject
+                        key={`${incident._id || i}`}
+                        subProjectId={
+                            incident.projectId._id || incident.projectId
+                        }
+                    >
+                        <IncidentStatus
+                            count={i}
+                            incident={incident}
+                            multiple={true}
+                        />
+                    </RenderIfUserInSubProject>
+                );
             });
         }
 
@@ -180,6 +255,41 @@ class Home extends Component {
                                                                         }
                                                                     />
                                                                 </ShouldRender>
+
+                                                                <div className="Box-root Margin-bottom--12">
+                                                                    {incidentslist &&
+                                                                    incidentslist.length >
+                                                                        0 ? (
+                                                                        incidentslist
+                                                                    ) : (
+                                                                        <div className="Box-root Margin-bottom--12 Card-shadow--medium Box-background--green Border-radius--4">
+                                                                            <div className="db-Trends-header Padding-vertical--48">
+                                                                                <div className="db-Trends-controls">
+                                                                                    <div className="ContentHeader-center Box-root Flex-flex Flex-direction--column Flex-justifyContent--center">
+                                                                                        <div className="Box-root Flex-flex Flex-direction--row Flex-justifyContent--spaceBetween">
+                                                                                            <div className="ContentHeader-center Box-root Flex-flex Flex-direction--column Flex-justifyContent--center">
+                                                                                                <span className="Box-root Flex-flex Flex-direction--row Flex-justifyContent--center">
+                                                                                                    <span
+                                                                                                        id="component-content-header"
+                                                                                                        className="ContentHeader-title Text-color--white Text-display--inline Text-fontSize--16 Text-fontWeight--medium Text-typeface--base Text-wrap--wrap"
+                                                                                                    >
+                                                                                                        You
+                                                                                                        currently
+                                                                                                        don&apos;t
+                                                                                                        have
+                                                                                                        any
+                                                                                                        active
+                                                                                                        incidents.
+                                                                                                    </span>
+                                                                                                </span>
+                                                                                            </div>
+                                                                                        </div>
+                                                                                    </div>
+                                                                                </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    )}
+                                                                </div>
                                                             </>
                                                         ) : (
                                                             ''
@@ -213,7 +323,7 @@ Home.displayName = 'Home';
 
 Home.propTypes = {
     currentProjectId: PropTypes.string.isRequired,
-    user: PropTypes.string.isRequired,
+    user: PropTypes.object.isRequired,
     location: PropTypes.shape({
         pathname: PropTypes.string,
     }),
@@ -222,6 +332,10 @@ Home.propTypes = {
     fetchUserSchedule: PropTypes.func,
     escalation: PropTypes.object,
     escalations: PropTypes.array,
+    incidents: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.oneOf([null, undefined]),
+    ]),
 };
 
 const mapStateToProps = (state, props) => {
@@ -232,6 +346,7 @@ const mapStateToProps = (state, props) => {
         user: state.profileSettings.profileSetting.data,
         escalation: state.schedule.escalation,
         escalations: state.schedule.escalations,
+        incidents: state.incident.unresolvedincidents.incidents,
     };
 };
 
