79ab9d5d10a53f119e2b970aedeeaeef0cffe797
deityhub <judoc96@gmail.com>
2020-08-05 15:27:04 +0100

    reducer implementation for scheduled event note

--- dashboard/src/reducers/scheduledEvent.js
+++ dashboard/src/reducers/scheduledEvent.js
@@ -14,6 +14,24 @@ import {
     FETCH_SCHEDULED_EVENT_SUCCESS,
     FETCH_SCHEDULED_EVENT_REQUEST,
     FETCH_SCHEDULED_EVENT_FAILURE,
+    CREATE_SCHEDULED_EVENT_NOTE_REQUEST,
+    CREATE_SCHEDULED_EVENT_NOTE_SUCCESS,
+    CREATE_SCHEDULED_EVENT_NOTE_FAILURE,
+    FETCH_SCHEDULED_EVENT_NOTES_INVESTIGATION_REQUEST,
+    FETCH_SCHEDULED_EVENT_NOTES_INVESTIGATION_SUCCESS,
+    FETCH_SCHEDULED_EVENT_NOTES_INVESTIGATION_FAILURE,
+    FETCH_SCHEDULED_EVENT_NOTES_INTERNAL_REQUEST,
+    FETCH_SCHEDULED_EVENT_NOTES_INTERNAL_SUCCESS,
+    FETCH_SCHEDULED_EVENT_NOTES_INTERNAL_FAILURE,
+    UPDATE_SCHEDULED_EVENT_NOTE_INVESTIGATION_REQUEST,
+    UPDATE_SCHEDULED_EVENT_NOTE_INVESTIGATION_SUCCESS,
+    UPDATE_SCHEDULED_EVENT_NOTE_INVESTIGATION_FAILURE,
+    UPDATE_SCHEDULED_EVENT_NOTE_INTERNAL_REQUEST,
+    UPDATE_SCHEDULED_EVENT_NOTE_INTERNAL_SUCCESS,
+    UPDATE_SCHEDULED_EVENT_NOTE_INTERNAL_FAILURE,
+    DELETE_SCHEDULED_EVENT_NOTE_REQUEST,
+    DELETE_SCHEDULED_EVENT_NOTE_SUCCESS,
+    DELETE_SCHEDULED_EVENT_NOTE_FAILURE,
 } from '../constants/scheduledEvent';
 
 const INITIAL_STATE = {
@@ -42,6 +60,47 @@ const INITIAL_STATE = {
         error: null,
         requesting: false,
         success: false,
+    },
+    scheduledEventInvestigationList: {
+        error: null,
+        requesting: false,
+        success: false,
+        skip: null,
+        limit: null,
+        count: null,
+        scheduledEventNotes: [],
+    },
+    scheduledEventInternalList: {
+        error: null,
+        requesting: false,
+        success: false,
+        skip: null,
+        limit: null,
+        count: null,
+        scheduledEventNotes: [],
+    },
+    newScheduledEventNote: {
+        scheduledEventNote: null,
+        requesting: false,
+        success: false,
+        error: null,
+    },
+    updateScheduledEventNoteInternal: {
+        requesting: false,
+        success: false,
+        error: null,
+        scheduledEventNote: null,
+    },
+    updateScheduledEventNoteInvestigation: {
+        requesting: false,
+        success: false,
+        error: null,
+        scheduledEventNote: null,
+    },
+    deleteScheduledEventNote: {
+        requesting: false,
+        success: false,
+        error: null,
     },
 };
 
@@ -237,6 +296,307 @@ export default function scheduledEvent(state = INITIAL_STATE, action) {
                     success: false,
                 },
             });
+
+        case CREATE_SCHEDULED_EVENT_NOTE_REQUEST:
+            return {
+                ...state,
+                newScheduledEventNote: {
+                    ...state.newScheduledEventNote,
+                    requesting: true,
+                    success: false,
+                    error: null,
+                },
+            };
+
+        case CREATE_SCHEDULED_EVENT_NOTE_SUCCESS: {
+            let scheduledEventInternalList = {
+                ...state.scheduledEventInternalList,
+            };
+            let scheduledEventInvestigationList = {
+                ...state.scheduledEventInvestigationList,
+            };
+
+            if (action.payload.type === 'internal') {
+                scheduledEventInternalList = {
+                    ...scheduledEventInternalList,
+                    scheduledEventNotes: [
+                        action.payload,
+                        ...scheduledEventInternalList.scheduledEventNotes,
+                    ],
+                    count: scheduledEventInternalList.count + 1,
+                };
+            }
+
+            if (action.payload.type === 'investigation') {
+                scheduledEventInvestigationList = {
+                    ...scheduledEventInvestigationList,
+                    scheduledEventNotes: [
+                        action.payload,
+                        ...scheduledEventInvestigationList.scheduledEventNotes,
+                    ],
+                    count: scheduledEventInvestigationList.count + 1,
+                };
+            }
+
+            return {
+                ...state,
+                newScheduledEventNote: {
+                    requesting: false,
+                    success: true,
+                    error: null,
+                    scheduledEventNotes: action.payload,
+                },
+                scheduledEventInternalList,
+                scheduledEventInvestigationList,
+            };
+        }
+
+        case CREATE_SCHEDULED_EVENT_NOTE_FAILURE:
+            return {
+                ...state,
+                newScheduledEventNote: {
+                    ...state.newScheduledEventNote,
+                    requesting: false,
+                    success: false,
+                    error: action.payload,
+                },
+            };
+
+        case UPDATE_SCHEDULED_EVENT_NOTE_INVESTIGATION_REQUEST:
+            return {
+                ...state,
+                updateScheduledEventNoteInvestigation: {
+                    ...state.updateScheduledEventNoteInvestigation,
+                    requesting: true,
+                    success: false,
+                    error: null,
+                },
+            };
+
+        case UPDATE_SCHEDULED_EVENT_NOTE_INVESTIGATION_SUCCESS: {
+            const scheduledEventNotes = state.scheduledEventInvestigationList.scheduledEventNotes.map(
+                investigationNote => {
+                    if (
+                        String(investigationNote._id) ===
+                        String(action.payload._id)
+                    ) {
+                        return action.payload;
+                    }
+
+                    return investigationNote;
+                }
+            );
+
+            return {
+                ...state,
+                updateScheduledEventNoteInvestigation: {
+                    requesting: false,
+                    success: true,
+                    error: null,
+                    scheduledEventNote: action.payload,
+                },
+                scheduledEventInvestigationList: {
+                    ...state.scheduledEventInvestigationList,
+                    scheduledEventNotes,
+                },
+            };
+        }
+
+        case UPDATE_SCHEDULED_EVENT_NOTE_INVESTIGATION_FAILURE:
+            return {
+                ...state,
+                updateScheduledEventNoteInvestigation: {
+                    ...state.updateScheduledEventNoteInvestigation,
+                    requesting: false,
+                    success: false,
+                    error: action.payload,
+                },
+            };
+
+        case UPDATE_SCHEDULED_EVENT_NOTE_INTERNAL_REQUEST:
+            return {
+                ...state,
+                updateScheduledEventNoteInternal: {
+                    ...state.updateScheduledEventNoteInternal,
+                    requesting: true,
+                    success: false,
+                    error: null,
+                },
+            };
+
+        case UPDATE_SCHEDULED_EVENT_NOTE_INTERNAL_SUCCESS: {
+            const scheduledEventNotes = state.scheduledEventInternalList.scheduledEventNotes.map(
+                internalNote => {
+                    if (
+                        String(internalNote._id) === String(action.payload._id)
+                    ) {
+                        return action.payload;
+                    }
+
+                    return internalNote;
+                }
+            );
+            return {
+                ...state,
+                updateScheduledEventNoteInternal: {
+                    requesting: false,
+                    success: true,
+                    error: null,
+                    scheduledEventNote: action.payload,
+                },
+                scheduledEventInternalList: {
+                    ...state.scheduledEventInternalList,
+                    scheduledEventNotes,
+                },
+            };
+        }
+
+        case UPDATE_SCHEDULED_EVENT_NOTE_INTERNAL_FAILURE:
+            return {
+                ...state,
+                updateScheduledEventNoteInternal: {
+                    ...state.updateScheduledEventNoteInternal,
+                    requesting: false,
+                    success: false,
+                    error: action.payload,
+                },
+            };
+
+        case DELETE_SCHEDULED_EVENT_NOTE_REQUEST:
+            return {
+                ...state,
+                deleteScheduledEventNote: {
+                    requesting: true,
+                    success: false,
+                    error: null,
+                },
+            };
+
+        case DELETE_SCHEDULED_EVENT_NOTE_SUCCESS: {
+            let scheduledEventInternalList = {
+                ...state.scheduledEventInternalList,
+            };
+            let scheduledEventInvestigationList = {
+                ...state.scheduledEventInvestigationList,
+            };
+
+            if (action.payload.type === 'internal') {
+                scheduledEventInternalList = {
+                    ...scheduledEventInternalList,
+                    scheduledEventNotes: scheduledEventInternalList.scheduledEventNotes.filter(
+                        internalNote =>
+                            String(internalNote._id) !==
+                            String(action.payload._id)
+                    ),
+                    count: scheduledEventInternalList.count - 1,
+                };
+            }
+
+            if (action.payload.type === 'investigation') {
+                scheduledEventInvestigationList = {
+                    ...scheduledEventInvestigationList,
+                    scheduledEventNotes: scheduledEventInvestigationList.scheduledEventNotes.filter(
+                        investigationNote =>
+                            String(investigationNote._id) !==
+                            String(action.payload._id)
+                    ),
+                    count: scheduledEventInvestigationList.count - 1,
+                };
+            }
+
+            return {
+                ...state,
+                deleteScheduledEventNote: {
+                    requesting: false,
+                    success: true,
+                    error: null,
+                },
+                scheduledEventInternalList,
+                scheduledEventInvestigationList,
+            };
+        }
+
+        case DELETE_SCHEDULED_EVENT_NOTE_FAILURE:
+            return {
+                ...state,
+                deleteScheduledEventNote: {
+                    requesting: false,
+                    success: false,
+                    error: action.payload,
+                },
+            };
+
+        case FETCH_SCHEDULED_EVENT_NOTES_INVESTIGATION_REQUEST:
+            return {
+                ...state,
+                scheduledEventInvestigationList: {
+                    ...state.scheduledEventInvestigationList,
+                    error: null,
+                    requesting: true,
+                    success: false,
+                },
+            };
+
+        case FETCH_SCHEDULED_EVENT_NOTES_INVESTIGATION_SUCCESS:
+            return {
+                ...state,
+                scheduledEventInvestigationList: {
+                    error: null,
+                    requesting: false,
+                    success: true,
+                    skip: action.payload.skip,
+                    limit: action.payload.limit,
+                    count: action.payload.count,
+                    scheduledEventNotes: action.payload.data,
+                },
+            };
+
+        case FETCH_SCHEDULED_EVENT_NOTES_INVESTIGATION_FAILURE:
+            return {
+                ...state,
+                scheduledEventInvestigationList: {
+                    ...state.scheduledEventInvestigationList,
+                    error: action.payload,
+                    requesting: false,
+                    success: false,
+                },
+            };
+
+        case FETCH_SCHEDULED_EVENT_NOTES_INTERNAL_REQUEST:
+            return {
+                ...state,
+                scheduledEventInternalList: {
+                    ...state.scheduledEventInternalList,
+                    error: null,
+                    requesting: true,
+                    success: false,
+                },
+            };
+
+        case FETCH_SCHEDULED_EVENT_NOTES_INTERNAL_SUCCESS:
+            return {
+                ...state,
+                scheduledEventInternalList: {
+                    error: null,
+                    requesting: false,
+                    success: true,
+                    skip: action.payload.skip,
+                    limit: action.payload.limit,
+                    count: action.payload.count,
+                    scheduledEventNotes: action.payload.data,
+                },
+            };
+
+        case FETCH_SCHEDULED_EVENT_NOTES_INTERNAL_FAILURE:
+            return {
+                ...state,
+                scheduledEventInternalList: {
+                    ...state.scheduledEventInternalList,
+                    error: action.payload,
+                    requesting: false,
+                    success: false,
+                },
+            };
 
         default:
             return state;
