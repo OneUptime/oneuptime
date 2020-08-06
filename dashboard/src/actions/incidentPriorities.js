78b278f67a1cbf52f0d1b373d7d22b9991477d4c
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-06 22:24:01 +0100

    Update fetchIncidentPriorities to take skip and limit as parameters

--- dashboard/src/actions/incidentPriorities.js
+++ dashboard/src/actions/incidentPriorities.js
@@ -22,9 +22,9 @@ function fetchIncidentPrioritiesFailure(error) {
   });
 }
 
-export function fetchIncidentPriorities(projectId) {
+export function fetchIncidentPriorities(projectId,skip,limit) {
   return function (dispatch) {
-    const promise = getApi(`incidentPriorities/${projectId}`);
+    const promise = getApi(`incidentPriorities/${projectId}?skip=${skip||0}&limit=${limit||10}`);
     dispatch(fetchIncidentPrioritiesRequest());
     promise.then(
       function (incidentsPriorities) {
