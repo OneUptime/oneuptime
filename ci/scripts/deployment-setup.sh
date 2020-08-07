1987fc76f34ed82e0d3d65a1df4b03f81fb79532
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-08-07 20:33:12 +0200

    Install kubectl.

--- ci/scripts/deployment-setup.sh
+++ ci/scripts/deployment-setup.sh
@@ -1,10 +1,8 @@
-echo "Remove Google Cloud SDK"
-sudo rm -rf /home/gitlab-runner/google-cloud-sdk
-echo "Remove Google Cloud Logs"
-sudo rm -rf /home/gitlab-runner/.config/gcloud
-curl -sSL https://sdk.cloud.google.com | bash > /dev/null;
-source $HOME/google-cloud-sdk/path.bash.inc
-$HOME/google-cloud-sdk/bin/gcloud components update kubectl
+# Install Kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+sudo kubectl version --client
 # Auth with DigitalOcean Client
 echo "Install doctl"
 sudo snap install doctl
