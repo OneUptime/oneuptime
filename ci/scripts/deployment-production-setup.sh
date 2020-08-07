6d57cf1f52ae19399e641babf93ac8a4ecef7ac5
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-08-07 20:35:52 +0200

    Fix context in kubectl.

--- ci/scripts/deployment-production-setup.sh
+++ ci/scripts/deployment-production-setup.sh
@@ -1,2 +1,3 @@
 echo "Get KubeCluster Config"
 sudo doctl kubernetes cluster kubeconfig save fyipe-production
+sudo kubectl config use-context do-nyc3-fyipe-production
--- ci/scripts/deployment-staging-setup.sh
+++ ci/scripts/deployment-staging-setup.sh
@@ -1,2 +1,3 @@
 echo "Get KubeCluster Config"
 sudo doctl kubernetes cluster kubeconfig save fyipe-staging
+sudo kubectl config use-context do-nyc3-fyipe-staging
