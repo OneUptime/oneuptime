fdd44c46e716526a47c89ce3695fd29c902cd650
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-10 12:28:04 +0100

    Add new dashboard test: 'Should create incident priority'

---
+++ dashboard/src/test/puppeteer/incidentPrioriy.test.js
@@ -0,0 +1,75 @@
+const puppeteer = require('puppeteer');
+const utils = require('./test-utils');
+const init = require('./test-init');
+const { Cluster } = require('puppeteer-cluster');
+
+require('should');
+
+// user credentials
+const email = utils.generateRandomBusinessEmail();
+const password = '1234567890';
+
+describe('Monitor API', () => {
+  const operationTimeOut = 500000;
+
+  let cluster;
+
+  beforeAll(async () => {
+    jest.setTimeout(500000);
+
+    cluster = await Cluster.launch({
+      concurrency: Cluster.CONCURRENCY_PAGE,
+      puppeteerOptions: utils.puppeteerLaunchConfig,
+      puppeteer,
+      timeout: utils.timeout,
+    });
+
+    cluster.on('taskerror', err => {
+      throw err;
+    });
+
+    return await cluster.execute(null, async ({ page }) => {
+      const user = {
+        email,
+        password,
+      };
+      await init.registerUser(user, page);
+      await init.loginUser(user, page);
+    });
+  });
+
+  afterAll(async () => {
+    await cluster.idle();
+    await cluster.close();
+  });
+
+  test(
+    'Should create incident priority',
+    async () => {
+      return await cluster.execute(null, async ({ page }) => {
+        await page.goto(utils.DASHBOARD_URL, {
+          waitUntil: 'networkidle0',
+        });
+        await page.waitForSelector('#projectSettings');
+        await page.click('#projectSettings');
+        await page.waitForSelector('#incidentSettings');
+        await page.click('#incidentSettings');
+        await page.waitForSelector('#addNewPriority');
+        await page.click('#addNewPriority');
+        await page.waitForSelector('#CreateIncidentPrioriy');
+        await page.type('input[name=name]','High');
+        await page.click('#CreateIncidentPrioriy');
+        await page.waitFor(3000);
+        await page.reload({
+          waitUntil: 'networkidle0',
+        });
+        const firstRowFirstColumnIndentifier= '#incidentPrioritiesList>div>div>div>div.bs-ObjectList-row>div:first-child';
+        await page.waitForSelector(firstRowFirstColumnIndentifier);
+        const content = await page.$eval(firstRowFirstColumnIndentifier, e=> e.textContent)
+        expect(content).toEqual('High');
+      })
+    },
+    operationTimeOut
+  );
+
+});
