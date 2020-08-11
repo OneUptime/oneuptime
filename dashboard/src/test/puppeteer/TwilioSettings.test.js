5eb4f0511e4d435361e36a33bc06db9d6c1ed447
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-11 16:29:46 +0100

    Fix TwilioSettings

--- dashboard/src/test/puppeteer/TwilioSettings.test.js
+++ dashboard/src/test/puppeteer/TwilioSettings.test.js
@@ -63,10 +63,10 @@ describe('Custom Twilio Settings', () => {
                 await page.click('#projectSettings');
                 await page.waitForSelector('#email');
                 await page.click('#sms');
-                await page.waitForSelector('label[for=smssmtpswitch]', {
+                await page.waitForSelector('label[for=enabled]', {
                     visible: true,
                 });
-                await page.click('label[for=smssmtpswitch]');
+                await page.click('label[for=enabled]');
                 await page.type('#accountSid', twilioCredentials.accountSid);
                 await page.type('#authToken', twilioCredentials.authToken);
                 await page.type('#phoneNumber', twilioCredentials.phoneNumber);
