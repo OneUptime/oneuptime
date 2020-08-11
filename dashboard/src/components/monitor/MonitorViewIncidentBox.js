36d73d598a7ccfb21c2eb51e23b0922ba1dc4202
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-11 10:08:30 +0000

    Fix twilio settings tests

--- dashboard/src/components/monitor/MonitorViewIncidentBox.js
+++ dashboard/src/components/monitor/MonitorViewIncidentBox.js
@@ -100,7 +100,10 @@ export class MonitorViewIncidentBox extends Component {
     };
 
     render() {
-        const { createIncidentModalId, filteredIncidents } = this.state;
+        const {
+            createIncidentModalId,
+            filteredIncidents,
+        } = this.state;
         const creating = this.props.create ? this.props.create : false;
 
         return (
