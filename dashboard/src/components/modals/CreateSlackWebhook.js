81043220d4645895245fd21d3d29ef23e254c595
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-11 11:52:50 +0100

    Add description to CreateSlackWebhook modal

--- dashboard/src/components/modals/CreateSlackWebhook.js
+++ dashboard/src/components/modals/CreateSlackWebhook.js
@@ -88,14 +88,22 @@ class CreateSlack extends React.Component {
                     <div className="bs-Modal">
                         <div className="bs-Modal-header">
                             <div
-                                className="bs-Modal-header-copy"
+                                className="bs-Modal-header-copy bs-u-flex Flex-direction--column"
                                 style={{
                                     marginBottom: '10px',
                                     marginTop: '10px',
                                 }}
                             >
-                                <span className="Text-color--inherit Text-display--inline Text-fontSize--20 Text-fontWeight--medium Text-lineHeight--24 Text-typeface--base Text-wrap--wrap">
+                                <span className="Text-color--inherit Text-display--inline Text-fontSize--20 Text-fontWeight--medium Text-lineHeight--24 Text-typeface--base Text-wrap--wrap Margin-bottom--4">
                                     <span>Create Slack Webhook</span>
+                                </span>
+                                <span className="ContentHeader-description Text-color--inherit Text-display--inline Text-fontSize--14 Text-fontWeight--regular Text-lineHeight--20 Text-typeface--base Text-wrap--wrap">
+                                    <span>Click {' '}
+                                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Fyipe/feature-docs/blob/master/Webhooks.md#slack" style={{textDecoration: "underline"}}>
+                                            here
+                                        </a> {' '}
+                                        to check documentation on how to integrate Slack with Fyipe.
+                                    </span>
                                 </span>
                             </div>
                         </div>
