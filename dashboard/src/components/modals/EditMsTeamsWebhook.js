204ef8b92fe372d47af6af28e18180577b388aef
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-11 11:52:35 +0100

    Add description to EditMsTeamsWebhook modal

--- dashboard/src/components/modals/EditMsTeamsWebhook.js
+++ dashboard/src/components/modals/EditMsTeamsWebhook.js
@@ -92,14 +92,22 @@ class EditWebHook extends React.Component {
                     <div className="bs-Modal">
                         <div className="bs-Modal-header">
                             <div
-                                className="bs-Modal-header-copy"
+                                className="bs-Modal-header-copy bs-u-flex Flex-direction--column"
                                 style={{
                                     marginBottom: '10px',
                                     marginTop: '10px',
                                 }}
                             >
-                                <span className="Text-color--inherit Text-display--inline Text-fontSize--20 Text-fontWeight--medium Text-lineHeight--24 Text-typeface--base Text-wrap--wrap">
-                                    <span>Update Webhook</span>
+                                <span className="Text-color--inherit Text-display--inline Text-fontSize--20 Text-fontWeight--medium Text-lineHeight--24 Text-typeface--base Text-wrap--wrap Text-wrap--wrap Margin-bottom--4">
+                                    <span>Update Microsoft Teams Webhook</span>
+                                </span>
+                                <span className="ContentHeader-description Text-color--inherit Text-display--inline Text-fontSize--14 Text-fontWeight--regular Text-lineHeight--20 Text-typeface--base Text-wrap--wrap">
+                                    <span>Click {' '}
+                                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/Fyipe/feature-docs/blob/master/Webhooks.md#microsoft-teams" style={{textDecoration: "underline"}}>
+                                            here
+                                        </a> {' '}
+                                        to check documentation on how to integrate Microsoft Teams with Fyipe.
+                                    </span>
                                 </span>
                             </div>
                         </div>
