b0432329419118ee42144494ca158814582c6505
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-08-13 13:27:30 +0200

    Push changes.

--- ci/scripts/deployment-staging-setup.sh
+++ ci/scripts/deployment-staging-setup.sh
@@ -3,4 +3,4 @@ sudo doctl kubernetes cluster kubeconfig save fyipe-staging
 sudo cp /root/.kube/config /gitlab-runner/.kube/config || echo "Unable to copy"
 sudo kubectl config --kubeconfig=/root/.kube/config get-contexts
 sudo kubectl config --kubeconfig=/root/.kube/config use-context do-nyc3-fyipe-staging
-sudo kubectl get pods 
+sudo kubectl --kubeconfig=/root/.kube/config get pods 
