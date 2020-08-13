d4af454a26a4de3840c6d5c07f4d2eebf5bb00d4
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-08-13 13:18:43 +0200

    Deployment fix.

--- ci/scripts/deployment-staging-setup.sh
+++ ci/scripts/deployment-staging-setup.sh
@@ -1,5 +1,5 @@
 echo "Get KubeCluster Config"
 sudo doctl kubernetes cluster kubeconfig save fyipe-staging
 sudo cp /root/.kube/config /gitlab-runner/.kube/config || echo "Unable to copy"
-sudo kubectl config get-contexts
-sudo kubectl config use-context do-nyc3-fyipe-staging
+sudo kubectl config --kubeconfig=/root/.kube/config get-contexts
+sudo kubectl config --kubeconfig=/root/.kube/config use-context do-nyc3-fyipe-staging
