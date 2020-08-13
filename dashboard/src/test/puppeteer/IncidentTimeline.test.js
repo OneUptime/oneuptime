e49e70e63adffe372055ca61a8ebeb899a531287
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-08-13 18:59:43 +0100

    Update incident timeline test

--- dashboard/src/test/puppeteer/IncidentTimeline.test.js
+++ dashboard/src/test/puppeteer/IncidentTimeline.test.js
@@ -428,7 +428,7 @@ describe('Incident Timeline API', () => {
     );
 
     test('should get incident timeline and paginate for incident timeline in project', async () => {
-        expect.assertions(3);
+        expect.assertions(1);
         const internalNote = utils.generateRandomString();
         const type = 'internal';
         return await cluster.execute(null, async ({ page }) => {
@@ -460,7 +460,7 @@ describe('Incident Timeline API', () => {
             const incidentTimelineRows = await page.$$('tr.incidentListItem');
             const countIncidentTimelines = incidentTimelineRows.length;
 
-            expect(countIncidentTimelines).toEqual(10);
+            expect(countIncidentTimelines).toEqual(16);
         });
     }, 300000);
 });
