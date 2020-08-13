d08a8ab1b400d7dc0870117708ea94296bb1c9e8
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-13 22:36:41 +0100

    Set the current status to the worst active incident

--- status-page/src/components/MonitorInfo.js
+++ status-page/src/components/MonitorInfo.js
@@ -26,11 +26,12 @@ const calculateTime = (statuses, start, range) => {
             downTime: 0,
             upTime: 0,
             degradedTime: 0,
+            status: null,
             emptytime: dayStart.toISOString(),
         };
 
         reversedStatuses.forEach(monitor => {
-            let monitorStatus = Object.assign({},monitor);
+            const monitorStatus = Object.assign({}, monitor);
             if (monitorStatus.endTime === null) {
                 monitorStatus.endTime = new Date().toISOString();
             }
@@ -39,6 +40,16 @@ const calculateTime = (statuses, start, range) => {
                 moment(monitorStatus.startTime).isBefore(dayEnd) &&
                 moment(monitorStatus.endTime).isAfter(dayStartIn)
             ) {
+                if (
+                    monitor.endTime === null &&
+                    (monitor.status === 'offline' ||
+                        (monitorStatus.status === 'degraded' &&
+                            timeObj.status !== 'offline') ||
+                        timeObj.status === null)
+                ) {
+                    timeObj.status = monitorStatus.status;
+                }
+
                 const start = moment(monitorStatus.startTime).isBefore(
                     dayStartIn
                 )
