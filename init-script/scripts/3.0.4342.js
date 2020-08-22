9373d05f1313c43d879115e3f88c6c964571e88a
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-22 16:17:47 +0100

    Update the init-script to assign idNumber for every incident

--- init-script/scripts/3.0.4342.js
+++ init-script/scripts/3.0.4342.js
@@ -1,6 +1,8 @@
 const { find, update } = require('../util/db');
 
 const monitorCollection = 'monitors';
+const projectsCollection = 'projects';
+const incidentsCollection = 'incidents';
 
 async function run() {
     const monitors = await find(monitorCollection, {
@@ -10,6 +12,24 @@ async function run() {
     for (let i = 0; i < monitors.length; i++) {
         const monitor = monitors[i];
         await update(monitorCollection, { _id: monitor._id }, { pollTime: [] });
+    }
+
+    const projects = await find(projectsCollection, {
+        deleted: false,
+    });
+
+    for (const project of projects) {
+        const query = {
+            projectId: project._id,
+        };
+        const incidents = await global.db
+            .collection(incidentsCollection)
+            .find(query)
+            .sort('createdAt', 1)
+            .toArray();
+        for (let i = 0; i < incidents.length; i++) {
+            await update(incidentsCollection, { _id: incidents[i]._id }, { idNumber: i });
+        }
     }
 }
 module.exports = run;
