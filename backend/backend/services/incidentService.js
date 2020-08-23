a1dde5cbdf54d12b8e369d0c7c57b7a80278d3c4
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-23 23:51:29 +0100

    Remove an extra call for RealTimeService.sendCreatedIncident from IncidentService.create

--- backend/backend/services/incidentService.js
+++ backend/backend/services/incidentService.js
@@ -81,8 +81,6 @@ module.exports = {
                     status: data.incidentType,
                 });
 
-                RealTimeService.sendCreatedIncident(incident);
-
                 return incident;
             } else {
                 const error = new Error('Monitor is not present.');
