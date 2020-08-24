cbeb3d53003976d6c34ac5c48dab4792f45d911b
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-24 21:23:04 +0100

    Implement the incident settings reducer

---
+++ dashboard/src/reducers/incidentBasicSettings.js
@@ -0,0 +1,88 @@
+import {
+    FETCH_INCIDENT_BASIC_SETTINGS_REQUEST,
+    FETCH_INCIDENT_BASIC_SETTINGS_SUCCESS,
+    FETCH_INCIDENT_BASIC_SETTINGS_FAILURE,
+    UPDATE_INCIDENT_BASIC_SETTINGS_REQUEST,
+    UPDATE_INCIDENT_BASIC_SETTINGS_SUCCESS,
+    UPDATE_INCIDENT_BASIC_SETTINGS_FAILURE,
+} from '../constants/incidentBasicSettings';
+
+const INITIAL_STATE = {
+    incidentBasicSettings: {
+        title: '',
+        description: '',
+    },
+    fetchIncidentBasicSettings: {
+        requesting: false,
+        error: null,
+        success: null,
+    },
+    updateIncidentBasicSettings: {
+        requesting: false,
+        error: null,
+        success: null,
+    },
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case FETCH_INCIDENT_BASIC_SETTINGS_REQUEST:
+            return Object.assign({}, state, {
+                fetchIncidentBasicSettings: {
+                    ...state.fetchIncidentBasicSettings,
+                    requesting: true,
+                    error: null,
+                    success: null,
+                },
+            });
+        case FETCH_INCIDENT_BASIC_SETTINGS_SUCCESS:
+            return Object.assign({}, state, {
+                fetchIncidentBasicSettings: {
+                    ...state.fetchIncidentBasicSettings,
+                    requesting: false,
+                    error: null,
+                    success: true,
+                },
+                incidentBasicSettings: action.payload,
+            });
+        case FETCH_INCIDENT_BASIC_SETTINGS_FAILURE:
+            return Object.assign({}, state, {
+                fetchIncidentBasicSettings: {
+                    ...state.fetchIncidentBasicSettings,
+                    requesting: false,
+                    error: action.payload,
+                    success: false,
+                },
+            });
+        case UPDATE_INCIDENT_BASIC_SETTINGS_REQUEST:
+            return Object.assign({}, state, {
+                updateIncidentBasicSettings: {
+                    ...state.updateIncidentBasicSettings,
+                    requesting: true,
+                    error: null,
+                    success: null,
+                },
+            });
+        case UPDATE_INCIDENT_BASIC_SETTINGS_SUCCESS:
+            return Object.assign({}, state, {
+                updateIncidentBasicSettings: {
+                    ...state.updateIncidentBasicSettings,
+                    requesting: false,
+                    error: null,
+                    success: true,
+                },
+                incidentBasicSettings: action.payload,
+            });
+        case UPDATE_INCIDENT_BASIC_SETTINGS_FAILURE:
+            return Object.assign({}, state, {
+                updateIncidentBasicSettings: {
+                    ...state.updateIncidentBasicSettings,
+                    requesting: false,
+                    error: action.payload,
+                    success: false,
+                },
+            });
+        default:
+            return state;
+    }
+};
