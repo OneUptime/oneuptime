867e60e69ee12c7a007bbbe7ea29b631e166d91f
Zadat Olayinka <bundayy@outlook.com>
2020-08-24 10:46:54 +0100

    lint fix

--- dashboard/src/pages/IncidentPriorities.js
+++ dashboard/src/pages/IncidentPriorities.js
@@ -92,10 +92,20 @@ class IncidentPriorities extends React.Component {
                                             <div className="Box-root Flex-flex Flex-direction--row Flex-justifyContent--spaceBetween">
                                                 <div className="ContentHeader-center Box-root Flex-flex Flex-direction--column Flex-justifyContent--center">
                                                     <span className="ContentHeader-title Text-color--inherit Text-display--inline Text-fontSize--16 Text-fontWeight--medium Text-lineHeight--28 Text-typeface--base Text-wrap--wrap">
-                                                        <span>Incident Priority</span>
+                                                        <span>
+                                                            Incident Priority
+                                                        </span>
                                                     </span>
                                                     <span className="ContentHeader-description Text-color--inherit Text-display--inline Text-fontSize--14 Text-fontWeight--regular Text-lineHeight--20 Text-typeface--base Text-wrap--wrap">
-                                                        <span>Incidents can have different severity or priority. You can define your organization incident severity policy here.</span>
+                                                        <span>
+                                                            Incidents can have
+                                                            different severity
+                                                            or priority. You can
+                                                            define your
+                                                            organization
+                                                            incident severity
+                                                            policy here.
+                                                        </span>
                                                     </span>
                                                 </div>
                                                 <div className="ContentHeader-end Box-root Flex-flex Flex-alignItems--center Margin-left--16">
