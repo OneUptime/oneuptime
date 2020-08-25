52c5a30a842935daa4f8dc09b3c81846f6a6b3cc
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-25 23:09:47 +0100

    Add new test 'Should update default title/description fields'

--- dashboard/src/test/puppeteer/incidentSettings.test.js
+++ dashboard/src/test/puppeteer/incidentSettings.test.js
@@ -9,6 +9,11 @@ require('should');
 const email = utils.generateRandomBusinessEmail();
 const password = '1234567890';
 
+const componentName = utils.generateRandomString();
+const monitorName = utils.generateRandomString();
+const newDefaultIncidentTitle = 'TEST: {{monitorName}}';
+const newDefaultIncidentDescription = 'TEST: {{incidentType}}';
+
 describe('Incident Priority API', () => {
     const operationTimeOut = 500000;
 
@@ -35,6 +40,7 @@ describe('Incident Priority API', () => {
             };
             await init.registerUser(user, page);
             await init.loginUser(user, page);
+            await init.addMonitorToComponent(componentName, monitorName, page);
         });
     });
 
@@ -65,5 +71,41 @@ describe('Incident Priority API', () => {
         operationTimeOut
     );
 
+    test(
+      'Should update default title/description fields',
+      async () => {
+          return await cluster.execute(null, async ({ page }) => {
+              await page.goto(utils.DASHBOARD_URL, {
+                  waitUntil: 'networkidle0',
+              });
+              await page.waitForSelector('#projectSettings');
+              await page.click('#projectSettings');
+              await page.waitForSelector('#incidentSettings');
+              await page.click('#incidentSettings');
+              await page.waitForSelector('input[name=title]');
+              await page.waitFor(3000);
+              await page.click('input[name=title]',{clickCount: 3});
+              await page.keyboard.press('Backspace');
+              await page.type('input[name=title]', newDefaultIncidentTitle);
+              
+              await page.click('#ace-editor');
+              await page.keyboard.down('Control');
+              await page.keyboard.press('A');
+              await page.keyboard.up('Control');
+              await page.type('#ace-editor', newDefaultIncidentDescription);
+              await page.click('#saveButton');
+              await page.reload({
+                waitUntil: 'networkidle0',
+              });
+              await page.waitFor(3000);
+              await page.waitForSelector('input[name=title]');
+              const titleFieldValue = await page.$eval('input[name=title]', e => e.value);
+              expect(titleFieldValue).toEqual(newDefaultIncidentTitle);
+              const descriptionFieldValue = await page.$eval('.ace_layer.ace_text-layer', e => e.textContent);
+              expect(descriptionFieldValue).toEqual(newDefaultIncidentDescription);
+          });
+      },
+      operationTimeOut
+  );
 
 });
