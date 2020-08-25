fbef6b017ab96ad4887ada07059ad13fc25e2d59
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-25 09:31:03 +0100

    Defines the default basic settings for the incidents

---
+++ backend/backend/config/incidentDefaultSettings.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: "{{monitorName}} is {{incidentType}}.",
+  description:"{{monitorName}} is {{incidentType}}. This incident is currently being investigated by our team and more information will be added soon.",
+}
