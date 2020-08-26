8465ab48a48b87ddefcaa2bedcaba79d196fe783
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-08-26 08:48:53 +0100

    Cleanup docker after jobs

--- ci/scripts/deployment-cleanup.sh
+++ ci/scripts/deployment-cleanup.sh
@@ -3,3 +3,5 @@ curl -X POST "https://api.cloudflare.com/client/v4/zones/${CF_ZONE}/purge_cache"
         -H "X-Auth-Key: ${CF_API_KEY}" \
         -H "Content-Type: application/json" \
         --data '{"purge_everything":true}'
+
+sudo docker system prune -a --volumes --force
