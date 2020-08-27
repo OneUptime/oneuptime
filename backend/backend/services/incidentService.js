822583b0df10ff1610a66336dd66929775bb7a3e
deityhub <judoc96@gmail.com>
2020-08-27 13:05:38 +0100

    send out the created incident before creating an incident timeline

--- backend/backend/services/incidentService.js
+++ backend/backend/services/incidentService.js
@@ -74,14 +74,14 @@ module.exports = {
                 incident = await _this.findOneBy({ _id: incident._id });
                 await _this._sendIncidentCreatedAlert(incident);
 
+                await RealTimeService.sendCreatedIncident(incident);
+
                 await IncidentTimelineService.create({
                     incidentId: incident._id,
                     createdById: data.createdById,
                     probeId: data.probeId,
                     status: data.incidentType,
                 });
-
-                RealTimeService.sendCreatedIncident(incident);
 
                 return incident;
             } else {
