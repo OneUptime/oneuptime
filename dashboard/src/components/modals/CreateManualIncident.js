7a30b9e3505ea6457a1e8e5e9a5105ade3a5ce9e
Mohamed ABDELLANI <abdellani@gmail.com>
2020-08-28 10:04:34 +0100

    Fix the create manual incident modal

--- dashboard/src/components/modals/CreateManualIncident.js
+++ dashboard/src/components/modals/CreateManualIncident.js
@@ -35,7 +35,7 @@ class CreateManualIncident extends Component {
             values.incidentType,
             values.title,
             values.description,
-            values.incidentPriority
+            (values.incidentPriority ==='')?null :values.incidentPriority
         ).then(() => {
             createIncidentReset();
             closeModal({
@@ -164,7 +164,7 @@ class CreateManualIncident extends Component {
                                                             }
                                                             name="incidentPriority"
                                                             id="incidentPriority"
-                                                            placeholder="Incident Priority"
+                                                            placeholder='Incident Priority'
                                                             disabled={
                                                                 this.props
                                                                     .newIncident
@@ -174,7 +174,7 @@ class CreateManualIncident extends Component {
                                                                 {
                                                                     value: '',
                                                                     label:
-                                                                        'Select type',
+                                                                        'Incident Priority',
                                                                 },
                                                                 ...incidentPriorities.map(
                                                                     incidentPriority => ({
