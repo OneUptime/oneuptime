9d422be28d3f6695196e05636404e57f40bd655c
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-08-30 22:19:44 +0100

    Fix smoke tests

--- smoke-test/backend.test.js
+++ smoke-test/backend.test.js
@@ -5,6 +5,7 @@ let page, browser;
 
 describe('Check Backend', () => {
     beforeAll(async () => {
+        jest.setTimeout(30000);
         browser = await puppeteer.launch(utils.puppeteerLaunchConfig);
         page = await browser.newPage();
     });
@@ -17,11 +18,9 @@ describe('Check Backend', () => {
         await page.goto(utils.BACKEND_URL, {
             waitUntil: 'networkidle0',
         });
-        const response = await page.$eval('body > pre', e => {
+        const response = await page.$eval('body > p', e => {
             return e.innerHTML;
         });
-        expect(response).toBe(
-            '{"status":200,"message":"Service Status - OK","serviceType":"fyipe-api"}'
-        );
+        expect(response).toBe('Status: 200');
     });
 });
