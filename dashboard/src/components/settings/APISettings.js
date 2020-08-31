1cc815f782dc622a9b981683818dad7aa2e66b39
Zadat Olayinka <bundayy@outlook.com>
2020-08-31 12:20:43 +0100

    project api setting tooltip and cursor

--- dashboard/src/components/settings/APISettings.js
+++ dashboard/src/components/settings/APISettings.js
@@ -11,6 +11,7 @@ import ResetAPIKey from '../modals/ResetAPIKey';
 import { openModal } from '../../actions/modal';
 import { logEvent } from '../../analytics';
 import { API_URL, SHOULD_LOG_ANALYTICS } from '../../config';
+import TooltipMini from '../basic/TooltipMini';
 
 export class APISettings extends Component {
     constructor(props) {
@@ -161,15 +162,22 @@ export class APISettings extends Component {
                                                                 }
                                                                 className="Flex-flex Flex-alignItems--center Padding-left--8"
                                                             >
-                                                                <img
-                                                                    alt="hide"
-                                                                    src="/dashboard/assets/img/hide.svg"
-                                                                    style={{
-                                                                        width:
-                                                                            '15px',
-                                                                        height:
-                                                                            '15px',
-                                                                    }}
+                                                                <TooltipMini
+                                                                    title="Hide API Key"
+                                                                    content={
+                                                                        <img
+                                                                            alt="hide"
+                                                                            src="/dashboard/assets/img/hide.svg"
+                                                                            style={{
+                                                                                width:
+                                                                                    '15px',
+                                                                                height:
+                                                                                    '15px',
+                                                                                cursor:
+                                                                                    'pointer',
+                                                                            }}
+                                                                        />
+                                                                    }
                                                                 />
                                                             </div>
                                                         </div>
