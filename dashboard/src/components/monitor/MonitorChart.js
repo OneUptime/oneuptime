f15d59fca5065829ddb14c0957c5e19a4b0989cb
Ibukun Dairo <ibukundairo@hackerbay.io>
2020-09-02 22:42:59 +0100

    bug: fix lint

--- dashboard/src/components/monitor/MonitorChart.js
+++ dashboard/src/components/monitor/MonitorChart.js
@@ -122,7 +122,7 @@ export function MonitorChart({
     const responseTime = checkLogs ? data[0].responseTime : '0';
     const monitorStatus = toPascalCase(checkLogs ? data[0].status : status);
     const uptime =
-        uptimePercent != 100 ? uptimePercent.toFixed(3) : uptimePercent;
+        uptimePercent !== 100 ? uptimePercent.toFixed(3) : uptimePercent;
 
     useEffect(() => {
         setNow(Date.now());
