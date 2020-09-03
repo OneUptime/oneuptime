7839d6e3ab33618855b6e5a1376194492023e287
Mohamed ABDELLANI <abdellani@gmail.com>
2020-09-03 20:35:59 +0100

    Copy the encryption/description function from the backend to the init-script subproject.

---
+++ init-script/util/encryptDecrypt.js
@@ -0,0 +1,35 @@
+const crypto = require('crypto');
+const EncryptionKeys = require('./encryptionKeys');
+const algorithm = EncryptionKeys.algorithm;
+const key = EncryptionKeys.key;
+const iv = EncryptionKeys.iv;
+
+module.exports = {
+    encrypt: plainText => {
+        const promise = new Promise((resolve, reject) => {
+            try {
+                const cipher = crypto.createCipheriv(algorithm, key, iv);
+                let encoded = cipher.update(plainText, 'utf8', 'hex');
+                encoded += cipher.final('hex');
+                resolve(encoded);
+            } catch (error) {
+                reject(error);
+            }
+        });
+        return promise;
+    },
+
+    decrypt: encText => {
+        const promise = new Promise((resolve, reject) => {
+            try {
+                const decipher = crypto.createDecipheriv(algorithm, key, iv);
+                let decoded = decipher.update(encText, 'hex', 'utf8');
+                decoded += decipher.final('utf8');
+                resolve(decoded);
+            } catch (error) {
+                reject(error);
+            }
+        });
+        return promise;
+    },
+};
