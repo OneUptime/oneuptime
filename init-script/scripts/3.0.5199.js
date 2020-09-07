2452390fcbf3b53c36dde4f04838b12956eaf93d
Mohamed ABDELLANI <abdellani@gmail.com>
2020-09-07 17:02:31 +0100

    Update init-script to re-encrypt the authTokens, stored in the globalConfig, and encrypted with the old IV stored in the ENV.

--- init-script/scripts/3.0.5199.js
+++ init-script/scripts/3.0.5199.js
@@ -5,15 +5,18 @@ const EncryptDecrypt = require('../util/encryptDecrypt');
 const globalconfigsCollection = 'globalconfigs';
 
 async function run() {
-    //Get All globalConfig having authToken not encrypted. 
-    const globalconfigs = await find(globalconfigsCollection, {
-        name: 'twilio',
-        'value.authentication-token': { $exists: true },
-    });
-    for (let i = 0; i < globalconfigs.length; i++) {
+    //Get all globalConfig having authToken not encrypted.
+    const globalConfigsWithPlainTextAuthTokens = await find(
+        globalconfigsCollection,
+        {
+            name: 'twilio',
+            'value.authentication-token': { $exists: true },
+        }
+    );
+    for (let i = 0; i < globalConfigsWithPlainTextAuthTokens.length; i++) {
         const iv = Crypto.randomBytes(16);
-        const globalconfig = globalconfigs[i];
-        const { value } = globalconfig;
+        const globalConfig = globalConfigsWithPlainTextAuthTokens[i];
+        const { value } = globalConfig;
         value['encrypted-authentication-token'] = await EncryptDecrypt.encrypt(
             value['authentication-token'],
             iv
@@ -22,7 +25,34 @@ async function run() {
         delete value['authentication-token'];
         await update(
             globalconfigsCollection,
-            { _id: globalconfig._id },
+            { _id: globalConfig._id },
+            { value }
+        );
+    }
+    //Get all global configs that have the authTokens encrypted with the old IV stored in the ENV.
+    const globalconfigsWithOldIVEncryptedAuthTokens = await find(
+        globalconfigsCollection,
+        {
+            name: 'twilio',
+            'value.encrypted-authentication-token': { $exists: true },
+            'value.iv': { $exists: false },
+        }
+    );
+    for (let i = 0; i < globalconfigsWithOldIVEncryptedAuthTokens.length; i++) {
+        const iv = Crypto.randomBytes(16);
+        const globalConfig = globalconfigsWithOldIVEncryptedAuthTokens[i];
+        const { value } = globalConfig;
+        const decryptedToken = await EncryptDecrypt.decrypt(
+            value['encrypted-authentication-token']
+        );
+        value['encrypted-authentication-token'] = await EncryptDecrypt.encrypt(
+            decryptedToken,
+            iv
+        );
+        value['iv'] = iv;
+        await update(
+            globalconfigsCollection,
+            { _id: globalConfig._id },
             { value }
         );
     }
