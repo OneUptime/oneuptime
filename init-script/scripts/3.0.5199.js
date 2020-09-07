9062f1680836aa03a65f177afac0a2dca3e84084
Mohamed ABDELLANI <abdellani@gmail.com>
2020-09-07 16:06:31 +0100

    Updates the init script to ecrypt the plain text authTokens, in global configs, with a unique IVs.

--- init-script/scripts/3.0.5199.js
+++ init-script/scripts/3.0.5199.js
@@ -1,19 +1,24 @@
+const Crypto = require('crypto');
 const { find, update } = require('../util/db');
 const EncryptDecrypt = require('../util/encryptDecrypt');
 
 const globalconfigsCollection = 'globalconfigs';
 
 async function run() {
+    //Get All globalConfig having authToken not encrypted. 
     const globalconfigs = await find(globalconfigsCollection, {
         name: 'twilio',
         'value.authentication-token': { $exists: true },
     });
     for (let i = 0; i < globalconfigs.length; i++) {
+        const iv = Crypto.randomBytes(16);
         const globalconfig = globalconfigs[i];
         const { value } = globalconfig;
         value['encrypted-authentication-token'] = await EncryptDecrypt.encrypt(
-            value['authentication-token']
+            value['authentication-token'],
+            iv
         );
+        value['iv'] = iv;
         delete value['authentication-token'];
         await update(
             globalconfigsCollection,
