af15ddea2a4e7e1451688d64a4ff5064e9563887
Mohamed ABDELLANI <abdellani@gmail.com>
2020-09-08 14:20:23 +0100

    Update the init script to avoid checking the global configurations having the authToken encrypted.

--- init-script/scripts/3.0.5199.js
+++ init-script/scripts/3.0.5199.js
@@ -30,33 +30,6 @@ async function run() {
             { value }
         );
     }
-    //Get all global configs that have the authTokens encrypted with the old IV stored in the ENV.
-    const globalconfigsWithOldIVEncryptedAuthTokens = await find(
-        globalconfigsCollection,
-        {
-            name: 'twilio',
-            'value.encrypted-authentication-token': { $exists: true },
-            'value.iv': { $exists: false },
-        }
-    );
-    for (let i = 0; i < globalconfigsWithOldIVEncryptedAuthTokens.length; i++) {
-        const iv = Crypto.randomBytes(16);
-        const globalConfig = globalconfigsWithOldIVEncryptedAuthTokens[i];
-        const { value } = globalConfig;
-        const decryptedToken = await EncryptDecrypt.decrypt(
-            value['encrypted-authentication-token']
-        );
-        value['encrypted-authentication-token'] = await EncryptDecrypt.encrypt(
-            decryptedToken,
-            iv
-        );
-        value['iv'] = iv;
-        await update(
-            globalconfigsCollection,
-            { _id: globalConfig._id },
-            { value }
-        );
-    }
     //Get All the custom twilio settings not having a IV
     const customTwilioSettingsArray = await find(twiliosCollection, {
         iv: { $exists: false },
