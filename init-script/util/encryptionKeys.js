2c58b51143d5fb37d202fc56942f47a2c82af1be
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-09-10 15:58:43 +0900

    Push changes.

--- init-script/util/encryptionKeys.js
+++ init-script/util/encryptionKeys.js
@@ -1,5 +1,4 @@
 module.exports = {
-    algorithm: process.env['ENCRYPTION_ALGORITHM'] || 'aes-256-cbc',
-    key: process.env['ENCRYPTION_KEY'] || 'pg#K^C$-)IGOg6DYXDGq2jdmfws0MiR7',
-    iv: '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f',
+    algorithm: 'aes-256-cbc',
+    key: process.env['ENCRYPTION_KEY']
 };
