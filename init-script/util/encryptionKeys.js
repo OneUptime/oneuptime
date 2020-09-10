a3c18a45a54df5ab9b178bbe0c07e239f80fabe1
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-09-10 16:06:32 +0900

    add iv in inti script.

--- init-script/util/encryptionKeys.js
+++ init-script/util/encryptionKeys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    algorithm: 'aes-256-cbc',
-    key: process.env['ENCRYPTION_KEY']
+    algorithm: process.env['ENCRYPTION_ALGORITHM'] || 'aes-256-cbc',
+    key: process.env['ENCRYPTION_KEY'] || 'pg#K^C$-)IGOg6DYXDGq2jdmfws0MiR7',
+    iv: '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f',
 };
