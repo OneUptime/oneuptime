fb75f0cd00fe5aa9214d416186b476516b587c2e
deityhub <judoc96@gmail.com>
2020-09-11 14:48:55 +0100

    specify categories to include in final report

--- accounts/lighthouseWorker.js
+++ accounts/lighthouseWorker.js
@@ -27,6 +27,12 @@ const config = {
         },
         // Skip the h2 audit so it doesn't lie to us. See https://github.com/GoogleChrome/lighthouse/issues/6539
         skipAudits: ['uses-http2'],
+        onlyCategories: [
+            'performance',
+            'accessibility',
+            'best-practices',
+            'seo',
+        ],
     },
 };
 
