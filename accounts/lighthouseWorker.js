caceddb9f2f8fa1de36b51ce099938ded6d44f12
deityhub <judoc96@gmail.com>
2020-09-14 08:38:07 +0100

    debugging

--- accounts/lighthouseWorker.js
+++ accounts/lighthouseWorker.js
@@ -46,7 +46,7 @@ function launchChromeAndRunLighthouse(url, flags = {}, config = null) {
 }
 
 const flags = {
-    chromeFlags: ['--headless', '--no-sandbox'],
+    chromeFlags: ['--no-sandbox'],
     emulatedFormFactor: 'desktop',
 };
 
@@ -57,21 +57,21 @@ process.on('message', function(data) {
     spinner.color = 'green';
     launchChromeAndRunLighthouse(data.url, flags, config)
         .then(results => {
-            // results.artifacts = 'ignore';
-            // results.reportGroups = 'ignore';
-            // results.timing = 'ignore';
-            // results.userAgent = 'ignore';
-            // results.lighthouseVersion = 'ignore';
-            // results.runWarnings = 'runWarnings';
-            // results.report = 'ignore';
-            // results.runtimeConfig = 'ignore';
+            results.artifacts = 'ignore';
+            results.reportGroups = 'ignore';
+            results.timing = 'ignore';
+            results.userAgent = 'ignore';
+            results.lighthouseVersion = 'ignore';
+            results.runWarnings = 'runWarnings';
+            results.report = 'ignore';
+            results.runtimeConfig = 'ignore';
 
-            // results.lhr.userAgent = 'ignore';
-            // results.lhr.environment = 'ignore';
-            // results.lhr.configSettings = 'ignore';
-            // results.lhr.metrics = 'ignore';
-            // results.lhr.audits = 'ignore';
-            // results.lhr.categoryGroups = 'ignore';
+            results.lhr.userAgent = 'ignore';
+            results.lhr.environment = 'ignore';
+            results.lhr.configSettings = 'ignore';
+            results.lhr.metrics = 'ignore';
+            results.lhr.audits = 'ignore';
+            results.lhr.categoryGroups = 'ignore';
 
             scores.performance = Math.ceil(
                 results.lhr.categories.performance.score * 100
