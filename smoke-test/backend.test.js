593dee96b257c776be716949d8bbc8a669ad9ee2
deityhub <judoc96@gmail.com>
2020-09-16 12:11:57 +0100

    fix unending test suite for smoke-test

--- smoke-test/backend.test.js
+++ smoke-test/backend.test.js
@@ -4,17 +4,19 @@ const puppeteer = require('puppeteer');
 let page, browser;
 
 describe('Check Backend', () => {
-    beforeAll(async () => {
+    beforeAll(async done => {
         jest.setTimeout(30000);
         browser = await puppeteer.launch(utils.puppeteerLaunchConfig);
         page = await browser.newPage();
+        done();
     });
 
-    afterAll(async () => {
+    afterAll(async done => {
         await browser.close();
+        done();
     });
 
-    test('should get status ok from backend', async () => {
+    test('should get status ok from backend', async done => {
         await page.goto(utils.BACKEND_URL, {
             waitUntil: 'networkidle0',
         });
@@ -24,5 +26,6 @@ describe('Check Backend', () => {
         expect(response).toBe(
             '{"status":200,"message":"Service Status - OK","serviceType":"fyipe-api"}'
         );
+        done();
     });
 });
--- smoke-test/server.test.enterprise.js
+++ smoke-test/server.test.enterprise.js
@@ -4,16 +4,18 @@ const puppeteer = require('puppeteer');
 let page, browser;
 
 describe('Check Enterprise Server', () => {
-    beforeAll(async () => {
+    beforeAll(async done => {
         browser = await puppeteer.launch(utils.puppeteerLaunchConfig);
         page = await browser.newPage();
+        done();
     });
 
-    afterAll(async () => {
+    afterAll(async done => {
         await browser.close();
+        done();
     });
 
-    test('should get saas status false from server', async () => {
+    test('should get saas status false from server', async done => {
         await page.goto(`${utils.BACKEND_URL}/server/is-saas-service`, {
             waitUntil: 'networkidle0',
         });
@@ -21,5 +23,6 @@ describe('Check Enterprise Server', () => {
             return e.innerHTML;
         });
         expect(response).toBe('{"result":false}');
+        done();
     });
 });
