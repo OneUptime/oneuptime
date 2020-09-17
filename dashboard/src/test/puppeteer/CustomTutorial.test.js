927a74235281f1ee90baa304136303d59c7b480e
Zadat Olayinka <bundayy@outlook.com>
2020-09-17 17:58:44 +0100

    restored custom tutorial tests

--- dashboard/src/test/puppeteer/CustomTutorial.test.js
+++ dashboard/src/test/puppeteer/CustomTutorial.test.js
@@ -106,50 +106,50 @@ describe('Custom Tutorial With SubProjects', () => {
         },
         operationTimeOut
     );
-    // test(
-    //     'Should show indicator on how to create monitor, and after closing, it should not reapprear',
-    //     async () => {
-    //         return await cluster.execute(null, async ({ page }) => {
-    //             await init.loginUser(user, page);
-    //             const customTutorialType = 'monitor';
-    //             // Navigate to home page
-    //             await page.goto(utils.DASHBOARD_URL, {
-    //                 waitUntil: 'networkidle0',
-    //             });
-    //             await page.waitFor(5000);
-    //             const componentBoxElement = await page.waitForSelector(
-    //                 `#info-${customTutorialType}`
-    //             );
-    //             expect(componentBoxElement).toBeDefined();
+    test(
+        'Should show indicator on how to create monitor, and after closing, it should not reapprear',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                await init.loginUser(user, page);
+                const customTutorialType = 'monitor';
+                // Navigate to home page
+                await page.goto(utils.DASHBOARD_URL, {
+                    waitUntil: 'networkidle0',
+                });
+                await page.waitFor(5000);
+                const componentBoxElement = await page.waitForSelector(
+                    `#info-${customTutorialType}`
+                );
+                expect(componentBoxElement).toBeDefined();
 
-    //             // click on the call to action button
-    //             await page.waitForSelector(`#close-${customTutorialType}`);
-    //             await page.click(`#close-${customTutorialType}`);
-    //         });
-    //     },
-    //     operationTimeOut
-    // );
-    // test(
-    //     'Should show indicator on how to invite team member, and after closing, it should not reapprear',
-    //     async () => {
-    //         return await cluster.execute(null, async ({ page }) => {
-    //             await init.loginUser(user, page);
-    //             const customTutorialType = 'teamMember';
-    //             // Navigate to home page
-    //             await page.goto(utils.DASHBOARD_URL, {
-    //                 waitUntil: 'networkidle0',
-    //             });
-    //             await page.waitFor(5000);
-    //             const componentBoxElement = await page.waitForSelector(
-    //                 `#info-${customTutorialType}`
-    //             );
-    //             expect(componentBoxElement).toBeDefined();
+                // click on the call to action button
+                await page.waitForSelector(`#close-${customTutorialType}`);
+                await page.click(`#close-${customTutorialType}`);
+            });
+        },
+        operationTimeOut
+    );
+    test(
+        'Should show indicator on how to invite team member, and after closing, it should not reapprear',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                await init.loginUser(user, page);
+                const customTutorialType = 'teamMember';
+                // Navigate to home page
+                await page.goto(utils.DASHBOARD_URL, {
+                    waitUntil: 'networkidle0',
+                });
+                await page.waitFor(5000);
+                const componentBoxElement = await page.waitForSelector(
+                    `#info-${customTutorialType}`
+                );
+                expect(componentBoxElement).toBeDefined();
 
-    //             // click on the call to action button
-    //             await page.waitForSelector(`#close-${customTutorialType}`);
-    //             await page.click(`#close-${customTutorialType}`);
-    //         });
-    //     },
-    //     operationTimeOut
-    // );
+                // click on the call to action button
+                await page.waitForSelector(`#close-${customTutorialType}`);
+                await page.click(`#close-${customTutorialType}`);
+            });
+        },
+        operationTimeOut
+    );
 });
