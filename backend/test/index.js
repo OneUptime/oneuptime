8faad974b6f7b26ffffd98742cc8f53e9148378e
Zadat Olayinka <bundayy@outlook.com>
2020-09-21 12:00:08 +0100

    webhook typo fix

--- backend/test/index.js
+++ backend/test/index.js
@@ -38,7 +38,7 @@ try {
     require('./version.test');
     require('./applicationLog.test');
     // require('./slack.test.txt');
-    require('./webhooks.test');
+    require('./webhook.test');
     require('./zapier.test');
     require('./rateLimit.test');
 } catch (error) {
