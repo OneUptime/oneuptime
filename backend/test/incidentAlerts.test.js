48110ac274502daeafbf66b2b9f9408be3d5605a
Mohamed ABDELLANI <abdellani@gmail.com>
2020-10-01 18:31:33 +0100

    Implement the test 'should send SMS/Call alerts to on-call teams and subscribers if the SMS/Call alerts are enabled globally and for the project.'

--- backend/test/incidentAlerts.test.js
+++ backend/test/incidentAlerts.test.js
@@ -3,6 +3,7 @@ process.env.PORT = 3020;
 process.env.IS_SAAS_SERVICE = true;
 let userData = require('./data/user');
 let chai = require('chai');
+const expect = require('chai').expect;
 chai.use(require('chai-http'));
 chai.use(require('chai-subset'));
 let app = require('../server');
@@ -18,10 +19,14 @@ let AirtableService = require('../backend/services/airtableService');
 
 let VerificationTokenModel = require('../backend/models/verificationToken');
 let UserModel = require('../backend/models/user');
+let GlobalConfigModel = require('../backend/models/globalConfig');
+
+const sleep = waitTimeInMs =>
+  new Promise(resolve => setTimeout(resolve, waitTimeInMs));
 
 let authorization, token, userId, airtableId, projectId, componentId, monitorId, scheduleId;
 
-describe('Monitor API', function () {
+describe('Incident Alerts', function () {
   this.timeout(30000);
 
   before(function (done) {
@@ -68,6 +73,7 @@ describe('Monitor API', function () {
                       data: {},
                     });
                   componentId = component.body._id;
+
                   const monitor = await request
                     .post(`/monitor/${projectId}`)
                     .set('Authorization', authorization)
@@ -83,6 +89,11 @@ describe('Monitor API', function () {
                   monitorId = monitor.body._id;
 
                   await request
+                    .post(`/stripe/${projectId}/addBalance`)
+                    .set('Authorization', authorization)
+                    .send({ rechargeBalanceAmount: "2000" });
+
+                  await request
                     .post(`/subscriber/${projectId}/subscribe/${monitorId}`)
                     .set('Authorization', authorization)
                     .send({
@@ -90,15 +101,18 @@ describe('Monitor API', function () {
                       contactPhone: "9173976235",
                       countryCode: "us",
                     });
+
                   const schedule = await request
                     .post(`/schedule/${projectId}`)
                     .set('Authorization', authorization)
                     .send({ name: "test schedule" })
                   scheduleId = schedule.body._id;
+
                   await request
                     .put(`/schedule/${projectId}/${scheduleId}`)
                     .set('Authorization', authorization)
-                    .send({monitorIds:[monitorId]});
+                    .send({ monitorIds: [monitorId] });
+
                   await request
                     .post(`/schedule/${projectId}/${scheduleId}/addescalation`)
                     .set('Authorization', authorization)
@@ -125,8 +139,7 @@ describe('Monitor API', function () {
                           }
                         ]
                       }]
-                    )
-
+                    );
                   done();
                 });
             });
@@ -144,6 +157,103 @@ describe('Monitor API', function () {
     await UserService.hardDeleteBy({ _id: userId });
     await NotificationService.hardDeleteBy({ projectId: projectId });
     await AirtableService.deleteUser(airtableId);
+  });
+
+  describe('Global twilio credentials set (Custom twilio settings not set)', async () => {
+    /**
+     * Global twilio settings: set
+     * Custom twilio settings: not set
+     * Global twilio settings (SMS/Call) enable : true
+     * SMS/Call alerts enabled for the project (billing): true
+     */
+
+    it('should send SMS/Call alerts to on-call teams and subscribers if the SMS/Call alerts are enabled globally and for the project.', async function () {
+      const globalSettings = await GlobalConfigModel.findOne(
+        { name: 'twilio' },
+      );
+      const { value } = globalSettings;
+      value['sms-enabled'] = true;
+      value['call-enabled'] = true;
+      await GlobalConfigModel.findOneAndUpdate(
+        { name: 'twilio' },
+        { value },
+      );
+
+      const billingEndpointResponse = await request
+        .put(`/project/${projectId}/alertOptions`)
+        .set('Authorization', authorization)
+        .send({
+          alertEnable: true,
+          billingNonUSCountries: true,
+          billingRiskCountries: true,
+          billingUS: true,
+          minimumBalance: "100",
+          rechargeToBalance: "200",
+          _id: projectId,
+        });
+      expect(billingEndpointResponse).to.have.status(200);
+
+      const incidentCreationEndpointResponse = await request
+        .post(`/incident/${projectId}/${monitorId}`)
+        .set('Authorization', authorization)
+        .send({
+          monitorId,
+          projectId,
+          title: "test monitor  is offline.",
+          incidentType: "offline",
+          description: 'Incident description',
+        });
+      expect(incidentCreationEndpointResponse).to.have.status(200);
+
+      const { _id: incidentId } = incidentCreationEndpointResponse.body
+
+      const incidentResolveEndpointResponse = await request
+        .post(`/incident/${projectId}/resolve/${incidentId}`)
+        .set('Authorization', authorization);
+
+      expect(incidentResolveEndpointResponse).to.have.status(200);
+
+      await sleep(10 * 1000);
+
+      const subscribersAlertsEndpointReponse = await request
+        .get(`/subscriberAlert/${projectId}/incident/${incidentId}?skip=0&limit=999`)
+        .set('Authorization', authorization);
+
+      expect(subscribersAlertsEndpointReponse).to.have.status(200);
+      expect(subscribersAlertsEndpointReponse.body).to.an('object');
+      expect(subscribersAlertsEndpointReponse.body.count).to.equal(2);
+      expect(subscribersAlertsEndpointReponse.body.data).to.an('array');
+      expect(subscribersAlertsEndpointReponse.body.data.length).to.equal(2);
+
+      const eventTypesSent = []
+      for (const event of subscribersAlertsEndpointReponse.body.data) {
+        const { alertStatus, alertVia, eventType } = event;
+        eventTypesSent.push(eventType);
+        expect(alertStatus).to.equal('Success');
+        expect(alertVia).to.equal('sms');
+      }
+      expect(eventTypesSent.includes('resolved')).to.equal(true);
+      expect(eventTypesSent.includes('identified')).to.equal(true);
+
+      const oncallAlertsEndpointReponse = await request
+        .get(`/alert/${projectId}/incident/${incidentId}?skip=0&limit=999`)
+        .set('Authorization', authorization);
+
+      expect(oncallAlertsEndpointReponse).to.have.status(200);
+      expect(oncallAlertsEndpointReponse.body).to.an('object');
+      expect(oncallAlertsEndpointReponse.body.count).to.equal(2);
+      expect(oncallAlertsEndpointReponse.body.data).to.an('array');
+      expect(oncallAlertsEndpointReponse.body.data.length).to.equal(2);
+      const alertsSentList = [];
+      for (const event of oncallAlertsEndpointReponse.body.data) {
+        const { alertVia, alertStatus, error } = event;
+        expect(alertStatus).to.equal('Success');
+        expect(error).to.equal(false)
+        alertsSentList.push(alertVia)
+      }
+      expect(alertsSentList.includes('sms')).to.equal(true);
+      expect(alertsSentList.includes('call')).to.equal(true);
+    });
   });
 
 });
