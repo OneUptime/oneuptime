7640c8877209059ba772dc89cfccbc6c25c03de5
Nawaz Dhandala <nawazdhandala@outlook.com>
2020-10-05 11:16:09 +0900

    Delete HTML cache form service worker.

--- dashboard/src/serviceWorker.js
+++ dashboard/src/serviceWorker.js
@@ -66,6 +66,7 @@ function registerValidSW(swUrl, config) {
                         // Return true if you want to remove this cache,
                         // but remember that caches are shared across
                         // the whole origin
+                        return true;
                     }).map(function (cacheName) {
                         return caches.delete(cacheName);
                     })
