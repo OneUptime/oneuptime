9e8081eb949a142c240a9894fc1abc1eef4465b1
deityhub <judoc96@gmail.com>
2020-10-10 12:56:56 +0100

    add statusPageUrl to sms template

--- backend/backend/config/smsTemplateVariables.js
+++ backend/backend/config/smsTemplateVariables.js
@@ -5,6 +5,7 @@ module.exports = {
         '{{projectName}} : Name of the project on which the incident has occured.',
         '{{incidentType}} : Type of incident.',
         '{{componentName}} : Name of the component the monitor belongs to',
+        '{{statusPageUrl}} : Url of the status page. If there is a verified domain, it will be used',
     ],
     'Subscriber Incident Acknowldeged': [
         '{{incidentTime}} : Time at which this incident occured.',
@@ -12,6 +13,7 @@ module.exports = {
         '{{projectName}} : Name of the project on which the incident has occured.',
         '{{incidentType}} : Type of incident.',
         '{{componentName}} : Name of the component the monitor belongs to',
+        '{{statusPageUrl}} : Url of the status page. If there is a verified domain, it will be used',
     ],
     'Subscriber Incident Resolved': [
         '{{incidentTime}} : Time at which this incident occured.',
@@ -19,5 +21,6 @@ module.exports = {
         '{{projectName}} : Name of the project on which the incident has occured.',
         '{{incidentType}} : Type of incident.',
         '{{componentName}} : Name of the component the monitor belongs to',
+        '{{statusPageUrl}} : Url of the status page. If there is a verified domain, it will be used',
     ],
 };
