9056c62118903be37bbfbdea4918a0d5142a8008
Mohamed ABDELLANI <abdellani@gmail.com>
2020-10-19 10:00:54 +0100

    Implement mailService.hasCustomSmtpSetting

--- backend/backend/services/mailService.js
+++ backend/backend/services/mailService.js
@@ -1704,6 +1704,13 @@ const _this = {
             throw error;
         }
     },
+    hasCustomSmtpSettings: async(projectId) => {
+        const smtpConfigurations = await EmailSmtpService.findOneBy({
+            projectId,
+            enabled: true,
+        });
+        return Object.keys(smtpConfigurations).length ? smtpConfigurations :false;
+    }
 };
 
 module.exports = _this;
