c92de80f50e82ca69fe734279e9eca7732749279
augustine igwe <augustusbest00@gmail.com>
2020-10-22 10:36:37 +0100

    i dynamically designed how the webhook should look from the slack service UI

--- backend/backend/services/webHookService.js
+++ backend/backend/services/webHookService.js
@@ -1,10 +1,12 @@
 module.exports = {
     // process messages to be sent to slack workspace channels
-    sendNotification: async function(
+    sendNotification: async function (
         projectId,
         incident,
         monitor,
-        incidentStatus
+        incidentStatus,
+        component,
+        duration
     ) {
         try {
             const self = this;
@@ -47,7 +49,9 @@ module.exports = {
                     monitor,
                     incident,
                     integration,
-                    monitorStatus ? monitorStatus.status : null
+                    monitorStatus ? monitorStatus.status : null,
+                    component,
+                    duration
                 );
             }
             return response;
@@ -58,8 +62,125 @@ module.exports = {
     },
 
     // send notification to slack workspace channels
-    async notify(project, monitor, incident, integration, monitorStatus) {
+    async notify(project, monitor, incident, integration, monitorStatus, component, duration) {
         try {
+            const uri = `${global.dashboardHost}/project/${component.projectId._id}/${component._id}/incidents/${incident._id}`;
+            const yellow = '#fedc56';
+            const green = '#028A0F';
+            let payload;
+            if (incident.resolved) {
+                payload = {
+                    attachments: [
+                        {
+                            color: green,
+                            title: `Incident Resolved`,
+                            title_link: uri,
+                            text: `Incident on *${component.name} / ${
+                                monitor.name
+                                }* is resolved by ${
+                                incident.resolvedBy
+                                    ? incident.resolvedBy.name
+                                    : 'Fyipe'
+                                } after being ${
+                                incident.incidentType
+                                } for ${duration}`,
+                        },
+                    ],
+                };
+            } else if (incident.acknowledged) {
+                payload = {
+                    attachments: [
+                        {
+                            color: yellow,
+                            title: `Incident Acknowledged`,
+                            title_link: uri,
+                            text: `Incident on *${component.name} / ${
+                                monitor.name
+                                }* is acknowledged by ${
+                                incident.acknowledgedBy
+                                    ? incident.acknowledgedBy.name
+                                    : 'Fyipe'
+                                } after being ${
+                                incident.incidentType
+                                } for ${duration}`,
+                        },
+                    ],
+                };
+            } else {
+                payload = {
+                    attachments: [
+                        {
+                            color:
+                                incident.incidentType === 'online'
+                                    ? green
+                                    : incident.incidentType === 'degraded'
+                                        ? yellow
+                                        : '#f00',
+                            title: `New ${incident.incidentType} incident for ${monitor.name}`,
+                            title_link: uri,
+                            fields: [
+                                {
+                                    title: 'Project Name:',
+                                    value: project.name,
+                                    short: true,
+                                },
+                                {
+                                    title: 'Monitor Name:',
+                                    value: `${component.name} / ${monitor.name}`,
+                                    short: true,
+                                },
+                                ...(incident.title
+                                    ? [
+                                        {
+                                            title: 'Incident Title:',
+                                            value: incident.title,
+                                            short: true,
+                                        },
+                                    ]
+                                    : []),
+                                ...(incident.description
+                                    ? [
+                                        {
+                                            title: 'Incident Description:',
+                                            value: incident.description,
+                                            short: true,
+                                        },
+                                    ]
+                                    : []),
+                                ...(incident.incidentPriority
+                                    ? [
+                                        {
+                                            title: 'Incident Priority:',
+                                            value:
+                                                incident.incidentPriority
+                                                    .name,
+                                            short: true,
+                                        },
+                                    ]
+                                    : []),
+                                {
+                                    title: 'Created By:',
+                                    value: incident.createdById
+                                        ? incident.createdById.name
+                                        : 'Fyipe',
+                                    short: true,
+                                },
+                                {
+                                    title: 'Incident Status:',
+                                    value:
+                                        incident.incidentType === 'online'
+                                            ? 'Online'
+                                            : incident.incidentType ===
+                                                'degraded'
+                                                ? 'Degraded'
+                                                : 'Offline',
+                                    short: true,
+                                },
+                            ],
+                        },
+                    ],
+                };
+            }
             const data = {
                 monitorName: monitor.name,
                 monitorId: monitor._id,
@@ -96,7 +217,7 @@ module.exports = {
                 await axios.post(
                     integration.data.endpoint,
                     {
-                        ...data,
+                        ...payload
                     },
                     {
                         headers: {
@@ -109,11 +230,11 @@ module.exports = {
             } else {
                 const error = new Error('Webhook endpoint type missing');
                 error.code = 400;
-                ErrorService.log('WebHookService.notify', error);
+                ErrorService.log('WebHookService.notify', 'error');
                 throw error;
             }
         } catch (error) {
-            ErrorService.log('WebHookService.notify', error);
+            ErrorService.log('WebHookService.notify', 'error');
             throw error;
         }
     },
