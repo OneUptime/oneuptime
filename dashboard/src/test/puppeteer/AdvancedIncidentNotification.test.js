403e9a40be5febef5959512183b3df9c40b13fc0
deityhub <judoc96@gmail.com>
2020-10-30 23:00:19 +0100

    frontend test for advanced options for incident sms notification

--- dashboard/src/test/puppeteer/AdvancedIncidentNotification.test.js
+++ dashboard/src/test/puppeteer/AdvancedIncidentNotification.test.js
@@ -212,4 +212,173 @@ describe('Project Settings Page - (Email and SMS & Calls)', () => {
         },
         operationTimeOut
     );
+
+    test(
+        'should enable sending sms notification when incident is created, acknowledged or resolved',
+        async done => {
+            await cluster.execute(null, async ({ page }) => {
+                await page.goto(utils.DASHBOARD_URL);
+
+                await page.waitForSelector('#projectSettings', {
+                    visible: true,
+                });
+                await page.click('#projectSettings');
+                await page.waitForSelector('#smsCalls', { visible: true });
+                await page.click('#smsCalls');
+
+                await page.waitForSelector('#createdIncidentNotification', {
+                    visible: true,
+                });
+                let sendCreatedIncidentNotification = await page.$eval(
+                    '#createdIncidentNotification',
+                    elem => elem.value
+                );
+                let sendAcknowledgedIncidentNotification = await page.$eval(
+                    '#acknowledgedIncidentNotification',
+                    elem => elem.value
+                );
+                let sendResolvedIncidentNotification = await page.$eval(
+                    '#resolvedIncidentNotification',
+                    elem => elem.value
+                );
+                sendCreatedIncidentNotification = utils.parseBoolean(
+                    sendCreatedIncidentNotification
+                );
+                sendAcknowledgedIncidentNotification = utils.parseBoolean(
+                    sendAcknowledgedIncidentNotification
+                );
+                sendResolvedIncidentNotification = utils.parseBoolean(
+                    sendResolvedIncidentNotification
+                );
+                expect(sendCreatedIncidentNotification).toBeTruthy();
+                expect(sendAcknowledgedIncidentNotification).toBeTruthy();
+                expect(sendResolvedIncidentNotification).toBeTruthy();
+            });
+            done();
+        },
+        operationTimeOut
+    );
+
+    test(
+        'should disable sending sms notification when incident is created',
+        async done => {
+            await cluster.execute(null, async ({ page }) => {
+                await page.goto(utils.DASHBOARD_URL);
+
+                await page.waitForSelector('#projectSettings', {
+                    visible: true,
+                });
+                await page.click('#projectSettings');
+                await page.waitForSelector('#smsCalls', { visible: true });
+                await page.click('#smsCalls');
+
+                await page.waitForSelector('#createdIncidentNotification', {
+                    visible: true,
+                });
+                await page.$eval('#createdIncidentNotification', elem =>
+                    elem.click()
+                );
+                await page.click('#saveIncidentNotification');
+                await page.waitForSelector('.ball-beat', { visible: true });
+                await page.waitForSelector('.ball-beat', { hidden: true });
+
+                await page.reload({ waitUntil: 'networkidle0' });
+                await page.waitForSelector('#createdIncidentNotification', {
+                    visible: true,
+                });
+                let checkedState = await page.$eval(
+                    '#createdIncidentNotification',
+                    elem => elem.value
+                );
+                checkedState = utils.parseBoolean(checkedState);
+                expect(checkedState).toBeFalsy();
+            });
+            done();
+        },
+        operationTimeOut
+    );
+
+    test(
+        'should disable sending sms notification when incident is acknowledged',
+        async done => {
+            await cluster.execute(null, async ({ page }) => {
+                await page.goto(utils.DASHBOARD_URL);
+
+                await page.waitForSelector('#projectSettings', {
+                    visible: true,
+                });
+                await page.click('#projectSettings');
+                await page.waitForSelector('#smsCalls', { visible: true });
+                await page.click('#smsCalls');
+
+                await page.waitForSelector(
+                    '#acknowledgedIncidentNotification',
+                    {
+                        visible: true,
+                    }
+                );
+                await page.$eval('#acknowledgedIncidentNotification', elem =>
+                    elem.click()
+                );
+                await page.click('#saveIncidentNotification');
+                await page.waitForSelector('.ball-beat', { visible: true });
+                await page.waitForSelector('.ball-beat', { hidden: true });
+
+                await page.reload({ waitUntil: 'networkidle0' });
+                await page.waitForSelector(
+                    '#acknowledgedIncidentNotification',
+                    {
+                        visible: true,
+                    }
+                );
+                let checkedState = await page.$eval(
+                    '#acknowledgedIncidentNotification',
+                    elem => elem.value
+                );
+                checkedState = utils.parseBoolean(checkedState);
+                expect(checkedState).toBeFalsy();
+            });
+            done();
+        },
+        operationTimeOut
+    );
+
+    test(
+        'should disable sending sms notification when incident is resolved',
+        async done => {
+            await cluster.execute(null, async ({ page }) => {
+                await page.goto(utils.DASHBOARD_URL);
+
+                await page.waitForSelector('#projectSettings', {
+                    visible: true,
+                });
+                await page.click('#projectSettings');
+                await page.waitForSelector('#smsCalls', { visible: true });
+                await page.click('#smsCalls');
+
+                await page.waitForSelector('#resolvedIncidentNotification', {
+                    visible: true,
+                });
+                await page.$eval('#resolvedIncidentNotification', elem =>
+                    elem.click()
+                );
+                await page.click('#saveIncidentNotification');
+                await page.waitForSelector('.ball-beat', { visible: true });
+                await page.waitForSelector('.ball-beat', { hidden: true });
+
+                await page.reload({ waitUntil: 'networkidle0' });
+                await page.waitForSelector('#resolvedIncidentNotification', {
+                    visible: true,
+                });
+                let checkedState = await page.$eval(
+                    '#resolvedIncidentNotification',
+                    elem => elem.value
+                );
+                checkedState = utils.parseBoolean(checkedState);
+                expect(checkedState).toBeFalsy();
+            });
+            done();
+        },
+        operationTimeOut
+    );
 });
