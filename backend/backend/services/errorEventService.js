c626d243773ec3236f8b021baaf55b47ac19a4c8
Zadat Olayinka <bundayy@outlook.com>
2020-11-03 17:21:41 +0100

    create and find one by error event

---
+++ backend/backend/services/errorEventService.js
@@ -0,0 +1,58 @@
+module.exports = {
+    create: async function(data) {
+        try {
+            const _this = this;
+
+            // prepare error event model
+            let errorEvent = new ErrorEventModel();
+
+            errorEvent.content = data.exception;
+            errorEvent.device = data.deviceDetails;
+            errorEvent.tags = data.tags;
+            errorEvent.createdById = data.createdById;
+            errorEvent.type = data.type;
+
+            // generate hash from fingerprint
+            const hash = await bcrypt.hash(
+                data.fingerprint.join(''), // join all fingerprints as a string
+                constants.saltRounds
+            );
+            errorEvent.fingerprintHash = hash;
+            errorEvent.fingerprint = data.fingerprint;
+
+            // set error trackerid
+            errorEvent.errorTrackerId = data.errorTrackerId;
+
+            const savedErrorEvent = await errorEvent.save();
+            errorEvent = await _this.findOneBy({
+                _id: savedErrorEvent._id,
+            });
+            return errorEvent;
+        } catch (error) {
+            ErrorService.log('errorEventService.create', error);
+            throw error;
+        }
+    },
+    async findOneBy(query) {
+        try {
+            if (!query) {
+                query = {};
+            }
+
+            if (!query.deleted) query.deleted = false;
+            const errorEvent = await ErrorEventModel.findOne(query).populate(
+                'errorTrackerId',
+                'name'
+            );
+            return errorEvent;
+        } catch (error) {
+            ErrorService.log('errorEventService.findOneBy', error);
+            throw error;
+        }
+    },
+};
+
+const ErrorEventModel = require('../models/errorEvent');
+const ErrorService = require('./errorService');
+const bcrypt = require('bcrypt');
+const constants = require('../config/constants.json');
