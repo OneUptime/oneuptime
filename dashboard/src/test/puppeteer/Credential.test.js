3d28a3d90ab8369cd68adc286fe2600fb7ea1c0c
Mohamed ABDELLANI <abdellani@gmail.com>
2020-11-03 10:37:24 +0100

    Fix test 'should cancel adding a git credential to a project'

--- dashboard/src/test/puppeteer/Credential.test.js
+++ dashboard/src/test/puppeteer/Credential.test.js
@@ -74,7 +74,7 @@ describe('Credential Page', () => {
                 await page.waitForSelector('#gitCredentialForm', {
                     visible: true,
                 });
-                await page.click('#cancelCredentialModalBtn');
+                await page.$eval('#cancelCredentialModalBtn', e => e.click());
 
                 await page.waitForSelector('#gitCredentialForm', {
                     hidden: true,
