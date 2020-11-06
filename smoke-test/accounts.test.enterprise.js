7540a3ea0b1b58c61bb01192f7397c869f4f31e7
deityhub <judoc96@gmail.com>
2020-11-06 10:54:07 +0100

    fix enterprise account test for e2e_docker_compose_enterprise_test

--- smoke-test/accounts.test.enterprise.js
+++ smoke-test/accounts.test.enterprise.js
@@ -78,7 +78,6 @@ describe('Enterprise Accounts API', () => {
                     return json;
                 });
 
-                await page.waitFor(10000);
                 localStorageData.should.have.property('access_token');
                 localStorageData.should.have.property('email', email);
                 page.url().should.containEql(utils.DASHBOARD_URL);
