c100f3d0c6119cf33defbe57fc39df25cff4b7d3
Mohamed ABDELLANI <abdellani@gmail.com>
2020-11-07 19:26:36 +0100

    Fix test: 'should create an incident in parent project for valid `admin`'

--- dashboard/src/test/puppeteer/IncidentSubProject.test.js
+++ dashboard/src/test/puppeteer/IncidentSubProject.test.js
@@ -111,17 +111,17 @@ describe('Incident API With SubProjects', () => {
                 });
                 await page.$eval('#closeIncident_0', elem => elem.click());
 
-                await page.waitForSelector('#incident_span_0', {
+                await page.waitForSelector('#incident_0', {
                     visible: true,
                 });
-                const incidentTitleSelector = await page.$('#incident_span_0');
+                const incidentTitleSelector = await page.$('#incident_0  .bs-font-header');
 
                 let textContent = await incidentTitleSelector.getProperty(
                     'innerText'
                 );
                 textContent = await textContent.jsonValue();
                 expect(textContent.toLowerCase()).toEqual(
-                    `${projectMonitorName}'s Incident Status`.toLowerCase()
+                    `${projectMonitorName} is offline`.toLowerCase()
                 );
                 await init.logout(page);
             });
