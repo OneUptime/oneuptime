6f383d88b11bae9ab9e03211186b3498dc26f007
Mohamed ABDELLANI <abdellani@gmail.com>
2020-11-10 00:33:25 +0100

    Fix test 'should delete message related to internal incident message thread'

--- dashboard/src/test/puppeteer/IncidentTimeline.test.js
+++ dashboard/src/test/puppeteer/IncidentTimeline.test.js
@@ -350,7 +350,7 @@ describe('Incident Timeline API', () => {
                 await page.click(`#more-details-${projectMonitorName}`);
 
                 await page.waitForSelector(`#incident_${projectMonitorName}_0`);
-                await page.click(`#incident_${projectMonitorName}_0`);
+                await page.$eval(`#incident_${projectMonitorName}_0`, e=> e.click());
 
                 // click on incident notes tab
                 await init.gotoTab(
