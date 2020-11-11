1add5ff03024972309d91a9d65cf188788a9c1cb
Zadat Olayinka <bundayy@outlook.com>
2020-11-11 13:09:41 +0100

    browser + operating system + sdk version

--- dashboard/src/components/errorTracker/ErrorEventDetail.js
+++ dashboard/src/components/errorTracker/ErrorEventDetail.js
@@ -592,7 +592,6 @@ class ErrorEventDetail extends Component {
                                                     </tr>
                                                 </thead>
                                                 <tbody className="Table-body">
-                                
                                                     <tr>
                                                         <td>
                                                             <div className="db-ListViewItem-link">
@@ -802,6 +801,143 @@ class ErrorEventDetail extends Component {
                                                     </tr>
                                                 </tbody>
                                             </table>
+                                        </div>
+                                    </div>
+                                    <div className="Box-divider--border-top-1 Padding-vertical--20">
+                                        <div className="Flex-flex Flex-justifyContent--spaceBetween">
+                                            <p className="SubHeader">Browser</p>
+                                        </div>
+                                        <div className="Margin-vertical--8">
+                                            <div className="Flex-flex Margin-vertical--4 ">
+                                                <span
+                                                    className="Flex-flex Flex-alignItems--center"
+                                                    style={{ width: '25%' }}
+                                                >
+                                                    Brand
+                                                </span>
+                                                <span
+                                                    style={{
+                                                        backgroundColor:
+                                                            '#F7F8F9',
+                                                        padding: '15px',
+                                                        width: '75%',
+                                                    }}
+                                                >
+                                                    {' '}
+                                                    Chrome Mobile
+                                                </span>
+                                            </div>
+                                            <div className="Flex-flex Margin-vertical--4">
+                                                <span
+                                                    className="Flex-flex Flex-alignItems--center"
+                                                    style={{ width: '25%' }}
+                                                >
+                                                    Version
+                                                </span>
+                                                <span
+                                                    style={{
+                                                        backgroundColor:
+                                                            '#F7F8F9',
+                                                        padding: '15px',
+                                                        width: '75%',
+                                                    }}
+                                                >
+                                                    {' '}
+                                                    85.9.100
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="Box-divider--border-top-1 Padding-vertical--20">
+                                        <div className="Flex-flex Flex-justifyContent--spaceBetween">
+                                            <p className="SubHeader">
+                                                Operating System
+                                            </p>
+                                        </div>
+                                        <div className="Margin-vertical--8">
+                                            <div className="Flex-flex Margin-vertical--4 ">
+                                                <span
+                                                    className="Flex-flex Flex-alignItems--center"
+                                                    style={{ width: '25%' }}
+                                                >
+                                                    Brand
+                                                </span>
+                                                <span
+                                                    style={{
+                                                        backgroundColor:
+                                                            '#F7F8F9',
+                                                        padding: '15px',
+                                                        width: '75%',
+                                                    }}
+                                                >
+                                                    {' '}
+                                                    Nexus 5
+                                                </span>
+                                            </div>
+                                            <div className="Flex-flex Margin-vertical--4">
+                                                <span
+                                                    className="Flex-flex Flex-alignItems--center"
+                                                    style={{ width: '25%' }}
+                                                >
+                                                    Family
+                                                </span>
+                                                <span
+                                                    style={{
+                                                        backgroundColor:
+                                                            '#F7F8F9',
+                                                        padding: '15px',
+                                                        width: '75%',
+                                                    }}
+                                                >
+                                                    {' '}
+                                                    Nexus 5
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="Box-divider--border-top-1 Padding-vertical--20">
+                                        <div className="Flex-flex Flex-justifyContent--spaceBetween">
+                                            <p className="SubHeader">SDK</p>
+                                        </div>
+                                        <div className="Margin-vertical--8">
+                                            <div className="Flex-flex Margin-vertical--4 ">
+                                                <span
+                                                    className="Flex-flex Flex-alignItems--center"
+                                                    style={{ width: '25%' }}
+                                                >
+                                                    Name
+                                                </span>
+                                                <span
+                                                    style={{
+                                                        backgroundColor:
+                                                            '#F7F8F9',
+                                                        padding: '15px',
+                                                        width: '75%',
+                                                    }}
+                                                >
+                                                    {' '}
+                                                    Fyipe.Tracker.JavaScript
+                                                </span>
+                                            </div>
+                                            <div className="Flex-flex Margin-vertical--4">
+                                                <span
+                                                    className="Flex-flex Flex-alignItems--center"
+                                                    style={{ width: '25%' }}
+                                                >
+                                                    Version
+                                                </span>
+                                                <span
+                                                    style={{
+                                                        backgroundColor:
+                                                            '#F7F8F9',
+                                                        padding: '15px',
+                                                        width: '75%',
+                                                    }}
+                                                >
+                                                    {' '}
+                                                    3.0.165
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
