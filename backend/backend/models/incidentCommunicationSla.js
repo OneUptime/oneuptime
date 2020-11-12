2b482163017b4c6115bc250e09106e459254f526
deityhub <judoc96@gmail.com>
2020-11-12 15:33:12 +0100

    add schema for incident communication sla

---
+++ backend/backend/models/incidentCommunicationSla.js
@@ -0,0 +1,24 @@
+const mongoose = require('../config/db');
+const Schema = mongoose.Schema;
+
+const incidentCommunicationSlaSchema = new Schema(
+    {
+        name: String,
+        monitors: [
+            {
+                monitorId: { type: Schema.Types.ObjectId, ref: 'Monitor' },
+            },
+        ],
+        projectId: { ref: 'Project', type: Schema.Types.ObjectId },
+        isDefault: { type: Boolean, default: false },
+        duration: { type: String, default: '3600' },
+        deleted: { type: Boolean, default: false },
+        deletedAt: Date,
+    },
+    { timestamps: true } //automatically adds createdAt and updatedAt to the collection
+);
+
+module.exports = mongoose.model(
+    'IncidentCommunicationSla',
+    incidentCommunicationSlaSchema
+);
