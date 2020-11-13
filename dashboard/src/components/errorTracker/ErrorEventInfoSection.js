0ac8dce104db22c2d09c7fa9d0ccbdf2f3ff8ae2
Zadat Olayinka <bundayy@outlook.com>
2020-11-13 18:46:17 +0100

    error event info refactored

--- dashboard/src/components/errorTracker/ErrorEventInfoSection.js
+++ dashboard/src/components/errorTracker/ErrorEventInfoSection.js
@@ -1,51 +1,94 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function ErrorEventInfoSection() {
+function RenderTagContent({ title, value }) {
     return (
-        <div className="Box-divider--border-top-1 Padding-vertical--20">
-            <div className="Flex-flex Flex-justifyContent--spaceBetween">
-                <p className="SubHeader">Browser</p>
-            </div>
-            <div className="Margin-vertical--8">
-                <div className="Flex-flex Margin-vertical--4 ">
-                    <span
-                        className="Flex-flex Flex-alignItems--center"
-                        style={{ width: '25%' }}
-                    >
-                        Brand
-                    </span>
-                    <span
-                        style={{
-                            backgroundColor: '#F7F8F9',
-                            padding: '15px',
-                            width: '75%',
-                        }}
-                    >
-                        {' '}
-                        Chrome Mobile
-                    </span>
-                </div>
-                <div className="Flex-flex Margin-vertical--4">
-                    <span
-                        className="Flex-flex Flex-alignItems--center"
-                        style={{ width: '25%' }}
-                    >
-                        Version
-                    </span>
-                    <span
-                        style={{
-                            backgroundColor: '#F7F8F9',
-                            padding: '15px',
-                            width: '75%',
-                        }}
-                    >
-                        {' '}
-                        85.9.100
-                    </span>
-                </div>
-            </div>
+        <div className="Flex-flex Margin-vertical--4 ">
+            <span
+                className="Flex-flex Flex-alignItems--center"
+                style={{ width: '25%' }}
+            >
+                {title}
+            </span>
+            <span
+                style={{
+                    backgroundColor: '#F7F8F9',
+                    padding: '15px',
+                    width: '75%',
+                }}
+            >
+                {value}
+            </span>
         </div>
     );
 }
+RenderTagContent.propTypes = {
+    title: PropTypes.string,
+    value: PropTypes.string,
+};
+RenderTagContent.displayName = 'RenderTagContent';
+function ErrorEventInfoSection({ errorEvent }) {
+    const errorEventDetails = errorEvent.errorEvent;
+    return (
+        <div>
+            {errorEventDetails &&
+            errorEventDetails.device &&
+            errorEventDetails.device.browser ? (
+                <div className="Box-divider--border-top-1 Padding-vertical--20">
+                    <div className="Flex-flex Flex-justifyContent--spaceBetween">
+                        <p className="SubHeader">Browser</p>
+                    </div>
+                    <div className="Margin-vertical--8">
+                        <RenderTagContent
+                            title="Brand"
+                            value={errorEventDetails.device.browser.name}
+                        />
+                        <RenderTagContent
+                            title="Version"
+                            value={errorEventDetails.device.browser.version}
+                        />
+                    </div>
+                </div>
+            ) : null}
+
+            {errorEventDetails &&
+            errorEventDetails.device &&
+            errorEventDetails.device.device ? (
+                <div className="Box-divider--border-top-1 Padding-vertical--20">
+                    <div className="Flex-flex Flex-justifyContent--spaceBetween">
+                        <p className="SubHeader">Operating System</p>
+                    </div>
+                    <div className="Margin-vertical--8">
+                        <RenderTagContent
+                            title="Device"
+                            value={errorEventDetails.device.device.join(' ')}
+                        />
+                    </div>
+                </div>
+            ) : null}
+
+            {errorEventDetails && errorEventDetails.sdk ? (
+                <div className="Box-divider--border-top-1 Padding-vertical--20">
+                    <div className="Flex-flex Flex-justifyContent--spaceBetween">
+                        <p className="SubHeader">SDK</p>
+                    </div>
+                    <div className="Margin-vertical--8">
+                        <RenderTagContent
+                            title="Name"
+                            value={errorEventDetails.sdk.name}
+                        />
+                        <RenderTagContent
+                            title="Version"
+                            value={errorEventDetails.sdk.version}
+                        />
+                    </div>
+                </div>
+            ) : null}
+        </div>
+    );
+}
+ErrorEventInfoSection.propTypes = {
+    errorEvent: PropTypes.object,
+};
 ErrorEventInfoSection.displayName = 'ErrorEventInfoSection';
 export default ErrorEventInfoSection;
