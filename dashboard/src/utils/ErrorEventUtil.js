a06c352f2d48014895a5c62585cfcc262942611d
Zadat Olayinka <bundayy@outlook.com>
2020-11-17 15:53:24 +0100

    filter option util

--- dashboard/src/utils/ErrorEventUtil.js
+++ dashboard/src/utils/ErrorEventUtil.js
@@ -9,6 +9,35 @@ const _this = {
         }
         return indicator;
     },
+    generateFilterOption: function(unformattedFilters) {
+        const option = {};
+        if (!unformattedFilters) {
+            return option;
+        }
+        for (let index = 0; index < unformattedFilters.length; index++) {
+            const element = unformattedFilters[index];
+            switch (element.value) {
+                case 'is:resolved':
+                    option.resolved = true;
+                    break;
+                case 'is:unresolved':
+                    option.resolved = false;
+                    break;
+                case 'is:assigned':
+                    option.assigned = true;
+                    break;
+                case 'is:unassigned':
+                    option.assigned = false;
+                    break;
+                case 'is:ignored':
+                    option.ignored = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        return option;
+    },
 };
 
 export default _this;
