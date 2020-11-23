6b8168f3202b7c13082857f6d78d57704905bfb7
Mohamed ABDELLANI <abdellani@gmail.com>
2020-11-23 17:23:07 +0100

    Add the fetch of the custom twilio settings when the Home page is rendered

--- dashboard/src/pages/Home.js
+++ dashboard/src/pages/Home.js
@@ -19,6 +19,7 @@ import RenderIfUserInSubProject from '../components/basic/RenderIfUserInSubProje
 import IncidentStatus from '../components/incident/IncidentStatus';
 import { fetchSubProjectOngoingScheduledEvents } from '../actions/scheduledEvent';
 import { subProjectTeamLoading } from '../actions/team';
+import { getSmtpConfig } from '../actions/smsTemplates';
 import OngoingScheduledEvent from '../components/scheduledEvent/OngoingScheduledEvent';
 import flattenArray from '../utils/flattenArray';
 import CustomTutorial from '../components/tutorial/CustomTutorial';
@@ -30,6 +31,7 @@ class Home extends Component {
             logEvent('PAGE VIEW: DASHBOARD > PROJECT > HOME');
         }
         this.props.userScheduleRequest();
+        this.props.getSmtpConfig(this.props.currentProjectId);
         if (this.props.currentProjectId && this.props.user.id) {
             this.props.fetchUserSchedule(
                 this.props.currentProjectId,
@@ -526,6 +528,7 @@ const mapDispatchToProps = dispatch => {
             fetchUserSchedule,
             subProjectTeamLoading,
             fetchSubProjectOngoingScheduledEvents,
+            getSmtpConfig,
         },
         dispatch
     );
