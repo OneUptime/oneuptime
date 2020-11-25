d4552f07837dc6f3ee7386b62f52b589b5ae0e1c
Abhishek <abhishek.prajapat@outlook.com>
2020-11-25 18:22:01 +0530

    removed it

--- backend/backend/services/probeService.js
+++ backend/backend/services/probeService.js
@@ -826,7 +826,6 @@ module.exports = {
                         TRIVY_AUTH_URL: dockerCredential.dockerRegistryUrl,
                         TRIVY_USERNAME: dockerCredential.dockerUsername,
                         TRIVY_PASSWORD: dockerCredential.dockerPassword,
-                        PATH: process.env.PATH,
                     },
                     shell: true,
                 });
