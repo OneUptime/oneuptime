641c954f5ece64f51a9aa9c64ff3e465007a2823
adeoluwadavid <adeoluwadavid@gmail.com>
2020-11-30 13:03:05 +0100

    logger

--- dashboard/src/store.js
+++ dashboard/src/store.js
@@ -34,6 +34,6 @@ if (process.env.NODE_ENV === 'development') {
     }
 }
 
-const composedEnhancers = compose(applyMiddleware(...middleware,logger), ...enhancers);
+const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
 
-export default createStore(rootReducer, initialState, composedEnhancers);
+export default createStore(rootReducer, initialState, applyMiddleware(logger), composedEnhancers);
