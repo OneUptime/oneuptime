d9ea311d30c9f3ae99b8348187f01e65bc34a27c
adeoluwadavid <adeoluwadavid@gmail.com>
2020-11-30 13:13:12 +0100

    unfixed

--- dashboard/src/store.js
+++ dashboard/src/store.js
@@ -36,4 +36,4 @@ if (process.env.NODE_ENV === 'development') {
 
 const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
 
-export default createStore(rootReducer, initialState, applyMiddleware(logger), composedEnhancers);
+export default createStore(rootReducer, initialState, composedEnhancers);
