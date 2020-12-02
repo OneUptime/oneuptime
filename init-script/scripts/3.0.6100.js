9b6c81eebacc80ef5632110ba0b6ee50f03646aa
deityhub <judoc96@gmail.com>
2020-12-02 13:02:10 +0100

    migration script to update status page collection with ipWhitelist

---
+++ init-script/scripts/3.0.6100.js
@@ -0,0 +1,22 @@
+const { find, update } = require('../util/db');
+
+const statusPageCollection = 'statuspages';
+
+async function run() {
+    const statusPages = await find(statusPageCollection, {
+        ipWhitelist: { $exists: false },
+        deleted: false,
+    });
+
+    statusPages.forEach(async statusPage => {
+        const data = {
+            ipWhitelist: [],
+        };
+
+        await update(statusPageCollection, { _id: statusPage._id }, data);
+    });
+
+    return `Script ran for ${statusPages.length} status pages`;
+}
+
+module.exports = run;
