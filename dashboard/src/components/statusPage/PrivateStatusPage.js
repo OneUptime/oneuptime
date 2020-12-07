55847e68b33ea659962f80119d81f8d475357c6e
deityhub <judoc96@gmail.com>
2020-12-07 11:24:48 +0100

    edit or remove an ip

--- dashboard/src/components/statusPage/PrivateStatusPage.js
+++ dashboard/src/components/statusPage/PrivateStatusPage.js
@@ -28,16 +28,33 @@ export class PrivateStatusPage extends Component {
         this.state = {
             subscriberAdvanceOptionModalId: uuid.v4(),
             showMoreOptions: false,
+            editIpWhitelist: false,
         };
     }
 
     submitForm = values => {
         const { status } = this.props.statusPage;
+        const { editIpWhitelist } = this.state;
         const { projectId } = status;
 
-        if (values.ipWhitelist && values.ipWhitelist.length > 0) {
+        if (
+            editIpWhitelist &&
+            values.editIpWhitelist &&
+            values.editIpWhitelist.length > 0
+        ) {
+            const ipWhitelist = values.editIpWhitelist.filter(
+                ip => typeof ip === 'string'
+            );
+            values.ipWhitelist = ipWhitelist;
+        }
+
+        if (
+            !editIpWhitelist &&
+            values.ipWhitelist &&
+            values.ipWhitelist.length > 0
+        ) {
             const ipWhitelist = values.ipWhitelist.filter(
-                monitorId => typeof monitorId === 'string'
+                ip => typeof ip === 'string'
             );
             values.ipWhitelist = ipWhitelist;
         }
@@ -52,9 +69,15 @@ export class PrivateStatusPage extends Component {
                 moveIncidentToTheTop: values.moveIncidentToTheTop,
                 ipWhitelist: values.ipWhitelist,
                 enableIpWhitelist: values.enableIpWhitelist,
+                editIpWhitelist,
             })
             .then(() => {
-                document.querySelector('#removeAllFields').click(); // hack to remove all fields of redux fieldArray
+                !editIpWhitelist &&
+                    document.querySelector('#removeAllFields').click(); // hack to remove all fields of redux fieldArray
+
+                this.setState({
+                    editIpWhitelist: false,
+                });
                 this.props.fetchProjectStatusPage(
                     projectId._id || projectId,
                     true
@@ -72,6 +95,91 @@ export class PrivateStatusPage extends Component {
             showMoreOptions: !prevState.showMoreOptions,
         }));
 
+    renderEditIpWhitelist = ({ fields }) => {
+        return (
+            <div
+                style={{
+                    width: '100%',
+                }}
+            >
+                <span
+                    id="removeAllEditFields"
+                    onClick={() => fields.removeAll()}
+                ></span>
+                <div className="Flex-flex">
+                    <button
+                        id="editIpList"
+                        className="Button bs-ButtonLegacy ActionIconParent"
+                        type="button"
+                        onClick={() => {
+                            this.setState({
+                                editIpWhitelist: true,
+                            });
+                        }}
+                    >
+                        <span className="bs-Button bs-FileUploadButton bs-Button--icon bs-Button--new">
+                            <span>Edit IPs</span>
+                        </span>
+                    </button>
+                    <button
+                        id="cancelEditIpList"
+                        className="Button bs-ButtonLegacy ActionIconParent"
+                        type="button"
+                        onClick={() => {
+                            this.setState({
+                                editIpWhitelist: false,
+                            });
+                        }}
+                        style={{ marginLeft: 10 }}
+                    >
+                        <span className="bs-Button bs-FileUploadButton">
+                            <span>Cancel</span>
+                        </span>
+                    </button>
+                </div>
+                {fields.map((field, index) => {
+                    return (
+                        <div
+                            style={{
+                                width: '65%',
+                                marginBottom: 10,
+                                marginTop: 10,
+                            }}
+                            key={index}
+                        >
+                            <Field
+                                component={RenderField}
+                                name={field}
+                                id={`ipWhitelist_${index}`}
+                                placeholder="118.127.63.27"
+                                className="bs-TextInput"
+                                style={{
+                                    width: '100%',
+                                    padding: '3px 5px',
+                                }}
+                            />
+                            <button
+                                id="removeIp"
+                                className="Button bs-ButtonLegacy ActionIconParent"
+                                style={{
+                                    marginTop: 10,
+                                }}
+                                type="button"
+                                onClick={() => {
+                                    fields.remove(index);
+                                }}
+                            >
+                                <span className="bs-Button bs-Button--icon bs-Button--delete">
+                                    <span>Remove IP</span>
+                                </span>
+                            </button>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    };
+
     renderIpWhitelist = ({ fields }) => {
         const { formValues } = this.props;
         return (
@@ -86,18 +194,39 @@ export class PrivateStatusPage extends Component {
                             id="removeAllFields"
                             onClick={() => fields.removeAll()}
                         ></span>
-                        <button
-                            id="addIpList"
-                            className="Button bs-ButtonLegacy ActionIconParent"
-                            type="button"
-                            onClick={() => {
-                                fields.push();
-                            }}
-                        >
-                            <span className="bs-Button bs-FileUploadButton bs-Button--icon bs-Button--new">
-                                <span>Add IP</span>
-                            </span>
-                        </button>
+                        <div className="Flex-flex">
+                            <button
+                                id="addIpList"
+                                className="Button bs-ButtonLegacy ActionIconParent"
+                                type="button"
+                                onClick={() => {
+                                    this.setState({
+                                        editIpWhitelist: false,
+                                    });
+                                    fields.push();
+                                }}
+                            >
+                                <span className="bs-Button bs-FileUploadButton bs-Button--icon bs-Button--new">
+                                    <span>Add IP</span>
+                                </span>
+                            </button>
+                            <button
+                                id="editIpList"
+                                className="Button bs-ButtonLegacy ActionIconParent"
+                                type="button"
+                                onClick={() => {
+                                    this.setState({
+                                        editIpWhitelist: true,
+                                    });
+                                    fields.removeAll();
+                                }}
+                                style={{ marginLeft: 10 }}
+                            >
+                                <span className="bs-Button bs-FileUploadButton bs-Button--icon bs-Button--new">
+                                    <span>Edit IPs</span>
+                                </span>
+                            </button>
+                        </div>
                         {fields.map((field, index) => {
                             return (
                                 <div
@@ -112,7 +241,7 @@ export class PrivateStatusPage extends Component {
                                         component={RenderField}
                                         name={field}
                                         id={`ipWhitelist_${index}`}
-                                        placeholder="127.0.0.1"
+                                        placeholder="118.127.63.27"
                                         className="bs-TextInput"
                                         style={{
                                             width: '100%',
@@ -722,13 +851,25 @@ export class PrivateStatusPage extends Component {
                                                                                     marginTop: 10,
                                                                                 }}
                                                                             >
-                                                                                <FieldArray
-                                                                                    name="ipWhitelist"
-                                                                                    component={
-                                                                                        this
-                                                                                            .renderIpWhitelist
-                                                                                    }
-                                                                                />
+                                                                                {this
+                                                                                    .state
+                                                                                    .editIpWhitelist ? (
+                                                                                    <FieldArray
+                                                                                        name="editIpWhitelist"
+                                                                                        component={
+                                                                                            this
+                                                                                                .renderEditIpWhitelist
+                                                                                        }
+                                                                                    />
+                                                                                ) : (
+                                                                                    <FieldArray
+                                                                                        name="ipWhitelist"
+                                                                                        component={
+                                                                                            this
+                                                                                                .renderIpWhitelist
+                                                                                        }
+                                                                                    />
+                                                                                )}
                                                                             </div>
                                                                         </div>
                                                                     </div>
@@ -840,6 +981,7 @@ const mapStateToProps = state => {
         initialValues.showScheduledEvents = status.showScheduledEvents;
         initialValues.moveIncidentToTheTop = status.moveIncidentToTheTop;
         initialValues.enableIpWhitelist = status.enableIpWhitelist;
+        initialValues.editIpWhitelist = status.ipWhitelist;
     }
     initialValues.showIpWhitelistInput = true;
 
