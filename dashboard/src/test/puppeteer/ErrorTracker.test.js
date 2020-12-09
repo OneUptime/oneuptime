7d84a683c0cddbb0f816e58fb505fcf6fb6ce4fe
Zadat Olayinka <bundayy@outlook.com>
2020-12-09 21:58:41 +0100

    tests for error tracker

--- dashboard/src/test/puppeteer/ErrorTracker.test.js
+++ dashboard/src/test/puppeteer/ErrorTracker.test.js
@@ -12,6 +12,7 @@ const user = {
 };
 const componentName = utils.generateRandomString();
 const errorTrackerName = utils.generateRandomString();
+let errorTrackerKey = '';
 
 describe('Error Trackers', () => {
     const operationTimeOut = 900000;
@@ -99,6 +100,271 @@ describe('Error Trackers', () => {
                 spanElement = await spanElement.jsonValue();
                 const title = `${errorTrackerName} (0)`;
                 spanElement.should.be.exactly(title);
+            });
+        },
+        operationTimeOut
+    );
+    test(
+        'Should create new resource category then redirect to error tracker page to create a error tracker under that',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                const categoryName = 'Random-Category';
+                const newErrorTrackerName = `${errorTrackerName}-sample`;
+                // create a new resource category
+                await init.addResourceCategory(categoryName, page);
+                //navigate to component details
+                await init.navigateToComponentDetails(componentName, page);
+                // go to logs
+                await page.waitForSelector('#errorTracking');
+                await page.click('#errorTracking');
+                // create a new error tracker and select the category
+                // Fill and submit New Error Tracker form
+                await page.waitForSelector('#form-new-error-tracker');
+                await page.click('input[id=name]');
+                await page.type('input[id=name]', newErrorTrackerName);
+                await init.selectByText(
+                    '#resourceCategory',
+                    categoryName,
+                    page
+                );
+                await page.click('button[type=submit]');
+                // goto the details page
+                await page.waitForSelector(
+                    `#more-details-${newErrorTrackerName}`
+                );
+                await page.click(`#more-details-${newErrorTrackerName}`);
+                // confirm the category shows in the details page.
+                await page.waitForSelector(`#${newErrorTrackerName}-badge`, {
+                    visible: true,
+                });
+                let spanElement = await page.$(`#${newErrorTrackerName}-badge`);
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+                spanElement.should.be.exactly(categoryName.toUpperCase());
+            });
+        },
+        operationTimeOut
+    );
+    test(
+        'Should not create new error tracker ',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                // Navigate to Component details
+                await init.navigateToComponentDetails(componentName, page);
+                await page.waitForSelector('#errorTracking');
+                await page.click('#errorTracking');
+
+                // Fill and submit New Error Tracker form
+                await page.waitForSelector('#form-new-error-tracker');
+                await page.click('input[id=name]');
+                await page.type('input[id=name]', '');
+                await page.click('button[type=submit]');
+
+                await page.waitForSelector(
+                    '#form-new-error-tracker span#field-error',
+                    { visible: true }
+                );
+                let spanElement = await page.$(
+                    '#form-new-error-tracker span#field-error'
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+                spanElement.should.be.exactly(
+                    'This field cannot be left blank'
+                );
+            });
+        },
+        operationTimeOut
+    );
+    test(
+        'Should open details page of created error tracker',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                await init.navigateToErrorTrackerDetails(
+                    componentName,
+                    errorTrackerName,
+                    page
+                );
+
+                let spanElement = await page.waitForSelector(
+                    `#error-tracker-title-${errorTrackerName}`
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+                const title = `${errorTrackerName} (0)`;
+                spanElement.should.be.exactly(title);
+            });
+        },
+        operationTimeOut
+    );
+    test(
+        'Should open edit created error tracker',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                await init.navigateToErrorTrackerDetails(
+                    componentName,
+                    errorTrackerName,
+                    page
+                );
+                await page.waitForSelector(`#edit_${errorTrackerName}`);
+                await page.click(`#edit_${errorTrackerName}`);
+
+                let spanElement = await page.waitForSelector(
+                    `#error-tracker-edit-title-${errorTrackerName}`
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+                spanElement.should.be.exactly(
+                    `Edit Tracker ${errorTrackerName}`
+                );
+            });
+        },
+        operationTimeOut
+    );
+    test(
+        'Should open tracker key for created error tracker',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                await init.navigateToErrorTrackerDetails(
+                    componentName,
+                    errorTrackerName,
+                    page
+                );
+                // open modal
+                await page.waitForSelector(`#key_${errorTrackerName}`);
+                await page.click(`#key_${errorTrackerName}`);
+
+                // click show applicaion log key
+                await page.waitForSelector(
+                    `#show_error_tracker_key_${errorTrackerName}`
+                );
+                await page.click(`#show_error_tracker_key_${errorTrackerName}`);
+
+                // get error tracker key
+                let spanElement = await page.waitForSelector(
+                    `#error_tracker_key_${errorTrackerName}`
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                errorTrackerKey = await spanElement.jsonValue();
+                expect(spanElement).toBeDefined();
+
+                // click cancel
+                await page.waitForSelector(
+                    `#cancel_error_tracker_key_${errorTrackerName}`
+                );
+                await page.click(
+                    `#cancel_error_tracker_key_${errorTrackerName}`
+                );
+            });
+        },
+        operationTimeOut
+    );
+    test(
+        'Should open tracker key for created error tracker container and hide it back',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                await init.navigateToErrorTrackerDetails(
+                    componentName,
+                    errorTrackerName,
+                    page
+                );
+                await page.waitForSelector(`#key_${errorTrackerName}`);
+                await page.click(`#key_${errorTrackerName}`);
+
+                // click show error tracker  key
+                await page.waitForSelector(
+                    `#show_error_tracker_key_${errorTrackerName}`
+                );
+                await page.click(`#show_error_tracker_key_${errorTrackerName}`);
+                let spanElement = await page.waitForSelector(
+                    `#error_tracker_key_${errorTrackerName}`
+                );
+                expect(spanElement).toBeDefined();
+
+                // find the eye icon to hide error tracker key
+                await page.waitForSelector(
+                    `#hide_error_tracker_key_${errorTrackerName}`
+                );
+                await page.click(`#hide_error_tracker_key_${errorTrackerName}`);
+
+                spanElement = await page.waitForSelector(
+                    `#show_error_tracker_key_${errorTrackerName}`
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+
+                expect(spanElement).toEqual(
+                    'Click here to reveal Tracker API key'
+                );
+            });
+        },
+        operationTimeOut
+    );
+    test(
+        'Should reset tracker key for created errir tracker',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                await init.navigateToApplicationLogDetails(
+                    componentName,
+                    errorTrackerName,
+                    page
+                );
+                // open modal
+                await page.waitForSelector(`#key_${errorTrackerName}`);
+                await page.click(`#key_${errorTrackerName}`);
+
+                // click show error tracker key
+                await page.waitForSelector(
+                    `#show_error_tracker_key_${errorTrackerName}`
+                );
+                await page.click(`#show_error_tracker_key_${errorTrackerName}`);
+
+                // get error tracker key
+                let spanElement = await page.waitForSelector(
+                    `#error_tracker_key_${errorTrackerName}`
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                errorTrackerKey = await spanElement.jsonValue();
+
+                // click reset key
+                await page.waitForSelector(
+                    `#reset_error_tracker_key_${errorTrackerName}`
+                );
+                await page.click(
+                    `#reset_error_tracker_key_${errorTrackerName}`
+                );
+
+                // click confirm reset key
+                await page.waitForSelector(
+                    `#confirm_reset_error_tracker_key_${errorTrackerName}`
+                );
+                await page.click(
+                    `#confirm_reset_error_tracker_key_${errorTrackerName}`
+                );
+                await page.waitForSelector(
+                    `#confirm_reset_error_tracker_key_${errorTrackerName}`,
+                    { hidden: true }
+                );
+
+                // open modal
+                await page.waitForSelector(`#key_${errorTrackerName}`);
+                await page.click(`#key_${errorTrackerName}`);
+
+                // click show error tracker key
+                await page.waitForSelector(
+                    `#show_error_tracker_key_${errorTrackerName}`
+                );
+                await page.click(`#show_error_tracker_key_${errorTrackerName}`);
+
+                // get tracker container key
+                spanElement = await page.waitForSelector(
+                    `#error_tracker_key_${errorTrackerName}`
+                );
+                spanElement = await spanElement.getProperty('innerText');
+                spanElement = await spanElement.jsonValue();
+
+                expect(spanElement).toBeDefined();
+                spanElement.should.not.be.equal(errorTrackerKey);
             });
         },
         operationTimeOut
