681e9cef4862d7fd3a16802587df219a97708eb3
adeoluwadavid <adeoluwadavid@gmail.com>
2020-12-14 23:05:24 +0100

    a-span

--- dashboard/src/components/alert/AlertTable.js
+++ dashboard/src/components/alert/AlertTable.js
@@ -319,9 +319,8 @@ function TD7({ text }) {
             className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
             style={{ height: '1px' }}
         >
-            <a
+            <span
                 className="db-ListViewItem-link"
-                href="#"
             >
                 <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
                     <span className="db-ListViewItem-text Text-color--inherit Text-display--inline Text-fontSize--14 Text-fontWeight--regular Text-lineHeight--20 Text-typeface--base Text-wrap--wrap">
@@ -355,7 +354,7 @@ function TD7({ text }) {
                         </div>
                     </span>
                 </div>
-            </a>
+            </span>
         </td>
     );
 }
