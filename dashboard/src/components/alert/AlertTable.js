5e63158e1a6a199632b567fc836df61db10a9c67
adeoluwadavid <adeoluwadavid@gmail.com>
2020-12-15 05:45:02 +0100

    branch-adjustment

--- dashboard/src/components/alert/AlertTable.js
+++ dashboard/src/components/alert/AlertTable.js
@@ -319,8 +319,9 @@ function TD7({ text }) {
             className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
             style={{ height: '1px' }}
         >
-            <span
+            <a
                 className="db-ListViewItem-link"
+                href="/radar/lists/rsl_1C6makKGKS4tO8UaygcziemN"
             >
                 <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
                     <span className="db-ListViewItem-text Text-color--inherit Text-display--inline Text-fontSize--14 Text-fontWeight--regular Text-lineHeight--20 Text-typeface--base Text-wrap--wrap">
@@ -354,7 +355,7 @@ function TD7({ text }) {
                         </div>
                     </span>
                 </div>
-            </span>
+            </a>
         </td>
     );
 }
