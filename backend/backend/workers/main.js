20188a80285e69dc924775428116866e9d9e83ba
deityhub <judoc96@gmail.com>
2020-12-16 22:57:42 +0100

    run cron job every 24th hour

--- backend/backend/workers/main.js
+++ backend/backend/workers/main.js
@@ -2,12 +2,14 @@ const cron = require('node-cron');
 //const iotMonitorCron = require('./iotMonitor');
 const escalationPolicy = require('./escalationPolicy');
 const serverMonitorCron = require('./serverMonitor');
+const subscription = require('./subscription');
 
 // Generate a random number betwen 1 and 50 and use that to run cron jobs.
 // This is done because there will be many instances of backend in production, and one instance of backend
 // should run at a random second every minute so they dont collide.
 const cronMinuteStartTime = Math.floor(Math.random() * 50);
 const serverMonitorCronMinuteStartTime = Math.floor(Math.random() * 50);
+const subscriptionCronMinutesStartTime = Math.floor(Math.random() * 50);
 
 // Esclation Policy: This cron runs every minute
 cron.schedule('* * * * *', () => {
@@ -21,6 +23,13 @@ cron.schedule('* * * * *', () => {
     setTimeout(() => {
         serverMonitorCron.checkAllServerMonitors();
     }, serverMonitorCronMinuteStartTime * 1000);
+});
+
+cron.schedule('0 0 * * *', () => {
+    setTimeout(
+        () => subscription.handleUnpaidSubscription(),
+        subscriptionCronMinutesStartTime * 1000
+    );
 });
 
 // IoT Monitor: This cron runs every minute
