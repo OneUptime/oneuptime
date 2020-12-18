fbb04d05b8c4102e1e79a5755df44ff0a5dff1fe
deityhub <judoc96@gmail.com>
2020-12-18 10:38:17 +0100

    refactor to handle all unpaid subscription instead of just the first 100 unpaid subscriptions

--- backend/backend/workers/subscription.js
+++ backend/backend/workers/subscription.js
@@ -6,19 +6,47 @@ const UserService = require('../services/userService');
 const AlertService = require('../services/alertService');
 const { IS_SAAS_SERVICE } = require('../config/server');
 
-module.exports = {
+const handleFetchingUnpaidSubscriptions = async startAfter => {
+    if (startAfter) {
+        return await stripe.subscriptions.list({
+            status: 'unpaid',
+            limit: 100, // default limit is 10 and limit can range from 1 to 100
+            starting_after: startAfter,
+        });
+    } else {
+        return await stripe.subscriptions.list({
+            status: 'unpaid',
+            limit: 100, // default limit is 10 and limit can range from 1 to 100
+        });
+    }
+};
+
+let data = [];
+const _this = {
     /**
      * use stripe sdk to check for unpaid subscriptions (saas mode)
      * send email notification once every 24 hours to project owners about the unpaid subscription
-     * deactive the project (soft delete) on the 14th day, if the subscription remains unpaid
+     * deactive the project (soft delete) and cancel the subscription on the 14th day, if the subscription remains unpaid
      */
-    handleUnpaidSubscription: async () => {
+    handleUnpaidSubscription: async startAfter => {
         try {
             if (IS_SAAS_SERVICE) {
-                const subscriptions = await stripe.subscriptions.list({
-                    status: 'unpaid',
-                });
-                const unpaidSubscriptions = subscriptions.data;
+                const subscriptions = await handleFetchingUnpaidSubscriptions(
+                    startAfter
+                );
+                // apply recursion here in order to fetch all the unpaid subscriptions and store it in data array
+                // since stripe have a limit on the amount of items to return in the subscription list
+                // and also because, if we cancel a subscription and the subscription happens to be the last item in the subscription data array
+                // there is no way to fetch the next 100 item, so we are using this approach to fetch every thing once before proceeding to the next stage
+                data = [...data, ...subscriptions.data];
+                if (subscriptions && subscriptions.has_more) {
+                    const lastIndex = subscriptions.data.length - 1;
+                    await _this.handleUnpaidSubscription(
+                        subscriptions.data[lastIndex]
+                    );
+                }
+
+                const unpaidSubscriptions = [...data];
                 for (const unpaidSubscription of unpaidSubscriptions) {
                     const stripeCustomerId = unpaidSubscription.customer;
                     const stripeSubscriptionId = unpaidSubscription.id;
@@ -54,6 +82,11 @@ module.exports = {
                             Number(project.unpaidSubscriptionNotifications) ===
                                 14
                         ) {
+                            // cancel subscription
+                            await stripe.subscriptions.del(
+                                stripeSubscriptionId
+                            );
+
                             // delete project
                             await ProjectService.updateOneBy(
                                 { stripeSubscriptionId },
@@ -90,6 +123,8 @@ module.exports = {
                         }
                     }
                 }
+
+                data = []; // reset to empty array
             }
         } catch (error) {
             ErrorService.log('serverMonitorCron.checkAllServerMonitor', error);
@@ -97,3 +132,5 @@ module.exports = {
         }
     },
 };
+
+module.exports = _this;
