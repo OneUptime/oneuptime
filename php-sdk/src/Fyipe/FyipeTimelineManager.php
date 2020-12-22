b82f4f1a8093c355c3dbda749fd25eefc7e316e8
Zadat Olayinka <bundayy@outlook.com>
2020-12-23 00:41:20 +0100

    fyipe timeline manager

---
+++ php-sdk/src/Fyipe/FyipeTimelineManager.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * @author bunday
+ */
+
+namespace Fyipe;
+
+
+class FyipeTimelineManager
+{
+    private $options;
+    private $timeLineStack = [];
+
+    /**
+     * FyipeTimelineManager constructor.
+     * @param array $options
+     */
+    public function __construct($options){
+        $this->options = $options;
+    }
+    private function addItemToTimeline($item) {
+        // get the size of the stack
+        if (sizeof($this->timeLineStack) === $this->options['maxTimeline']) {
+            return; // It discards new timline update once maximum is reached
+        }
+        // add time to it
+        $item->timestamp = date("Y-m-d");
+        // add a new item to the stack
+        array_push($this->timeLineStack, $item);
+        return true;
+    }
+    public function addToTimeline($item) {
+        $this->addItemToTimeline($item);
+    }
+     // return the timeline
+     public function getTimeline() {
+        return $this->timeLineStack;
+    }
+    // clear the timeline
+    public function clearTimeline() {
+        $this->timeLineStack = [];
+    }
+}
