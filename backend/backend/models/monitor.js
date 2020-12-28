c68ce112af2bbb8f3cf3df38ad1a9e3a85bf40fa
deityhub <judoc96@gmail.com>
2020-12-28 14:59:28 +0100

    add variable key to monitor collection

--- backend/backend/models/monitor.js
+++ backend/backend/models/monitor.js
@@ -77,6 +77,7 @@ const monitorSchema = new Schema({
     },
     breachedMonitorSla: { type: Boolean, default: false },
     breachClosedBy: [{ type: String, ref: 'User' }],
+    variables: [String],
 });
 
 monitorSchema.virtual('project', {
