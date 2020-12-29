8d1686cff54d7d2fc843665aba8d052c47a01e32
Mohamed ABDELLANI <abdellani@gmail.com>
2020-12-29 09:01:41 +0100

    Fix paymentService

--- backend/backend/services/paymentService.js
+++ backend/backend/services/paymentService.js
@@ -12,7 +12,7 @@ module.exports = {
         project,
         alertType,
         alertPhoneNumber,
-        segments
+        segments=1
     ) {
         let release;
         try {
@@ -24,7 +24,7 @@ module.exports = {
                 alertType,
                 countryType
             );
-            const chargeAmount = alertChargeAmount.price * segments;
+            const chargeAmount = alertType ===Call ? alertChargeAmount.price : alertChargeAmount.price * segments;
 
             const updatedProject = await this.chargeAlert(
                 userId,
@@ -458,5 +458,5 @@ const ProjectService = require('./projectService');
 const ProjectModel = require('../models/project');
 const StripeService = require('./stripeService');
 const NotificationService = require('./notificationService');
-const { getAlertChargeAmount, getCountryType } = require('../config/alertType');
+const { getAlertChargeAmount, getCountryType, Call } = require('../config/alertType');
 const getProjectMutex = require('../constants/projectMutexProvider');
