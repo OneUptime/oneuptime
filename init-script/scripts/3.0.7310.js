7cc07cae6197d1e0f6d8cb7fa093d648c244277f
deityhub <judoc96@gmail.com>
2020-12-29 19:42:06 +0100

    migration script to use thirdPartyVariable in monitors collection

---
+++ init-script/scripts/3.0.7310.js
@@ -0,0 +1,30 @@
+const { find, update, removeField } = require('../util/db');
+
+const monitorCollection = 'monitors';
+
+async function run() {
+    const monitors = await find(monitorCollection, {
+        thirdPartyVariable: { $exists: false },
+        deleted: false,
+    });
+
+    for (const monitor of monitors) {
+        const data = {
+            thirdPartyVariable: [monitor.name],
+        };
+
+        await update(monitorCollection, { _id: monitor._id }, data);
+
+        await removeField(
+            monitorCollection,
+            { _id: monitor._id },
+            { variables: '' }
+        );
+    }
+
+    return `Script ran for ${monitors.length} status pages`;
+}
+
+module.exports = run;
+
+module.exports = run;
