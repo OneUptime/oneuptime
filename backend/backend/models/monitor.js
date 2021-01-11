8d81ff5666590b3a0749164e4ef08f2147ab7f0d
deityhub <judoc96@gmail.com>
2021-01-11 13:26:32 +0100

    switch to custom fields and remove third party variables

--- backend/backend/models/monitor.js
+++ backend/backend/models/monitor.js
@@ -77,7 +77,7 @@ const monitorSchema = new Schema({
     },
     breachedMonitorSla: { type: Boolean, default: false },
     breachClosedBy: [{ type: String, ref: 'User' }],
-    thirdPartyVariable: [Schema.Types.Mixed],
+    customFields: [{ fieldName: String, fieldValue: Schema.Types.Mixed }],
 });
 
 monitorSchema.virtual('project', {
