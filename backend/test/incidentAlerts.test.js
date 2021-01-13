9b90a088a14738d09cf097cd998fa5ff1d80de51
Mohamed ABDELLANI <abdellani@gmail.com>
2021-01-13 10:21:46 +0100

    Fix test: 'should send Email alerts if global SMTP configurations are set, email alerts disabled in global configurations, and custom SMTP settings are set.'

--- backend/test/incidentAlerts.test.js
+++ backend/test/incidentAlerts.test.js
@@ -2689,14 +2689,22 @@ describe('Email Incident Alerts', function() {
         });
         expect(onCallAlerts).to.have.status(200);
         expect(onCallAlerts.body).to.an('object');
-        expect(onCallAlerts.body.count).to.equal(1);
+        expect(onCallAlerts.body.count).to.equal(2);
         expect(onCallAlerts.body.data).to.an('array');
-        expect(onCallAlerts.body.data.length).to.equal(1);
-        const onCallAlert = onCallAlerts.body.data[0];
-        const { alertVia, alertStatus, error } = onCallAlert;
-        expect(alertVia).to.equal('email');
-        expect(alertStatus).to.equal('Success');
-        expect(error).to.equal(false);
+        expect(onCallAlerts.body.data.length).to.equal(2);
+
+        const eventTypesSentToTeamMembers = [];
+        for(let onCallAlert of onCallAlerts.body.data){
+            const { alertVia, eventType, alertStatus, error, errorMessage } = onCallAlert;
+            eventTypesSentToTeamMembers.push(eventType);
+            expect(alertVia).to.equal('email');
+            expect(alertStatus).to.equal('Success');
+            expect(error).to.equal(false);
+            expect(errorMessage).to.be.undefined;
+        }
+
+        expect(eventTypesSentToTeamMembers.includes('resolved')).to.equal(true);
+        expect(eventTypesSentToTeamMembers.includes('identified')).to.equal(true);
         await GlobalConfigService.hardDeleteBy({ name: 'smtp' });
         await EmailSmtpService.hardDeleteBy({ projectId });
     });
