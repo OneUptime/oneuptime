9a04a5aa33fdeaafb3fa77df0c3a98bd134dd3d8
deityhub <judoc96@gmail.com>
2021-01-15 13:33:59 +0100

    remove commented line

--- backend/backend/models/incomingRequest.js
+++ backend/backend/models/incomingRequest.js
@@ -22,19 +22,6 @@ const incomingRequestSchema = new Schema(
         url: String,
         deleted: { type: Boolean, default: false },
         deletedAt: Date,
-        // filterCriteria: String,
-        // filterCondition: {
-        //     type: String,
-        //     enum: [
-        //         'equalTo',
-        //         'notEqualTo',
-        //         'lessThan',
-        //         'greaterThan',
-        //         'greaterThanOrEqualTo',
-        //         'lessThanOrEqualTo',
-        //     ],
-        // },
-        // filterText: Schema.Types.Mixed, // expected to store both string and number
         incidentTitle: String,
         incidentType: { type: String },
         incidentPriority: {
