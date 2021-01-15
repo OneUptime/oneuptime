0f1e615f7faf9755a5cb502fcc8e198349c773e8
Zadat Olayinka <bundayy@outlook.com>
2021-01-15 17:56:29 +0100

    quick start docs for more languages

--- dashboard/src/config.js
+++ dashboard/src/config.js
@@ -1076,7 +1076,7 @@ export const logLibraries = {
                 
     // constructor
     const tracker = new ErrorTracker(
-        'API_URL', // https://fyipe.com/api
+        'https://fyipe.com/api'
         'ERROR_TRACKER_ID',
         'ERROR_TRACKER_KEY',
         options // Optional Field
@@ -1093,6 +1093,131 @@ export const logLibraries = {
         tracker.captureException(error);
     }
 `,
+                logs: `
+#### NPM Install                                
+    $ npm install fyipe
+#### Usage
+    // In a FrontEnd Environment
+    import { Logger } from 'fyipe';
+                
+    // In a Backend Environment
+    const { Logger } = require('fyipe');
+                
+    // in a Backend Environment with ES6
+    import Fyipe from 'fyipe';
+    const { Logger } = Fyipe;
+
+    // constructor
+    const logger = new Logger(
+        'https://fyipe.com/api'
+        'APPLICATION_LOG_ID',
+        'APPLICATION_LOG_KEY'
+    );
+    // Sending a JSON object log to the server    
+    const item = {
+        user: 'Test User',
+        page: {
+            title: 'Landing Page',
+            loadTime: '6s',
+        },
+    };
+    logger.log(item); // returns a promise
+
+`,
+                old: 'No Docs available at the moment',
+            },
+            {
+                id: 'php',
+                language: 'PHP',
+                errorTracking: 'No Docs available at the moment',
+                logs: `
+#### Composer Install                                
+    $ composer require fyipe/sdk
+#### Usage
+    use Fyipe\\FyipeLogger;
+
+    // constructor
+    $logger = new Fyipe\\FyipeLogger(
+        'https://fyipe.com/api'
+        'APPLICATION_LOG_ID',
+        'APPLICATION_LOG_KEY'
+    );
+
+    // Sending an object log to the server
+    $item = new stdClass();
+    $item->user = 'Test User';
+    $item->page = 'Landing Page';
+
+    $response = $logger->log($item);
+    // response after logging a request
+    var_dump($response);
+
+`,
+            },
+            {
+                id: 'java',
+                language: 'Java',
+                errorTracking: 'No Docs available at the moment',
+                logs: `
+#### Maven Install                                
+    <dependency>
+        <groupId>io.hackerbay.fyipe</groupId>
+        <artifactId>java-sdk</artifactId>
+        <version>CURRENT_VERSION</version>
+    </dependency>
+#### Usage
+    import com.google.gson.JsonObject;
+    import io.hackerbay.fyipe.FyipeLogger;
+
+    // constructor
+    FyipeLogger logger = new FyipeLogger(
+        "https://fyipe.com/api"
+        "APPLICATION_LOG_ID",
+        "APPLICATION_LOG_KEY"
+    );
+    // Logging any object of a class
+    public void logACustomClassInformation(CustomClass customClass) {
+        String content = new Gson().toJson(customClass); // converts your custom class to a json object
+        JsonObject response = logger.log(content); // returns a JsonObject of response
+        System.out.println(response);
+    }
+
+`,
+            },
+            {
+                id: 'python',
+                language: 'Python',
+                errorTracking: 'No Docs available at the moment',
+                logs: 'No Docs available at the moment',
+            },
+            {
+                id: 'dotnet',
+                language: '.Net',
+                errorTracking: 'No Docs available at the moment',
+                logs: 'No Docs available at the moment',
+            },
+            {
+                id: 'swift',
+                language: 'Swift',
+                errorTracking: 'No Docs available at the moment',
+                logs: 'No Docs available at the moment',
+            },
+            {
+                id: 'rust',
+                language: 'Rust',
+                errorTracking: 'No Docs available at the moment',
+                logs: 'No Docs available at the moment',
+            },
+            {
+                id: 'ruby',
+                language: 'Ruby',
+                errorTracking: 'No Docs available at the moment',
+                logs: 'No Docs available at the moment',
+            },
+            {
+                id: 'go',
+                language: 'Go',
+                errorTracking: 'No Docs available at the moment',
                 logs: 'No Docs available at the moment',
             },
         ];
