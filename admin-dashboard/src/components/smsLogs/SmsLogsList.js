bd9592c4583912833c14eb58657d0263ba57ba33
Abhishek <abhishek.prajapat@outlook.com>
2021-01-17 17:03:30 +0530

    Project details page redirection done

--- admin-dashboard/src/components/smsLogs/SmsLogsList.js
+++ admin-dashboard/src/components/smsLogs/SmsLogsList.js
@@ -10,6 +10,8 @@ import DeleteConfirmationModal from './DeleteConfirmationModal';
 import SmsLogsContentViewModal from './SmsLogsContentViewModal';
 import SmsLogsErrorViewModal from './SmsLogsErrorViewModal';
 
+import { history } from '../../store';
+
 export class SmsLogsList extends Component {
     constructor(props) {
         super(props);
@@ -159,7 +161,16 @@ export class SmsLogsList extends Component {
                                         >
                                             <td
                                                 className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--wrap db-ListViewItem-cell db-ListViewItem-cell--breakWord"
-                                                style={{ height: '1px' }}
+                                                style={{
+                                                    height: '1px',
+                                                    cursor: 'pointer',
+                                                }}
+                                                onClick={() => {
+                                                    history.push(
+                                                        '/admin/projects/' +
+                                                            smsLog.projectId._id
+                                                    );
+                                                }}
                                             >
                                                 <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
                                                     <span className="db-ListViewItem-text Text-color--cyan Text-display--inline Text-fontSize--14 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--base Text-wrap--wrap">
@@ -193,7 +204,16 @@ export class SmsLogsList extends Component {
                                             </td>
                                             <td
                                                 className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--wrap db-ListViewItem-cell"
-                                                style={{ height: '1px' }}
+                                                style={{
+                                                    height: '1px',
+                                                    cursor: 'pointer',
+                                                }}
+                                                onClick={() => {
+                                                    history.push(
+                                                        '/admin/projects/' +
+                                                            smsLog.projectId._id
+                                                    );
+                                                }}
                                             >
                                                 <div className="db-ListViewItem-link">
                                                     <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
@@ -213,7 +233,16 @@ export class SmsLogsList extends Component {
                                             </td>
                                             <td
                                                 className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--wrap db-ListViewItem-cell db-ListViewItem-cell--breakWord"
-                                                style={{ height: '1px' }}
+                                                style={{
+                                                    height: '1px',
+                                                    cursor: 'pointer',
+                                                }}
+                                                onClick={() => {
+                                                    history.push(
+                                                        '/admin/projects/' +
+                                                            smsLog.projectId._id
+                                                    );
+                                                }}
                                             >
                                                 <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
                                                     <span className="db-ListViewItem-text Text-color--cyan Text-display--inline Text-fontSize--14 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--base Text-wrap--wrap">
@@ -231,7 +260,16 @@ export class SmsLogsList extends Component {
                                             </td>
                                             <td
                                                 className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--wrap db-ListViewItem-cell"
-                                                style={{ height: '1px' }}
+                                                style={{
+                                                    height: '1px',
+                                                    cursor: 'pointer',
+                                                }}
+                                                onClick={() => {
+                                                    history.push(
+                                                        '/admin/projects/' +
+                                                            smsLog.projectId._id
+                                                    );
+                                                }}
                                             >
                                                 <div className="db-ListViewItem-link">
                                                     <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
