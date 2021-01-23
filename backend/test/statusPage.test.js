6afb2f477ce0917eb0b09971116f34e2ebdd0210
adeoluwadavid <adeoluwadavid@gmail.com>
2021-01-23 18:07:04 +0100

    backend-fixing

--- backend/test/statusPage.test.js
+++ backend/test/statusPage.test.js
@@ -1033,16 +1033,16 @@ describe('StatusPage API with Sub-Projects', function() {
         });
     });
 
-    it('should not get private status page for authorized user that is not in project', async function() {
-      //  this.timeout(100000)
+    it('should not get private status page for authorized user that is not in project', function(done) {
         const authorization = `Basic ${newUserToken}`;
-        await request
+        request
             .get(`/statusPage/${privateStatusPageId}`)
             .set('Authorization', authorization)
             .end(function(err, res) {
                 if (err) throw err;
                 expect(res).to.have.status(400);
-            });
+                done();
+            }).then(done,done);
     });
 
     it('should not create a statusPage for user that is not `admin` in sub-project.', function(done) {
