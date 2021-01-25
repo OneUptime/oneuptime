b8d73e87aa75c9895f6178e4c5b2ea09d3c3e1b3
adeoluwadavid <adeoluwadavid@gmail.com>
2021-01-25 14:25:37 +0100

    backend-fixing

--- backend/test/incident.test.js
+++ backend/test/incident.test.js
@@ -418,6 +418,7 @@ describe('Incident API', function() {
                 type: 'internal',
                 incident_state: 'just test',
             });
+            console.log("Response body: ",res.body)
         internalMessageId = res.body._id;
         expect(res).to.have.status(200);
         expect(res.body.incidentId._id).to.be.equal(incidentId);
