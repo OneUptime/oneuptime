c469ab688a42f28c1666b0d4ff89d3fe2c38730f
deityhub <judoc96@gmail.com>
2021-01-25 16:33:33 +0100

    clean up: remove unnecessary files

--- haraka/plugins/reseed_rng.js
+++
@@ -1,8 +0,0 @@
-const crypto = require('crypto');
-
-exports.hook_init_child = function(next) {
-    const plugin = this;
-    Math.seedrandom(crypto.randomBytes(256).toString('hex'));
-    plugin.logdebug('reseeded rng');
-    next();
-};
