ceeb9469a66e6d4af344210259fa5a39ab655c2a
Igwe Augustine <augustusbest00@gmail.com>
2021-01-26 12:21:20 +0100

    added push event listener

--- dashboard/public/sw.js
+++ dashboard/public/sw.js
@@ -1,15 +1,9 @@
-console.log("Service Worker Loaded...");
 
 self.addEventListener("push", e => {
   const data = e.data.json();
-  console.log("Push Recieved...");
   self.registration.showNotification(data.title, {
-    body: "Notified by Traversy Media!",
-    icon: "http://image.ibb.co/frYOFd/tmlogo.png"
+    body: "Notified by Augustine Igwe!!!",
+    icon: "https://www.dropbox.com/s/dwawm02f1toxnm8/Fyipe-Icon.png?dl=0&raw=1"
   });
 });
   
-// self.addEventListener('message', async (event) => {
-//   console.log('Got message in the service worker', event);
-// });
-// console.log('trust working')
