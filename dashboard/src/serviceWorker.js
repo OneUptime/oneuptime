1c041065671590be9e48b3aa8ee0223670fe6afe
deityhub <judoc96@gmail.com>
2021-01-28 14:35:58 +0100

    debugging

--- dashboard/src/serviceWorker.js
+++ dashboard/src/serviceWorker.js
@@ -74,10 +74,12 @@ function registerValidSW(swUrl, config) {
         .then(registration => {
             registration.onupdatefound = () => {
                 const installingWorker = registration.installing;
+                console.log('******* registration *********', registration)
                 if (installingWorker == null) {
                     return;
                 }
                 installingWorker.onstatechange = () => {
+                    console.log('****** worker state ********', installingWorker);
                     if(installingWorker.state === 'installing'){
                         self.skipWaiting();
                     }
