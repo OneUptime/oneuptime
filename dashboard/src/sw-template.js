875def4e5212273120a0a096817c1d1371a443eb
deityhub <judoc96@gmail.com>
2021-01-28 09:22:34 +0100

    update workbox version

--- dashboard/src/sw-template.js
+++ dashboard/src/sw-template.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 if ('function' === typeof importScripts) {
     importScripts(
-        'https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js'
+        'https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js'
     );
     /* global workbox */
     if (workbox) {
