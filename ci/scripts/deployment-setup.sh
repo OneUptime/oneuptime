57e0cb772c45052c9488004dd7c6d28ba2ebe430
deityhub <judoc96@gmail.com>
2021-02-03 10:19:37 +0100

    setup private, public, tls_cert and tls_key key and export to machine

--- ci/scripts/deployment-setup.sh
+++ ci/scripts/deployment-setup.sh
@@ -15,3 +15,20 @@ sudo mkdir /root/.kube || echo "Directory already created."
 #Init auth
 echo "Auth doctl"
 sudo doctl auth init -t $DIGITALOCEAN_TOKEN
+
+# create private key and public key
+echo "Setup private and public key"
+openssl genrsa -out private 2048
+chmod 0400 private
+openssl rsa -in private -out public -pubout
+# value of DKIM dns record
+echo "DKIM DNS TXT Record"
+echo "DNS Selector: fyipe._domainkey"
+echo "DNS Value: v=DKIM1;p=$(grep -v '^-' public | tr -d '\n')"
+export PRIVATE_KEY=$(cat private | base64)
+# generate tls_cert.pem and tls_key.pem files with there keys
+echo "Setup tls_cert and tls_key"
+openssl req -x509 -nodes -days 2190 -newkey rsa:2048 -keyout tls_key.pem -out tls_cert.pem -subj "/C=US/ST=Massachusetts/L=Boston/O=Hackerbay/CN=globalminimalism.com"
+# Encode your tls to base64 and export it
+export TLS_KEY=$(cat tls_key.pem | base64)
+export TLS_CERT=$(cat tls_cert.pem | base64)
