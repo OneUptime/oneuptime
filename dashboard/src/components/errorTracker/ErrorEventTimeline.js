ffa12d7eec664b4bb5def771625db6d43f83d4f6
Zadat Olayinka <bundayy@outlook.com>
2021-02-04 17:20:01 +0100

    error evennt time

--- dashboard/src/components/errorTracker/ErrorEventTimeline.js
+++ dashboard/src/components/errorTracker/ErrorEventTimeline.js
@@ -2,15 +2,13 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import PropTypes from 'prop-types';
 import ShouldRender from '../basic/ShouldRender';
-import TooltipMini from '../basic/TooltipMini';
+import momentTz from 'moment-timezone';
+import { currentTimeZone } from '../basic/TimezoneArray';
 
 class ErrorEventTimeline extends Component {
     constructor(props) {
         super(props);
         this.props = props;
-        this.state = {
-            isAbsolute: true,
-        };
     }
     renderTimelineContent = timeline => {
         let rendered = '';
@@ -148,51 +146,16 @@ class ErrorEventTimeline extends Component {
                                             </div>
                                         </td>
                                         <td
-                                            className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                            className="Table-cell Table-cell--align--right Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
                                             style={{
                                                 height: '1px',
                                                 minWidth: '100px',
                                             }}
                                         >
-                                            {/* <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                            <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
                                                 <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
-                                                    <span>Priority</span>
+                                                    <span>Time</span>
                                                 </span>
-                                            </div> */}
-                                            <div
-                                                className="db-ListViewItem-cellContent Padding-vertical--8 Flex-flex Flex-justifyContent--spaceBetween Flex-alignItems--center"
-                                                style={{
-                                                    height: '100%',
-                                                }}
-                                            >
-                                                <TooltipMini
-                                                    title={`Switch to ${
-                                                        this.state.isAbsolute
-                                                            ? 'absolute'
-                                                            : 'relative'
-                                                    }`}
-                                                    content={
-                                                        <img
-                                                            onClick={() =>
-                                                                this.setState(
-                                                                    this.setState(
-                                                                        state => ({
-                                                                            isAbsolute: !state.isAbsolute,
-                                                                        })
-                                                                    )
-                                                                )
-                                                            }
-                                                            src="/dashboard/assets/img/http.svg"
-                                                            alt=""
-                                                            style={{
-                                                                height: '25px',
-                                                                width: '25px',
-                                                                padding: '5px',
-                                                            }}
-                                                        />
-                                                    }
-                                                />
-                                                Time
                                             </div>
                                         </td>
 
@@ -413,61 +376,32 @@ class ErrorEventTimeline extends Component {
                                                             </div>
                                                         </td>
                                                         <td
-                                                            className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--wrap db-ListViewItem-cell db-ListViewItem-cell--breakWord"
+                                                            className="Table-cell Table-cell--align--right  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
                                                             style={{
                                                                 height: '1px',
+                                                                minWidth:
+                                                                    '100px',
                                                             }}
                                                         >
-                                                            <div className="db-ListViewItem-link">
-                                                                <div className="db-ListViewItem-cellContent Box-root Padding-horizontal--2 Padding-vertical--8 Flex-flex Flex-alignItems--center">
-                                                                    <div>
-                                                                        <div className="Box-root Flex-flex">
-                                                                            <div className="db-RadarRulesListUserName Box-root Flex-flex Flex-alignItems--center Flex-direction--row Flex-justifyContent--flexStart">
-                                                                                <div className="Box-root Flex-inlineFlex Flex-alignItems--center Padding-horizontal--8 Padding-vertical--2">
-                                                                                    <span className="Text-display--inline Text-fontSize--14 Text-lineHeight--16 Text-wrap--noWrap">
-                                                                                        <ShouldRender
-                                                                                            if={
-                                                                                                this
-                                                                                                    .state
-                                                                                                    .isAbsolute
-                                                                                            }
-                                                                                        >
-                                                                                            <span>
-                                                                                                {moment(
-                                                                                                    timeline.timestamp
-                                                                                                ).format(
-                                                                                                    'h:mm:ss a'
-                                                                                                )}
-                                                                                            </span>
-                                                                                        </ShouldRender>
-                                                                                        <ShouldRender
-                                                                                            if={
-                                                                                                !this
-                                                                                                    .state
-                                                                                                    .isAbsolute
-                                                                                            }
-                                                                                        >
-                                                                                            <span>
-                                                                                                {' '}
-                                                                                                {moment(
-                                                                                                    timeline.timestamp
-                                                                                                ).diff(
-                                                                                                    moment(
-                                                                                                        errorEventDetails.createdAt
-                                                                                                    ),
-                                                                                                    'seconds'
-                                                                                                )}
-
-                                                                                                s
-                                                                                            </span>
-                                                                                        </ShouldRender>
-                                                                                    </span>
-                                                                                </div>
-                                                                            </div>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
+                                                            <span className="Text-display--inline Text-fontSize--14 Text-lineHeight--16 Text-wrap--noWrap">
+                                                                <span>
+                                                                    {currentTimeZone
+                                                                        ? momentTz(
+                                                                              timeline.timestamp
+                                                                          )
+                                                                              .tz(
+                                                                                  currentTimeZone
+                                                                              )
+                                                                              .format(
+                                                                                  'lll'
+                                                                              )
+                                                                        : moment(
+                                                                              timeline.timestamp
+                                                                          ).format(
+                                                                              'lll'
+                                                                          )}
+                                                                </span>
+                                                            </span>
                                                         </td>
                                                     </tr>
                                                 );
