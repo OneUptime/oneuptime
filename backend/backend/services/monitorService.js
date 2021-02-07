52591017acffb48f7421bd677bd1875b1ac2f4fb
deityhub <judoc96@gmail.com>
2021-02-07 15:58:54 +0100

    include kubernetes monitor type

--- backend/backend/services/monitorService.js
+++ backend/backend/services/monitorService.js
@@ -508,6 +508,7 @@ module.exports = {
                                                 'device',
                                                 'api',
                                                 'incomingHttpRequest',
+                                                'kubernetes',
                                             ],
                                         },
                                     },
