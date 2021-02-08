bfe20a1d4577562ca4203f167136d3cc1a71642c
deityhub <judoc96@gmail.com>
2021-02-08 13:35:55 +0100

    migration script to fix old monitor collection not being probed

---
+++ init-script/scripts/3.0.7800.js
@@ -0,0 +1,23 @@
+const { find, update } = require('../util/db');
+
+const monitorCollection = 'monitors';
+
+async function run() {
+    // get all monitors that have a monitorCategoryId
+    const monitors = await find(monitorCollection, {
+        disabled: { $exists: false },
+        deleted: false,
+    });
+
+    for (const monitor of monitors) {
+        await update(
+            monitorCollection,
+            { _id: monitor._id },
+            { disabled: false }
+        );
+    }
+
+    return `Script ran for ${monitors.length} monitors`;
+}
+
+module.exports = run;
