c36661765c5c6623870425b033ae81880fcc9716
augustinebest <augustusbest00@gmaile.com>
2021-02-10 22:02:30 +0100

    imported serviceWorker

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -5,11 +5,10 @@ import ReactGA from 'react-ga';
 import { ThroughProvider } from 'react-through';
 import ErrorBoundary from './components/basic/ErrorBoundary';
 import { render } from 'react-dom';
-// import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import store, { history, isServer } from './store';
 import App from './App';
 import './index.css';
-import * as sw from './useNotification.js'
 
 if (!isServer) {
     ReactGA.initialize('UA-115085157-1');
@@ -31,5 +30,4 @@ render(
 );
 
 // this will enable the app to work offline and load faster
-// serviceWorker.register();
-sw.registerServiceWorker();
+serviceWorker.register();
