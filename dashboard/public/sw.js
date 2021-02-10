ded2632f8f62c255475c1e5f200846feb8f225f3
augustinebest <augustusbest00@gmaile.com>
2021-02-10 22:01:48 +0100

    listening for push event in the service worker

--- dashboard/public/sw.js
+++ dashboard/public/sw.js
@@ -2,7 +2,7 @@
 self.addEventListener("push", e => {
   const data = e.data.json();
   self.registration.showNotification(data.title, {
-    body: "Notified by Augustine Igwe!!!",
+    body: data.body,
     icon: "https://www.dropbox.com/s/dwawm02f1toxnm8/Fyipe-Icon.png?dl=0&raw=1"
   });
 });
