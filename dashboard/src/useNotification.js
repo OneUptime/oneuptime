1edb5b19b0e9b1bfbc79d5f84a7dca8780d8cc87
augustinebest <augustusbest00@gmaile.com>
2021-02-10 21:59:04 +0100

    exported askUserPermission and getTheSubscription func

--- dashboard/src/useNotification.js
+++ dashboard/src/useNotification.js
@@ -1,58 +1,24 @@
+import { registerService } from './serviceWorker';
 const publicVapidKey =
-    "BFAPbOTTU14VbTe_dnoYlVnOPLKUNm8GYmC50n3i4Ps64sk1Xqx8e894Clrscn1L2PsQ8-l4SsJVw7NRg4cx69Y";
+    process.env.REACT_APP_VAPID_PUBLIC_KEY;
 
 // Adk for permission
 export async function askUserPermission() {
     return await Notification.requestPermission();
 }
 
-// Check for service worker
-export function registerServiceWorker() {
-    if ("serviceWorker" in navigator) {
-        // console.log('sending this asap')
-        // send().catch(err => console.log('BIG ERROR', err));
-    }
-}
-
 export async function getTheSubscription() {
-    // Register Service Worker
-    // console.log("Registering service worker...");
-    const register = await navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/sw.js`, { scope: `${process.env.PUBLIC_URL}/` });
-    // console.log("Service Worker Registered...");
-    await navigator.serviceWorker.ready;
-    // Register Push
-    // console.log("Registering Push...");
-    const subscription = await register.pushManager.subscribe({
+    const subscription = await registerService.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
     });
-    // console.log("Push Registered...");
-    // console.log('Sending Push...2222', subscription)
     return subscription;
-}
-
-// getTheSubscription()
-
-// Register SW, Register Push, Send Push
-async function send() {
-    const subscription = await getTheSubscription()
-
-    // Send Push Notification
-    console.log("Sending Push...", subscription);
-    await fetch("http://localhost:3002/subscribe", {
-        method: "POST",
-        body: JSON.stringify(subscription),
-        headers: {
-            "content-type": "application/json"
-        }
-    });
-    // console.log("Push Sent...", subscription);
 }
 
 function urlBase64ToUint8Array(base64String) {
     const padding = "=".repeat((4 - base64String.length % 4) % 4);
     const base64 = (base64String + padding)
-        .replace(/\-/g, "+")
+        .replace(/-/g, "+")
         .replace(/_/g, "/");
 
     const rawData = window.atob(base64);
