c693511cf35ee3cd07671bb2e38c3ac7ef104894
deityhub <judoc96@gmail.com>
2021-02-22 18:04:05 +0100

    fix script

--- init-script/scripts/3.0.8100.js
+++ init-script/scripts/3.0.8100.js
@@ -40,79 +40,97 @@ async function run() {
     });
 
     for (const monitor of monitorsWithOldCriteria) {
-        for (const up of monitor.criteria.up) {
-            const and = [...up.and],
-                or = [...up.or];
-            const cr = { and: [], or: [] };
-            if (and && and.length > 0) {
-                and[0].match = 'all';
-            } else if (or && or.length > 0) {
-                or[0].match = 'any';
-            }
+        if (
+            monitor.criteria &&
+            monitor.criteria.up &&
+            monitor.criteria.up.length > 0
+        ) {
+            for (const up of monitor.criteria.up) {
+                const and = [...up.and],
+                    or = [...up.or];
+                const cr = { and: [], or: [] };
+                if (and && and.length > 0) {
+                    and[0].match = 'all';
+                } else if (or && or.length > 0) {
+                    or[0].match = 'any';
+                }
 
-            if (and && and.length > 0) {
-                cr.and.push(and);
-                up.and = cr;
-            }
-            if (and && and.length === 0) {
-                up.and = { and: [], or: [] };
-            }
-            if (or && or.length > 0) {
-                cr.and.push(or);
-                up.or = cr;
-            }
-            if (or && or.length === 0) {
-                up.or = { and: [], or: [] };
+                if (and && and.length > 0) {
+                    cr.and.push(and);
+                    up.and = cr;
+                }
+                if (and && and.length === 0) {
+                    up.and = { and: [], or: [] };
+                }
+                if (or && or.length > 0) {
+                    cr.and.push(or);
+                    up.or = cr;
+                }
+                if (or && or.length === 0) {
+                    up.or = { and: [], or: [] };
+                }
             }
         }
-        for (const down of monitor.criteria.down) {
-            const and = [...down.and],
-                or = [...down.or];
-            const cr = { and: [], or: [] };
-            if (and && and.length > 0) {
-                and[0].match = 'all';
-            } else if (or && or.length > 0) {
-                or[0].match = 'any';
-            }
+        if (
+            monitor.criteria &&
+            monitor.criteria.down &&
+            monitor.criteria.down.length > 0
+        ) {
+            for (const down of monitor.criteria.down) {
+                const and = [...down.and],
+                    or = [...down.or];
+                const cr = { and: [], or: [] };
+                if (and && and.length > 0) {
+                    and[0].match = 'all';
+                } else if (or && or.length > 0) {
+                    or[0].match = 'any';
+                }
 
-            if (and && and.length > 0) {
-                cr.and.push(and);
-                down.and = cr;
-            }
-            if (and && and.length === 0) {
-                down.and = { and: [], or: [] };
-            }
-            if (or && or.length > 0) {
-                cr.and.push(or);
-                down.or = cr;
-            }
-            if (or && or.length === 0) {
-                down.or = { and: [], or: [] };
+                if (and && and.length > 0) {
+                    cr.and.push(and);
+                    down.and = cr;
+                }
+                if (and && and.length === 0) {
+                    down.and = { and: [], or: [] };
+                }
+                if (or && or.length > 0) {
+                    cr.and.push(or);
+                    down.or = cr;
+                }
+                if (or && or.length === 0) {
+                    down.or = { and: [], or: [] };
+                }
             }
         }
-        for (const degraded of monitor.criteria.degraded) {
-            const and = [...degraded.and],
-                or = [...degraded.or];
-            const cr = { and: [], or: [] };
-            if (and && and.length > 0) {
-                and[0].match = 'all';
-            } else if (or && or.length > 0) {
-                or[0].match = 'any';
-            }
+        if (
+            monitor.criteria &&
+            monitor.criteria.degraded &&
+            monitor.criteria.degraded.length > 0
+        ) {
+            for (const degraded of monitor.criteria.degraded) {
+                const and = [...degraded.and],
+                    or = [...degraded.or];
+                const cr = { and: [], or: [] };
+                if (and && and.length > 0) {
+                    and[0].match = 'all';
+                } else if (or && or.length > 0) {
+                    or[0].match = 'any';
+                }
 
-            if (and && and.length > 0) {
-                cr.and.push(and);
-                degraded.and = cr;
-            }
-            if (and && and.length === 0) {
-                degraded.and = { and: [], or: [] };
-            }
-            if (or && or.length > 0) {
-                cr.and.push(or);
-                degraded.or = cr;
-            }
-            if (or && or.length === 0) {
-                degraded.or = { and: [], or: [] };
+                if (and && and.length > 0) {
+                    cr.and.push(and);
+                    degraded.and = cr;
+                }
+                if (and && and.length === 0) {
+                    degraded.and = { and: [], or: [] };
+                }
+                if (or && or.length > 0) {
+                    cr.and.push(or);
+                    degraded.or = cr;
+                }
+                if (or && or.length === 0) {
+                    degraded.or = { and: [], or: [] };
+                }
             }
         }
 
