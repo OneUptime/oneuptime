d9181a3e1491b7dff6613c4a4275e591c45dfd82
Zadat Olayinka <bundayy@outlook.com>
2021-03-03 09:45:12 +0100

    tracker test

--- php-sdk/tests/Fyipe/TrackerTest.php
+++ php-sdk/tests/Fyipe/TrackerTest.php
@@ -93,168 +93,168 @@ class TrackerTest extends TestCase
         static::$ready = true;
     }
 
-    // public function test_should_take_in_custom_timeline_event()
-    // {
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
-    //     $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
-    //     $timeline = $tracker->getTimeline();
-    //     $this->assertIsArray($timeline);
-    //     $this->assertCount(1, $timeline);
-    //     $this->assertEquals($this->customTimeline->category, $timeline[0]->category);
-    // }
-    // public function test_should_ensure_timeline_event_contains_eventId_and_timestamp()
-    // {
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
-    //     $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
-    //     $timeline = $tracker->getTimeline();
-    //     $this->assertIsString($timeline[0]->eventId);
-    //     $this->assertIsNumeric($timeline[0]->timestamp);
-    // }
-    // public function test_should_ensure_different_timeline_event_have_the_same_eventId() {
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
-    //     $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
-    //     $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, 'error');
-    //     $timeline = $tracker->getTimeline();
-    //     $this->assertCount(2, $timeline); // two timeline events
-    //     $this->assertEquals($timeline[0]->eventId, $timeline[1]->eventId); // their eventId is the same, till there is an error sent to the server
-    // }
-    // public function test_should_ensure_max_timline_cant_be_set_as_a_negative_number() {
-    //     $options = new stdClass();
-    //     $options->maxTimeline = -5;
+    public function test_should_take_in_custom_timeline_event()
+    {
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
+        $timeline = $tracker->getTimeline();
+        $this->assertIsArray($timeline);
+        $this->assertCount(1, $timeline);
+        $this->assertEquals($this->customTimeline->category, $timeline[0]->category);
+    }
+    public function test_should_ensure_timeline_event_contains_eventId_and_timestamp()
+    {
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
+        $timeline = $tracker->getTimeline();
+        $this->assertIsString($timeline[0]->eventId);
+        $this->assertIsNumeric($timeline[0]->timestamp);
+    }
+    public function test_should_ensure_different_timeline_event_have_the_same_eventId() {
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
+        $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, 'error');
+        $timeline = $tracker->getTimeline();
+        $this->assertCount(2, $timeline); // two timeline events
+        $this->assertEquals($timeline[0]->eventId, $timeline[1]->eventId); // their eventId is the same, till there is an error sent to the server
+    }
+    public function test_should_ensure_max_timline_cant_be_set_as_a_negative_number() {
+        $options = new stdClass();
+        $options->maxTimeline = -5;
 
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key, [$options]);
-    //     $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
-    //     $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, 'error');
-    //     $timeline = $tracker->getTimeline();
-    //     $this->assertCount(2, $timeline);  // two timeline events
-    // }
-    // public function test_should_ensure_new_timeline_event_after_max_timeline_are_discarded() {
-    //     $options = new stdClass();
-    //     $options->maxTimeline = 2;
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key, [$options]);
+        $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
+        $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, 'error');
+        $timeline = $tracker->getTimeline();
+        $this->assertCount(2, $timeline);  // two timeline events
+    }
+    public function test_should_ensure_new_timeline_event_after_max_timeline_are_discarded() {
+        $options = new stdClass();
+        $options->maxTimeline = 2;
 
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key, [$options]);
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key, [$options]);
 
-    //     $customTimeline2 = new stdClass();
-    //     $customTimeline2->category = 'logout';
-    //     $customTimeline2->type = 'success';
-    //     $content = new stdClass();
-    //     $content->message = 'test-content';
-    //     $customTimeline2->content = $content;
+        $customTimeline2 = new stdClass();
+        $customTimeline2->category = 'logout';
+        $customTimeline2->type = 'success';
+        $content = new stdClass();
+        $content->message = 'test-content';
+        $customTimeline2->content = $content;
 
-    //     // add 3 timelinee events
-    //     $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
-    //     $tracker->addToTimeline($customTimeline2->category, $customTimeline2->content, $customTimeline2->type);
-    //     $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, 'debug');
+        // add 3 timelinee events
+        $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, $this->customTimeline->type);
+        $tracker->addToTimeline($customTimeline2->category, $customTimeline2->content, $customTimeline2->type);
+        $tracker->addToTimeline($this->customTimeline->category, $this->customTimeline->content, 'debug');
 
-    //     $timeline = $tracker->getTimeline();
+        $timeline = $tracker->getTimeline();
         
-    //     $this->assertEquals(sizeof($timeline), $options->maxTimeline);
-    //     $this->assertEquals($timeline[0]->type, $this->customTimeline->type);
-    //     $this->assertEquals($timeline[1]->category, $customTimeline2->category);
-    // }
-    // public function test_should_add_tags() {
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
-    //     $tag = new stdClass();
-    //     $tag->key = 'location';
-    //     $tag->value = 'Ontario';
-    //     $tracker->setTag($tag->key, $tag->value);
+        $this->assertEquals(sizeof($timeline), $options->maxTimeline);
+        $this->assertEquals($timeline[0]->type, $this->customTimeline->type);
+        $this->assertEquals($timeline[1]->category, $customTimeline2->category);
+    }
+    public function test_should_add_tags() {
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $tag = new stdClass();
+        $tag->key = 'location';
+        $tag->value = 'Ontario';
+        $tracker->setTag($tag->key, $tag->value);
         
-    //     $availableTags = $tracker->getTags();
-    //     $this->assertIsArray($availableTags);
-    //     $this->assertCount(1, $availableTags);
-    //     $this->assertEquals($tag->key, $availableTags[0]->key);
-    // }
-    // public function test_should_add_multiple_tags() {
+        $availableTags = $tracker->getTags();
+        $this->assertIsArray($availableTags);
+        $this->assertCount(1, $availableTags);
+        $this->assertEquals($tag->key, $availableTags[0]->key);
+    }
+    public function test_should_add_multiple_tags() {
         
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
-    //     $tags = [];
-    //     $tag = new stdClass();
-    //     $tag->key = 'location';
-    //     $tag->value = 'Ontario';
-    //     array_push($tags, $tag);
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $tags = [];
+        $tag = new stdClass();
+        $tag->key = 'location';
+        $tag->value = 'Ontario';
+        array_push($tags, $tag);
 
-    //     $tagB = new stdClass();
-    //     $tagB->key = 'city';
-    //     $tagB->value = 'Houston';
-    //     array_push($tags, $tagB);
+        $tagB = new stdClass();
+        $tagB->key = 'city';
+        $tagB->value = 'Houston';
+        array_push($tags, $tagB);
 
-    //     $tagC = new stdClass();
-    //     $tagC->key = 'device';
-    //     $tagC->value = 'iPhone';
-    //     array_push($tags, $tagC);
+        $tagC = new stdClass();
+        $tagC->key = 'device';
+        $tagC->value = 'iPhone';
+        array_push($tags, $tagC);
 
-    //     $tracker->setTags($tags);
+        $tracker->setTags($tags);
 
-    //     $availableTags = $tracker->getTags();
-    //     $this->assertIsArray($availableTags);
-    //     $this->assertCount(sizeof($tags), $availableTags);
-    // }
-    // public function test_should_overwrite_existing_keys_to_avoid_duplicate_tags() {
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
-    //     $tags = [];
-    //     $tag = new stdClass();
-    //     $tag->key = 'location';
-    //     $tag->value = 'Ontario';
-    //     array_push($tags, $tag);
+        $availableTags = $tracker->getTags();
+        $this->assertIsArray($availableTags);
+        $this->assertCount(sizeof($tags), $availableTags);
+    }
+    public function test_should_overwrite_existing_keys_to_avoid_duplicate_tags() {
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $tags = [];
+        $tag = new stdClass();
+        $tag->key = 'location';
+        $tag->value = 'Ontario';
+        array_push($tags, $tag);
 
-    //     $tagB = new stdClass();
-    //     $tagB->key = 'city';
-    //     $tagB->value = 'Houston';
-    //     array_push($tags, $tagB);
+        $tagB = new stdClass();
+        $tagB->key = 'city';
+        $tagB->value = 'Houston';
+        array_push($tags, $tagB);
 
-    //     $tagC = new stdClass();
-    //     $tagC->key = 'location';
-    //     $tagC->value = 'Paris';
-    //     array_push($tags, $tagC);
+        $tagC = new stdClass();
+        $tagC->key = 'location';
+        $tagC->value = 'Paris';
+        array_push($tags, $tagC);
 
-    //     $tagD = new stdClass();
-    //     $tagD->key = 'device';
-    //     $tagD->value = 'iPhone';
-    //     array_push($tags, $tagD);
+        $tagD = new stdClass();
+        $tagD->key = 'device';
+        $tagD->value = 'iPhone';
+        array_push($tags, $tagD);
 
-    //     $tagE = new stdClass();
-    //     $tagE->key = 'location';
-    //     $tagE->value = 'London';
-    //     array_push($tags, $tagE);
+        $tagE = new stdClass();
+        $tagE->key = 'location';
+        $tagE->value = 'London';
+        array_push($tags, $tagE);
 
-    //     $tracker->setTags($tags);
+        $tracker->setTags($tags);
 
-    //     $availableTags = $tracker->getTags();
-    //     $this->assertIsArray($availableTags);
-    //     $this->assertCount(3, $availableTags); // only 3 unique tags
-    //     $this->assertEquals($tagE->key, $availableTags[0]->key);
-    //     $this->assertEquals($tagE->value, $availableTags[0]->value); // latest value for that tag location
-    // }
-    // public function test_should_create_fingerprint_as_message_for_error_capture_without_any_fingerprint() {
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $availableTags = $tracker->getTags();
+        $this->assertIsArray($availableTags);
+        $this->assertCount(3, $availableTags); // only 3 unique tags
+        $this->assertEquals($tagE->key, $availableTags[0]->key);
+        $this->assertEquals($tagE->value, $availableTags[0]->value); // latest value for that tag location
+    }
+    public function test_should_create_fingerprint_as_message_for_error_capture_without_any_fingerprint() {
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
 
-    //     $errorMessage = 'Uncaught Exception';
-    //     $tracker->captureMessage($errorMessage);
-    //     $event = $tracker->getCurrentEvent();
-    //     $this->assertEquals($event->fingerprint[0], $errorMessage);
-    // }
-    // public function test_should_use_defined_fingerprint_array_for_error_capture_with_fingerprint() {
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $errorMessage = 'Uncaught Exception';
+        $tracker->captureMessage($errorMessage);
+        $event = $tracker->getCurrentEvent();
+        $this->assertEquals($event->fingerprint[0], $errorMessage);
+    }
+    public function test_should_use_defined_fingerprint_array_for_error_capture_with_fingerprint() {
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
 
         
-    //     $fingerprints = ['custom', 'errors'];
-    //     $tracker->setFingerPrint($fingerprints);
-    //     $errorMessage = 'Uncaught Exception';
-    //     $tracker->captureMessage($errorMessage);
-    //     $event = $tracker->getCurrentEvent();
-    //     $this->assertEquals($event->fingerprint[0], $fingerprints[0]);
-    //     $this->assertEquals($event->fingerprint[1], $fingerprints[1]);
-    // }
-    // public function test_should_use_defined_fingerprint_string_for_error_capture_with_fingerprint() {
-    //     $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
+        $fingerprints = ['custom', 'errors'];
+        $tracker->setFingerPrint($fingerprints);
+        $errorMessage = 'Uncaught Exception';
+        $tracker->captureMessage($errorMessage);
+        $event = $tracker->getCurrentEvent();
+        $this->assertEquals($event->fingerprint[0], $fingerprints[0]);
+        $this->assertEquals($event->fingerprint[1], $fingerprints[1]);
+    }
+    public function test_should_use_defined_fingerprint_string_for_error_capture_with_fingerprint() {
+        $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
 
-    //     $fingerprint = 'custom-fingerprint';
-    //     $tracker->setFingerPrint($fingerprint);
-    //     $errorMessage = 'Uncaught Exception';
-    //     $tracker->captureMessage($errorMessage);
-    //     $event = $tracker->getCurrentEvent();
-    //     $this->assertEquals($event->fingerprint[0], $fingerprint);
-    // }
+        $fingerprint = 'custom-fingerprint';
+        $tracker->setFingerPrint($fingerprint);
+        $errorMessage = 'Uncaught Exception';
+        $tracker->captureMessage($errorMessage);
+        $event = $tracker->getCurrentEvent();
+        $this->assertEquals($event->fingerprint[0], $fingerprint);
+    }
     public function test_should_create_an_event_ready_for_the_server() {
         $tracker = new Fyipe\FyipeTracker($this->apiUrl, static::$errorTracker->_id, static::$errorTracker->key);
 
