b3ac7058c54df56efa764e5d2e34fa137a606e21
Zadat Olayinka <bundayy@outlook.com>
2021-03-03 18:54:17 +0100

    option dump

--- php-sdk/src/Fyipe/FyipeTimelineManager.php
+++ php-sdk/src/Fyipe/FyipeTimelineManager.php
@@ -17,9 +17,12 @@ class FyipeTimelineManager
      * @param array $options
      */
     public function __construct($options){
+        var_dump($options);
         $this->options = $options;
+        var_dump($this->options);
     }
     private function addItemToTimeline($item) {
+        var_dump($this->options);
         // get the size of the stack
         if (sizeof($this->timeLineStack) === $this->options['maxTimeline']) {
             return; // It discards new timline update once maximum is reached
