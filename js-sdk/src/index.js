d0054618929abeff50a1b5c8cf3bcbfc42bf881c
Dave <callremz@gmail.com>
2021-03-06 19:30:25 +0100

    update build script config

--- js-sdk/src/index.js
+++ js-sdk/src/index.js
@@ -1,5 +1,5 @@
 import Logger from './logger.js';
 import ErrorTracker from './tracker.js';
-import serverMonitor from './cli/server-monitor/lib/api';
+const serverMonitor = require('./cli/server-monitor/lib/api');
 
 export default { Logger, ErrorTracker, serverMonitor };
