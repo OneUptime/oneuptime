cd047db5097e40d62eea36b79a90a4253f4e50a7
Nawaz Dhandala <nawazdhandala@outlook.com>
2021-03-06 13:24:28 +0000

    update all

--- ci/scripts/version-setup.sh
+++ ci/scripts/version-setup.sh
@@ -2,36 +2,26 @@
 echo "
 This script changes version of every project
 "
-
 function version {
   cd $1
-  npm version "3.0.2" || echo "Not a node project. Unable to version $1"
+  npm version "3.0.$CI_PIPELINE_IID"
   cd ..
 }
 
-# curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-# sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
-# Version root. 
+version dashboard
+version accounts 
+version backend
+version home 
+version status-page 
+version api-docs
+version server-monitor
+version probe
+version admin-dashboard
+version init-script
+version licensing
+version helm-chart
+version js-sdk
 version .
-
-for entry in "$(ls .)"
-do
-  # Version subprojects. 
-  version "$entry"
-done
-
-# version dashboard
-# version accounts 
-# version backend
-# version home 
-# version status-page 
-# version api-docs
-# version server-monitor
-# version probe
-# version admin-dashboard
-# version init-script
-# version licensing
-# version helm-chart
-# version js-sdk
-# version .
