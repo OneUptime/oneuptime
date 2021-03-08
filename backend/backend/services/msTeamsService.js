4368a4ee55ee892e5f47545f0aa1850533bdee91
atakpu ikhide godson <ikhide.atakpu@gmail.com>
2021-03-08 06:53:31 +0100

    create function

--- backend/backend/services/msTeamsService.js
+++ backend/backend/services/msTeamsService.js
@@ -194,6 +194,116 @@ module.exports = {
             throw error;
         }
     },
+    sendIncidentNoteNotification: async function(
+        projectId,
+        incident,
+        monitor,
+        incidentStatus,
+        component,
+        duration
+    ) {
+        try {
+            const self = this;
+            let response;
+            const project = await ProjectService.findOneBy({ _id: projectId });
+            if (project && project.parentProjectId) {
+                projectId = project.parentProjectId._id;
+            }
+            let query = {
+                projectId: projectId,
+                integrationType: 'msteams',
+                monitorId: monitor._id,
+            };
+            if (incidentStatus === INCIDENT_RESOLVED) {
+                query = {
+                    ...query,
+                    'notificationOptions.incidentResolved': true,
+                };
+            } else if (incidentStatus === INCIDENT_CREATED) {
+                query = {
+                    ...query,
+                    'notificationOptions.incidentCreated': true,
+                };
+            } else if (incidentStatus === INCIDENT_ACKNOWLEDGED) {
+                query = {
+                    ...query,
+                    'notificationOptions.incidentAcknowledged': true,
+                };
+            } else {
+                return;
+            }
+            const integrations = await IntegrationService.findBy(query);
+
+            for (const integration of integrations) {
+                response = await self.notify(
+                    project,
+                    monitor,
+                    incident,
+                    integration,
+                    component,
+                    duration
+                );
+            }
+            return response;
+        } catch (error) {
+            ErrorService.log('msTeamsService.sendNotification', error);
+            throw error;
+        }
+    },
+
+    // send notification to slack workspace channels
+    async noteNotify(
+        project,
+        monitor,
+        incident,
+        integration,
+        component,
+        duration
+    ) {
+        try {
+            const uri = `${global.dashboardHost}/project/${component.projectId._id}/${component._id}/incidents/${incident._id}`;
+            const yellow = '#fedc56';
+            let payload;
+
+            payload = {
+                '@context': 'https://schema.org/extensions',
+                // '@type': 'MessageCard',
+                // themeColor: yellow,
+                // summary: 'Incident Acknowledged',
+                // sections: [
+                //     {
+                //         activityTitle: `[Incident Acknowledged](${uri})`,
+                //         activitySubtitle: `Incident on **${
+                //             component.name
+                //         } / ${monitor.name}** is acknowledged by ${
+                //             incident.acknowledgedBy
+                //                 ? incident.acknowledgedBy.name
+                //                 : 'Fyipe'
+                //         } after being ${
+                //             incident.incidentType
+                //         } for ${duration}`,
+                //     },
+                // ],
+            };
+
+            await axios.post(
+                integration.data.endpoint,
+                {
+                    ...payload,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+
+            return 'Webhook successfully pinged';
+        } catch (error) {
+            ErrorService.log('msTeams.notify', error);
+            throw error;
+        }
+    },
 };
 
 const IntegrationService = require('./integrationService');
