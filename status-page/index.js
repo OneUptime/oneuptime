aabfd68ae5934f0bded97bbf10afe9b12dcf801c
deityhub <judoc96@gmail.com>
2021-03-10 23:42:49 +0100

    implement dynamic ssl provisioning

--- status-page/index.js
+++ status-page/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const https = require('https');
+const http = require('http');
+const net = require('net');
+const tls = require('tls');
+const fs = require('fs');
+const fetch = require('node-fetch');
+
 app.get(['/env.js', '/status-page/env.js'], function(req, res) {
     let REACT_APP_FYIPE_HOST = null;
     let REACT_APP_BACKEND_PROTOCOL = null;
@@ -40,4 +47,141 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(3006);
+async function fetchCredential(apiHost, credentialName, configPath) {
+    return new Promise((resolve, reject) => {
+        fetch(`${apiHost}/file/${credentialName}`).then(res => {
+            const dest = fs.createWriteStream(configPath);
+            res.body.pipe(dest);
+            // at this point, writing to the specified file is complete
+            dest.on('finish', async () => {
+                resolve('done writing to file');
+            });
+
+            dest.on('error', async error => {
+                reject(error);
+            });
+        });
+    });
+}
+
+const options = {
+    cert: fs.readFileSync(
+        path.resolve(process.cwd(), 'src', 'credentials', 'fyipe.com.crt')
+    ),
+    key: fs.readFileSync(
+        path.resolve(process.cwd(), 'src', 'credentials', 'fyipe.com.key')
+    ),
+    SNICallback: async function(domain, cb) {
+        let apiHost;
+        if (process.env.FYIPE_HOST) {
+            apiHost = 'https://' + process.env.FYIPE_HOST + '/api';
+        } else {
+            apiHost = 'http://localhost:3002/api';
+        }
+
+        const res = await fetch(
+            `${apiHost}/statusPage/tlsCredential?domain=${domain}`
+        ).then(res => res.json());
+
+        let certPath, privateKeyPath;
+        if (res.data) {
+            const { cert, privateKey } = res.data;
+            if (cert && privateKey) {
+                certPath = path.resolve(
+                    process.cwd(),
+                    'src',
+                    'credentials',
+                    `${cert}.crt`
+                );
+                privateKeyPath = path.resolve(
+                    process.cwd(),
+                    'src',
+                    'credentials',
+                    `${privateKey}.key`
+                );
+
+                await Promise.all([
+                    fetchCredential(apiHost, cert, certPath),
+                    fetchCredential(apiHost, privateKey, privateKeyPath),
+                ]);
+            }
+        }
+
+        if (certPath && privateKeyPath) {
+            cb(
+                null,
+                tls.createSecureContext({
+                    key: fs.readFileSync(privateKeyPath),
+                    cert: fs.readFileSync(certPath),
+                })
+            );
+        } else {
+            cb(
+                null,
+                tls.createSecureContext({
+                    cert: fs.readFileSync(
+                        path.resolve(
+                            process.cwd(),
+                            'src',
+                            'credentials',
+                            'fyipe.com.crt'
+                        )
+                    ),
+                    key: fs.readFileSync(
+                        path.resolve(
+                            process.cwd(),
+                            'src',
+                            'credentials',
+                            'fyipe.com.key'
+                        )
+                    ),
+                })
+            );
+        }
+    },
+};
+
+function createServer(opts, handler) {
+    const server = net.createServer(socket => {
+        socket.once('data', buffer => {
+            // Pause the socket
+            socket.pause();
+
+            // Determine if this is an HTTP(s) request
+            const byte = buffer[0];
+
+            let protocol;
+            if (byte === 22) {
+                protocol = 'https';
+            } else if (32 < byte && byte < 127) {
+                protocol = 'http';
+            }
+
+            const proxy = server[protocol];
+            if (proxy) {
+                // Push the buffer back onto the front of the data stream
+                socket.unshift(buffer);
+
+                // Emit the socket to the HTTP(s) server
+                proxy.emit('connection', socket);
+            }
+
+            // As of NodeJS 10.x the socket must be
+            // resumed asynchronously or the socket
+            // connection hangs, potentially crashing
+            // the process. Prior to NodeJS 10.x
+            // the socket may be resumed synchronously.
+            process.nextTick(() => socket.resume());
+        });
+    });
+
+    server.http = http.createServer(handler);
+    server.https = https.createServer(opts, handler);
+    return server;
+}
+
+const PORT = process.env.PORT || 3006;
+createServer(options, app).listen(PORT, () =>
+    // eslint-disable-next-line no-console
+    console.log('server running on port ', PORT)
+);
