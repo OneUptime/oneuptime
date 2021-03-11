4a2ba6319be1e93b7f60c1e13349a4c14cd60f10
augustinebest <augustusbest00@gmaile.com>
2021-03-11 21:05:01 +0100

    deleted sw.js

--- dashboard/public/sw.js
+++
@@ -1,9 +0,0 @@
-
-self.addEventListener("push", e => {
-  const data = e.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: "https://www.dropbox.com/s/dwawm02f1toxnm8/Fyipe-Icon.png?dl=0&raw=1"
-  });
-});
-  
