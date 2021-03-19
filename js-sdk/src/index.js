8e1793aacac62172a1030c9594d70f8be757a732
deityhub <judoc96@gmail.com>
2021-03-19 15:36:24 +0100

    switch path to server-monitor build directory

--- js-sdk/src/index.js
+++ js-sdk/src/index.js
@@ -1,5 +1,8 @@
 import Logger from './logger.js';
 import ErrorTracker from './tracker.js';
-import ServerMonitor from './cli/server-monitor/lib/api';
+// Due to issue with handling exports/imports in the server-monitor cli
+// we need to build the server-monitor project into the build folder then point to it there
+// This way we won't worry about whether we are using module/commonjs syntax
+import ServerMonitor from '../build/server-monitor/lib/api';
 
 export default { Logger, ErrorTracker, ServerMonitor };
