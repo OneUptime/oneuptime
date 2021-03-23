63e6440f39eeb424753cda757a2abb771c5dd516
adeoluwadavid <adeoluwadavid@gmail.com>
2021-03-23 14:25:26 +0100

    incidentsettings-tests

--- dashboard/src/test/puppeteer/incidentSettings.test.js
+++ dashboard/src/test/puppeteer/incidentSettings.test.js
@@ -43,8 +43,7 @@ describe('Incident Settings API', () => {
                 email,
                 password,
             };
-            await init.registerUser(user, page);
-           // await init.loginUser(user, page);
+            await init.registerUser(user, page);          
             await init.addMonitorToComponent(componentName, monitorName, page);
         });
     });
@@ -67,12 +66,12 @@ describe('Incident Settings API', () => {
                 await page.click('#more');
                 await page.waitForSelector('#incidentSettings');
                 await page.click('#incidentSettings');
-                await page.waitForSelector('input[name=title]');
-                //await page.waitForTimeout(3000);
+                await page.waitForSelector('input[name=title]');                
                 const priorityFieldValue = await page.$eval(
                     '#incidentPriority',
                     e => e.textContent
                 );
+                
                 expect(priorityFieldValue).toEqual('High');
                 const titleFieldValue = await page.$eval(
                     'input[name=title]',
@@ -89,60 +88,7 @@ describe('Incident Settings API', () => {
             });
         },
         operationTimeOut
-    );
-
-    // test(
-    //     'Should not set the default incident priority to a removed value.',
-    //     async () => {
-    //         return await cluster.execute(null, async ({ page }) => {
-    //             //Create a new priority
-    //             const priorityName = utils.generateRandomString();
-    //             await page.goto(utils.DASHBOARD_URL, {
-    //                 waitUntil: 'networkidle0',
-    //             });
-    //             await page.waitForSelector('#projectSettings');
-    //             await page.click('#projectSettings');
-    //             await page.waitForSelector('#more');
-    //             await page.click('#more');
-    //             await page.waitForSelector('#incidentSettings');
-    //             await page.click('#incidentSettings');
-    //             await page.waitForSelector('#addNewPriority');
-    //             await page.click('#addNewPriority');
-    //             await page.waitForSelector('#CreateIncidentPriority');
-    //             await page.type('input[name=name]', priorityName);
-    //             await page.click('#CreateIncidentPriority');
-    //             await page.waitForTimeout(3000);
-    //             await page.reload({
-    //                 waitUntil: 'networkidle0',
-    //             });
-
-    //             //Select the priority in incidentSettings form
-    //             await init.selectByText(
-    //                 '#incidentPriority',
-    //                 priorityName,
-    //                 page
-    //             );
-    //             //Delete the new incident priority.
-    //             const deleteButtonLastRowIndentifier =
-    //                 '#incidentPrioritiesList>div>div>div>div.bs-ObjectList-row:last-of-type>div:nth-child(2)>div>div:nth-child(2)>button';
-    //             await page.click(deleteButtonLastRowIndentifier);
-    //             await page.waitForSelector('#RemoveIncidentPriority');
-    //             await page.click('#RemoveIncidentPriority');
-    //             await page.waitForTimeout(3000);
-    //             //Try to save the incident settings
-    //             await page.click('#saveButton');
-    //             await page.waitForSelector('#errorInfo');
-    //             const errorMessage = await page.$eval(
-    //                 '#errorInfo',
-    //                 e => e.textContent
-    //             );
-    //             expect(errorMessage).toEqual(
-    //                 `Incident priority doesn't exist.`
-    //             );
-    //         });
-    //     },
-    //     operationTimeOut
-    // );
+    );   
 
     test(
         'Should not delete default priority',   
@@ -222,8 +168,94 @@ describe('Incident Settings API', () => {
             });
         },
         operationTimeOut
-    );   
-    
+    );      
+
+    test(
+        'Should fill title, description and priority fields on the incident creation form with the default values',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {
+                await init.navigateToMonitorDetails(
+                    componentName,
+                    monitorName,
+                    page
+                );
+                await page.reload({
+                    waitUntil: 'networkidle0',
+                });
+                await page.waitForSelector(
+                    `#monitorCreateIncident_${monitorName}`
+                );
+                await page.click(`#monitorCreateIncident_${monitorName}`);
+                await page.waitForSelector('#title');
+               
+                const priorityFieldValue = await page.$eval(
+                    '#incidentPriority',
+                    e => e.textContent
+                );
+                expect(priorityFieldValue).toEqual('Low');
+                const titleFieldValue = await page.$eval(
+                    '#title',
+                    e => e.value
+                );
+                expect(titleFieldValue).toEqual(
+                    inctidentTitleAfterSubstitution
+                );
+                const descriptionFieldValue = await page.$eval(
+                    '.ace_layer.ace_text-layer',
+                    e => e.textContent
+                );
+                expect(descriptionFieldValue).toEqual(
+                    inctidentDescriptionAfterSubstitution
+                );
+                await init.selectByText('#incidentType', incidentType, page);
+                await page.click('#createIncident');
+                await page.waitForSelector('#closeIncident_0');
+                await page.click('#closeIncident_0');
+            });
+        },
+        operationTimeOut
+    );
+
+    test(
+        'Should substitute variables in title, description when an incident is created',
+        async () => {
+            return await cluster.execute(null, async ({ page }) => {                
+                // Since the incident was created in the previous test and it is only one, navigating to component details still gives access to the created incident
+                await init.navigateToComponentDetails(
+                    componentName,                    
+                    page
+                );                
+                const incidentTitleSelector =
+                    '#incidentTitle';
+                const incidentDescriptionSelector =
+                    '#incidentDescription';
+                const incidentPrioritySelector =
+                    '#incidentPriority';
+
+                await page.waitForSelector(incidentTitleSelector);
+                const title = await page.$eval(
+                    incidentTitleSelector,
+                    e => e.textContent
+                );
+                const description = await page.$eval(
+                    incidentDescriptionSelector,
+                    e => e.textContent
+                );
+                const incidentPriority = await page.$eval(
+                    incidentPrioritySelector,
+                    e => e.textContent
+                );
+                expect(title).toMatch(inctidentTitleAfterSubstitution);
+                expect(description).toMatch(
+                    inctidentDescriptionAfterSubstitution
+                );
+                expect(incidentPriority).toMatch('Low');
+            });
+        },
+        operationTimeOut
+    );    
+
+     
     test(
         'Should delete non-default priority',
         async () => {
@@ -253,147 +285,6 @@ describe('Incident Settings API', () => {
         },
         operationTimeOut
     )
-
-    // test(
-    //     'Should fill title, description and priority fields on the incident creation form with the default values',
-    //     async () => {
-    //         return await cluster.execute(null, async ({ page }) => {
-    //             await init.navigateToMonitorDetails(
-    //                 componentName,
-    //                 monitorName,
-    //                 page
-    //             );
-    //             await page.reload({
-    //                 waitUntil: 'networkidle0',
-    //             });
-    //             await page.waitForSelector(
-    //                 `#monitorCreateIncident_${monitorName}`
-    //             );
-    //             await page.click(`#monitorCreateIncident_${monitorName}`);
-    //             await page.waitForSelector('#title');
-    //             //await page.waitForTimeout(3000);
-    //             const priorityFieldValue = await page.$eval(
-    //                 '#incidentPriority',
-    //                 e => e.textContent
-    //             );
-    //             expect(priorityFieldValue).toEqual('Low');
-    //             const titleFieldValue = await page.$eval(
-    //                 '#title',
-    //                 e => e.value
-    //             );
-    //             expect(titleFieldValue).toEqual(
-    //                 inctidentTitleAfterSubstitution
-    //             );
-    //             const descriptionFieldValue = await page.$eval(
-    //                 '.ace_layer.ace_text-layer',
-    //                 e => e.textContent
-    //             );
-    //             expect(descriptionFieldValue).toEqual(
-    //                 inctidentDescriptionAfterSubstitution
-    //             );
-    //             await init.selectByText('#incidentType', incidentType, page);
-    //             await page.click('#createIncident');
-    //             await page.waitForSelector('#closeIncident_0');
-    //             await page.click('#closeIncident_0');
-    //         });
-    //     },
-    //     operationTimeOut
-    // );
-
-    // test(
-    //     'Should substitute variables in title, description when an incident is created',
-    //     async () => {
-    //         return await cluster.execute(null, async ({ page }) => {
-    //             await init.navigateToMonitorDetails(
-    //                 componentName,
-    //                 monitorName,
-    //                 page
-    //             );
-    //             await page.waitForSelector(
-    //                 'tr.incidentListItem:first-of-type > td:nth-of-type(3)'
-    //             );
-    //             await page.click(
-    //                 'tr.incidentListItem:first-of-type > td:nth-of-type(3)'
-    //             );
-    //             const incidentTitleSelector =
-    //                 '.bs-Fieldset-rows > .bs-right-side > .bs-content:nth-of-type(1) > div';
-    //             const incidentDescriptionSelector =
-    //                 '.bs-Fieldset-rows > .bs-right-side > .bs-content:nth-of-type(2) > div';
-    //             const incidentPrioritySelector =
-    //                 '.bs-Fieldset-rows > .bs-right-side > .bs-content:nth-of-type(4) > div';
-
-    //             await page.waitForSelector(incidentTitleSelector);
-    //             const title = await page.$eval(
-    //                 incidentTitleSelector,
-    //                 e => e.textContent
-    //             );
-    //             const description = await page.$eval(
-    //                 incidentDescriptionSelector,
-    //                 e => e.textContent
-    //             );
-    //             const incidentPriority = await page.$eval(
-    //                 incidentPrioritySelector,
-    //                 e => e.textContent
-    //             );
-    //             expect(title).toEqual(inctidentTitleAfterSubstitution);
-    //             expect(description).toEqual(
-    //                 inctidentDescriptionAfterSubstitution
-    //             );
-    //             expect(incidentPriority).toEqual('Low');
-    //         });
-    //     },
-    //     operationTimeOut
-    // );
-
-    // test(
-    //     'Should remove incident priority on incident, if the default priority is removed',
-    //     async () => {
-    //         return await cluster.execute(null, async ({ page }) => {
-    //             await page.goto(utils.DASHBOARD_URL, {
-    //                 waitUntil: 'networkidle0',
-    //             });
-    //             await page.waitForSelector('#projectSettings');
-    //             await page.click('#projectSettings');
-    //             await page.waitForSelector('#more');
-    //             await page.click('#more');
-    //             await page.waitForSelector('#incidentSettings');
-    //             await page.click('#incidentSettings');
-    //             await page.waitForSelector('#incidentPrioritiesList');
-    //             //change default priority before remove the priority
-    //             await init.selectByText('#incidentPriority', 'high', page);
-    //             await page.click('#saveButton');                
-    //             //await page.waitForTimeout(3000);
-    //             const lowPriorityDeleteButton =
-    //                 '#incidentPrioritiesList .bs-ObjectList-row.db-UserListRow.db-UserListRow--withName:nth-of-type(2) .bs-ObjectList-cell.bs-u-v-middle:nth-of-type(2)>div>div:last-child>button';
-    //             await page.click(lowPriorityDeleteButton);
-    //             await page.waitForSelector('#RemoveIncidentPriority');
-    //             await page.click('#RemoveIncidentPriority');
-    //             //check in the monitor's incident list if the priority has been removed.
-    //             await init.navigateToMonitorDetails(
-    //                 componentName,
-    //                 monitorName,
-    //                 page
-    //             );
-    //             await page.waitForSelector(
-    //                 'tr.incidentListItem:first-of-type > td:nth-of-type(3)'
-    //             );
-    //             await page.click(
-    //                 'tr.incidentListItem:first-of-type > td:nth-of-type(3)'
-    //             );
-    //             const incidentTitleSelector =
-    //                 '.bs-Fieldset-rows > .bs-right-side > .bs-content:nth-of-type(1) > div';
-    //             const incidentStatusBoxSelector = '#incident_0';
-    //             await page.waitForSelector(incidentTitleSelector);
-    //             const incidentStatusBoxContent = await page.$eval(
-    //                 incidentStatusBoxSelector,
-    //                 e => e.textContent
-    //             );
-    //             expect(incidentStatusBoxContent).not.toContain('Priority');
-    //             expect(incidentStatusBoxContent).not.toContain('Low');
-    //         });
-    //     },
-    //     operationTimeOut
-    // );
 
     test(
         'Should create a priority and set it as default',
