3634add2cc6442d8122dd3072b482c6b5129d278
Nawaz Dhandala <nawazdhandala@outlook.com>
2021-03-23 20:08:09 +0000

    merge

--- ci/scripts/deployment-staging-setup.sh
+++ ci/scripts/deployment-staging-setup.sh
@@ -1,4 +1,4 @@
 echo "Get KubeCluster Config"
 sudo doctl kubernetes cluster kubeconfig save fyipe-staging
 sudo kubectl config --kubeconfig=/root/.kube/config get-contexts
-sudo kubectl config --kubeconfig=/root/.kube/config use-context do-nyc3-fyipe-staging
+sudo kubectl config --kubeconfig=/root/.kube/config use-context do-nyc1-fyipe-staging
