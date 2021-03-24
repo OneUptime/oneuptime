0cbcf3f5a44744a610e36525030684cdf15eff9c
Zadat Olayinka <bundayy@outlook.com>
2021-03-24 10:49:13 +0100

    log error, log custom, get timeline and clear timeline

--- java-sdk/src/main/java/io/hackerbay/fyipe/FyipeListener.java
+++ java-sdk/src/main/java/io/hackerbay/fyipe/FyipeListener.java
@@ -1,6 +1,10 @@
 package io.hackerbay.fyipe;
 
+import com.google.gson.JsonObject;
+import io.hackerbay.fyipe.model.Timeline;
 import io.hackerbay.fyipe.model.TrackerOption;
+
+import java.util.ArrayList;
 
 public class FyipeListener {
     private String currentEventId;
@@ -11,5 +15,27 @@ public class FyipeListener {
         this.fyipeTimelineManager = new FyipeTimelineManager(options);
         this.currentEventId = currentEventId;
         this.util = new Util(options);
+    }
+    public void logErrorEvent(JsonObject content, String category){
+        Timeline timeline = new Timeline(category, content, ErrorEventType.error.name());
+        timeline.setEventId(this.currentEventId);
+
+        this.fyipeTimelineManager.addItemToTimeline(timeline);
+    }
+    public void logCustomTimelineEvent(Timeline timelineObj) {
+        timelineObj.setEventId(this.currentEventId);
+
+        // add timeline to the stack
+        this.fyipeTimelineManager.addItemToTimeline(timelineObj);
+    }
+    public ArrayList<Timeline> getTimeline() {
+        // this always get the current state of the timeline array list
+        return this.fyipeTimelineManager.getTimeline();
+    }
+    public void clearTimeline(String eventId) {
+        // set a new eventId
+        this.currentEventId = eventId;
+        // this will reset the state of the timeline array list
+        this.fyipeTimelineManager.clearTimeline();
     }
 }
