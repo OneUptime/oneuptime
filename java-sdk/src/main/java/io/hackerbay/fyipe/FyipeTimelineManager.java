d6f0fef89803008e6c606ea0d58cb5055b197c7e
Zadat Olayinka <bundayy@outlook.com>
2021-03-24 10:49:56 +0100

    timeline manager method implemented

--- java-sdk/src/main/java/io/hackerbay/fyipe/FyipeTimelineManager.java
+++ java-sdk/src/main/java/io/hackerbay/fyipe/FyipeTimelineManager.java
@@ -1,11 +1,36 @@
 package io.hackerbay.fyipe;
 
+import io.hackerbay.fyipe.model.Timeline;
 import io.hackerbay.fyipe.model.TrackerOption;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
 public class FyipeTimelineManager {
     private TrackerOption options;
+    private ArrayList<Timeline> timelineStack = new ArrayList<Timeline>();
+    private Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 
     public FyipeTimelineManager(TrackerOption options) {
         this.options = options;
     }
+    public void addItemToTimeline(Timeline item) {
+        // get the size of the stack
+        if(this.timelineStack.size() == this.options.getMaxTimeline()) {
+            return; // It discards new timeline update once maximum is reached
+        }
+        // add time to the timeline item
+        item.setTime(timestamp.getTime());
+
+        // add a new item to the stack
+        this.timelineStack.add(item);
+        return;
+    }
+    public ArrayList<Timeline> getTimeline() {
+        return this.timelineStack;
+    }
+    public void clearTimeline() {
+        this.timelineStack.clear();
+    }
+
 }
