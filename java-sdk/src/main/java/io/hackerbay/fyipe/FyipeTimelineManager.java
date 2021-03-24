4527de2806eef3f63dcf86d0e7063bb987a7504e
Zadat Olayinka <bundayy@outlook.com>
2021-03-24 09:58:39 +0100

    timeline manager

---
+++ java-sdk/src/main/java/io/hackerbay/fyipe/FyipeTimelineManager.java
@@ -0,0 +1,11 @@
+package io.hackerbay.fyipe;
+
+import io.hackerbay.fyipe.model.TrackerOption;
+
+public class FyipeTimelineManager {
+    private TrackerOption options;
+
+    public FyipeTimelineManager(TrackerOption options) {
+        this.options = options;
+    }
+}
