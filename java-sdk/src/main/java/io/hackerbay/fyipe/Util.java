b7327b525ddd389b932d737351378acad0d847ba
Zadat Olayinka <bundayy@outlook.com>
2021-03-25 17:19:28 +0100

    get exception stact trace + build frame for objects

--- java-sdk/src/main/java/io/hackerbay/fyipe/Util.java
+++ java-sdk/src/main/java/io/hackerbay/fyipe/Util.java
@@ -1,7 +1,10 @@
 package io.hackerbay.fyipe;
 
+import io.hackerbay.fyipe.model.Frame;
+import io.hackerbay.fyipe.model.StackTrace;
 import io.hackerbay.fyipe.model.TrackerOption;
 
+import java.util.ArrayList;
 import java.util.UUID;
 enum ErrorEventType {
     error,
@@ -23,6 +26,37 @@ public class Util {
     public String generateV4EventId() {
         UUID uuid = UUID.randomUUID();
         return uuid.toString();
+    }
+    public StackTrace getExceptionStackTrace(Throwable throwable) {
+        String message = throwable.getMessage();
+        StackTraceElement[] stackTraceElement = throwable.getStackTrace();
+
+        int lineNumber = stackTraceElement[0].getLineNumber();
+        String fileName = stackTraceElement[0].getFileName();
+        String className = stackTraceElement[0].getClassName();
+        String methodName = stackTraceElement[0].getMethodName();
+
+        StackTrace stackTrace = new StackTrace(
+                methodName+" @ "+className,
+                message,
+                lineNumber
+        );
+        stackTrace.setStackTraceFrame(this.buildFrame(stackTraceElement));
+
+        return stackTrace;
+    }
+    private ArrayList<Frame> buildFrame(StackTraceElement[] stackTraceElements) {
+        ArrayList<Frame> frameArrayList = new ArrayList<>();
+        for (int i = 0; i < stackTraceElements.length; i++) {
+            frameArrayList.add(
+                    new Frame(
+                            stackTraceElements[i].getMethodName(),
+                            stackTraceElements[i].getLineNumber(),
+                            stackTraceElements[i].getFileName()
+                    )
+            );
+        }
+        return  frameArrayList;
     }
     
 }
