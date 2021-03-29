57f337324df9bbfb59f70a18e1d30a6138807181
Abhishek <abhishek.prajapat@outlook.com>
2021-03-29 11:30:29 +0530

    Link update for "View and Resolve Incident" for Emails

--- backend/backend/api/incident.js
+++ backend/backend/api/incident.js
@@ -1056,7 +1056,7 @@ router.get(
                 title_message: 'Incident Resolved',
                 body_message: 'Your incident is now resolved.',
                 action: 'resolve',
-                dashboard_url: global.dashboardHost + '/dashboard',
+                dashboard_url: global.dashboardHost,
             });
         } catch (error) {
             return sendErrorResponse(req, res, error);
@@ -1087,7 +1087,7 @@ router.get(
                 title_message: 'Incident Acknowledged',
                 body_message: 'Your incident is now acknowledged',
                 action: 'acknowledge',
-                dashboard_url: global.dashboardHost + '/dashboard',
+                dashboard_url: global.dashboardHost,
             });
         } catch (error) {
             return sendErrorResponse(req, res, error);
