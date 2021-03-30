8c9325068609877fd09b7225b352f9ba77b850cb
augustinebest <augustusbest00@gmaile.com>
2021-03-30 21:36:19 +0100

    init-script for hideIncident

---
+++ init-script/scripts/3.0.8397.js
@@ -0,0 +1,20 @@
+const { find, update } = require('../util/db');
+
+const incidentsCollection = 'incidents';
+
+async function run() {
+    const incidents = await find(incidentsCollection, {
+        hideIncident: { $exists: false },
+    });
+
+    for (let i = 0; i < incidents.length; i++) {
+        const incident = incidents[i];
+        await update(
+            incidentsCollection,
+            { _id: incident._id },
+            { hideIncident: false }
+        );
+    }
+}
+
+module.exports = run;
