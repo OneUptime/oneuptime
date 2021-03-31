9187c1fed6b8d3e41246b6f02827a0519e237b8c
Nawaz Dhandala <nawazdhandala@outlook.com>
2021-03-31 22:05:45 +0100

    fix lang

--- dashboard/src/components/scheduledEvent/ScheduledEventDescription.js
+++ dashboard/src/components/scheduledEvent/ScheduledEventDescription.js
@@ -294,7 +294,7 @@ function ScheduledEventDescription({
                                                                         <span>
                                                                             Resolve
                                                                             this
-                                                                            maintenance
+                                                                            event
                                                                         </span>
                                                                     </label>
                                                                 </ShouldRender>
