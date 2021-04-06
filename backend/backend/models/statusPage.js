4b75d4748b79d41a526e4506caeb2f6a5455a60e
Nawaz Dhandala <nawazdhandala@outlook.com>
2021-04-06 20:32:58 +0100

    revert

--- backend/backend/models/statusPage.js
+++ backend/backend/models/statusPage.js
@@ -13,8 +13,8 @@ const statusSchema = new Schema({
             domain: String, // complete domain eg status.fyipe.com
             cert: String, // filename gridfs
             privateKey: String, // filename gridfs
-            enableHttps: { type: Boolean, default: true },
-            autoProvisioning: { type: Boolean, default: true },
+            enableHttps: { type: Boolean, default: false },
+            autoProvisioning: { type: Boolean, default: false },
             domainVerificationToken: {
                 type: Schema.Types.ObjectId,
                 ref: 'DomainVerificationToken',
