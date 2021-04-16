7f57652df14b39a40790b463b48b7276b5eb1973
Zadat Olayinka <bundayy@outlook.com>
2021-04-16 10:04:08 +0100

    set up basic calss for logger + logtype enum

---
+++ python-sdk/src/fyipe_pkg/logger.py
@@ -0,0 +1,31 @@
+from logtype import LogType
+
+class Logger:
+    def __init__(self, apiUrl, applicationLogId, applicationLogKey):
+        self.applicationLogId = applicationLogId
+        self.applicationLogKey = applicationLogKey
+        self.apiUrl = apiUrl+"/application-log/"+applicationLogId+"/log";
+
+    def log(self, data, tags = null):
+        """
+        Sends a log with type info
+        """
+
+        # validate the data is of type string or type object/dictionary
+        if(isinstance(data, (str, dict)) != True):
+            return 'Invalid Content to be logged'
+        
+        # if a tag is passed validate that it is of type string or array/list
+        if(tags is not None):
+            if(isinstance(tags, (str, list)) != True):
+                return 'Invalid Content Tags to be logged'
+        
+        # make request to the API
+        return self._makeApiRequest_(data, LogType.INFO, tags)
+
+    
+    def _makeApiRequest_(self, data, logType, tags):
+        print("API request happens here")
+        
+
+
---
+++ python-sdk/src/fyipe_pkg/logtype.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class LogType(Enum):
+    INFO = "info"
+    WARNING = "waring"
+    ERROR = "error"
