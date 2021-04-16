c283a8b26e29fecd128ecf3d83ad1a66a45b120c
courage173 <courageosemwengie@gmail.com>
2021-04-16 12:25:14 +0100

    added the incident idNumber to the incident response sent to zapier

--- zapier/triggers/incident.js
+++ zapier/triggers/incident.js
@@ -76,6 +76,7 @@ module.exports = {
             projectName: 'New Project',
             projectId: '1',
             incidentId: '1',
+            idNumber: 1,
             acknowledged: false,
             resolved: false,
             internalNote: 'New Note',
