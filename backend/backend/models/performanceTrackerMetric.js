56098c423b99bb40e494601b29b09921fe634f34
deityhub <judoc96@gmail.com>
2021-04-22 09:41:30 +0100

    performance tracker metric schema definition

---
+++ backend/backend/models/performanceTrackerMetric.js
@@ -0,0 +1,25 @@
+const mongoose = require('../config/db');
+
+const Schema = mongoose.Schema;
+const performanceTrackerMetricSchema = new Schema({
+    type: String,
+    metrics: Object,
+    callIdentifier: String,
+    performanceTrackerId: {
+        type: Schema.Types.ObjectId,
+        ref: 'PerformanceTracker',
+        index: true,
+    },
+    throughput: Number,
+    deleted: { type: Boolean, default: false },
+    deletedAt: {
+        type: Date,
+    },
+    createdAt: Date,
+    updatedAt: Date,
+});
+
+module.exports = mongoose.model(
+    'PerformanceTrackerMetric',
+    performanceTrackerMetricSchema
+);
