903c62fcce53ed543e740baa4901960ddcee7312
deityhub <judoc96@gmail.com>
2021-04-22 09:36:50 +0100

    start any of the listeners individually as the case may be

--- js-sdk/src/performanceTracker.js
+++ js-sdk/src/performanceTracker.js
@@ -8,18 +8,17 @@ import OutgoingListener from './listeners/outgoingListener';
 import PerfTimer from './utils/perfTimer';
 import HrTimer from './utils/hrTimer';
 class PerformanceTracker {
-    #BASE_URL = 'http://localhost:3002/api'; // TODO proper base url config
-    #isWindow;
     #apiUrl;
     #appId;
+    #appKey;
     #nodeVer;
     #nodeUse;
     #start;
     #end;
-    constructor(isWindow, options) {
-        this.#apiUrl = options.apiUrl;
-        this.#appId = options.appId;
-        this.#isWindow = isWindow;
+    constructor({ apiUrl, appId, appKey }) {
+        this.#apiUrl = apiUrl;
+        this.#appId = appId;
+        this.#appKey = appKey;
         this.#nodeVer = process.versions.node;
         if (semver.satisfies(this.#nodeVer, '>10.0.0')) {
             this.#nodeUse = true;
@@ -27,26 +26,21 @@ class PerformanceTracker {
             this.#nodeUse = false;
         }
         if (this.#nodeUse) {
-            const perf = new PerfTimer(this.#apiUrl, this.#appId);
+            const perf = new PerfTimer(this.#apiUrl, this.#appId, this.#appKey);
             const { start, end } = perf;
             this.#start = start;
             this.#end = end;
         } else {
-            const hrt = new HrTimer(this.#apiUrl, this.#appId);
+            const hrt = new HrTimer(this.#apiUrl, this.#appId, this.#appKey);
             const { start, end } = hrt;
             this.#start = start;
             this.#end = end;
         }
-        if (!this.#isWindow) {
-            this._setUpOutgoingListener();
-            this._setUpDataBaseListener();
-            this._setUpIncomingListener();
-        }
     }
-    _setUpOutgoingListener() {
+    setUpOutgoingListener() {
         return new OutgoingListener(this.#start, this.#end);
     }
-    _setUpDataBaseListener() {
+    setUpDataBaseListener() {
         const load = Module._load;
         const _this = this;
         Module._load = function(request, parent) {
@@ -58,7 +52,7 @@ class PerformanceTracker {
             return res;
         };
     }
-    _setUpIncomingListener() {
+    setUpIncomingListener() {
         return new IncomingListener(this.#start, this.#end);
     }
 }
