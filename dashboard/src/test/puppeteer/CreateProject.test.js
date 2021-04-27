0a51e7fec2bbe92cbe54529df0ca48566110ecb2
adeoluwadavid <adeoluwadavid@gmail.com>
2021-04-27 10:39:41 +0100

    enterprise-puppeteer-5

--- dashboard/src/test/puppeteer/CreateProject.test.js
+++ dashboard/src/test/puppeteer/CreateProject.test.js
@@ -1,98 +1,64 @@
 const puppeteer = require('puppeteer');
 const utils = require('./test-utils');
 const init = require('./test-init');
-const { Cluster } = require('puppeteer-cluster');
 
 require('should');
-
+let browser, page;
 // user credentials
 const email = utils.generateRandomBusinessEmail();
 const password = '1234567890';
+const user = {
+    email,
+    password
+};
 
 describe('Project API', () => {
     const operationTimeOut = 50000;
 
     beforeAll(async done => {
         jest.setTimeout(200000);
-
-        const cluster = await Cluster.launch({
-            concurrency: Cluster.CONCURRENCY_PAGE,
-            puppeteerOptions: utils.puppeteerLaunchConfig,
-            puppeteer,
-            timeout: 120000,
-        });
-
-        cluster.on('taskerror', err => {
-            throw err;
-        });
-
-        // Register user
-        await cluster.task(async ({ page, data }) => {
-            const user = {
-                email: data.email,
-                password: data.password,
-            };
-            // user
-            await init.registerUser(user, page);
-        });
-
-        await cluster.queue({ email, password });
-
-        await cluster.idle();
-        await cluster.close();
+       
+        browser = await puppeteer.launch(utils.puppeteerLaunchConfig);
+        page = await browser.newPage();
+        await page.setUserAgent(
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+        );  
+         // user
+            await init.registerUser(user, page);       
+        
         done();
     });
 
     afterAll(async done => {
+        await browser.close();
         done();
     });
 
     test(
         'Should create new project from dropdown after login',
         async done => {
-            const cluster = await Cluster.launch({
-                concurrency: Cluster.CONCURRENCY_PAGE,
-                puppeteerOptions: utils.puppeteerLaunchConfig,
-                puppeteer,
-                timeout: 100000,
+            const projectName = utils.generateRandomString();
+            //Login is no longer required as Dashboard page is loaded automatically.
+            await page.waitForSelector('#selector', { visible: true });
+            await page.$eval('#create-project', e => e.click());
+            await page.waitForSelector('#name', { visible: true });
+            await page.click('input[id=name]');
+            await page.type('input[id=name]', projectName);
+            await page.click('input[id=Startup_month]');
+            await page.click('button[type=submit]');
+            await page.waitForSelector(`#cb${projectName}`, { visible: true });
+            // eslint-disable-next-line no-undef
+            localStorageData = await page.evaluate(() => {
+                const json = {};
+                for (let i = 0; i < localStorage.length; i++) {
+                    const key = localStorage.key(i);
+                    json[key] = localStorage.getItem(key);
+                }
+                return json;
             });
+            // eslint-disable-next-line no-undef
+            localStorageData.should.have.property('project');
 
-            cluster.on('taskerror', err => {
-                throw err;
-            });
-
-            await cluster.task(async ({ page, data }) => {
-                const user = {
-                    email: data.email,
-                    password: data.password,
-                };
-
-                await init.loginUser(user, page);
-                await page.waitForSelector('#selector');
-                await page.$eval('#create-project', e => e.click());
-                await page.waitForTimeout(1000);
-                await page.waitForSelector('#name');
-                await page.click('input[id=name]');
-                await page.type('input[id=name]', utils.generateRandomString());
-                await page.click('input[id=Startup_month]');
-                await page.click('button[type=submit]');
-                await page.waitForTimeout(2000);
-                // eslint-disable-next-line no-undef
-                localStorageData = await page.evaluate(() => {
-                    const json = {};
-                    for (let i = 0; i < localStorage.length; i++) {
-                        const key = localStorage.key(i);
-                        json[key] = localStorage.getItem(key);
-                    }
-                    return json;
-                });
-                // eslint-disable-next-line no-undef
-                localStorageData.should.have.property('project');
-            });
-
-            cluster.queue({ email, password });
-            await cluster.idle();
-            await cluster.close();
             done();
         },
         operationTimeOut
