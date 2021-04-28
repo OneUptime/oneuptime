40347a211e27076b0dd06974c0d091dfb0b83dc9
Zadat Olayinka <bundayy@outlook.com>
2021-04-28 08:08:17 +0100

    timeline tests

--- python-sdk/fyipe_sdk/tests/testFyipeTracker.py
+++ python-sdk/fyipe_sdk/tests/testFyipeTracker.py
@@ -79,12 +79,64 @@ class TrackerTest(unittest.TestCase):
         return response.json()
     
     def test_should_take_in_custom_timeline_event(self):
-        tracker = FyipeTracker(self.apiUrl, self.errorTracker["_id"], self.errorTracker["key"]);
+        tracker = FyipeTracker(self.apiUrl, self.errorTracker["_id"], self.errorTracker["key"])
 
-        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], self.customTimeline["type"]);
-        timeline = tracker.getTimeline();
-        print(timeline)
-        # $this->assertIsArray($timeline);
-        # $this->assertCount(1, $timeline);
-        # $this->assertEquals(static::$customTimeline->category, $timeline[0]->category);
+        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], self.customTimeline["type"])
+        timeline = tracker.getTimeline()
+        self.assertIsInstance(timeline, list)
+        self.assertEqual(1, len(timeline))
+        self.assertEqual(self.customTimeline["category"], timeline[0]["category"])
+    
+    def test_should_ensure_timeline_event_contains_eventId_and_timestamp(self):
+    
+        tracker = FyipeTracker(self.apiUrl, self.errorTracker["_id"], self.errorTracker["key"])
+
+        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], self.customTimeline["type"])
+        timeline = tracker.getTimeline()
+        self.assertIsInstance(timeline[0]["eventId"], str)
+        self.assertIsInstance(timeline[0]["timestamp"], float)
+    
+    def test_should_ensure_different_timeline_event_have_the_same_eventId(self): 
+        tracker = FyipeTracker(self.apiUrl, self.errorTracker["_id"], self.errorTracker["key"])
+
+        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], self.customTimeline["type"])
+        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], "error")
+        timeline = tracker.getTimeline()
+        self.assertEqual(2, len(timeline)) # two timeline events
+        self.assertEqual(timeline[0]["eventId"], timeline[1]["eventId"])# their eventId is the same, till there is an error sent to the server
+    
+    def test_should_ensure_max_timline_cant_be_set_as_a_negative_number(self):
+        options = {
+            "maxTimeline": -5
+        }
+        tracker = FyipeTracker(self.apiUrl, self.errorTracker["_id"], self.errorTracker["key"], options)
+
+        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], self.customTimeline["type"])
+        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], "error")
+        timeline = tracker.getTimeline()
+        self.assertEqual(2, len(timeline)) # two timeline events
+    
+    def test_should_ensure_new_timeline_event_after_max_timeline_are_discarded(self): 
+        options = {
+            "maxTimeline": 2
+        }
+        tracker = FyipeTracker(self.apiUrl, self.errorTracker["_id"], self.errorTracker["key"], options)
+
+        customTimeline2 = {
+            "category": "logout",
+            "type": "success",
+            "content": {"message": "tester"}
+        }
+
+        # add 3 timelinee events
+        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], self.customTimeline["type"])
+        tracker.addToTimeline(customTimeline2["category"], customTimeline2["content"], customTimeline2["type"])
+        tracker.addToTimeline(self.customTimeline["category"], self.customTimeline["content"], "debug")
+        timeline = tracker.getTimeline()
+        
+        self.assertEqual(options["maxTimeline"], len(timeline)) # three timeline events
+        self.assertEqual(timeline[0]["type"], self.customTimeline["type"]) 
+        self.assertEqual(timeline[1]["category"], customTimeline2["category"])
+    
+    
     
