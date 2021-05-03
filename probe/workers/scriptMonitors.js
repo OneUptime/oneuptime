7e876c2d89cab212022d11e3a3b1cc6ec04eeed1
Dave <callremz@gmail.com>
2021-05-03 12:05:23 +0100

    refactor script monitor

--- probe/workers/scriptMonitors.js
+++ probe/workers/scriptMonitors.js
@@ -2,17 +2,6 @@
 const ApiService = require('../utils/apiService');
 const ErrorService = require('../utils/errorService');
 const scriptSandbox = require('../utils/scriptSandbox');
-// const { NodeVM } = require('vm2');
-
-// const vm = new NodeVM({
-//     timeout: 30000,
-//     console: 'inherit',
-//     sandbox: {},
-//     require: {
-//         external: true,
-//         root: './',
-//     },
-// });
 
 // it collects all monitors then ping them one by one to store their response
 // checks if the website of the url in the monitors is up or down
@@ -22,15 +11,28 @@ module.exports = {
         try {
             if (monitor && monitor.type) {
                 if (monitor.data.script) {
-                    const code = `const axios = require('axios');
-                    const lighthouse = require('lighthouse');
-                    module.exports = (success, error) => {
-                        ${monitor.data.script}
-                    }`;
-                    const { res, resp } = await scriptSandbox.runScript(code);
+                    const code = monitor.data.script; // redundant now but may be expanded in future
+                    const {
+                        success,
+                        message,
+                        errors,
+                    } = await scriptSandbox.runScript(code, true);
+
+                    // convert array to string
+                    const errorString = Array.isArray(errors)
+                        ? errors.join(', ')
+                        : errors;
+
+                    // normalize response
+                    const resp = {
+                        status: success ? 'success' : 'failed',
+                        error: success
+                            ? undefined
+                            : message + ': ' + errorString,
+                    };
+
                     await ApiService.ping(monitor._id, {
                         monitor,
-                        res,
                         resp,
                         type: monitor.type,
                     });
@@ -42,26 +44,3 @@ module.exports = {
         }
     },
 };
-
-// const runScript = async code => {
-//     const now = new Date().getTime();
-//     let resp = null;
-//     let res = null;
-//     const success = value => {
-//         res = new Date().getTime() - now;
-//         resp = { status: 'success', body: value };
-//     };
-
-//     const error = value => {
-//         res = new Date().getTime() - now;
-//         resp = { status: 'failed', body: value };
-//     };
-//     try {
-//         const vmRunner = vm.run(code, 'vm.js');
-//         vmRunner(success, error);
-//     } catch (err) {
-//         res = new Date().getTime() - now;
-//         resp = { status: 'timeout', body: err };
-//     }
-//     return { res, resp };
-// };
