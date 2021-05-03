51d785385351e4680cf35547ebab51739a7384f4
adeoluwadavid <adeoluwadavid@gmail.com>
2021-05-03 11:27:34 +0100

    status-page-monitor-initial-values

--- dashboard/src/components/statusPage/RenderMonitor.js
+++ dashboard/src/components/statusPage/RenderMonitor.js
@@ -315,7 +315,7 @@ const mapStateToProps = (state, ownProps) => {
         .flat();
     let monitors = selector(state, 'monitors');
     
-    /** On Theme change, the updated monitor state becomes a monitor nested object
+    /** On Theme change, the updated monitors state becomes a monitor nested object within an array
      * This monitor nested object(monitor.monitor._id) is then extracted and used to update 'monitor.monitor'.
      * monitor.monitor is the required id used in setting the initial values by default.
      */
