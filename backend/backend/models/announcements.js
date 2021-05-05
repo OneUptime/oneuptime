addd772da45f2fe634058ac881537a77e2e4e869
augustinebest <augustusbest00@gmaile.com>
2021-05-05 22:17:46 +0100

    created announcementSchema

---
+++ backend/backend/models/announcements.js
@@ -0,0 +1,50 @@
+const mongoose = require('../config/db');
+
+const Schema = mongoose.Schema;
+const announcementSchema = new Schema(
+    {
+        monitors: [
+            {
+                monitorId: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Monitor',
+                    index: true,
+                },
+            },
+        ],
+        statusPageId: {
+            type: Schema.Types.ObjectId,
+            ref: 'StatusPage',
+            index: true,
+        },
+        projectId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Project',
+            index: true,
+        },
+        slug: String,
+        hideAnnouncement: { type: Boolean, default: false },
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: {
+            type: Date,
+        },
+        deletedById: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            index: true,
+        },
+        createdById: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            index: true,
+        },
+        name: String,
+        description: String,
+        resolved: { type: Boolean, default: false },
+    },
+    { timestamps: true }
+);
+module.exports = mongoose.model('Announcement', announcementSchema);
