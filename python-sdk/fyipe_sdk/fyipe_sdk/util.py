7dfe28d71897fd63b581446b3f81c0941ace2dd8
Zadat Olayinka <bundayy@outlook.com>
2021-05-07 09:11:24 +0100

    upade stactrace frame ad return for code capture

--- python-sdk/fyipe_sdk/fyipe_sdk/util.py
+++ python-sdk/fyipe_sdk/fyipe_sdk/util.py
@@ -56,6 +56,9 @@ class Util:
             # update content of each frame
             self.updateFrameContent(frame)
             contentFrame.append(updateFrame)
+        
+        errorObj["stacktrace"]["frames"] = frames
+        return errorObj
 
     def getFrameContent(self, frame):
     
