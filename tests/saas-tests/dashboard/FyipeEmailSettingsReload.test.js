9f2598af4c9662d879554057a982a025ed659ef2
adeoluwadavid <adeoluwadavid@gmail.com>
2021-05-13 22:11:35 +0100

    fyipe-reload-12

--- tests/saas-tests/dashboard/FyipeEmailSettingsReload.test.js
+++ tests/saas-tests/dashboard/FyipeEmailSettingsReload.test.js
@@ -39,16 +39,13 @@ describe('Fyipe Page Reload', () => {
             });
             await init.pageClick(page, '#projectSettings');
             await init.pageClick(page, '#more');
-            await init.pageClick(page, '#incidentSettings');
-            await init.pageClick(page, '.incident-priority-tab'); // Navigate to the Incident Priority Tab
-            await init.pageClick(page, '#priorityDefault_Low_1');
-            await init.pageClick(page, '#SetDefaultIncidentPriority');
-            await page.waitForSelector('#SetDefaultIncidentPriority', { hidden: true });
-
+            await init.pageClick(page, '#email');
+            await page.waitForSelector('#showsmtpForm', { visible: true });                        
             //To confirm no errors and stays on the same page on reload
             await page.reload({ waitUntil: 'networkidle2' });
             await page.waitForSelector('#cbProjectSettings', { visible: true });
-            const spanElement = await page.waitForSelector('#cbIncidents', { visible: true });
+            await page.waitForSelector('#cbEmail', { visible: true });
+            const spanElement = await page.waitForSelector('#showsmtpForm', { visible: true });
             expect(spanElement).toBeDefined();
             done();
         },
