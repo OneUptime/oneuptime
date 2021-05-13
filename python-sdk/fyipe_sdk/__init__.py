4b1e6ba15310906ead92518b27ab4c01ec796a64
Zadat Olayinka <bundayy@outlook.com>
2021-05-13 09:24:03 +0100

    fix import to use * approach

--- python-sdk/fyipe_sdk/__init__.py
+++ python-sdk/fyipe_sdk/__init__.py
@@ -1,5 +1,5 @@
-from fyipe_sdk.fyipe_sdk.logger import FyipeLogger
-from fyipe_sdk.fyipe_sdk.tracker import FyipeTracker
-from fyipe_sdk.fyipe_sdk.util import Util
-from fyipe_sdk.fyipe_sdk.logtype import LogType
+from fyipe_sdk.fyipe_sdk.logger import *
+from fyipe_sdk.fyipe_sdk.tracker import *
+from fyipe_sdk.fyipe_sdk.util import *
+from fyipe_sdk.fyipe_sdk.logtype import *
 
