f103e98fde2bce14dd35f7f109fce1efb4bf134b
Nawaz Dhandala <nawazdhandala@outlook.com>
2021-05-14 13:59:15 +0100

    remove react-env

--- dashboard/index.js
+++ dashboard/index.js
@@ -20,7 +20,6 @@ process.on('uncaughtException', err => {
 const express = require('express');
 const path = require('path');
 const app = express();
-const child_process = require('child_process');
 const cors = require('cors');
 const fs = require('fs');
 
