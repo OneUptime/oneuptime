b9af883da67053840020ec575f8a5e8ec6b52d8b
adeoluwadavid <adeoluwadavid@gmail.com>
2021-05-18 16:30:46 +0100

    admin-dashboard-final-test

--- backend/backend/api/user.js
+++ backend/backend/api/user.js
@@ -221,7 +221,7 @@ router.post('/signup', async function(req, res) {
                     message: 'Company phone number  is not in string format.',
                 });
             }
-            // Call the UserService.            
+            // Call the UserService.
             user = await UserService.signup(data);
             // Call the MailService.
             MailService.sendSignupMail(user.email, user.name);
