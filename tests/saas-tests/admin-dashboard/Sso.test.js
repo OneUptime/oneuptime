7c0eb62252a80caef4af62971cf82278be236086
adeoluwadavid <adeoluwadavid@gmail.com>
2021-05-19 17:27:39 +0100

    admin-dashboard-review

--- tests/saas-tests/admin-dashboard/Sso.test.js
+++ tests/saas-tests/admin-dashboard/Sso.test.js
@@ -178,57 +178,57 @@ describe('SSO API', () => {
         operationTimeOut
     );
 
-    // it(
-    //     'should enable Next/Previous buttons when there are more than 10 SSOs',
-    //     async done => {
-    //         await page.goto(utils.ADMIN_DASHBOARD_URL);
-    //         await moveToSsoPage(page);
-    //         await init.pageWaitForSelector(page, '#no-sso-message');
+    it(
+        'should enable Next/Previous buttons when there are more than 10 SSOs',
+        async done => {
+            await page.goto(utils.ADMIN_DASHBOARD_URL);
+            await moveToSsoPage(page);
+            await init.pageWaitForSelector(page, '#no-sso-message');
 
-    //         for (let i = 0; i <= 11; i++) {
-    //             await createSso(page, {
-    //                 domain: `subdomain.${i}.test.hackerbay.io`,
-    //                 entityId: 'hackerbay.io', //Updated UI
-    //                 remoteLoginUrl: 'test.hackerbay.io/login',
-    //                 certificateFingerprint: 'AZERTYUIOP',
-    //                 remoteLogoutUrl: 'test.hackerbay.io/logout',
-    //                 ipRanges: '127.0.0.1',
-    //             });
-    //         }
+            for (let i = 0; i <= 11; i++) {
+                await createSso(page, {
+                    domain: `subdomain.${i}.test.hackerbay.io`,
+                    entityId: 'hackerbay.io', //Updated UI
+                    remoteLoginUrl: 'test.hackerbay.io/login',
+                    certificateFingerprint: 'AZERTYUIOP',
+                    remoteLogoutUrl: 'test.hackerbay.io/logout',
+                    ipRanges: '127.0.0.1',
+                });
+            }
 
-    //         await init.pageWaitForSelector(page, '#sso-domain');
+            await init.pageWaitForSelector(page, '#sso-domain');
 
-    //         const ssoCount = await page.$eval('#sso-count', e => {
-    //             return e.innerHTML;
-    //         });
+            const ssoCount = await page.$eval('#sso-count', e => {
+                return e.innerHTML;
+            });
 
-    //         expect(ssoCount).toContain('12');
+            expect(ssoCount).toContain('12');
 
-    //         const firstPageTbody = await page.$eval('tbody', e => {
-    //             return e.innerHTML;
-    //         });
-    //         expect(firstPageTbody).toContain('subdomain.11.test.hackerbay.io');
-    //         expect(firstPageTbody).toContain('subdomain.2.test.hackerbay.io');
+            const firstPageTbody = await page.$eval('tbody', e => {
+                return e.innerHTML;
+            });
+            expect(firstPageTbody).toContain('subdomain.11.test.hackerbay.io');
+            expect(firstPageTbody).toContain('subdomain.2.test.hackerbay.io');
 
-    //         await init.pageClick(page, '#next-button');
+            await init.pageClick(page, '#next-button');
 
-    //         const secondPageTbody = await page.$eval('tbody', e => {
-    //             return e.innerHTML;
-    //         });
-    //         expect(secondPageTbody).toContain('subdomain.1.test.hackerbay.io');
-    //         expect(secondPageTbody).toContain('subdomain.0.test.hackerbay.io');
+            const secondPageTbody = await page.$eval('tbody', e => {
+                return e.innerHTML;
+            });
+            expect(secondPageTbody).toContain('subdomain.1.test.hackerbay.io');
+            expect(secondPageTbody).toContain('subdomain.0.test.hackerbay.io');
 
-    //         await init.pageClick(page, '#previous-button');
+            await init.pageClick(page, '#previous-button');
 
-    //         const initalPageTbody = await page.$eval('tbody', e => {
-    //             return e.innerHTML;
-    //         });
+            const initalPageTbody = await page.$eval('tbody', e => {
+                return e.innerHTML;
+            });
 
-    //         expect(initalPageTbody).toContain('subdomain.11.test.hackerbay.io');
-    //         expect(initalPageTbody).toContain('subdomain.2.test.hackerbay.io');
+            expect(initalPageTbody).toContain('subdomain.11.test.hackerbay.io');
+            expect(initalPageTbody).toContain('subdomain.2.test.hackerbay.io');
 
-    //         done();
-    //     },
-    //     operationTimeOut
-    // );
+            done();
+        },
+        operationTimeOut
+    );
 });
