899d84345999589fd9cff95eb5c5493e091c9db9
Zadat Olayinka <bundayy@outlook.com>
2021-05-19 09:08:04 +0100

    set up constructor

--- ruby-sdk/fyipe/lib/fyipe.rb
+++ ruby-sdk/fyipe/lib/fyipe.rb
@@ -1,7 +1,25 @@
-module Fyipe
-    class
-        def hi
-            "welcome"
-        end
+class Fyipe
+    
+    
+    # FyipeLogger constructor.
+    # @param string apiUrl
+    # @param string applicationLogId
+    # @param string applicationLogKey
+    
+    def initialize(applicationLogId, applicationLogKey, apiUrl)
+        # instance variable intialzation
+        @applicationLogId = applicationLogId
+        setApiUrl(apiUrl)
+        @applicationLogKey = applicationLogKey
     end
+
+    def setApiUrl(apiUrl)
+        @apiUrl = apiUrl + '/application-log/' + @applicationLogId + '/log';
+    end
+
+    def display
+        puts "Value of APPLICATION LOG ID is: #{@applicationLogId}"
+        puts "Value of APPLICATION LOG KEY is: #{@applicationLogKey}"
+        puts "Value of APPLICATION LOG URL is: #{@apiUrl}"
+    end 
 end
