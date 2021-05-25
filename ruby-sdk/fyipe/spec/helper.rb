c7890a8d53f2175bd516cb65446aeefa3aa4716d
Zadat Olayinka <bundayy@outlook.com>
2021-05-25 01:05:02 +0100

    helper method for api calls and title generatror

--- ruby-sdk/fyipe/spec/helper.rb
+++ ruby-sdk/fyipe/spec/helper.rb
@@ -1,13 +1,19 @@
 require 'faker'
+require 'httparty'
 
 class Helper 
+    include HTTParty
+
+    def getTitle() 
+        return Faker::Movie.title
+    end
     def getSampleUser()
         user = {
-            'name' => Faker::Name.name,
+            'name'=> Faker::Name.name,
             'password' => '1234567890',
             'confirmPassword' => '1234567890',
             'email' => Faker::Internet.email,
-            'company' => Faker::Company.name,
+            'companyName' => Faker::Company.name,
             'jobTitle' => Faker::Company.profession,
             'companySize' => Faker::Number.between(from: 1, to: 100),
             'card' => {
@@ -28,6 +34,16 @@ class Helper
             'companyPhoneNumber' => Faker::Company.profession,
             'reference' => 'Github',
         } 
-        puts user
+        return user
+    end
+
+    def makeApiRequest(url, data, token = nil)
+        # make api request and return response 
+        params = { body: data, headers: token == nil ? nil : {Authorization: 'Basic '+token}}
+
+
+        response = self.class.post(url, params).parsed_response
+        return response
+    
     end
 end 
