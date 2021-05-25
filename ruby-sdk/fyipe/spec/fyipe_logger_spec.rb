35a284fdbc7a8a08eb774dd02d6003a309071941
Zadat Olayinka <bundayy@outlook.com>
2021-05-25 01:05:38 +0100

    create user, component and application log for test, wrap in begin, rescue and ensure

--- ruby-sdk/fyipe/spec/fyipe_logger_spec.rb
+++ ruby-sdk/fyipe/spec/fyipe_logger_spec.rb
@@ -7,9 +7,31 @@ RSpec.configure do |config|
         # using $ registers the variable as a global variable
         # ref: https://stackoverflow.com/a/19167379/6800815
         # $logger = FyipeLogger.new()
-        $apiUrl = 'http://localhost:3002/api';
+        $apiUrl = 'http://localhost:3002/api'
         $helper = Helper.new()
-        $helper.getSampleUser()
+        sampleUser = $helper.getSampleUser()
+
+        begin
+            # create user
+            createdUser = $helper.makeApiRequest($apiUrl+"/user/signup", sampleUser)
+
+            # get token and project
+            $token = createdUser['tokens']['jwtAccessToken']
+            $project = createdUser['project']
+
+            # create a component
+            component = { 'name' => $helper.getTitle() }
+            $createdComponent = $helper.makeApiRequest($apiUrl+"/component/"+$project["_id"], component, $token) 
+            
+            # create an applicationlog and set it as the global application Log.
+            appLog = { 'name' => $helper.getTitle() }
+            $applicationLog = $helper.makeApiRequest($apiUrl+"/application-log/"+$project["_id"]+"/"+$createdComponent["_id"]+"/create", appLog, $token)
+        rescue => exception
+            puts "Couldnt create an application log to run a test, Error occured: " + exception.message
+        ensure
+            puts "All clear, Tests will commence now"
+        end 
+                        
     }
 end
 RSpec.describe FyipeLogger do
