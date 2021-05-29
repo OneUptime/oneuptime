0fe4358dbbbfa09d1cf2a8111a333305f7ada73e
Bosah Arthur <arthurbosah412gmail.com>
2021-05-29 07:15:04 +0100

    re-arranged the code in a way that if there is no scripts, you will not see the script card

--- dashboard/src/components/automationScript/AutomatedTabularList.js
+++ dashboard/src/components/automationScript/AutomatedTabularList.js
@@ -25,130 +25,142 @@ const AutomatedTabularList = props => {
                     className="Box-root"
                     style={{ overflow: 'hidden', overflowX: 'auto' }}
                 >
-                    <table className="Table">
-                        <thead className="Table-body">
-                            <tr className="Table-row db-ListViewItem db-ListViewItem-header">
-                                <td
-                                    className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
-                                    style={{ height: '1px', minWidth: '210px' }}
-                                >
-                                    <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                        <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
-                                            <span>Name</span>
-                                        </span>
-                                    </div>
-                                </td>
-                                <td
-                                    className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
-                                    style={{ height: '1px', minWidth: '210px' }}
-                                >
-                                    <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                        <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
-                                            <span>Script</span>
-                                        </span>
-                                    </div>
-                                </td>
-                                <td
-                                    className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
-                                    style={{ height: '1px' }}
-                                >
-                                    <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                        <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
-                                            <span>View</span>
-                                        </span>
-                                    </div>
-                                </td>
-                                <td
-                                    className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
-                                    style={{ height: '1px' }}
-                                >
-                                    <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                        <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
-                                            {/* <span>View</span> */}
-                                        </span>
-                                    </div>
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody className="Table-body">
-                            {scripts.map((x, i) => (
-                                <tr
-                                    key={i}
-                                    className="Table-row db-ListViewItem bs-ActionsParent db-ListViewItem--hasLink incidentListItem"
-                                    style={{
-                                        height: '50px',
-                                        width: '100%',
-                                    }}
-                                >
+                    {scripts.length > 0 && (
+                        <table className="Table">
+                            <thead className="Table-body">
+                                <tr className="Table-row db-ListViewItem db-ListViewItem-header">
                                     <td
-                                        className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                        className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
                                         style={{
                                             height: '1px',
                                             minWidth: '210px',
                                         }}
                                     >
                                         <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                            {x.name}
+                                            <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
+                                                <span>Name</span>
+                                            </span>
                                         </div>
                                     </td>
                                     <td
-                                        className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                        className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
                                         style={{
                                             height: '1px',
                                             minWidth: '210px',
                                         }}
                                     >
                                         <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                            {x.script}
+                                            <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
+                                                <span>Script</span>
+                                            </span>
                                         </div>
                                     </td>
                                     <td
-                                        className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
-                                        style={{
-                                            height: '1px',
-                                        }}
+                                        className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                        style={{ height: '1px' }}
                                     >
                                         <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                            <ShouldRender if={true}>
-                                                <button
-                                                    className="bs-Button"
-                                                    disabled={false}
-                                                >
-                                                    <span>View</span>
-                                                </button>
-                                            </ShouldRender>
+                                            <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
+                                                <span>View</span>
+                                            </span>
                                         </div>
                                     </td>
                                     <td
-                                        className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
-                                        style={{
-                                            height: '1px',
-                                        }}
+                                        className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                        style={{ height: '1px' }}
                                     >
                                         <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                            <ShouldRender if={true}>
-                                                <button
-                                                    className="bs-Button Box-background--red Text-color--white"
-                                                    // disabled={loading}
-                                                    onClick={() => {
-                                                        deleteScrip(x._id);
-                                                    }}
-                                                >
-                                                    <ShouldRender if={!loading}>
-                                                        <span>Delete</span>
-                                                    </ShouldRender>
-
-                                                    <ShouldRender if={loading}>
-                                                        <FormLoader />
-                                                    </ShouldRender>
-                                                </button>
-                                            </ShouldRender>
+                                            <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
+                                                {/* <span>View</span> */}
+                                            </span>
                                         </div>
                                     </td>
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody className="Table-body">
+                                {scripts.map((x, i) => (
+                                    <tr
+                                        key={i}
+                                        className="Table-row db-ListViewItem bs-ActionsParent db-ListViewItem--hasLink incidentListItem"
+                                        style={{
+                                            height: '50px',
+                                            width: '100%',
+                                        }}
+                                    >
+                                        <td
+                                            className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                            style={{
+                                                height: '1px',
+                                                minWidth: '210px',
+                                            }}
+                                        >
+                                            <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                                {x.name}
+                                            </div>
+                                        </td>
+                                        <td
+                                            className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                            style={{
+                                                height: '1px',
+                                                minWidth: '210px',
+                                            }}
+                                        >
+                                            <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                                {x.script}
+                                            </div>
+                                        </td>
+                                        <td
+                                            className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                            style={{
+                                                height: '1px',
+                                            }}
+                                        >
+                                            <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                                <ShouldRender if={true}>
+                                                    <button
+                                                        className="bs-Button"
+                                                        disabled={false}
+                                                    >
+                                                        <span>View</span>
+                                                    </button>
+                                                </ShouldRender>
+                                            </div>
+                                        </td>
+                                        <td
+                                            className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                            style={{
+                                                height: '1px',
+                                            }}
+                                        >
+                                            <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                                <ShouldRender if={true}>
+                                                    <button
+                                                        className="bs-Button Box-background--red Text-color--white"
+                                                        // disabled={loading}
+                                                        onClick={() => {
+                                                            deleteScrip(x._id);
+                                                        }}
+                                                    >
+                                                        <ShouldRender
+                                                            if={!loading}
+                                                        >
+                                                            <span>Delete</span>
+                                                        </ShouldRender>
+
+                                                        <ShouldRender
+                                                            if={loading}
+                                                        >
+                                                            <FormLoader />
+                                                        </ShouldRender>
+                                                    </button>
+                                                </ShouldRender>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    )}
                 </div>
             </div>
         </div>
