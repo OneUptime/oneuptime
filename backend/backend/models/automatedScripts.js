8778cf37664494746db91844e383b4e49d6456da
Bosah Arthur <arthurbosah412gmail.com>
2021-05-31 16:27:49 +0100

    added slug in automated script schema

--- backend/backend/models/automatedScripts.js
+++ backend/backend/models/automatedScripts.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const automatedScriptSchema = new Schema({
     name: String,
     script: String,
+    slug: String,
     createdAt: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model('scripts', automatedScriptSchema);
