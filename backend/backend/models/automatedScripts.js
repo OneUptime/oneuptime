e47fefa1d0717ae2c428ad146347e088242bf132
augustinebest <augustusbest00@gmaile.com>
2021-06-09 22:28:54 +0100

    redesign automated script model

--- backend/backend/models/automatedScripts.js
+++ backend/backend/models/automatedScripts.js
@@ -1,10 +1,68 @@
 const mongoose = require('../config/db');
 
 const Schema = mongoose.Schema;
-const automatedScriptSchema = new Schema({
-    name: String,
-    script: String,
-    slug: String,
-    createdAt: { type: Date, default: Date.now },
-});
-module.exports = mongoose.model('scripts', automatedScriptSchema);
+const automatedScriptSchema = new Schema(
+    {
+        name: String,
+        script: String,
+        scriptType: String,
+        slug: String,
+        projectId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Project',
+            index: true,
+        },
+        createdById: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            index: true,
+        },
+        deleted: {
+            type: Boolean,
+            default: false,
+        },
+        deletedAt: {
+            type: Date,
+        },
+        deletedById: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            index: true,
+        },
+        successEvent: [
+            {
+                automatedScript: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'AutomationSript',
+                    index: true,
+                },
+                callSchedule: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Schedule',
+                    index: true,
+                },
+            },
+        ],
+        failureEvent: [
+            {
+                automatedScript: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'AutomationSript',
+                    index: true,
+                },
+                callSchedule: {
+                    type: Schema.Types.ObjectId,
+                    ref: 'Schedule',
+                    index: true,
+                },
+            },
+        ],
+        status: {
+            type: String,
+            enum: ['success', 'running', 'failed'],
+            default: 'running',
+        },
+    },
+    { timestamps: true }
+);
+module.exports = mongoose.model('AutomationSript', automatedScriptSchema);
