b8dd845c8407a5ac4d0edc70a6424e012183e999
Zadat Olayinka <bundayy@outlook.com>
2021-06-09 18:38:21 +0100

    listener set up

---
+++ ruby-sdk/fyipe/lib/fyipe/fyipeListener.rb
@@ -0,0 +1,43 @@
+require_relative 'timelineManager'
+require_relative 'util'
+
+class FyipeListener
+    def initialize(eventId, options)
+        # start the timeline manager
+        @timelineObj = FyipeTimelineManager.new(options)
+        @currentEventId = eventId
+        @utilObj = Util.new(options)
+    end
+
+    def logErrorEvent(content, category = 'exception')
+
+        timelineObj = {
+            "category": category,
+            "data": content,
+            "type": @utilObj.getErrorType('ERROR'),
+            "eventId": @currentEventId
+        } 
+
+        # add timeline to the stack
+        @timelineObj.addToTimeline(timelineObj)
+    end
+
+    def logCustomTimelineEvent(timelineObj)
+        timelineObj["eventId"] = @currentEventId
+
+        # add timeline to the stack
+        @timelineObj.addToTimeline(timelineObj)
+    end
+
+    def getTimeline()
+        # this always get the current state of the timeline array
+        return @timelineObj.getTimeline()
+    end
+
+    def clearTimeline(eventId):
+        # set a new eventId
+        @currentEventId = eventId
+        # this will reset the state of the timeline array
+        return @timelineObj.clearTimeline()
+    
+end
