6e010031771805891b8a9a9aab73ba23e6e730b5
augustinebest <augustusbest00@gmaile.com>
2021-06-10 08:22:00 +0100

    added fetch automation script and improved api

--- backend/backend/api/automatedScript.js
+++ backend/backend/api/automatedScript.js
@@ -16,16 +16,44 @@ const { getUser } = require('../middlewares/user');
 
 router.get('/:projectId', getUser, isAuthorized, async function(req, res) {
     try {
-        const scripts = await AutomatedService.findBy();
-        return sendListResponse(req, res, scripts);
+        const { projectId } = req.params;
+        const { skip, limit } = req.query;
+        const scripts = await AutomatedService.findBy(
+            { projectId },
+            skip,
+            limit
+        );
+        const count = await AutomatedService.countBy({ projectId });
+        return sendListResponse(req, res, scripts, count);
     } catch (error) {
         return sendErrorResponse(req, res, error);
     }
 });
 
+router.get(
+    '/:projectId/:automatedSlug',
+    getUser,
+    isAuthorized,
+    async (req, res) => {
+        try {
+            const { automatedSlug, projectId } = req.params;
+            const response = await AutomatedService.getAutomatedService({
+                projectId,
+                slug: automatedSlug,
+            });
+            return sendListResponse(req, res, response);
+        } catch (error) {
+            return sendErrorResponse(req, res, error);
+        }
+    }
+);
+
 router.post('/:projectId', getUser, isAuthorized, async (req, res) => {
     try {
         const data = req.body;
+        const userId = req.user ? req.user.id : null;
+        data.projectId = req.params.projectId;
+        data.createdById = userId;
         if (!data) {
             return sendErrorResponse(req, res, {
                 code: 400,
@@ -35,18 +63,42 @@ router.post('/:projectId', getUser, isAuthorized, async (req, res) => {
         if (!data.name || !data.name.trim()) {
             return sendErrorResponse(req, res, {
                 code: 400,
-                message: 'Automated Script name is required',
+                message: 'Script name is required',
             });
         }
 
-        if (!data.script || !data.script.trim()) {
+        if (!data.scriptType || data.scriptType.trim().length === 0) {
             return sendErrorResponse(req, res, {
                 code: 400,
-                message: 'Automated Script script is required',
+                message: 'Script Type is required',
             });
         }
-        const callLog = await AutomatedService.create(data);
-        return sendItemResponse(req, res, callLog);
+
+        if (!data.script || data.script.trim().length === 0) {
+            return sendErrorResponse(req, res, {
+                code: 400,
+                message: 'Script is required',
+            });
+        }
+        const formatEvent = arr => {
+            const result = [];
+            for (const a of arr) {
+                if (a.type === 'callSchedule') {
+                    result.push({ callSchedule: a.resource });
+                } else {
+                    result.push({ automatedScript: a.resource });
+                }
+            }
+            return result;
+        };
+        if (data.successEvent.length > 0) {
+            data.successEvent = formatEvent(data.successEvent);
+        }
+        if (data.failureEvent.length > 0) {
+            data.failureEvent = formatEvent(data.failureEvent);
+        }
+        const response = await AutomatedService.create(data);
+        return sendItemResponse(req, res, response);
     } catch (error) {
         return sendErrorResponse(req, res, error);
     }
