08e334b6cfdd97d90f6acfc67b59a268c95389ab
adeoluwadavid <adeoluwadavid@gmail.com>
2021-06-10 12:40:58 +0100

    lighthouse-bug

--- backend/backend/api/subscriber.js
+++ backend/backend/api/subscriber.js
@@ -317,9 +317,7 @@ router.get('/:projectId', async function(req, res) {
 //get subscribers by monitorId
 // req.params-> {projectId, monitorId};
 // Returns: response subscriber, error message
-router.get('/:projectId/monitor/:monitorId', async function(req, res) {   
-    console.log("Subscriber Project ID: ", req.params.projectId);
-    console.log("Subscriber Monitor ID: ", req.params.monitorId); 
+router.get('/:projectId/monitor/:monitorId', async function(req, res) {
     try {
         const monitorId = req.params.monitorId;
         const skip = req.query.skip || 0;
@@ -387,8 +385,6 @@ router.get('/monitorList/:subscriberId', async function(req, res) {
 // Returns: response subscriber, error message
 router.get('/:projectId/:subscriberId', async function(req, res) {
     try {
-     console.log("Second Subscriber Project ID: ", req.params.projectId);
-    console.log("Second Subscriber ID: ", req.params.subscriberId);
         const projectId = req.params.projectId;
         const subscriberId = req.params.subscriberId;
         const subscriber = await SubscriberService.findByOne({
