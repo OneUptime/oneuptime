53739af8b7bf216bb5c28a208cce94b30c57653d
augustinebest <augustusbest00@gmaile.com>
2021-06-10 08:26:29 +0100

    refactored reducer

--- dashboard/src/reducers/automatedScript.js
+++ dashboard/src/reducers/automatedScript.js
@@ -1,198 +1,115 @@
-/* eslint-disable no-console */
 import {
     FETCH_AUTOMATED_SCRIPT_SUCCESS,
-    FETCH_AUTOMATED_SCRIPT_FAILURE,
-    FETCH_AUTOMATED_SCRIPT_RESET,
-    FETCH_AUTOMATED_SCRIPT_REQUEST,
-    // CREATE_AUTOMATED_SCRIPT_SUCCESS,
-    // CREATE_AUTOMATED_SCRIPT_FAILURE,
+    CREATE_AUTOMATED_SCRIPT_SUCCESS,
+    CREATE_AUTOMATED_SCRIPT_FAILURE,
     CREATE_AUTOMATED_SCRIPT_REQUEST,
-    EDIT_AUTOMATED_SCRIPT_SUCCESS,
-    EDIT_AUTOMATED_SCRIPT_FAILURE,
-    EDIT_AUTOMATED_SCRIPT_RESET,
-    EDIT_AUTOMATED_SCRIPT_REQUEST,
-    EDIT_AUTOMATED_SCRIPT_SWITCH,
+    RESET_AUTOMATED_SCRIPT,
+    FETCH_SINGLE_SCRIPT_SUCCESS,
+    FETCH_SINGLE_SCRIPT_REQUEST,
+    FETCH_SINGLE_SCRIPT_FAILURE,
 } from '../constants/automatedScript';
 
 const INITIAL_STATE = {
-    scripts: [],
+    addScripts: {
+        requesting: false,
+        success: false,
+        error: null,
+    },
+    fetchScripts: {
+        scripts: [],
+        requesting: false,
+        success: false,
+        error: null,
+    },
+    individualScript: {
+        data: {},
+        requesting: false,
+        success: false,
+        error: null,
+    },
 };
 
 export default function component(state = INITIAL_STATE, action) {
     switch (action.type) {
-        // case CREATE_AUTOMATED_SCRIPT_FAILURE:
-        //     return Object.assign({}, state, {
-        //         newComponent: {
-        //             ...state.newComponent,
-        //             requesting: false,
-        //             error: action.payload,
-        //             success: false,
-        //         },
-        //     });
-
+        case RESET_AUTOMATED_SCRIPT:
+            return Object.assign({}, state, {
+                addScripts: {
+                    ...state.addScripts,
+                    requesting: false,
+                    error: null,
+                    success: false,
+                },
+            });
+        case CREATE_AUTOMATED_SCRIPT_FAILURE:
+            return Object.assign({}, state, {
+                addScripts: {
+                    ...state.addScripts,
+                    requesting: false,
+                    error: action.payload,
+                    success: false,
+                },
+            });
         case CREATE_AUTOMATED_SCRIPT_REQUEST:
             return Object.assign({}, state, {
-                newComponent: {
-                    ...state.newComponent,
+                addScripts: {
+                    ...state.addScripts,
                     requesting: true,
+                    error: null,
+                    success: false,
+                },
+            });
+
+        case CREATE_AUTOMATED_SCRIPT_SUCCESS:
+            return Object.assign({}, state, {
+                addScripts: {
+                    ...state.addScripts,
+                    requesting: false,
+                    error: null,
+                    success: true,
                 },
             });
 
         case FETCH_AUTOMATED_SCRIPT_SUCCESS:
             return Object.assign({}, state, {
-                scripts: action.payload,
+                fetchScripts: {
+                    ...state.fetchScripts,
+                    scripts: action.payload.data,
+                    count: action.payload.count,
+                    skip: action.payload.skip,
+                    limit: action.payload.limit,
+                    requesting: false,
+                    error: null,
+                    success: true,
+                },
             });
 
-        case FETCH_AUTOMATED_SCRIPT_FAILURE:
+        case FETCH_SINGLE_SCRIPT_SUCCESS:
             return Object.assign({}, state, {
-                componentList: {
-                    ...state.componentList,
+                individualScript: {
+                    data: action.payload,
                     requesting: false,
+                    success: true,
+                    error: null,
+                },
+            });
+
+        case FETCH_SINGLE_SCRIPT_FAILURE:
+            return Object.assign({}, state, {
+                individualScript: {
+                    data: {},
+                    requesting: false,
+                    success: false,
                     error: action.payload,
-                    success: false,
                 },
             });
 
-        case FETCH_AUTOMATED_SCRIPT_RESET:
+        case FETCH_SINGLE_SCRIPT_REQUEST:
             return Object.assign({}, state, {
-                componentList: INITIAL_STATE.componentList,
-            });
-
-        case FETCH_AUTOMATED_SCRIPT_REQUEST:
-            return Object.assign({}, state, {
-                componentList: {
-                    ...state.componentList,
+                individualScript: {
+                    data: {},
                     requesting: true,
+                    success: false,
                     error: null,
-                    success: false,
-                },
-            });
-
-        case EDIT_AUTOMATED_SCRIPT_SUCCESS:
-            return Object.assign({}, state, {
-                componentList: {
-                    ...state.componentList,
-                    requesting: false,
-                    error: null,
-                    success: false,
-                    components: state.componentList.components.map(project => {
-                        const subProject = Object.assign({}, project);
-                        const subProjectComponents =
-                            subProject.components &&
-                            subProject.components.slice();
-
-                        const newComponent = Object.assign({}, action.payload);
-
-                        const componentIndex =
-                            subProjectComponents &&
-                            subProjectComponents.findIndex(
-                                component => component._id === newComponent._id
-                            );
-                        const isSubProjectComponent = componentIndex > -1;
-
-                        if (subProject._id === newComponent.projectId._id) {
-                            if (isSubProjectComponent) {
-                                const oldComponent = Object.assign(
-                                    {},
-                                    subProjectComponents[componentIndex]
-                                );
-
-                                if (!newComponent.skip)
-                                    newComponent.skip = oldComponent.skip;
-                                if (!newComponent.limit)
-                                    newComponent.limit = oldComponent.limit;
-                                if (!newComponent.count)
-                                    newComponent.count = oldComponent.count;
-
-                                subProjectComponents[
-                                    componentIndex
-                                ] = newComponent;
-                            } else {
-                                newComponent.skip = 0;
-                                newComponent.limit = 0;
-                                newComponent.count = 0;
-
-                                subProjectComponents.unshift(newComponent);
-                                subProject.count += 1;
-                            }
-                        } else {
-                            if (isSubProjectComponent) {
-                                subProjectComponents.splice(componentIndex, 1);
-                                subProject.count -= 1;
-                            }
-                        }
-
-                        subProject.components = subProjectComponents;
-                        return subProject;
-                    }),
-                },
-                editComponent: {
-                    requesting: false,
-                    error: null,
-                    success: false,
-                },
-                currentComponent: {
-                    ...state.currentComponent,
-                    component: action.payload,
-                },
-            });
-
-        case EDIT_AUTOMATED_SCRIPT_FAILURE:
-            return Object.assign({}, state, {
-                editComponent: {
-                    requesting: false,
-                    error: action.payload,
-                    success: false,
-                },
-            });
-
-        case EDIT_AUTOMATED_SCRIPT_RESET:
-            return Object.assign({}, state, {
-                editComponent: INITIAL_STATE.editComponent,
-            });
-
-        case EDIT_AUTOMATED_SCRIPT_REQUEST:
-            return Object.assign({}, state, {
-                editComponent: {
-                    requesting: true,
-                    error: null,
-                    success: false,
-                },
-            });
-
-        case EDIT_AUTOMATED_SCRIPT_SWITCH:
-            return Object.assign({}, state, {
-                componentList: {
-                    ...state.componentList,
-                    requesting: false,
-                    error: null,
-                    success: false,
-                    components: state.componentList.components.map(
-                        component => {
-                            component.components = component.components.map(
-                                (component, i) => {
-                                    if (
-                                        i === action.payload ||
-                                        component._id === action.payload
-                                    ) {
-                                        if (!component.editMode)
-                                            component.editMode = true;
-                                        else component.editMode = false;
-                                        return component;
-                                    } else {
-                                        component.editMode = false;
-                                        return component;
-                                    }
-                                }
-                            );
-                            return component;
-                        }
-                    ),
-                },
-                editComponent: {
-                    requesting: false,
-                    error: null,
-                    success: false,
                 },
             });
 
