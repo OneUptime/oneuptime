2f09a3f2fe8bf78534c4f83253719756382bc40b
augustinebest <augustusbest00@gmaile.com>
2021-06-11 08:30:15 +0100

    added new columns to table

--- dashboard/src/components/automationScript/AutomatedTabularList.js
+++ dashboard/src/components/automationScript/AutomatedTabularList.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ShouldRender from '../basic/ShouldRender';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { deleteAutomatedScript } from '../../actions/automatedScript';
+import { openModal } from '../../actions/modal';
+import ShouldRender from '../basic/ShouldRender';
+import moment from 'moment';
+import { v4 as uuidv4 } from 'uuid';
+import DataPathHoC from '../DataPathHoC';
+import { bindActionCreators } from 'redux';
+import RunAutomationScript from '../modals/RunAutomationScript';
 
 const AutomatedTabularList = props => {
+    const [automatedId] = useState(uuidv4);
     const { scripts } = props;
+    const projectId = props.currentProject && props.currentProject._id;
     const pathName = props.history.location.pathname;
 
     return (
@@ -55,6 +62,32 @@ const AutomatedTabularList = props => {
                                     </td>
                                     <td
                                         className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                        style={{
+                                            height: '1px',
+                                            minWidth: '210px',
+                                        }}
+                                    >
+                                        <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                            <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
+                                                <span>Script Type</span>
+                                            </span>
+                                        </div>
+                                    </td>
+                                    <td
+                                        className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                        style={{
+                                            height: '1px',
+                                            minWidth: '210px',
+                                        }}
+                                    >
+                                        <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                            <span className="db-ListViewItem-text Text-color--dark Text-display--inline Text-fontSize--13 Text-fontWeight--medium Text-lineHeight--20 Text-typeface--upper Text-wrap--wrap">
+                                                <span>Last ran at</span>
+                                            </span>
+                                        </div>
+                                    </td>
+                                    <td
+                                        className="Table-cell Table-cell--align--left Table-cell--verticalAlign--top Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
                                         style={{ height: '1px' }}
                                     >
                                         <div className="db-ListViewItem-cellContent Box-root Padding-all--8 bs-table-display-end">
@@ -66,18 +99,13 @@ const AutomatedTabularList = props => {
                                 </tr>
                             </thead>
                             <tbody className="Table-body">
-                                {scripts.map((x, i) => (
+                                {scripts.map((script, index) => (
                                     <tr
-                                        key={i}
+                                        key={index}
                                         className="Table-row db-ListViewItem bs-ActionsParent db-ListViewItem--hasLink incidentListItem"
                                         style={{
                                             height: '50px',
                                             width: '100%',
-                                        }}
-                                        onClick={() => {
-                                            props.history.push(
-                                                `${pathName}/${x.slug}`
-                                            );
                                         }}
                                     >
                                         <td
@@ -88,7 +116,56 @@ const AutomatedTabularList = props => {
                                             }}
                                         >
                                             <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
-                                                {x.name}
+                                                {script.name}
+                                            </div>
+                                        </td>
+                                        <td
+                                            className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                            style={{
+                                                height: '1px',
+                                                minWidth: '210px',
+                                            }}
+                                        >
+                                            <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                                {script.scriptType}
+                                            </div>
+                                        </td>
+                                        <td
+                                            className="Table-cell Table-cell--align--left  Table-cell--width--minimized Table-cell--wrap--noWrap db-ListViewItem-cell"
+                                            style={{
+                                                height: '1px',
+                                                minWidth: '210px',
+                                            }}
+                                        >
+                                            <div className="db-ListViewItem-cellContent Box-root Padding-all--8">
+                                                <ShouldRender
+                                                    if={
+                                                        script.createdAt ===
+                                                        script.updatedAt
+                                                    }
+                                                >
+                                                    <span>
+                                                        {moment(
+                                                            script.createdAt
+                                                        ).format(
+                                                            'MMMM Do YYYY, h:mm a'
+                                                        )}
+                                                    </span>
+                                                </ShouldRender>
+                                                <ShouldRender
+                                                    if={
+                                                        script.updatedAt >
+                                                        script.createdAt
+                                                    }
+                                                >
+                                                    <span>
+                                                        {moment(
+                                                            script.updatedAt
+                                                        ).format(
+                                                            'MMMM Do YYYY, h:mm a'
+                                                        )}
+                                                    </span>
+                                                </ShouldRender>
                                             </div>
                                         </td>
                                         <td
@@ -98,14 +175,42 @@ const AutomatedTabularList = props => {
                                             }}
                                         >
                                             <div className="db-ListViewItem-cellContent Box-root Padding-all--8 bs-table-display-end">
-                                                <ShouldRender if={true}>
-                                                    <button
-                                                        className="bs-Button"
-                                                        disabled={false}
-                                                    >
-                                                        <span>View</span>
-                                                    </button>
-                                                </ShouldRender>
+                                                <button
+                                                    className="bs-Button"
+                                                    title="run"
+                                                    disabled={false}
+                                                    onClick={() =>
+                                                        props.openModal({
+                                                            id: automatedId,
+                                                            content: DataPathHoC(
+                                                                RunAutomationScript,
+                                                                {
+                                                                    automatedScriptId:
+                                                                        script._id,
+                                                                    projectId,
+                                                                }
+                                                            ),
+                                                        })
+                                                    }
+                                                >
+                                                    <span>Run</span>
+                                                </button>
+                                                <button
+                                                    className="bs-Button"
+                                                    title="view"
+                                                    disabled={false}
+                                                    onClick={() => {
+                                                        props.history.push({
+                                                            pathname: `${pathName}/${script.slug}`,
+                                                            state: {
+                                                                script:
+                                                                    script.script,
+                                                            },
+                                                        });
+                                                    }}
+                                                >
+                                                    <span>View</span>
+                                                </button>
                                             </div>
                                         </td>
                                     </tr>
@@ -122,6 +227,8 @@ const AutomatedTabularList = props => {
 AutomatedTabularList.propTypes = {
     scripts: PropTypes.array.isRequired,
     history: PropTypes.object.isRequired,
+    openModal: PropTypes.func,
+    currentProject: PropTypes.object,
 };
 
 const mapStateToProps = state => {
@@ -130,6 +237,10 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps, { deleteAutomatedScript })(
-    AutomatedTabularList
-);
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ openModal }, dispatch);
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AutomatedTabularList);
