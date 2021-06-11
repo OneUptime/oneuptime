804859f0628eb803c1a1b47744adb4216865917a
augustinebest <augustusbest00@gmaile.com>
2021-06-11 08:27:13 +0100

    improved services

--- backend/backend/services/automatedScriptService.js
+++ backend/backend/services/automatedScriptService.js
@@ -1,4 +1,5 @@
 const ScriptModel = require('../models/automatedScripts');
+const ScriptModelLog = require('../models/automationScriptsLog');
 const getSlug = require('../utils/getSlug');
 const ErrorService = require('./errorService');
 
@@ -42,22 +43,90 @@ module.exports = {
         }
     },
 
+    countLogBy: async function(query) {
+        try {
+            if (!query) {
+                query = {};
+            }
+            query.deleted = false;
+            const count = await ScriptModelLog.countDocuments(query);
+            return count;
+        } catch (error) {
+            ErrorService.log('automatedScript.countLogBy', error);
+            throw error;
+        }
+    },
+
     create: async function(data) {
         try {
             const script = new ScriptModel();
             script.name = data.name || null;
-            script.script = data.script || null;
             script.slug = getSlug(data.name) || null;
             script.scriptType = data.scriptType || null;
             script.successEvent = data.successEvent || null;
             script.failureEvent = data.failureEvent || null;
             script.projectId = data.projectId || null;
-            script.createdById = data.createdById || null;
+            script.script = data.script || null;
             const newScript = await script.save();
 
             return newScript;
         } catch (error) {
             ErrorService.log('automatedScript.create', error);
+            throw error;
+        }
+    },
+
+    createLog: async function(id, data) {
+        try {
+            console.log(data);
+            const scriptLog = new ScriptModelLog();
+            scriptLog.automationScriptId = id || null;
+            scriptLog.triggerByUser = data.triggerByUser || null;
+            scriptLog.triggerByScript = data.triggerByScript || null;
+            const newScriptLog = await scriptLog.save();
+
+            return newScriptLog;
+        } catch (error) {
+            ErrorService.log('automatedScript.createLog', error);
+            throw error;
+        }
+    },
+
+    updateOne: async function(query, data) {
+        try {
+            if (!query) {
+                query = {};
+            }
+            query.deleted = false;
+            const response = ScriptModel.findOneAndUpdate(
+                query,
+                {
+                    $set: data,
+                },
+                {
+                    new: true,
+                }
+            );
+            return response;
+        } catch (error) {
+            ErrorService.log('automatedScript.updateOne', error);
+            throw error;
+        }
+    },
+
+    findAllLogs: async function(query) {
+        try {
+            if (!query) {
+                query = {};
+            }
+            query.deleted = false;
+            const response = await ScriptModelLog.find(query)
+                .populate('automationScriptId', 'name')
+                .populate('triggerByUser', 'name')
+                .populate('triggerByScript', 'name');
+            return response;
+        } catch (error) {
+            ErrorService.log('automatedScript.findAllLogs', error);
             throw error;
         }
     },
@@ -80,13 +149,24 @@ module.exports = {
         }
     },
 
-    getAutomatedService: async function(query) {
+    getAutomatedLogService: async function(query) {
         try {
             const _this = this;
-            const response = await _this.findOneBy(query);
+            const response = await _this.findAllLogs(query);
             return response;
         } catch (error) {
-            ErrorService.log('automatedScript.getAutomatedService', error);
+            ErrorService.log('automatedScript.getAutomatedLogService', error);
+            throw error;
+        }
+    },
+
+    createScript: async function(data) {
+        try {
+            const _this = this;
+            const response = await _this.create(data);
+            return response;
+        } catch (error) {
+            ErrorService.log('automatedScript.createScript', error);
             throw error;
         }
     },
