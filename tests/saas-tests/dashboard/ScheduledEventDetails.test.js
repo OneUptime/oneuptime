9366719d4230fbb1cddc70df3445928736416fa7
adeoluwadavid <adeoluwadavid@gmail.com>
2021-06-12 15:53:04 +0100

    saas-dashboard-S

--- tests/saas-tests/dashboard/ScheduledEventDetails.test.js
+++ tests/saas-tests/dashboard/ScheduledEventDetails.test.js
@@ -18,205 +18,205 @@ const user = {
 };
 require('should');
 
-// describe('Scheduled Event Note', () => {
-//     const operationTimeOut = init.timeout;
+describe('Scheduled Event Note', () => {
+    const operationTimeOut = init.timeout;
 
-//     beforeAll(async done => {
-//         jest.setTimeout(init.timeout);
+    beforeAll(async done => {
+        jest.setTimeout(init.timeout);
 
-//         browser = await puppeteer.launch(utils.puppeteerLaunchConfig);
-//         page = await browser.newPage();
-//         await page.setUserAgent(utils.agent);
-//         // Register user
+        browser = await puppeteer.launch(utils.puppeteerLaunchConfig);
+        page = await browser.newPage();
+        await page.setUserAgent(utils.agent);
+        // Register user
 
-//         // user
-//         await init.registerUser(user, page);
-//         // Create component
-//         await init.addComponent(componentName, page);
-//         // Create monitor
-//         await init.addMonitorToComponent(
-//             null,
-//             monitorName,
-//             page,
-//             componentName
-//         );
-//         // Create a scheduled maintenance
-//         await init.addScheduledMaintenance(
-//             monitorName,
-//             scheduledEventName,
-//             componentName,
-//             page
-//         );
+        // user
+        await init.registerUser(user, page);
+        // Create component
+        await init.addComponent(componentName, page);
+        // Create monitor
+        await init.addMonitorToComponent(
+            null,
+            monitorName,
+            page,
+            componentName
+        );
+        // Create a scheduled maintenance
+        await init.addScheduledMaintenance(
+            monitorName,
+            scheduledEventName,
+            componentName,
+            page
+        );
 
-//         done();
-//     });
+        done();
+    });
 
-//     afterAll(async done => {
-//         await browser.close();
-//         done();
-//     });
+    afterAll(async done => {
+        await browser.close();
+        done();
+    });
 
-//     test(
-//         'should create an internal note',
-//         async done => {
-//             await page.goto(utils.DASHBOARD_URL, {
-//                 waitUntil: ['networkidle2'],
-//             });
-//             await init.pageWaitForSelector(page, '#scheduledMaintenance', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#scheduledMaintenance');
+    test(
+        'should create an internal note',
+        async done => {
+            await page.goto(utils.DASHBOARD_URL, {
+                waitUntil: ['networkidle2'],
+            });
+            await init.pageWaitForSelector(page, '#scheduledMaintenance', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#scheduledMaintenance');
 
-//             await init.pageWaitForSelector(page, '#viewScheduledEvent_0', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#viewScheduledEvent_0');
-//             // navigate to the note tab section
-//             await init.pageClick(page, '.timeline-tab');
-//             await init.pageWaitForSelector(page, '#add-internal-message', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#add-internal-message');
-//             await init.pageWaitForSelector(page, '#event_state', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.selectDropdownValue(
-//                 '#event_state',
-//                 'investigating',
-//                 page
-//             );
-//             await init.pageClick(page, '#new-internal');
-//             await init.pageType(
-//                 page,
-//                 '#new-internal',
-//                 'Some random description'
-//             );
-//             await init.pageClick(page, '#internal-addButton');
-//             await init.pageWaitForSelector(
-//                 page,
-//                 '#form-new-schedule-investigation-message',
-//                 { hidden: true }
-//             );
-//             const note = await init.pageWaitForSelector(
-//                 page,
-//                 '#Internal_incident_message_0',
-//                 { visible: true, timeout: init.timeout }
-//             );
-//             expect(note).toBeDefined();
-//             done();
-//         },
-//         operationTimeOut
-//     );
+            await init.pageWaitForSelector(page, '#viewScheduledEvent_0', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#viewScheduledEvent_0');
+            // navigate to the note tab section
+            await init.pageClick(page, '.timeline-tab');
+            await init.pageWaitForSelector(page, '#add-internal-message', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#add-internal-message');
+            await init.pageWaitForSelector(page, '#event_state', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.selectDropdownValue(
+                '#event_state',
+                'investigating',
+                page
+            );
+            await init.pageClick(page, '#new-internal');
+            await init.pageType(
+                page,
+                '#new-internal',
+                'Some random description'
+            );
+            await init.pageClick(page, '#internal-addButton');
+            await init.pageWaitForSelector(
+                page,
+                '#form-new-schedule-investigation-message',
+                { hidden: true }
+            );
+            const note = await init.pageWaitForSelector(
+                page,
+                '#Internal_incident_message_0',
+                { visible: true, timeout: init.timeout }
+            );
+            expect(note).toBeDefined();
+            done();
+        },
+        operationTimeOut
+    );
 
-//     test(
-//         'should edit an internal note',
-//         async done => {
-//             await page.goto(utils.DASHBOARD_URL, {
-//                 waitUntil: ['networkidle2'],
-//             });
-//             await init.pageWaitForSelector(page, '#scheduledMaintenance', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#scheduledMaintenance');
+    test(
+        'should edit an internal note',
+        async done => {
+            await page.goto(utils.DASHBOARD_URL, {
+                waitUntil: ['networkidle2'],
+            });
+            await init.pageWaitForSelector(page, '#scheduledMaintenance', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#scheduledMaintenance');
 
-//             await init.pageWaitForSelector(page, '#viewScheduledEvent_0', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#viewScheduledEvent_0');
-//             // navigate to the note tab section
-//             await init.pageClick(page, '.timeline-tab');
+            await init.pageWaitForSelector(page, '#viewScheduledEvent_0', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#viewScheduledEvent_0');
+            // navigate to the note tab section
+            await init.pageClick(page, '.timeline-tab');
 
-//             await init.pageWaitForSelector(
-//                 page,
-//                 '#edit_Internal_incident_message_0',
-//                 {
-//                     visible: true,
-//                     timeout: init.timeout,
-//                 }
-//             );
-//             await init.pageClick(page, '#edit_Internal_incident_message_0');
-//             await init.pageWaitForSelector(page, '#update-internal', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#update-internal');
-//             await init.pageType(
-//                 page,
-//                 '#update-internal',
-//                 'An updated description'
-//             );
-//             await init.pageClick(page, '#internal-updateButton');
-//             await init.pageWaitForSelector(
-//                 page,
-//                 '#form-update-schedule-internal-message',
-//                 { hidden: true }
-//             );
-//             const edited = await init.pageWaitForSelector(
-//                 page,
-//                 '#edited_Internal_incident_message_0',
-//                 { visible: true, timeout: init.timeout }
-//             );
-//             expect(edited).toBeDefined();
-//             done();
-//         },
-//         operationTimeOut
-//     );
+            await init.pageWaitForSelector(
+                page,
+                '#edit_Internal_incident_message_0',
+                {
+                    visible: true,
+                    timeout: init.timeout,
+                }
+            );
+            await init.pageClick(page, '#edit_Internal_incident_message_0');
+            await init.pageWaitForSelector(page, '#update-internal', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#update-internal');
+            await init.pageType(
+                page,
+                '#update-internal',
+                'An updated description'
+            );
+            await init.pageClick(page, '#internal-updateButton');
+            await init.pageWaitForSelector(
+                page,
+                '#form-update-schedule-internal-message',
+                { hidden: true }
+            );
+            const edited = await init.pageWaitForSelector(
+                page,
+                '#edited_Internal_incident_message_0',
+                { visible: true, timeout: init.timeout }
+            );
+            expect(edited).toBeDefined();
+            done();
+        },
+        operationTimeOut
+    );
 
-//     test(
-//         'should delete an internal note',
-//         async done => {
-//             await page.goto(utils.DASHBOARD_URL, {
-//                 waitUntil: ['networkidle2'],
-//             });
-//             await init.pageWaitForSelector(page, '#scheduledMaintenance', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#scheduledMaintenance');
+    test(
+        'should delete an internal note',
+        async done => {
+            await page.goto(utils.DASHBOARD_URL, {
+                waitUntil: ['networkidle2'],
+            });
+            await init.pageWaitForSelector(page, '#scheduledMaintenance', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#scheduledMaintenance');
 
-//             await init.pageWaitForSelector(page, '#viewScheduledEvent_0', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#viewScheduledEvent_0');
-//             // navigate to the note tab section
-//             await init.pageClick(page, '.timeline-tab');
-//             await init.pageWaitForSelector(
-//                 page,
-//                 '#delete_Internal_incident_message_0',
-//                 {
-//                     visible: true,
-//                     timeout: init.timeout,
-//                 }
-//             );
-//             await init.pageClick(page, '#delete_Internal_incident_message_0');
-//             await init.pageWaitForSelector(page, '#deleteNote', {
-//                 visible: true,
-//                 timeout: init.timeout,
-//             });
-//             await init.pageClick(page, '#deleteNote');
-//             await init.pageWaitForSelector(page, '#deleteNote', {
-//                 hidden: true,
-//             });
+            await init.pageWaitForSelector(page, '#viewScheduledEvent_0', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#viewScheduledEvent_0');
+            // navigate to the note tab section
+            await init.pageClick(page, '.timeline-tab');
+            await init.pageWaitForSelector(
+                page,
+                '#delete_Internal_incident_message_0',
+                {
+                    visible: true,
+                    timeout: init.timeout,
+                }
+            );
+            await init.pageClick(page, '#delete_Internal_incident_message_0');
+            await init.pageWaitForSelector(page, '#deleteNote', {
+                visible: true,
+                timeout: init.timeout,
+            });
+            await init.pageClick(page, '#deleteNote');
+            await init.pageWaitForSelector(page, '#deleteNote', {
+                hidden: true,
+            });
 
-//             const note = await init.pageWaitForSelector(
-//                 page,
-//                 '#delete_Internal_incident_message_0',
-//                 { hidden: true }
-//             );
-//             expect(note).toBeNull();
-//             done();
-//         },
-//         operationTimeOut
-//     );
-//     // Deleted three tests that repeated
-// });
+            const note = await init.pageWaitForSelector(
+                page,
+                '#delete_Internal_incident_message_0',
+                { hidden: true }
+            );
+            expect(note).toBeNull();
+            done();
+        },
+        operationTimeOut
+    );
+    // Deleted three tests that repeated
+});
 
 describe('Scheduled Maintenance Note ==> Pagination and Deletion', () => {
     const operationTimeOut = init.timeout;
