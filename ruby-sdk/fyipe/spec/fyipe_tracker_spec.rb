6b1ab70f2845146c25c8fd152f4c8ac469f74f3b
Zadat Olayinka <bundayy@outlook.com>
2021-06-17 09:55:04 +0100

    capture exception tests so far

--- ruby-sdk/fyipe/spec/fyipe_tracker_spec.rb
+++ ruby-sdk/fyipe/spec/fyipe_tracker_spec.rb
@@ -39,186 +39,252 @@ RSpec.configure do |config|
 end
 
 RSpec.describe FyipeTracker do
-    it 'test_should_take_in_custom_timeline_event' do
-        tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
-        tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
-        timeline = tracker.getTimeline()
-        expect(timeline.class.to_s).to eql "Array"
-        expect(timeline.length()).to eql 1
-        expect($customTimeline["category"]).to eql timeline[0]["category"]
-    end
-    it 'test_should_ensure_timeline_event_contains_eventId_and_timestamp' do
-        tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
-        tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
+    # it 'test_should_take_in_custom_timeline_event' do
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
+    #     timeline = tracker.getTimeline()
+    #     expect(timeline.class.to_s).to eql "Array"
+    #     expect(timeline.length()).to eql 1
+    #     expect($customTimeline["category"]).to eql timeline[0]["category"]
+    # end
+    # it 'test_should_ensure_timeline_event_contains_eventId_and_timestamp' do
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
 
-        timeline = tracker.getTimeline()
+    #     timeline = tracker.getTimeline()
 
-        expect(timeline[0]["eventId"].class.to_s).to eql "String"
-        expect(timeline[0]["timestamp"].class.to_s).to eql "String"
-    end
-    it 'test_should_ensure_different_timeline_event_have_the_same_eventId' do
-        tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
-        tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
-        tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], "error")
+    #     expect(timeline[0]["eventId"].class.to_s).to eql "String"
+    #     expect(timeline[0]["timestamp"].class.to_s).to eql "String"
+    # end
+    # it 'test_should_ensure_different_timeline_event_have_the_same_eventId' do
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], "error")
 
-        timeline = tracker.getTimeline()
-        expect(timeline.length()).to eql 2 # two timeline events
-        expect(timeline[0]["eventId"]).to eql timeline[1]["eventId"] # their eventId is the same, till there is an error sent to the server
-    end
-    it 'test_should_ensure_max_timline_cant_be_set_as_a_negative_number' do
-        options = {
-            "maxTimeline": -5
-        }
-        tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"], options)
+    #     timeline = tracker.getTimeline()
+    #     expect(timeline.length()).to eql 2 # two timeline events
+    #     expect(timeline[0]["eventId"]).to eql timeline[1]["eventId"] # their eventId is the same, till there is an error sent to the server
+    # end
+    # it 'test_should_ensure_max_timline_cant_be_set_as_a_negative_number' do
+    #     options = {
+    #         "maxTimeline": -5
+    #     }
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"], options)
 
-        tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
-        tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], "error")
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], "error")
 
-        timeline = tracker.getTimeline()
-        expect(timeline.length()).to eql 2 # two timeline events
-    end
-    it 'test_should_ensure_new_timeline_event_after_max_timeline_are_discarded' do
-        options = {
-            "maxTimeline": 2
-        }
-        tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"], options)
+    #     timeline = tracker.getTimeline()
+    #     expect(timeline.length()).to eql 2 # two timeline events
+    # end
+    # it 'test_should_ensure_new_timeline_event_after_max_timeline_are_discarded' do
+    #     options = {
+    #         "maxTimeline": 2
+    #     }
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"], options)
 
-        customTimeline2 = {}
+    #     customTimeline2 = {}
 
-        customTimeline2["category"] = "logout"
-        customTimeline2["type"] = "success"
-        customTimeline2["content"] = {"message": "tester"}
+    #     customTimeline2["category"] = "logout"
+    #     customTimeline2["type"] = "success"
+    #     customTimeline2["content"] = {"message": "tester"}
 
-        # add 3 timeline events
-        tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
-        tracker.addToTimeline(customTimeline2["category"], customTimeline2["content"], customTimeline2["type"])
-        tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], "debug")
+    #     # add 3 timeline events
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
+    #     tracker.addToTimeline(customTimeline2["category"], customTimeline2["content"], customTimeline2["type"])
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], "debug")
 
-        timeline = tracker.getTimeline()
+    #     timeline = tracker.getTimeline()
         
-        expect(options[:maxTimeline]).to eql timeline.length() # three timeline events
-        expect(timeline[0]["type"]).to eql $customTimeline["type"]
-        expect(timeline[1]["category"]).to eql customTimeline2["category"]
-    end
+    #     expect(options[:maxTimeline]).to eql timeline.length() # three timeline events
+    #     expect(timeline[0]["type"]).to eql $customTimeline["type"]
+    #     expect(timeline[1]["category"]).to eql customTimeline2["category"]
+    # end
 
-    it 'test_should_add_tags' do 
-        tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+    # it 'test_should_add_tags' do 
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
 
-        tag = {
-            "key": "location",
-            "value": "Warsaw"
-        } 
-        tracker.setTag(tag[:key], tag[:value])
+    #     tag = {
+    #         "key": "location",
+    #         "value": "Warsaw"
+    #     } 
+    #     tracker.setTag(tag[:key], tag[:value])
         
-        availableTags = tracker.getTags()
-        expect(availableTags.class.to_s).to eql "Array"
-        expect(availableTags.length()).to eql 1
-        expect(tag[:key]).to eql availableTags[0]["key"]
-    end
+    #     availableTags = tracker.getTags()
+    #     expect(availableTags.class.to_s).to eql "Array"
+    #     expect(availableTags.length()).to eql 1
+    #     expect(tag[:key]).to eql availableTags[0]["key"]
+    # end
 
-    it 'test_should_add_multiple_tags' do
+    # it 'test_should_add_multiple_tags' do
         
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+
+    #     tags = []
+    #     tag = {
+    #         "key": "location",
+    #         "value": "Warsaw"
+    #     } 
+    #     tags.append(tag)
+
+    #     tagB = {
+    #         "key": "city",
+    #         "value": "Leeds"
+    #     } 
+    #     tags.append(tagB)
+
+    #     tagC = {
+    #         "key": "device",
+    #         "value": "iPhone"
+    #     } 
+    #     tags.append(tagC)
+
+    #     tracker.setTags(tags)
+
+    #     availableTags = tracker.getTags()
+    #     expect(availableTags.class.to_s).to eql "Array"
+    #     expect(availableTags.length()).to eql tags.length()
+    # end
+
+    # it 'test_should_overwrite_existing_keys_to_avoid_duplicate_tags' do
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+
+    #     tags = []
+    #     tag = {
+    #         "key": "location",
+    #         "value": "Warsaw"
+    #     } 
+    #     tags.append(tag)
+
+    #     tagB = {
+    #         "key": "city",
+    #         "value": "Leeds"
+    #     } 
+    #     tags.append(tagB)
+
+    #     tagC = {
+    #         "key": "location",
+    #         "value": "Paris"
+    #     } 
+    #     tags.append(tagC)
+
+    #     tagD = {
+    #         "key": "device",
+    #         "value": "iPhone"
+    #     } 
+    #     tags.append(tagD)
+
+    #     tagE = {
+    #         "key": "location",
+    #         "value": "London"
+    #     } 
+    #     tags.append(tagE)
+
+    #     tracker.setTags(tags)
+
+    #     availableTags = tracker.getTags()
+    #     expect(availableTags.class.to_s).to eql "Array"
+    #     expect(availableTags.length()).to eql 3 # only 3 unique tags
+    #     expect(tagC[:key]).to eql availableTags[0]["key"]
+    #     expect(tagC[:value]).not_to eql availableTags[0]["value"]# old value for that tag location
+    #     expect(tagE[:key]).to eql availableTags[0]["key"]
+    #     expect(tagE[:value]).to eql availableTags[0]["value"]# latest value for that tag location
+    # end
+
+    # it 'test_should_create_fingerprint_as_message_for_error_capture_without_any_fingerprint' do
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+
+    #     errorMessage = "Uncaught Exception"
+    #     tracker.captureMessage(errorMessage)
+    #     event = tracker.getCurrentEvent()
+    #     expect(event["fingerprint"][0]).to eql errorMessage
+    # end
+    # it 'test_should_use_defined_fingerprint_array_for_error_capture_with_fingerprint' do
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+
+
+    #     fingerprints = ['custom', 'errors']
+    #     tracker.setFingerPrint(fingerprints)
+    #     errorMessage = 'Uncaught Exception'
+    #     tracker.captureMessage(errorMessage)
+    #     event = tracker.getCurrentEvent()
+    #     expect(event["fingerprint"][0]).to eql fingerprints[0]
+    #     expect(event["fingerprint"][1]).to eql fingerprints[1]
+    # end
+    # it 'test_should_use_defined_fingerprint_string_for_error_capture_with_fingerprint' do
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+
+    #     fingerprint = 'custom-fingerprint'
+    #     tracker.setFingerPrint(fingerprint)
+    #     errorMessage = 'Uncaught Exception'
+    #     tracker.captureMessage(errorMessage)
+    #     event = tracker.getCurrentEvent()
+    #     expect(event["fingerprint"][0]).to eql fingerprint
+    # end
+    # it 'test_should_create_an_event_ready_for_the_server_using_capture_message' do 
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+
+    #     errorMessage = 'This is a test'
+    #     tracker.captureMessage(errorMessage)
+    #     event = tracker.getCurrentEvent()
+    #     expect(event["type"]).to eql "message"
+    #     expect(event["exception"]["message"]).to eql errorMessage
+    # end 
+    # it 'test_should_create_an_event_ready_for_the_server_while_having_the_timeline_with_same_event_id' do
+    #     tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
+    #     tracker.addToTimeline($customTimeline["category"], $customTimeline["content"], $customTimeline["type"])
+        
+    #     errorMessage = 'This is a test'
+    #     tracker.captureMessage(errorMessage)
+    #     event = tracker.getCurrentEvent()
+
+    #     expect(event["timeline"].length()).to eql 2
+    #     expect(event["eventId"]).to eql event["timeline"][0]["eventId"]
+    #     expect(event["exception"]["message"]).to eql errorMessage
+    # end
+    it 'test_should_create_an_event_ready_for_the_server_using_capture_exception' do
         tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
 
-        tags = []
-        tag = {
-            "key": "location",
-            "value": "Warsaw"
-        } 
-        tags.append(tag)
-
-        tagB = {
-            "key": "city",
-            "value": "Leeds"
-        } 
-        tags.append(tagB)
-
-        tagC = {
-            "key": "device",
-            "value": "iPhone"
-        } 
-        tags.append(tagC)
-
-        tracker.setTags(tags)
-
-        availableTags = tracker.getTags()
-        expect(availableTags.class.to_s).to eql "Array"
-        expect(availableTags.length()).to eql tags.length()
-    end
-
-    it 'test_should_overwrite_existing_keys_to_avoid_duplicate_tags' do
-        tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
-
-        tags = []
-        tag = {
-            "key": "location",
-            "value": "Warsaw"
-        } 
-        tags.append(tag)
-
-        tagB = {
-            "key": "city",
-            "value": "Leeds"
-        } 
-        tags.append(tagB)
-
-        tagC = {
-            "key": "location",
-            "value": "Paris"
-        } 
-        tags.append(tagC)
-
-        tagD = {
-            "key": "device",
-            "value": "iPhone"
-        } 
-        tags.append(tagD)
-
-        tagE = {
-            "key": "location",
-            "value": "London"
-        } 
-        tags.append(tagE)
-
-        tracker.setTags(tags)
-
-        availableTags = tracker.getTags()
-        expect(availableTags.class.to_s).to eql "Array"
-        expect(availableTags.length()).to eql 3 # only 3 unique tags
-        expect(tagC[:key]).to eql availableTags[0]["key"]
-        expect(tagC[:value]).not_to eql availableTags[0]["value"]# old value for that tag location
-        expect(tagE[:key]).to eql availableTags[0]["key"]
-        expect(tagE[:value]).to eql availableTags[0]["value"]# latest value for that tag location
-    end
-
-    it 'test_should_create_fingerprint_as_message_for_error_capture_without_any_fingerprint' do
-        tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
-
-        errorMessage = "Uncaught Exception"
-        tracker.captureMessage(errorMessage)
+        errorMessage = 'Error Found'
+        tracker.captureException(Exception.new(errorMessage))
         event = tracker.getCurrentEvent()
-        expect(event["fingerprint"][0]).to eql errorMessage
+
+        expect(event["type"]).to eql "exception"
+        expect(event["exception"]["message"]).to eql errorMessage
     end
-    it 'test_should_use_defined_fingerprint_array_for_error_capture_with_fingerprint' do
+    it 'test_should_create_an_event_with_array_of_stacktrace' do
         tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
 
-
-        fingerprints = ['custom', 'errors']
-        tracker.setFingerPrint(fingerprints)
-        errorMessage = 'Uncaught Exception'
-        tracker.captureMessage(errorMessage)
+        errorType = ""
+        begin
+            divByZero = 1/0
+        rescue => ex
+            errorType = ex.class
+            tracker.captureException(ex)
+        end
         event = tracker.getCurrentEvent()
-        expect(event["fingerprint"][0]).to eql fingerprints[0]
-        expect(event["fingerprint"][1]).to eql fingerprints[1]
-    end
-    it 'test_should_use_defined_fingerprint_string_for_error_capture_with_fingerprint' do
+
+        expect(event["type"]).to eql "exception"
+        expect(event["exception"]["type"]).to eql errorType
+        expect(event["exception"]["stacktrace"].class.to_s).to eql "Hash"
+        expect(event["exception"]["stacktrace"]["frames"].class.to_s).to eql "Array"
+    end   
+    it 'test_should_create_an_event_with_the_object_of_the_stacktrace_in_place' do
         tracker = FyipeTracker.new($apiUrl, $errorTracker["_id"], $errorTracker["key"])
 
-        fingerprint = 'custom-fingerprint'
-        tracker.setFingerPrint(fingerprint)
-        errorMessage = 'Uncaught Exception'
-        tracker.captureMessage(errorMessage)
+        errorType = ''
+        begin
+            divByZero= 1/0
+        rescue => ex
+            errorType = ex.class
+            tracker.captureException(ex)
+        end
+
         event = tracker.getCurrentEvent()
-        expect(event["fingerprint"][0]).to eql fingerprint
-    end
+        frame = event["exception"]["stacktrace"]["frames"][0]
+        
+        expect(frame.key?("methodName")).to eql true
+        expect(frame.key?("lineNumber")).to eql true
+        expect(frame.key?("fileName")).to eql true
+    end 
 end
