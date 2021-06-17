dd4a73d9c42c6762ee0781247e2e32e6ccd4a45c
Zadat Olayinka <bundayy@outlook.com>
2021-06-17 09:54:24 +0100

    stack trace generation

--- ruby-sdk/fyipe/lib/fyipe/util.rb
+++ ruby-sdk/fyipe/lib/fyipe/util.rb
@@ -21,4 +21,40 @@ class Util
         
         return enumValue
     end
+    def getExceptionStackTrace(exception)
+        frames = [];
+        lineNumber = 'N/A'
+
+        obj = {}
+        obj["type"] = exception.class
+        obj["message"] = exception.message
+        obj["lineNumber"] = lineNumber
+        
+        backtraces = exception.backtrace
+        backtraces != nil ?
+        backtraces.each do |backtrace|
+            breakdown = backtrace.split(":")
+            fileName = breakdown[0]
+            lineNumber = breakdown[1]
+            methodName = breakdown[2]
+            frame = {}
+            frame["fileName"] = fileName
+            frame["methodName"] = methodName
+            frame["lineNumber"] = lineNumber
+            frames.append(frame)
+        end
+        : nil
+
+        stacktrace = {}
+        stacktrace["frames"] = frames
+        
+        obj["stacktrace"] = stacktrace
+        obj["lineNumber"] = frames[0] ? frames[0]["lineNumber"] : lineNumber
+
+        # TODO run only if user agreed to use this feature
+        # if self.options['captureCodeSnippet'] is True:
+        #     obj = self.getErrorCodeSnippet(obj)
+
+        return obj
+    end
 end
