164b62223e675586a5246d87f145980ad9a326ad
deityhub <judoc96@gmail.com>
2021-06-21 20:47:23 +0100

    schema definition for default greenlock manager

---
+++ backend/backend/models/defaultManager.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * Copyright HackerBay, Inc.
+ *
+ */
+
+const mongoose = require('../config/db');
+
+const Schema = mongoose.Schema;
+const defaultManagerSchema = new Schema(
+    {
+        store: {
+            type: Object,
+            default: {
+                module: 'fyipe-le-store',
+            },
+        },
+        challenges: {
+            'http-01': {
+                type: Object,
+                default: {
+                    module: 'fyipe-acme-http-01',
+                },
+            },
+        },
+        renewOffset: { type: String, default: '-45d' },
+        renewStagger: { type: String, default: '3d' },
+        accountKeyType: String,
+        serverKeyType: String,
+        subscriberEmail: String,
+        agreeToTerms: { type: Boolean, default: true },
+        deleted: { type: Boolean, default: false },
+        deletedAt: Date,
+    },
+    { timestamps: true }
+);
+module.exports = mongoose.model('DefaultManager', defaultManagerSchema);
