8eef80cbe5308cd6f161825aa5df54ed6240bb7c
deityhub <judoc96@gmail.com>
2021-06-22 22:38:13 +0100

    refactor api to handle incident settings(template) setup

--- backend/backend/api/incidentSettings.js
+++ backend/backend/api/incidentSettings.js
@@ -4,6 +4,7 @@ const getUser = require('../middlewares/user').getUser;
 const { isAuthorized } = require('../middlewares/authorization');
 const sendErrorResponse = require('../middlewares/response').sendErrorResponse;
 const sendItemResponse = require('../middlewares/response').sendItemResponse;
+const sendListResponse = require('../middlewares/response').sendListResponse;
 const IncidentSettingsService = require('../services/incidentSettingsService');
 const IncidentPrioritiesService = require('../services/incidentPrioritiesService');
 const { variables } = require('../config/incidentDefaultSettings');
@@ -33,56 +34,76 @@ router.get('/:projectId', getUser, isAuthorized, async function(req, res) {
     }
 });
 
-router.put('/:projectId/setDefault', getUser, isAuthorized, async function(
-    req,
-    res
-) {
-    const { projectId } = req.params;
-    const { incidentPriority } = req.body;
-    if (!projectId)
-        return sendErrorResponse(req, res, {
-            code: 400,
-            message: 'Project Id must be present.',
-        });
-    if (!incidentPriority)
-        return sendErrorResponse(req, res, {
-            code: 400,
-            message: 'Incident priority must be present.',
-        });
-
+// fetch all incident template in a project
+router.get('/:projectId', getUser, isAuthorized, async function(req, res) {
     try {
-        // Update Default Incident Priority
-        const priority = await IncidentPrioritiesService.findOne({
-            _id: incidentPriority,
-        });
+        const { projectId } = req.params;
+        const { skip, limit } = req.query;
 
-        if (!priority) {
-            return sendErrorResponse(req, res, {
-                code: 400,
-                message: "Incident priority doesn't exist.",
-            });
+        if (!projectId) {
+            const error = new Error('Project Id must be present');
+            error.code = 400;
+            throw error;
         }
-        const defaultPrioritySetting = await IncidentSettingsService.updateOne(
-            {
-                projectId,
-            },
-            {
-                incidentPriority,
-            }
-        );
-        return sendItemResponse(req, res, defaultPrioritySetting);
+
+        const query = { projectId };
+        const templates = await IncidentSettingsService.findBy({
+            query,
+            limit,
+            skip,
+        });
+        const count = await IncidentSettingsService.countBy(query);
+        return sendListResponse(req, res, templates, count);
     } catch (error) {
         return sendErrorResponse(req, res, error);
     }
 });
 
-router.put('/:projectId', getUser, isAuthorized, async function(req, res) {
-    const { projectId } = req.params;
-    const { title, description, incidentPriority } = req.body;
+router.put(
+    '/:projectId/:templateId/setDefault',
+    getUser,
+    isAuthorized,
+    async function(req, res) {
+        const { projectId, templateId } = req.params;
+        if (!projectId)
+            return sendErrorResponse(req, res, {
+                code: 400,
+                message: 'Project Id must be present.',
+            });
+
+        try {
+            const defaultPrioritySetting = await IncidentSettingsService.updateOne(
+                {
+                    _id: templateId,
+                    projectId,
+                },
+                {
+                    isDefault: true,
+                }
+            );
+            return sendItemResponse(req, res, defaultPrioritySetting);
+        } catch (error) {
+            return sendErrorResponse(req, res, error);
+        }
+    }
+);
+
+router.put('/:projectId/:templateId', getUser, isAuthorized, async function(
+    req,
+    res
+) {
+    const { projectId, templateId } = req.params;
+    const { title, description, incidentPriority, isDefault } = req.body;
     if (!projectId)
         return sendErrorResponse(req, res, {
             code: 400,
             message: 'Project Id must be present.',
+        });
+
+    if (!templateId)
+        return sendErrorResponse(req, res, {
+            code: 400,
+            message: 'Incident settings Id must be present.',
         });
 
     if (!title)
@@ -112,14 +133,67 @@ router.put('/:projectId', getUser, isAuthorized, async function(req, res) {
         const incidentSettings = await IncidentSettingsService.updateOne(
             {
                 projectId,
+                _id: templateId,
             },
             {
                 title,
                 description,
                 incidentPriority,
+                isDefault,
             }
         );
         return sendItemResponse(req, res, incidentSettings);
+    } catch (error) {
+        return sendErrorResponse(req, res, error);
+    }
+});
+
+router.post('/:projectId', getUser, isAuthorized, async function(req, res) {
+    try {
+        const { projectId } = req.params;
+        // description is optional
+        const {
+            title,
+            description,
+            incidentPriority,
+            isDefault = false,
+        } = req.body;
+
+        if (!projectId) {
+            const error = new Error('Project Id must be present');
+            error.code = 400;
+            throw error;
+        }
+        if (!title) {
+            const error = new Error('Title must be present');
+            error.code = 400;
+            throw error;
+        }
+        if (!incidentPriority) {
+            const error = new Error('Incident priority must be present');
+            error.code = 400;
+            throw error;
+        }
+
+        const priority = await IncidentPrioritiesService.findOne({
+            _id: incidentPriority,
+        });
+        if (!priority) {
+            const error = new Error("Incident priority doesn't exist.");
+            error.code = 400;
+            throw error;
+        }
+
+        const data = {
+            projectId,
+            title,
+            description,
+            incidentPriority,
+            isDefault,
+        };
+        const incidentSetting = await IncidentSettingsService.create(data);
+
+        return sendItemResponse(req, res, incidentSetting);
     } catch (error) {
         return sendErrorResponse(req, res, error);
     }
