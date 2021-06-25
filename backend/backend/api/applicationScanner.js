9860d970303bf179038aa54d8950c994cc99aff5
David Adewole <adeoluwadavid@gmail.com>
2021-06-25 21:51:01 +0100

    application-scanner-task

--- backend/backend/api/applicationScanner.js
+++ backend/backend/api/applicationScanner.js
@@ -21,39 +21,6 @@ const storage = require('../middlewares/upload');
 // Params:
 // Param 1: req.headers-> {authorization}; req.user-> {id}; req.files-> {profilePic};
 // Returns: 200: Success, 400: Error; 500: Server Error.
-router.put('/update/image', getUser, async function (req, res) {
-    try {
-        const upload = multer({
-            storage,
-        }).fields([
-            {
-                name: 'applicationScannerImage',
-                maxCount: 1,
-            },
-        ]);
-        upload(req, res, async function (error) {
-            const applicationScannerId = req.body.id;
-            const data = req.body;
-
-            if (error) {
-                return sendErrorResponse(req, res, error);
-            }
-            if (
-                req.files &&
-                req.files.applicationScannerImage &&
-                req.files.applicationScannerImage[0].filename
-            ) {
-                data.applicationScannerImage = req.files.applicationScannerImage[0].filename;
-            }
-
-            // Call the ApplicationScannerService
-            const save = await ApplicationScannerService.updateOneBy({ _id: applicationScannerId }, data);
-            return sendItemResponse(req, res, save);
-        });
-    } catch (error) {
-        return sendErrorResponse(req, res, error);
-    }
-});
 
 
 router.get('/applicationSecurities', isAuthorizedApplicationScanner, async function (
