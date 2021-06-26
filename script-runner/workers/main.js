0d81137037d01d0b2a2a73d258e2a0aba1399a37
Dave <dave_okanlawon@yahoo.com>
2021-06-26 09:21:31 +0100

    implement scriptMonitorJob

--- script-runner/workers/main.js
+++ script-runner/workers/main.js
@@ -1,6 +1,19 @@
+const { getApi } = require('../utils/api');
+const errorService = require('../utils/errorService');
+const scriptMonitors = require('./scriptMonitors');
+
 module.exports = {
     runScriptMonitorsJob: async () => {
         // get all script monitors
         // run scripts
+        try {
+            let monitors = await getApi('script-runner/monitors');
+            monitors = JSON.parse(monitors.data); // parse the stringified data
+            await Promise.all(
+                monitors.map(monitor => scriptMonitors.ping(monitor))
+            );
+        } catch (error) {
+            errorService.log('getApi', error);
+        }
     },
 };
