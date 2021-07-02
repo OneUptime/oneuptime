3cb6010d564a25035c0d8749ac6c703e15fd3abd
David Adewole <adeoluwadavid@gmail.com>
2021-07-02 10:15:26 +0100

    application-scanner-review

--- accounts/src/config.js
+++ accounts/src/config.js
@@ -3,7 +3,7 @@ import isEmail from 'sane-email-validation';
 import validUrl from 'valid-url';
 import valid from 'card-validator';
 import FileSaver from 'file-saver';
-import { emaildomains } from './constants/emaildomains';
+//import { emaildomains } from './constants/emaildomains';
 
 let apiUrl = window.location.origin + '/api';
 let dashboardUrl = window.location.origin + '/dashboard';
@@ -174,7 +174,7 @@ export const Validate = {
         if (this.text(email)) return isEmail(email);
         return false;
     },
-
+    //eslint-disable-next-line
     isValidBusinessEmail(email) {
         //return emaildomains.test(email);
         return true;
