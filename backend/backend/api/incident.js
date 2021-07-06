e3f76e77ef38224a657b2c829212c53ade1e7f48
deityhub <judoc96@gmail.com>
2021-07-06 17:49:17 +0100

    insert many where feasible

--- backend/backend/api/incident.js
+++ backend/backend/api/incident.js
@@ -124,14 +124,16 @@ router.post(
                 monitors,
             });
             if (incident) {
+                const monitorItems = [];
                 for (const monitor of monitors) {
-                    await MonitorStatusService.create({
+                    monitorItems.push({
                         monitorId: monitor,
                         incidentId: incident._id,
                         manuallyCreated: true,
                         status: incidentType,
                     });
                 }
+                await MonitorStatusService.createMany(monitorItems);
             }
             return sendItemResponse(req, res, incident);
         } catch (error) {
