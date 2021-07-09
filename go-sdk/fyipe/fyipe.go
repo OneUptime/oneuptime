2005d3a3d965df95edc1db1d14adcb395485adb4
Zadat Olayinka <bundayy@outlook.com>
2021-07-09 16:41:30 +0100

    init and loginfo method set up and integrated

--- go-sdk/fyipe/fyipe.go
+++ go-sdk/fyipe/fyipe.go
@@ -3,7 +3,6 @@ package fyipe
 import (
 	"errors"
 	"log"
-	"reflect"
 )
 
 type LoggerOptions struct {
@@ -37,59 +36,26 @@ func NewFyipeLogger(options LoggerOptions) (*FyipeLogger, error) {
 
 // Init initializes the SDK with loggerOptions.
 // it returns the error if any of the options are invalid
-func Init(options LoggerOptions) (*FyipeLogger, error) {
-	fyipeLogger, err := NewFyipeLogger(options)
+func Init(options LoggerOptions) error {
+	currentFyipeLogger, err := NewFyipeLogger(options)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	// TODO confirm Logger is ready to be used
-	log.Printf(reflect.TypeOf(fyipeLogger).String())
-	return fyipeLogger, nil
+	// confirm Logger is ready to be used by binding user's fyipeLogger
+	logger := CurrentLogger()
+	logger.BindFyipeLogger(currentFyipeLogger)
+
+	return nil
 }
 
-// func validateItems(content, tags) {
-// 	// get the class of the content and convert to string for comparison
-// 	contentType = reflect.TypeOf(content)
-// 	tagType = nil
-// 	if(tags != nil) {
-// 		tagType= reflect.TypeOf(tags)
-// 	}
+func LogInfo(content string, tags []string) {
+	// access fyipe Logger and send an api request
+	logger := CurrentLogger()
+	var res, err = logger.MakeApiRequest(content, "info", tags)
 
-// 	// check if content type is not a string or hash object
-// 	if(!((contentType != "string") || (contentType.eql? "Hash")))
-// 		raise "Invalid Content to be logged"
-// 	end
-
-// 	# check if tag type is available and its not a string or hash object
-// 	if(tagType != nil && (!((tagType.eql? "String") || (tagType.eql? "Array"))))
-// 		raise "Invalid Content Tags to be logged"
-// 	end
-// end
-// }
-
-// func Log(content string, tags []string) {
-// 	makeApiRequest(content, "info", tags)
-// }
-
-// func makeApiRequest(data string, tagType string, tags []string) {
-// 	postBody, _ := json.Marshal(map[string]string{
-// 		"content":           data,
-// 		"type":              tagType,
-// 		"applicationLogKey": applicationLogKey,
-// 	})
-// 	responseBody := bytes.NewBuffer(postBody)
-
-// 	resp, err := http.Post(apiUrl, "application/json", responseBody)
-
-// 	if err != nil {
-// 		log.Fatalf("An Error Occured %v", err)
-// 	}
-
-// 	body, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-
-// 	sb := string(body)
-// 	log.Printf(sb)
-// }
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		log.Printf(res)
+	}
+}
