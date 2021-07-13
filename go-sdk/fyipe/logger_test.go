00fe16b71b378a2c57618a352e75e4c782ba491c
Zadat Olayinka <bundayy@outlook.com>
2021-07-13 11:25:58 +0100

    logger testg basic set up

---
+++ go-sdk/fyipe/logger_test.go
@@ -0,0 +1,34 @@
+package fyipe
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+)
+
+const apiUrl = "http://localhost:3002/api"
+
+func init() {
+	fmt.Println("set up will happen here")
+	fmt.Println(apiUrl)
+	GetUser()
+	GetTitle()
+}
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	if !want.MatchString(name) {
+		t.Fatalf(`Hello("Gladys") = %q,  want match for %#q, nil`, name, want)
+	}
+}
+func TestApplicationLogKeyRequired(t *testing.T) {
+	option := LoggerOptions{
+		ApplicationLogId:  "5eeb5bc23b0014dfbe070dbc",
+		ApplicationLogKey: "",
+		ApiUrl:            "http://localhost:3002/api",
+	}
+	Init(option)
+
+	// var res, errorRes = LogInfo("Random Info Message")
+
+}
