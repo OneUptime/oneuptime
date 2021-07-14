74b17b0988476fabde0143e83f1c18a169842822
Zadat Olayinka <bundayy@outlook.com>
2021-07-14 16:48:42 +0100

    completed create test component + app log

--- go-sdk/fyipe/logger_test.go
+++ go-sdk/fyipe/logger_test.go
@@ -8,13 +8,34 @@ import (
 
 const apiUrl = "http://localhost:3002/api"
 
+var appLog map[string]interface{}
+
 func init() {
 	fmt.Println("set up will happen here")
-	fmt.Println(apiUrl)
 	var sampleUser = GetUser()
+	var token, project interface{}
 
-	MakeTestApiRequest(apiUrl+"/user/signup", sampleUser)
+	// Created User
+	res, err := MakeTestApiRequest(apiUrl+"/user/signup", sampleUser, "") // pass empty for token here
 
+	if err != nil {
+		fmt.Printf("An error Occured %v", err)
+	}
+
+	// get token and project
+	token = res["tokens"].(map[string]interface{})["jwtAccessToken"]
+	project = res["project"]
+
+	// create a component
+	var sampleComponent = GetNameComponent()
+	createdComponent, err := MakeTestApiRequest(apiUrl+"/component/"+project.(map[string]interface{})["_id"].(string), sampleComponent, token.(string))
+
+	// create an applicationlog and set it as the global application Log.
+	var sampleAppLog = GetNameComponent()
+	localAppLog, err := MakeTestApiRequest(apiUrl+"/application-log/"+project.(map[string]interface{})["_id"].(string)+"/"+createdComponent["_id"].(string)+"/create", sampleAppLog, token.(string))
+
+	appLog = localAppLog
+	fmt.Println("Test setup done, Application Log created, tests will begin...")
 }
 func TestHelloName(t *testing.T) {
 	name := "Gladys"
@@ -24,13 +45,19 @@ func TestHelloName(t *testing.T) {
 	}
 }
 func TestApplicationLogKeyRequired(t *testing.T) {
+	expectedResponse := "Application Log Key cant be empty"
 	option := LoggerOptions{
-		ApplicationLogId:  "5eeb5bc23b0014dfbe070dbc",
+		ApplicationLogId:  appLog["_id"].(string),
 		ApplicationLogKey: "",
-		ApiUrl:            "http://localhost:3002/api",
+		ApiUrl:            apiUrl,
 	}
-	Init(option)
 
-	// var res, errorRes = LogInfo("Random Info Message")
+	setupResponse := Init(option)
+
+	if fmt.Sprint(setupResponse) != expectedResponse {
+		t.Errorf("TestApplicationLogKeyRequired failed expected %v, got %v", expectedResponse, setupResponse)
+	} else {
+		t.Logf("TestApplicationLogKeyRequired success expected %v, got %v", expectedResponse, setupResponse)
+	}
 
 }
