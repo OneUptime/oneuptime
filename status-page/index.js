9cdffeb976a8b5581f253832a7a3b71a228aa753
deityhub <judoc96@gmail.com>
2021-07-15 10:27:03 +0100

    use mongodb directly for tls credential operations

--- status-page/index.js
+++ status-page/index.js
@@ -11,7 +11,19 @@ const fetch = require('node-fetch');
 const { spawn } = require('child_process');
 const axios = require('axios');
 
+// mongodb
+const MongoClient = require('mongodb').MongoClient;
+const mongoUrl =
+    process.env['MONGO_URL'] || 'mongodb://localhost:27017/fyipedb';
+
 const { NODE_ENV } = process.env;
+
+function getMongoClient() {
+    return new MongoClient(mongoUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+}
 
 if (!NODE_ENV || NODE_ENV === 'development') {
     // Load env vars from /statuspage/.env
@@ -83,6 +95,33 @@ app.use('/.well-known/acme-challenge/:token', async function(req, res) {
     res.send(response.data);
 });
 
+// fetch details about a domain from the db
+async function handleCustomDomain(client, collection, domain) {
+    const statusPage = await client
+        .db('fyipedb')
+        .collection(collection)
+        .findOne({
+            domains: { $elemMatch: { domain } },
+        });
+
+    let domainObj = {};
+    statusPage &&
+        statusPage.domains &&
+        statusPage.domains.forEach(eachDomain => {
+            if (eachDomain.domain === domain) {
+                domainObj = eachDomain;
+            }
+        });
+
+    return {
+        cert: domainObj.cert,
+        privateKey: domainObj.privateKey,
+        autoProvisioning: domainObj.autoProvisioning,
+        enableHttps: domainObj.enableHttps,
+        domain: domainObj.domain,
+    };
+}
+
 app.use('/', async function(req, res, next) {
     const host = req.hostname;
     if (
@@ -94,21 +133,29 @@ app.use('/', async function(req, res, next) {
         return next();
     }
 
+    // setup mongodb connection
+    const client = getMongoClient();
+    await client.connect();
     try {
-        const response = await fetch(
-            `${apiHost}/statusPage/tlsCredential?domain=${host}`
-        ).then(res => res.json());
+        const response = await handleCustomDomain(client, 'statuspages', host);
+
+        // close mongodb connection
+        await client.close();
 
         const { enableHttps } = response;
         if (enableHttps) {
             if (!req.secure) {
-                res.writeHead(301, { Location: `https://${host}${req.url}` });
+                res.writeHead(301, {
+                    Location: `https://${host}${req.url}`,
+                });
                 return res.end();
             }
             return next();
         } else {
             if (req.secure) {
-                res.writeHead(301, { Location: `http://${host}${req.url}` });
+                res.writeHead(301, {
+                    Location: `http://${host}${req.url}`,
+                });
                 return res.end();
             }
             return next();
@@ -227,9 +274,17 @@ function createDir(dirPath) {
                 path.resolve(process.cwd(), 'src', 'credentials', 'private.key')
             ),
             SNICallback: async function(domain, cb) {
-                const res = await fetch(
-                    `${apiHost}/statusPage/tlsCredential?domain=${domain}`
-                ).then(res => res.json());
+                const client = getMongoClient();
+                await client.connect();
+
+                const res = await handleCustomDomain(
+                    client,
+                    'statuspages',
+                    domain
+                );
+
+                // close mongodb connection
+                await client.close();
 
                 let certPath, privateKeyPath;
                 if (res) {
