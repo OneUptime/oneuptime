ee59580a3fe622822832c35e289d66529a840b82
David Adewole <adeoluwadavid@gmail.com>
2021-07-22 20:50:51 +0100

    probe-not-creating

--- backend/backend/middlewares/probeAuthorization.js
+++ backend/backend/middlewares/probeAuthorization.js
@@ -82,23 +82,29 @@ module.exports = {
             }
 
             let probeId = null;
-
+                    
             if (clusterKey && clusterKey === CLUSTER_KEY) {
                 // if cluster key matches then just query by probe name,
                 // because if the probe key does not match, we can update probe key later
                 // without updating mognodb database manually.
-
-                if (global.probes[probeName]) {
+                if (global.probes[probeName]) {  // If probeName could not be found DB, the else statement is called.
                     probeId = global.probes[probeName]._id;
                 } else {
                     const probe = await ProbeService.findOneBy({ probeName });
-                    probeId = probe._id;
+                    /**
+                     *  If probe does not exist: 
+                     *  ProbeService.findOneBy({probeName}) returns null
+                    */ 
+                    if(probe && probe._id){ // This gets executed only if probe and probe_id exist. Else, the program will throw an error instead of creating a new probe.
+                        probeId = probe._id;
+                        global.probes[probeName] = {
+                            _id: probe._id,
+                            probeKey: probe.probeKey,
+                            version: probe.version,
+                        };
+                    }
 
-                    global.probes[probeName] = {
-                        _id: probe._id,
-                        probeKey: probe.probeKey,
-                        version: probe.version,
-                    };
+                    
                 }
             } else {
                 if (global.probes[probeName]) {
@@ -124,9 +130,8 @@ module.exports = {
                     message: 'Probe key and probe name do not match.',
                 });
             }
-
             if (!probeId) {
-                //create a new probe.
+                //create a new probe if it is not present or more is needed. 
                 const probe = await ProbeService.create({
                     probeKey,
                     probeName,
