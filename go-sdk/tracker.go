c02ebd69bcd543171f1459a5170e062a6c951613
Zadat Olayinka <bundayy@outlook.com>
2021-07-28 16:14:15 +0100

    tracker class to manage all usage

---
+++ go-sdk/tracker.go
@@ -0,0 +1,68 @@
+package fyipe
+
+import "sync"
+
+type Tracker struct {
+	stack *stack
+	mu    sync.RWMutex
+}
+
+var currentTracker = NewTracker(nil, NewRealm())
+
+// constructor for default fyipeTracker
+func NewTracker(fyipeTracker *FyipeTracker, realm *Realm) *Tracker {
+	tracker := Tracker{
+		stack: &stack{{
+			fyipeTracker: fyipeTracker,
+			realm:        realm,
+		}},
+	}
+	return &tracker
+}
+
+// return stored fyipeTracker
+func (l *layer) FyipeTracker() *FyipeTracker {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
+	return l.fyipeTracker
+}
+
+// set the current fyipeTracker
+func (l *layer) SetFyipeTracker(f *FyipeTracker) {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	l.fyipeTracker = f
+}
+
+// returns an instance of previously initialized Tracker.
+func CurrentTracker() *Tracker {
+	return currentTracker
+}
+
+// set the current fyipeTracker being used by the user
+func (tracker *Tracker) BindFyipeTracker(fyipeTracker *FyipeTracker) {
+	top := tracker.stackTop()
+	top.SetFyipeTracker(fyipeTracker)
+}
+
+// always return the topof the stack which contains one fyipeTracker
+func (tracker *Tracker) stackTop() *layer {
+	tracker.mu.RLock()
+	defer tracker.mu.RUnlock()
+
+	stack := tracker.stack
+	stackLen := len(*stack)
+	top := (*stack)[stackLen-1]
+	return top
+}
+
+// get the current fyipeTracker for usage
+func (tracker *Tracker) FyipeTracker() *FyipeTracker {
+	top := tracker.stackTop()
+	return top.FyipeTracker()
+}
+
+func (tracker *Tracker) Realm() *Realm {
+	top := tracker.stackTop()
+	return top.realm
+}
