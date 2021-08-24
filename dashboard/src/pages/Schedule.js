4bb4faafd4f91c97e18c17ad9aba05132fc4b0c6
deityhub <judoc96@gmail.com>
2021-08-24 17:40:31 +0100

    setup on-call schedule calendar view

--- dashboard/src/pages/Schedule.js
+++ dashboard/src/pages/Schedule.js
@@ -14,6 +14,7 @@ import { getEscalation } from '../actions/schedule';
 import { teamLoading } from '../actions/team';
 import BreadCrumbItem from '../components/breadCrumb/BreadCrumbItem';
 import getParentRoute from '../utils/getParentRoute';
+import ScheduleCalender from '../components/schedule/ScheduleCalender';
 class Schedule extends Component {
     constructor(props) {
         super(props);
@@ -106,6 +107,9 @@ class Schedule extends Component {
                                     <span>
                                         <div>
                                             <div>
+                                                <ScheduleCalender
+                                                    escalations={escalations}
+                                                />
                                                 <RenameScheduleBox />
                                                 <MonitorBox
                                                     schedule={schedule}
