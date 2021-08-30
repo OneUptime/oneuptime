098f76188a607be97658b81c504091fb8568c3cc
deityhub <judoc96@gmail.com>
2021-08-30 11:57:54 +0100

    set region

--- ci/scripts/deployment-staging-setup.sh
+++ ci/scripts/deployment-staging-setup.sh
@@ -7,4 +7,4 @@
 
 echo "Connect machine with to communicate with aws cluster"
 # This command will automatically switch to the cluster
-aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name fyipe-staging
+aws eks update-kubeconfig --region us-east-2 --name fyipe-staging
