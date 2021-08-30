ff0d4cfd37502e4da376615af6d46429e7577e4c
deityhub <judoc96@gmail.com>
2021-08-30 11:20:15 +0100

    setup aws eks

--- ci/scripts/deployment-setup.sh
+++ ci/scripts/deployment-setup.sh
@@ -4,15 +4,25 @@
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 sudo kubectl version --client
+
 # Auth with DigitalOcean Client
-echo "Install doctl"
-sudo snap install doctl
-sudo snap connect doctl:kube-config
-sudo snap connect doctl:ssh-keys :ssh-keys
-sudo snap connect doctl:dot-docker
+# echo "Install doctl"
+# sudo snap install doctl
+# sudo snap connect doctl:kube-config
+# sudo snap connect doctl:ssh-keys :ssh-keys
+# sudo snap connect doctl:dot-docker
+
+# Install and configure aws cli
+sudo apt-get install -y unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" # download latest aws cli version
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version # confirm installation
+
 # Make .config folder
 sudo mkdir /root/.config || echo "Directory already created."
 sudo mkdir /root/.kube || echo "Directory already created."
+
 #Init auth
-echo "Auth doctl"
-sudo doctl auth init -t $DIGITALOCEAN_TOKEN
+# echo "Auth doctl"
+# sudo doctl auth init -t $DIGITALOCEAN_TOKEN
--- ci/scripts/deployment-staging-setup.sh
+++ ci/scripts/deployment-staging-setup.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-echo "Get KubeCluster Config"
-sudo doctl kubernetes cluster kubeconfig save staging
-sudo kubectl config --kubeconfig=/root/.kube/config get-contexts
-sudo kubectl config --kubeconfig=/root/.kube/config use-context do-lon1-staging
+# echo "Get KubeCluster Config"
+# sudo doctl kubernetes cluster kubeconfig save staging
+# sudo kubectl config --kubeconfig=/root/.kube/config get-contexts
+# sudo kubectl config --kubeconfig=/root/.kube/config use-context do-lon1-staging
+
+echo "Connect machine with to communicate with aws cluster"
+# This command will automatically switch to the cluster
+aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name fyipe-staging
