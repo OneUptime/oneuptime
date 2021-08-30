82c57d655343beee279e122470f95ca337a55baa
deityhub <judoc96@gmail.com>
2021-08-30 13:27:24 +0100

    debug

--- ci/scripts/deployment-staging-setup.sh
+++ ci/scripts/deployment-staging-setup.sh
@@ -5,9 +5,6 @@
 # sudo kubectl config --kubeconfig=/root/.kube/config get-contexts
 # sudo kubectl config --kubeconfig=/root/.kube/config use-context do-lon1-staging
 
-echo  "All available environment variables"
-printenv
-
 echo "Connect machine with to communicate with aws cluster"
 # This command will automatically switch to the cluster
 aws eks update-kubeconfig --region us-east-2 --name fyipe-staging
