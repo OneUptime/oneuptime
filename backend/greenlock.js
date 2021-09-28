9951c68fbd102a0e37d076cfcdda8d146ec3704d
deityhub <judoc96@gmail.com>
2021-09-28 13:12:56 +0000

    debug and fixes

--- backend/greenlock.js
+++ backend/greenlock.js
@@ -1,30 +1,30 @@
-const Gl = require('greenlock');
+// const Gl = require('greenlock');
 
-const greenlock = Gl.create({
-    manager: 'fyipe-gl-manager',
-    packageRoot: __dirname,
-    maintainerEmail: 'certs@fyipe.com',
-    staging: false,
-    notify: function(event, details) {
-        if ('error' === event) {
-            // `details` is an error object in this case
-            // eslint-disable-next-line no-console
-            console.error(details);
-        }
-    },
-    challenges: {
-        'http-01': {
-            module: 'fyipe-acme-http-01',
-        },
-    },
-    store: {
-        module: 'fyipe-le-store',
-    },
-});
+// const greenlock = Gl.create({
+//     manager: 'fyipe-gl-manager',
+//     packageRoot: __dirname,
+//     maintainerEmail: 'certs@fyipe.com',
+//     staging: false,
+//     notify: function(event, details) {
+//         if ('error' === event) {
+//             // `details` is an error object in this case
+//             // eslint-disable-next-line no-console
+//             console.error(details);
+//         }
+//     },
+//     challenges: {
+//         'http-01': {
+//             module: 'fyipe-acme-http-01',
+//         },
+//     },
+//     store: {
+//         module: 'fyipe-le-store',
+//     },
+// });
 
-greenlock.manager.defaults({
-    agreeToTerms: true,
-    subscriberEmail: 'certs@fyipe.com',
-});
+// greenlock.manager.defaults({
+//     agreeToTerms: true,
+//     subscriberEmail: 'certs@fyipe.com',
+// });
 
-global.greenlock = greenlock; // make it available project wide
+// global.greenlock = greenlock; // make it available project wide
