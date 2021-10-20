5a5f324f1ebc4daf40f5e90d510b3706efc73a17
David Adewole <adeoluwadavid@gmail.com>
2021-10-20 19:20:36 +0100

    saas-front-end-i

--- dashboard/src/components/nav/TopNav.js
+++ dashboard/src/components/nav/TopNav.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import FeedBackModal from '../FeedbackModal';
+jj;
 import { showProfileMenu, updateProfileSetting } from '../../actions/profile';
 import { openNotificationMenu } from '../../actions/notification';
 import { openFeedbackModal, closeFeedbackModal } from '../../actions/feedback';
