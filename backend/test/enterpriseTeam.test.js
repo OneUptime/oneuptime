bc9e4d48dd219d0c0b17330634cbf97a7ed8dd62
augustinebest <augustusbest00@gmaile.com>
2021-10-22 10:29:42 +0100

    included array of emails to hardDelete

--- backend/test/enterpriseTeam.test.js
+++ backend/test/enterpriseTeam.test.js
@@ -12,6 +12,8 @@ const ProjectService = require('../backend/services/projectService');
 
 let token, projectId, newProjectId;
 
+const teamEmail = 'noreply1@fyipe.com';
+
 describe('Enterprise Team API', function() {
     this.timeout(30000);
 
@@ -42,7 +44,9 @@ describe('Enterprise Team API', function() {
             _id: { $in: [projectId, newProjectId] },
         });
         await UserService.hardDeleteBy({
-            email: userData.user.email.toLowerCase(),
+            email: {
+                $in: [userData.user.email.toLowerCase(), teamEmail],
+            },
         });
     });
 
@@ -52,7 +56,7 @@ describe('Enterprise Team API', function() {
             .post(`/team/${projectId}`)
             .set('Authorization', authorization)
             .send({
-                emails: 'noreply1@fyipe.com',
+                emails: teamEmail,
                 role: 'Member',
             })
             .end(function(err, res) {
