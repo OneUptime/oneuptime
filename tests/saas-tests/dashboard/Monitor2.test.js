1d97497482469008afc9c4e4e62f7e2ec3aa7a68
David Adewole <adeoluwadavid@gmail.com>
2021-10-28 17:43:31 +0100

    saas-a-review+

--- tests/saas-tests/dashboard/Monitor2.test.js
+++ tests/saas-tests/dashboard/Monitor2.test.js
@@ -15,7 +15,7 @@ describe('Monitor API', () => {
     const operationTimeOut = init.timeout;
 
     beforeAll(async () => {
-        jest.setTimeout(init.timeout);
+        jest.setTimeout(600000);
 
         browser = await puppeteer.launch(utils.puppeteerLaunchConfig);
         page = await browser.newPage();
@@ -34,87 +34,79 @@ describe('Monitor API', () => {
         done();
     });
 
-    test(
-        'should display lighthouse scores',
-        async done => {
-            // Navigate to Component details
-            // This navigates to the monitor created alongside the created component
-            await init.navigateToMonitorDetails(
-                componentName,
-                monitorName,
-                page
-            );
+    test('should display lighthouse scores', async done => {
+        // Navigate to Component details
+        // This navigates to the monitor created alongside the created component
+        await init.navigateToMonitorDetails(componentName, monitorName, page);
 
-            await init.pageWaitForSelector(page, '#website_scanning');
-            await init.pageWaitForSelector(page, '#website_postscan');
+        await init.pageWaitForSelector(page, '#website_scanning');
+        await init.pageWaitForSelector(page, '#website_postscan');
 
-            await init.pageWaitForSelector(
-                page,
-                `#lighthouseLogs_${monitorName}_0`,
-                {
-                    visible: true,
-                    timeout: operationTimeOut,
-                }
-            );
+        await init.pageWaitForSelector(
+            page,
+            `#lighthouseLogs_${monitorName}_0`,
+            {
+                visible: true,
+                timeout: 600000,
+            }
+        );
 
-            let lighthousePerformanceElement = await init.pageWaitForSelector(
-                page,
-                `#lighthouse-performance-${monitorName}`,
-                { visible: true, timeout: operationTimeOut }
-            );
-            lighthousePerformanceElement = await lighthousePerformanceElement.getProperty(
-                'innerText'
-            );
-            lighthousePerformanceElement = await lighthousePerformanceElement.jsonValue();
-            lighthousePerformanceElement.should.endWith('%');
+        let lighthousePerformanceElement = await init.pageWaitForSelector(
+            page,
+            `#lighthouse-performance-${monitorName}`,
+            { visible: true, timeout: 600000 }
+        );
+        lighthousePerformanceElement = await lighthousePerformanceElement.getProperty(
+            'innerText'
+        );
+        lighthousePerformanceElement = await lighthousePerformanceElement.jsonValue();
+        lighthousePerformanceElement.should.endWith('%');
 
-            let lighthouseAccessibilityElement = await init.pageWaitForSelector(
-                page,
-                `#lighthouse-accessibility-${monitorName}`,
-                { visible: true, timeout: operationTimeOut }
-            );
-            lighthouseAccessibilityElement = await lighthouseAccessibilityElement.getProperty(
-                'innerText'
-            );
-            lighthouseAccessibilityElement = await lighthouseAccessibilityElement.jsonValue();
-            lighthouseAccessibilityElement.should.endWith('%');
+        let lighthouseAccessibilityElement = await init.pageWaitForSelector(
+            page,
+            `#lighthouse-accessibility-${monitorName}`,
+            { visible: true, timeout: operationTimeOut }
+        );
+        lighthouseAccessibilityElement = await lighthouseAccessibilityElement.getProperty(
+            'innerText'
+        );
+        lighthouseAccessibilityElement = await lighthouseAccessibilityElement.jsonValue();
+        lighthouseAccessibilityElement.should.endWith('%');
 
-            let lighthouseBestPracticesElement = await init.pageWaitForSelector(
-                page,
-                `#lighthouse-bestPractices-${monitorName}`,
-                { visible: true, timeout: operationTimeOut }
-            );
-            lighthouseBestPracticesElement = await lighthouseBestPracticesElement.getProperty(
-                'innerText'
-            );
-            lighthouseBestPracticesElement = await lighthouseBestPracticesElement.jsonValue();
-            lighthouseBestPracticesElement.should.endWith('%');
+        let lighthouseBestPracticesElement = await init.pageWaitForSelector(
+            page,
+            `#lighthouse-bestPractices-${monitorName}`,
+            { visible: true, timeout: operationTimeOut }
+        );
+        lighthouseBestPracticesElement = await lighthouseBestPracticesElement.getProperty(
+            'innerText'
+        );
+        lighthouseBestPracticesElement = await lighthouseBestPracticesElement.jsonValue();
+        lighthouseBestPracticesElement.should.endWith('%');
 
-            let lighthouseSeoElement = await init.pageWaitForSelector(
-                page,
-                `#lighthouse-seo-${monitorName}`,
-                { visible: true, timeout: operationTimeOut }
-            );
-            lighthouseSeoElement = await lighthouseSeoElement.getProperty(
-                'innerText'
-            );
-            lighthouseSeoElement = await lighthouseSeoElement.jsonValue();
-            lighthouseSeoElement.should.endWith('%');
+        let lighthouseSeoElement = await init.pageWaitForSelector(
+            page,
+            `#lighthouse-seo-${monitorName}`,
+            { visible: true, timeout: operationTimeOut }
+        );
+        lighthouseSeoElement = await lighthouseSeoElement.getProperty(
+            'innerText'
+        );
+        lighthouseSeoElement = await lighthouseSeoElement.jsonValue();
+        lighthouseSeoElement.should.endWith('%');
 
-            let lighthousePwaElement = await init.pageWaitForSelector(
-                page,
-                `#lighthouse-pwa-${monitorName}`,
-                { visible: true, timeout: operationTimeOut }
-            );
-            lighthousePwaElement = await lighthousePwaElement.getProperty(
-                'innerText'
-            );
-            lighthousePwaElement = await lighthousePwaElement.jsonValue();
-            lighthousePwaElement.should.endWith('%');
-            done();
-        },
-        operationTimeOut
-    );
+        let lighthousePwaElement = await init.pageWaitForSelector(
+            page,
+            `#lighthouse-pwa-${monitorName}`,
+            { visible: true, timeout: operationTimeOut }
+        );
+        lighthousePwaElement = await lighthousePwaElement.getProperty(
+            'innerText'
+        );
+        lighthousePwaElement = await lighthousePwaElement.jsonValue();
+        lighthousePwaElement.should.endWith('%');
+        done();
+    }, 600000);
 
     test(
         'should display multiple probes and monitor chart on refresh',
