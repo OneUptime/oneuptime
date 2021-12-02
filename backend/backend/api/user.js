2b353bd05072f430480f30eaabc88abff06c6611
deityhub <judoc96@gmail.com>
2021-12-02 09:13:44 +0100

    remove unused variables

--- backend/backend/api/user.js
+++ backend/backend/api/user.js
@@ -513,8 +513,6 @@ router.post('/sso/callback', async function(req, res) {
 // Param 1: req.body-> {email, password }
 // Returns: 400: Error; 500: Server Error; 200: user
 router.post('/login', async function(req, res) {
-    const clientIP = ipClient.getClientIp(req).join();
-
     try {
         const data = req.body;
         const clientIP = Ip.getClientIp(req)[0];
