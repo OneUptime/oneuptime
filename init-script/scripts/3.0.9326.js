8d0fc47f1575d51f00604be049cb58a3004caee0
ikhide.atakpu <ikhide.atakpu@gmail.com>
2022-01-06 17:40:16 +0100

    add hidden admin to projects and subprojects

--- init-script/scripts/3.0.9326.js
+++ init-script/scripts/3.0.9326.js
@@ -4,31 +4,85 @@ const projectCollection = 'projects';
 
 async function run() {
     // all main projects
+    const adminUser = await find('users', {
+        role: 'master-admin',
+        deleted: false,
+    });
+
     const projects = await find(projectCollection, {
         deleted: false,
         parentProjectId: null,
     });
 
+    const adminUserId = adminUser[0]._id;
+
     for (const project of projects) {
-        const projectUsers =
-            project.users?.map(users => ({
-                ...users,
-                show: true,
-            })) || [];
-        const mainUserIds = projectUsers.map(user => user.userId);
+        let projectUsers = project.users;
+        let mainUserIds = projectUsers.map(user => user.userId);
+        if (adminUserId) {
+            if (mainUserIds.includes(adminUserId.toString())) {
+                projectUsers = project.users?.map(user => {
+                    if (
+                        user.userId !== adminUserId.toString() ||
+                        (user.userId === adminUserId.toString() &&
+                            user.role === 'Owner')
+                    ) {
+                        return {
+                            ...user,
+                            show: true,
+                        };
+                    } else {
+                        return user;
+                    }
+                });
+            } else {
+                projectUsers =
+                    project.users?.map(user => ({
+                        ...user,
+                        show: true,
+                    })) || [];
+                projectUsers.push({
+                    show: false,
+                    role: 'Member',
+                    userId: adminUserId.toString(),
+                });
+            }
+        } else {
+            projectUsers =
+                project.users?.map(users => ({
+                    ...users,
+                    show: true,
+                })) || [];
+        }
+
+        mainUserIds = projectUsers.map(user => user.userId);
+
         // all subProjects
         const subProjects = await find(projectCollection, {
-            parentProjectId: String(project._id),
+            parentProjectId: String(project._id.toString()),
         });
 
         for (const subProject of subProjects) {
             const subProjectUsers =
                 subProject.users?.map(user => {
-                    if (mainUserIds.includes(user.userId)) {
+                    if (mainUserIds.includes(user.userId.toString())) {
                         user.show = false;
                     }
                     return user;
                 }) || [];
+
+            const subProjectUserIds = subProjectUsers.map(user => user.userId);
+
+            if (
+                adminUserId &&
+                !subProjectUserIds.includes(adminUserId.toString())
+            ) {
+                subProjectUsers.push({
+                    show: false,
+                    role: 'Member',
+                    userId: adminUserId.toString(),
+                });
+            }
 
             await update(
                 projectCollection,
