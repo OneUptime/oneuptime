475c53a6b295beea04ccb243cd278b0bcc57b654
Abeeb Ayinla <abeebayinla.com>
2022-01-14 21:58:49 +0100

    fix nanoid in the updated package

--- backend/backend/utils/getSlug.js
+++ backend/backend/utils/getSlug.js
@@ -1,12 +1,12 @@
 const slugify = require('slugify');
-const generate = require('nanoid/generate');
+const { nanoid } = require('nanoid');
 
 module.exports = function getSlug(name) {
     name = String(name);
     if (!name || !name.trim()) return;
 
     let slug = slugify(name, { remove: /[&*+~.,\\/()|'"!:@]+/g });
-    slug = `${slug}-${generate('1234567890', 8)}`;
+    slug = `${slug}-${nanoid('1234567890', 8)}`;
     slug = slug.toLowerCase();
 
     return slug;
