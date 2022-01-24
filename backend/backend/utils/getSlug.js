495d6b4b31bab3bae5f06de093f9d4123e11f5f1
Ayinla Abeeb <abeebayinla@gmail.com>
2022-01-24 16:11:59 +0100

    fix backend

--- backend/backend/utils/getSlug.js
+++ backend/backend/utils/getSlug.js
@@ -1,12 +1,12 @@
 const slugify = require('slugify');
-const { nanoid } = require('nanoid');
+const generate = require('nanoid/generate');
 
 module.exports = function getSlug(name) {
     name = String(name);
     if (!name || !name.trim()) return;
 
     let slug = slugify(name, { remove: /[&*+~.,\\/()|'"!:@]+/g });
-    slug = `${slug}-${nanoid('1234567890', 8)}`;
+    slug = `${slug}-${generate('1234567890', 8)}`;
     slug = slug.toLowerCase();
 
     return slug;
