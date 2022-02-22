a3801fde69e59859961d1fbe6284094f1fd43d67
Nawaz Dhandala <hello@nawazdhandala.com>
2022-02-22 18:26:13 +0000

    fix porbe-api

--- probe-api/services/monitorService.js
+++ probe-api/services/monitorService.js
@@ -7,6 +7,8 @@ module.exports = {
             limit = Number(limit);
         }
 
+        const key = `${probeId}_pingtime`;
+
         const query = {
             deleted: false,
             disabled: false,
@@ -23,93 +25,22 @@ module.exports = {
             $or: [
                 {
                     // regions does not include the probeId yet
-                    regions: {
-                        $not: {
-                            $elemMatch: {
-                                probeId,
-                            },
-                        },
-                    },
+                    [key]: { $exists: false },
                 },
                 {
-                    regions: {
-                        $elemMatch: {
-                            probeId,
-                            lastPingTime: {
-                                $lt: date,
-                            },
-                        },
-                    },
+                    // regions does not include the probeId yet
+                    [key]: { $lt: date },
                 },
             ],
         };
         try {
-            const monitors = await monitorCollection
-                .aggregate([
-                    { $match: query },
-                    {
-                        $addFields: {
-                            regionLastPingTime: {
-                                $filter: {
-                                    input: '$regions',
-                                    as: 'region',
-                                    cond: {
-                                        $eq: [
-                                            '$$region.probeId',
-                                            ObjectId(probeId),
-                                        ],
-                                    },
-                                },
-                            },
-                        },
-                    },
-                    {
-                        $addFields: {
-                            regionLastPingTime: {
-                                $cond: {
-                                    if: {
-                                        $anyElementTrue: [
-                                            '$regionLastPingTime',
-                                        ],
-                                    },
-                                    then: '$regionLastPingTime',
-                                    else: [{ lastPingTime: 0 }],
-                                },
-                            },
-                        },
-                    },
-                    {
-                        $sort: {
-                            'regionLastPingTime.lastPingTime': 1,
-                        },
-                    },
-                    { $limit: limit },
-                ])
-                .toArray();
+            const monitors = await monitorCollection.find(query).limit(limit).toArray();
 
             if (monitors && monitors.length > 0) {
-                for (const monitor of monitors) {
-                    const newdate = new Date(moment().format());
-                    let updated = false;
-                    const regions = monitor.regions.map(region => {
-                        if (String(region.probeId) === String(probeId)) {
-                            updated = true;
-                            region.lastPingTime = newdate;
-                        }
-                        return region;
-                    });
-                    if (!updated) {
-                        regions.push({
-                            probeId,
-                            lastPingTime: newdate,
-                        });
-                    }
-
-                    await monitorCollection.updateOne(
-                        { _id: monitor._id },
-                        { $set: { regions } }
-                    );
-                }
+                await monitorCollection.updateMany(
+                    { _id: { $in: monitors.map((monitor) => monitor._id) } },
+                    { $set: { key: new Date(moment().format()) } }
+                );
 
                 return monitors;
             } else {
