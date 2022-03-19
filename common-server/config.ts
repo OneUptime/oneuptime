6bc88d9564bcff550c54832a895b7d9697a37adc
Nawaz Dhandala <hello@nawazdhandala.com>
2022-03-19 17:10:50 +0000

    fix lint.

--- common-server/config.ts
+++ common-server/config.ts
@@ -1,2 +1,3 @@
-export const databaseUrl: string = process.env.MONGO_URL || "mongodb://localhost:27017/oneuptimedb";
-export const databaseName: string = process.env.DB_NAME || "oneuptimedb";
+export const databaseUrl: string =
+    process.env.MONGO_URL || 'mongodb://localhost:27017/oneuptimedb';
+export const databaseName: string = process.env.DB_NAME || 'oneuptimedb';
