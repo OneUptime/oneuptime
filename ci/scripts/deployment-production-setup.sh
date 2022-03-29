ba255ec9ab571e5dd90cbfc214890d3fd491685d
Nawaz Dhandala <hello@nawazdhandala.com>
2022-03-29 14:15:44 +0100

    fix doctl

--- ci/scripts/deployment-production-setup.sh
+++ ci/scripts/deployment-production-setup.sh
@@ -7,4 +7,4 @@ echo "Connect machine with to communicate with aws cluster"
 #sudo aws eks update-kubeconfig --region $AWS_DEFAULT_REGION --name fyipe-production
 
 
-sudo doctl kubernetes cluster kubeconfig save 5c53f2a7-e462-48ab-9c02-3fbe281b2568
+doctl kubernetes cluster kubeconfig save 5c53f2a7-e462-48ab-9c02-3fbe281b2568
