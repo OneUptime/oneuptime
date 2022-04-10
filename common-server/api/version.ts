af12ce1f33fab0cf335961c5ec513ee0960a72ee
Nawaz Dhandala <hello@nawazdhandala.com>
2022-04-10 21:48:39 +0100

    register

--- common-server/api/version.ts
+++
@@ -1,9 +0,0 @@
-import Express, { ExpressRequest, ExpressResponse } from '../utils/Express';
-import { Version } from '../Config';
-const router = Express.getRouter();
-
-router.get('/version', (_req: ExpressRequest, res: ExpressResponse) => {
-    res.send({ version: Version });
-});
-
-export default router;
