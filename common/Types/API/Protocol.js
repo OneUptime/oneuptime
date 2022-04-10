33c3874a15c1da978a4add3ed5e81c652cdf9cc8
Nawaz Dhandala <hello@nawazdhandala.com>
2022-04-10 22:26:54 +0100

    fix

--- common/Types/API/Protocol.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Protocol;
-(function (Protocol) {
-    Protocol["HTTP"] = "http://";
-    Protocol["HTTPS"] = "https://";
-    Protocol["MONGO_DB"] = "mongodb://";
-})(Protocol || (Protocol = {}));
-exports.default = Protocol;
--- common/Types/API/ResponseType.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ResponseType;
-(function (ResponseType) {
-    ResponseType["HTML"] = "html";
-    ResponseType["JSON"] = "json";
-    ResponseType["CSV"] = "csv";
-})(ResponseType || (ResponseType = {}));
-exports.default = ResponseType;
