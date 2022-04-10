6ad8491237395abddd0b8c557ec20bbcb9f50c70
Nawaz Dhandala <hello@nawazdhandala.com>
2022-04-10 21:49:55 +0100

    register

--- common-server/utils/env.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-const { NODE_ENV } = process.env;
-if (!NODE_ENV || NODE_ENV === 'development') {
-    // Load env vars from /.env, do this only on development.
-    // Production values are supplied by Kubernetes Helm charts.
-    dotenv.config();
-}
