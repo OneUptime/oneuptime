38536a2ff8e13fcd0ba1eca9a1d56aa6006a7dcd
Nawaz Dhandala <hello@nawazdhandala.com>
2022-04-13 16:52:11 +0100

    fix

--- Scripts/Mongo/delete-old-data.js
+++
@@ -1,7 +0,0 @@
-var collectionNames = db.getCollectionNames();
-
-for (var collectionName of collectionNames) {
-    print("Deleting " + collectionName);
-    db.getCollection(collectionName).remove({ deleted: true });
-    print("Deleted " + collectionName);
-}
