aa625f45c66d01ca90917b770c948d5af9f1c9ad
Simon Larsen <simonlarsen@oneuptime.com>
2022-05-01 22:40:57 +0100

    add annotations

--- Common/Models/Probe.ts
+++ Common/Models/Probe.ts
@@ -5,28 +5,38 @@ import User from './User';
 import Project from './Project';
 import ObjectID from '../Types/ObjectID';
 import Version from '../Types/Version';
+import RequiredColumn from '../Types/Database/RequiredColumnDecorator';
+import UniqueColumn from '../Types/Database/UniqueColumnDecorator';
 
 @Entity({
     name: 'Probe',
 })
 export default class Probe extends BaseModel {
 
+    @RequiredColumn()
+    @UniqueColumn()
     @Column({
         type: 'text',
         nullable: false,
+        unique: true, 
         transformer: ObjectID.getDatabaseTransformer()
     })
     public key!: ObjectID;
 
+    @RequiredColumn()
     @Column({ nullable: false })
     public name!: string;
 
+    @RequiredColumn()
+    @UniqueColumn()
     @Column({ nullable: false })
     public slug!: string;
 
+    @RequiredColumn()
     @Column({ nullable: false })
     public probeVersion!: Version;
 
+    @RequiredColumn()
     @Column({ nullable: false, default: Date.now() })
     public lastAlive!: Date;
 
