57160d7631ce775f6946b7784dc51969fd4ad088
Sostene Munezero Bagira <munezerobagira@gmail.com>
2022-05-08 08:11:24 +0200

    Rename files

--- Common/Tests/API.Headers.test.ts
+++ Common/Tests/API.Headers.test.ts
@@ -3,8 +3,11 @@ describe('Headers', () => {
     test('should compile', () => {
         const headers: Headers = {
             accept: 'application/json',
-            'x-api-number': '2',
+            'x-api-limit': '2',
         };
         expect(headers).toBeTruthy();
+        expect(headers['accept']).toBe('application/json');
+        expect(headers['x-api-limit']).toBe('2');
+        expect(headers['undefined']).toBe(undefined);
     });
 });
--- Common/Tests/API.Route.test.ts
+++ Common/Tests/API.Route.test.ts
@@ -3,6 +3,7 @@ describe('Route', () => {
     test('new Route should return a create object', () => {
         const route: Route = new Route('/api/test');
         expect(route.route).toBe('/api/test');
+        expect(new Route('api test')).toThrow();
     });
     test('Route.toString() should return valid string', () => {
         const route: Route = new Route('/api/test');
