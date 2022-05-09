38a1fffd61cd1fa535b1237a1b0fae7db8297957
Sostene Munezero Bagira <munezerobagira@gmail.com>
2022-05-09 20:42:16 +0200

    fix exception typo

--- Common/Types/Exception/ExceptionCode.ts
+++ Common/Types/Exception/ExceptionCode.ts
@@ -2,7 +2,7 @@ enum ExceptionCode {
     NotImplementedException = 0,
     GeneralException = 1,
     APIException = 2,
-    DatabaseNotConnected = 3,
+    DatabaseNotConnectedException = 3,
     BadDataException = 400,
 }
 
