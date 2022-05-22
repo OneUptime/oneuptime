4791fefc6cae3008bee46466a94f20d3774f26a9
Simon Larsen <simonlarsen@oneuptime.com>
2022-05-22 09:13:41 +0100

    fix package.json files

--- CommonServer/Utils/Envrionment.ts
+++ CommonServer/Utils/Envrionment.ts
@@ -1,12 +1,3 @@
-import dotenv from 'dotenv';
-import AppEnvironment from 'Common/Types/AppEnvironment';
-
 const Env: string = process.env['NODE_ENV'] || '';
 
-if (Env === AppEnvironment.Development || Env === AppEnvironment.Test) {
-    /*
-     * Load env vars from /.env, do this only on development.
-     * Production values are supplied by Kubernetes Helm charts or docker compose files.
-     */
-    dotenv.config();
-}
+export default Env;
