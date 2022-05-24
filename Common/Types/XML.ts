08ca6c68f6080523781cd13fe8a2e29faee22437
Sostene MUNEZERO BAGIRA <munezerobagira@gmail.com>
2022-05-25 00:30:37 +0200

    Chore: test
    - add test for Role, SecuritySeverity, Text, XML

--- Common/Types/XML.ts
+++ Common/Types/XML.ts
@@ -1,9 +1,13 @@
+import BadDataException from './Exception/BadDataException';
 export default class XML {
     private _xml: string = '';
     public get xml(): string {
         return this._xml;
     }
     public set xml(v: string) {
+        if (!v) {
+            throw new BadDataException('XML is not in valid format.');
+        }
         this._xml = v;
     }
 
