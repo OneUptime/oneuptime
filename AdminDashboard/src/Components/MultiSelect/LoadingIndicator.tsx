5ec75ffd6f32e5c950d47b067bed0a1b092b0332
Simon Larsen <simonlarsen@oneuptime.com>
2022-06-06 14:12:03 +0100

    remove admin dash and status page

--- AdminDashboard/src/Components/MultiSelect/LoadingIndicator.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadingIndicator: Function = () => {
-    return <div className="loading-status" />;
-};
-
-LoadingIndicator.displayName = 'LoadingIndicator';
-
-export default LoadingIndicator;
