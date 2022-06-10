94b98b0ec92a0532913b1c0b205ebae73ff0ce31
sammychris <ebusameric@gmail.com>
2022-06-10 15:11:17 +0100

    fix

--- Common/Tests/TestingUtils/PuppeteerHelper.ts
+++ Common/Tests/TestingUtils/PuppeteerHelper.ts
@@ -1,7 +1,5 @@
-import { Page } from '../../../Home/node_modules/puppeteer';
-
 export default class PuppeteerHelper {
-    public static async getTextContent(page: Page, selector: string) {
+    public static async getTextContent(page: any, selector: string) {
         await page.waitForSelector(selector);
         return await page.$eval(selector, (e: Element) => {
             return e.textContent;
