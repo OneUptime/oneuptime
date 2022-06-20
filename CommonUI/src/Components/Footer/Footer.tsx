c4230a67ee355a7eb9b2b4318184ab01791b0a8b
Simon Larsen <simonlarsen@oneuptime.com>
2022-06-20 21:06:04 +0100

    fix css

--- CommonUI/src/Components/Footer/Footer.tsx
+++ CommonUI/src/Components/Footer/Footer.tsx
@@ -3,7 +3,16 @@ import React from "react";
 const Footer = () => {
   return (
     <React.Fragment>
-      <footer className="footer"><div className="container-fluid"><div className="row"><div className="col-md-6">2022 © Minia.</div><div className="col-md-6"><div className="text-sm-end d-none d-sm-block">Design &amp; Develop by<a className="ms-1 text-decoration-underline" href="/dashboard">Themesbrand</a></div></div></div></div></footer>
+      <footer className="footer">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">2022 © OneUptime Limited.</div>
+            <div className="col-md-6">
+              <div className="text-sm-end d-none d-sm-block">Design &amp; Develop by<a className="ms-1 text-decoration-underline" href="/dashboard">Themesbrand</a></div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </React.Fragment>
   );
 };
