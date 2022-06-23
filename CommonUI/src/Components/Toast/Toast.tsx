c913efb49031d84bb54c7c2d5af31e00d76a1258
Simon Larsen <simonlarsen@oneuptime.com>
2022-06-23 19:27:58 +0100

    fix toast

--- CommonUI/src/Components/Toast/Toast.tsx
+++ CommonUI/src/Components/Toast/Toast.tsx
@@ -1,22 +1,46 @@
 import React, { FunctionComponent, ReactElement, useState } from 'react';
 
+
+export enum ToastType {
+    DANGER, 
+    SUCCESS, 
+    INFO, 
+    WARNING
+}
+
+
 export interface ComponentProps {
     title: string;
     description: string;
-    onClose?: () => void
+    onClose?: () => void;
+    type?: ToastType;
 }
+
 
 const Component: FunctionComponent<ComponentProps> = (
     props: ComponentProps
 ): ReactElement => {
 
     const [show, setShow] = useState<boolean>(true);
+    let typeCssClass: string = "text-info";
+
+    if (props.type === ToastType.DANGER) {
+        typeCssClass = "text-danger"
+    }
+
+    if (props.type === ToastType.SUCCESS) {
+        typeCssClass = "text-success"
+    }
+
+    if (props.type === ToastType.WARNING) {
+        typeCssClass = "text-warning"
+    }
 
     if (show) {
         return (<div className="position-fixed top-0 end-0 p-3" style={{ "zIndex": "1005" }}>
             <div className="toast fade show" role="alert">
                 <div className="toast-header">
-                    <div role="status" className="spinner-grow-sm spinner-grow text-danger"><span className="visually-hidden">Loading...</span></div>
+                    <div role="status" className={`spinner-grow-sm spinner-grow ${typeCssClass}`}><span className="visually-hidden">Loading...</span></div>
                     <strong className="me-auto ms-2">{props.title}</strong><button onClick={() => {
                         setShow(false);
                         props.onClose && props.onClose();
