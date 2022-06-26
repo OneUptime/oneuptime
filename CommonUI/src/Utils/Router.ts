017c1c75d0aaa0ecefa77d1a9f18eb233a742037
Nawaz Dhandala <hello@nawazdhandala.com>
2022-06-26 13:46:30 +0100

    fix eslint.

---
+++ CommonUI/src/Utils/Router.ts
@@ -0,0 +1,18 @@
+import {
+    Routes,
+    Route as PageRoute,
+    useNavigate,
+    BrowserRouter,
+    useLocation,
+} from 'react-router-dom';
+
+import ReactDOM from 'react-dom/client';
+
+export default {
+    Routes,
+    PageRoute,
+    useNavigate,
+    useLocation,
+    BrowserRouter,
+    ReactDOM
+}
