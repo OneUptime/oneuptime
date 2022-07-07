84ea3c7bfeaae1ba1df5116ee7bdb62568f2cad5
chrissiemhrk <chrissiemhrk@gmail.com>
2022-07-07 21:11:43 +0200

    implement more comprehensive tests

--- CommonUI/src/Tests/Breadcrumbs.test.tsx
+++ CommonUI/src/Tests/Breadcrumbs.test.tsx
@@ -4,22 +4,29 @@ import Route from 'Common/Types/API/Route';
 import renderer from 'react-test-renderer';
 import Breadcrumbs from '../Components/Breadcrumbs/Breadcrumbs';
 import Link from 'Common/Types/Link';
-const links: Array<Link> = [
-    {
-        title: 'Home',
-        to: new Route('/'),
-    },
-    {
-        title: 'Projects',
-        to: new Route('/projects'),
-    },
-];
-
 describe('Breadcrumbs', () => {
     test('should render correctly', () => {
-        const tree: any = renderer
-            .create(<Breadcrumbs links={links} />)
-            .toJSON();
-        expect(tree).toMatchSnapshot();
+        const links: Array<Link> = [
+            {
+                title: 'Home',
+                to: new Route('/'),
+            },
+            {
+                title: 'Projects',
+                to: new Route('/projects'),
+            },
+        ];
+
+        const testRenderer: any = renderer.create(
+            <Breadcrumbs links={links} />
+        );
+        const testInstance: any = testRenderer.root;
+        expect(testInstance.findAllByType('li')).toContainEqual(
+            expect.objectContaining({
+                props: expect.objectContaining({
+                    className: expect.stringContaining('breadcrumb-item'),
+                }),
+            })
+        );
     });
 });
