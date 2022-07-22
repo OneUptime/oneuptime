140ba555ae55dd81cab5e74da70f44c3881d8805
Simon Larsen <simonlarsen@oneuptime.com>
2022-07-22 11:03:57 +0100

    add labels.

--- CommonUI/src/Components/Table/TableHeader.tsx
+++ CommonUI/src/Components/Table/TableHeader.tsx
@@ -1,20 +1,49 @@
-import React, { FunctionComponent, ReactElement } from 'react';
+import React, { FunctionComponent, ReactElement, useState } from 'react';
 import Column from './Types/Column';
 import Columns from './Types/Columns';
+import Icon, { IconProp, ThickProp } from '../Icon/Icon';
+import SortOrder from 'Common/Types/Database/SortOrder';
 
 export interface ComponentProps {
     columns: Columns;
     id: string;
+    onSortChanged: (sortBy: string, sortOrder: SortOrder) => void;
 }
 
 const TableHeader: FunctionComponent<ComponentProps> = (
     props: ComponentProps
 ): ReactElement => {
+
+
+    const [currentSortColumn, setCurrentSortColumn] = useState<string>("");
+    const [sortOrder, setSortOrder] = useState<SortOrder>(SortOrder.Ascending);
+
     return (
-        <thead id={props.id}>
+        <thead id={props.id} >
             <tr>
                 {props.columns.map((column: Column, i: number) => {
-                    return <th key={i}>{column.title}</th>;
+
+                    const canSort = !column.disableSort && column.key;
+
+                    return <th key={i} className={`${canSort ? "pointer" : ""}`} onClick={() => {
+                        if (!column.key) {
+                            return;
+                        }
+
+                        if (currentSortColumn === column.key) {
+                            setSortOrder(sortOrder === SortOrder.Ascending ? SortOrder.Descending : SortOrder.Ascending);
+                        } else {
+                            setCurrentSortColumn(column.key);
+                            setSortOrder(SortOrder.Ascending);
+                        }
+
+                        props.onSortChanged(currentSortColumn, sortOrder);
+
+                    }}>
+                        {column.title}
+                        {canSort && currentSortColumn === column.key && sortOrder === SortOrder.Ascending && <Icon icon={IconProp.ChevronUp} thick={ThickProp.Thick} />}
+                        {canSort && currentSortColumn === column.key && sortOrder === SortOrder.Descending && <Icon icon={IconProp.ChevronDown} thick={ThickProp.Thick} />}
+                    </th>;
                 })}
             </tr>
         </thead>
