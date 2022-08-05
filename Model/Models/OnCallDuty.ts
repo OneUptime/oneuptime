30983721b80f4dd6e02aaa9baa7b21e3602c1a6a
Simon Larsen <simonlarsen@oneuptime.com>
2022-08-05 22:37:49 +0100

    delete old models

---
+++ Model/Models/OnCallDuty.ts
@@ -0,0 +1,221 @@
+import { Column, Entity, Index, JoinColumn, ManyToOne } from 'typeorm';
+import BaseModel from 'Common/Models/BaseModel';
+import User from './User';
+import Project from './Project';
+import CrudApiEndpoint from 'Common/Types/Database/CrudApiEndpoint';
+import SlugifyColumn from 'Common/Types/Database/SlugifyColumn';
+import Route from 'Common/Types/API/Route';
+import TableColumnType from 'Common/Types/Database/TableColumnType';
+import TableColumn from 'Common/Types/Database/TableColumn';
+import ColumnType from 'Common/Types/Database/ColumnType';
+import ObjectID from 'Common/Types/ObjectID';
+import ColumnLength from 'Common/Types/Database/ColumnLength';
+import TableAccessControl from 'Common/Types/Database/AccessControl/TableAccessControl';
+import Permission from 'Common/Types/Permission';
+import ColumnAccessControl from 'Common/Types/Database/AccessControl/ColumnAccessControl';
+import ProjectColumn from 'Common/Types/Database/ProjectColumn';
+import EntityName from 'Common/Types/Database/EntityName';
+
+@ProjectColumn('projectId')
+@TableAccessControl({
+    create: [Permission.ProjectOwner, Permission.CanCreateProjectOnCallDuty],
+    read: [
+        Permission.ProjectOwner,
+        Permission.CanReadProjectOnCallDuty,
+        Permission.ProjectMember,
+    ],
+    delete: [Permission.ProjectOwner, Permission.CanDeleteProjectOnCallDuty],
+    update: [
+        Permission.ProjectOwner,
+        Permission.CanEditProjectOnCallDuty,
+    ],
+})
+@CrudApiEndpoint(new Route('/team'))
+@SlugifyColumn('name', 'slug')
+@Entity({
+    name: 'OnCallDuty',
+})
+@EntityName('OnCallDuty', 'OnCallDutys')
+export default class OnCallDuty extends BaseModel {
+    @ColumnAccessControl({
+        create: [Permission.ProjectOwner, Permission.CanCreateProjectOnCallDuty],
+        read: [
+            Permission.ProjectOwner,
+            Permission.CanReadProjectOnCallDuty,
+            Permission.ProjectMember,
+        ],
+        update: [],
+    })
+    @TableColumn({
+        manyToOneRelationColumn: 'projectId',
+        type: TableColumnType.Entity,
+        modelType: Project,
+    })
+    @ManyToOne(
+        (_type: string) => {
+            return Project;
+        },
+        {
+            eager: false,
+            nullable: true,
+            onDelete: 'CASCADE',
+            orphanedRowAction: 'nullify',
+        }
+    )
+    @JoinColumn({ name: 'projectId' })
+    public project?: Project = undefined;
+
+    @ColumnAccessControl({
+        create: [Permission.ProjectOwner, Permission.CanCreateProjectOnCallDuty],
+        read: [
+            Permission.ProjectOwner,
+            Permission.CanReadProjectOnCallDuty,
+            Permission.ProjectMember,
+        ],
+        update: [],
+    })
+    @Index()
+    @TableColumn({ type: TableColumnType.ObjectID , required: true })
+    @Column({
+        type: ColumnType.ObjectID,
+        nullable: false,
+        transformer: ObjectID.getDatabaseTransformer(),
+    })
+    public projectId?: ObjectID = undefined;
+
+    @ColumnAccessControl({
+        create: [Permission.ProjectOwner, Permission.CanCreateProjectOnCallDuty],
+        read: [
+            Permission.ProjectOwner,
+            Permission.CanReadProjectOnCallDuty,
+            Permission.ProjectMember,
+        ],
+        update: [Permission.ProjectOwner, Permission.CanEditProjectOnCallDuty],
+    })
+    @Index()
+    @TableColumn({ required: true, type: TableColumnType.ShortText })
+    @Column({
+        nullable: false,
+        type: ColumnType.ShortText,
+        length: ColumnLength.ShortText,
+    })
+    public name?: string = undefined;
+
+    @ColumnAccessControl({
+        create: [Permission.ProjectOwner, Permission.CanCreateProjectOnCallDuty],
+        read: [
+            Permission.ProjectOwner,
+            Permission.CanReadProjectOnCallDuty,
+            Permission.ProjectMember,
+        ],
+        update: [Permission.ProjectOwner, Permission.CanEditProjectOnCallDuty],
+    })
+    @TableColumn({ required: false, type: TableColumnType.LongText })
+    @Column({
+        nullable: true,
+        type: ColumnType.LongText,
+        length: ColumnLength.LongText,
+    })
+    public description?: string = undefined;
+
+    @Index()
+    @ColumnAccessControl({
+        create: [Permission.ProjectOwner, Permission.CanCreateProjectOnCallDuty],
+        read: [
+            Permission.ProjectOwner,
+            Permission.CanReadProjectOnCallDuty,
+            Permission.ProjectMember,
+        ],
+        update: [],
+    })
+    @TableColumn({ required: true, unique: true, type: TableColumnType.Slug })
+    @Column({
+        nullable: false,
+        type: ColumnType.Slug,
+        length: ColumnLength.Slug,
+        unique: true,
+    })
+    public slug?: string = undefined;
+
+    @ColumnAccessControl({
+        create: [Permission.ProjectOwner, Permission.CanCreateProjectOnCallDuty],
+        read: [
+            Permission.ProjectOwner,
+            Permission.CanReadProjectOnCallDuty,
+            Permission.ProjectMember,
+        ],
+        update: [],
+    })
+    @TableColumn({
+        manyToOneRelationColumn: 'createdByUserId',
+        type: TableColumnType.Entity,
+        modelType: Project,
+    })
+    @ManyToOne(
+        (_type: string) => {
+            return User;
+        },
+        {
+            eager: false,
+            nullable: true,
+            onDelete: 'CASCADE',
+            orphanedRowAction: 'nullify',
+        }
+    )
+    @JoinColumn({ name: 'createdByUserId' })
+    public createdByUser?: User = undefined;
+
+    @ColumnAccessControl({
+        create: [Permission.ProjectOwner, Permission.CanCreateProjectOnCallDuty],
+        read: [
+            Permission.ProjectOwner,
+            Permission.CanReadProjectOnCallDuty,
+            Permission.ProjectMember,
+        ],
+        update: [],
+    })
+    @TableColumn({ type: TableColumnType.ObjectID })
+    @Column({
+        type: ColumnType.ObjectID,
+        nullable: true,
+        transformer: ObjectID.getDatabaseTransformer(),
+    })
+    public createdByUserId?: ObjectID = undefined;
+
+    @ColumnAccessControl({
+        create: [],
+        read: [],
+        update: [],
+    })
+    @TableColumn({
+        manyToOneRelationColumn: 'deletedByUserId',
+        type: TableColumnType.ObjectID,
+    })
+    @ManyToOne(
+        (_type: string) => {
+            return User;
+        },
+        {
+            cascade: false,
+            eager: false,
+            nullable: true,
+            onDelete: 'CASCADE',
+            orphanedRowAction: 'nullify',
+        }
+    )
+    @JoinColumn({ name: 'deletedByUserId' })
+    public deletedByUser?: User = undefined;
+
+    @ColumnAccessControl({
+        create: [],
+        read: [Permission.ProjectMember],
+        update: [],
+    })
+    @TableColumn({ type: TableColumnType.ObjectID })
+    @Column({
+        type: ColumnType.ObjectID,
+        nullable: true,
+        transformer: ObjectID.getDatabaseTransformer(),
+    })
+    public deletedByUserId?: ObjectID = undefined;
+}
