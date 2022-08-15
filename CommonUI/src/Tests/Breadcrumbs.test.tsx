c283fd037407c1dc75fa20946fda724ae1fe9004
Victor Okpon <vikzy2rhyme@yahoo.com>
2022-08-15 01:19:30 +0100

    test breadcrumb

--- CommonUI/src/Tests/Breadcrumbs.test.tsx
+++ CommonUI/src/Tests/Breadcrumbs.test.tsx
@@ -1,36 +1,44 @@
 import * as React from 'react';
 import { describe, test, expect } from '@jest/globals';
 import Route from 'Common/Types/API/Route';
-import renderer, {
-    ReactTestRenderer,
-    ReactTestInstance,
-} from 'react-test-renderer';
+import { create, ReactTestInstance } from 'react-test-renderer';
 import Breadcrumbs from '../Components/Breadcrumbs/Breadcrumbs';
 import Link from 'Common/Types/Link';
 
 describe('Breadcrumbs', () => {
-    test('should render correctly', () => {
-        const links: Array<Link> = [
-            {
-                title: 'Home',
-                to: new Route('/'),
-            },
-            {
-                title: 'Projects',
-                to: new Route('/projects'),
-            },
-        ];
+    const links: Array<Link> = [
+        {
+            title: 'Home',
+            to: new Route('/'),
+        },
+        {
+            title: 'Projects',
+            to: new Route('/projects'),
+        },
+    ];
 
-        const testRenderer: ReactTestRenderer = renderer.create(
-            <Breadcrumbs links={links} />
-        );
-        const testInstance: ReactTestInstance = testRenderer.root;
-        expect(testInstance.findAllByType('li')).toContainEqual(
+    const instance: ReactTestInstance = create(
+        <Breadcrumbs links={links} />
+    ).root;
+
+    test('should render correctly', () => {
+        expect(instance.findAllByType('li')).toContainEqual(
             expect.objectContaining({
                 props: expect.objectContaining({
                     className: expect.stringContaining('breadcrumb-item'),
                 }),
             })
         );
+    });
+
+    test('it should have a link to defined', () => {
+        expect(instance.props['links'][0].to).toBeDefined();
+    });
+
+    test('it should have a link title defined', () => {
+        const text: string | undefined = links[0]?.title;
+        const element: boolean =
+            instance.findByProps({ children: text }).type === 'link';
+        expect(element).toBeDefined();
     });
 });
