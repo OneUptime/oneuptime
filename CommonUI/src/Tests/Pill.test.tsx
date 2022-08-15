7e92dc4bbf50ce0b1c4f9b6289f5bc651c9ea6d9
hershey <hershey2776@gmail.com>
2022-08-16 03:41:50 +0530

    Added test for CommonUi/src/Components/Pill/Pill.ts

---
+++ CommonUI/src/Tests/Pill.test.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { describe, test, expect } from '@jest/globals';
+import renderer, {
+    ReactTestInstance,
+    ReactTestRenderer,
+} from 'react-test-renderer';
+import Pill, { PillSize } from '../Components/Pill/Pill';
+import { Black } from '../Utils/BrandColors';
+
+describe('Test for Pill', () => {
+    const testRenderer: ReactTestRenderer = renderer.create(
+        <Pill text="Love" color={Black} size={PillSize.Small} />
+    );
+    const testInstance: ReactTestInstance = testRenderer.root;
+    test('should render the component', () => {
+        expect(testInstance.findAllByType('span')).toContainEqual(
+            expect.objectContaining({
+                props: expect.objectContaining({
+                    className: expect.stringContaining('rounded-pill'),
+                }),
+            })
+        );
+        expect(
+            testInstance.findByProps({ className: 'rounded-pill badge' })
+                .children
+        ).toEqual([' ', 'Love', ' ']);
+    });
+    test('Checking text', () => {
+        expect(
+            testInstance.findByProps({ className: 'rounded-pill badge' })
+                .children
+        ).toEqual([' ', 'Love', ' ']);
+    });
+});
+
+//  expect(screen.getByTestId('my-test-id')).toHaveTextContent('some text');
