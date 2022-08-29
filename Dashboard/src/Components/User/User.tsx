63b858cc428a63df5170631f43e74ada1f9e0d31
Simon Larsen <simonlarsen@oneuptime.com>
2022-08-29 12:28:36 +0100

    add user icon

--- Dashboard/src/Components/User/User.tsx
+++ Dashboard/src/Components/User/User.tsx
@@ -1,3 +1,4 @@
+import Icon, { IconProp, ThickProp } from 'CommonUI/src/Components/Icon/Icon';
 import User from 'Model/Models/User';
 import React, { FunctionComponent, ReactElement } from 'react';
 
@@ -9,15 +10,20 @@ const UserElement: FunctionComponent<ComponentProps> = (
     props: ComponentProps
 ): ReactElement => {
     return (
-        <div>
-            <div className="bold">
-                {(props.user.toJSONObject()['name']?.toString() as string) ||
-                    'User not signed up so far'}
+        <div className="flex">
+            <div className='user-circle'>
+                <Icon icon={IconProp.User} thick={ThickProp.Thick} />
             </div>
+            <div>
+                <div className="bold">
+                    {(props.user.toJSONObject()['name']?.toString() as string) ||
+                        'User not signed up so far'}
+                </div>
 
-            <div className="color-light-grey">
-                {(props.user.toJSONObject()['email']?.toString() as string) ||
-                    ''}
+                <div className="color-light-grey">
+                    {(props.user.toJSONObject()['email']?.toString() as string) ||
+                        ''}
+                </div>
             </div>
         </div>
     );
