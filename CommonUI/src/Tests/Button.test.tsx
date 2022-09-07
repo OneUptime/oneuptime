5acad02f37a1bf3e5f9870fdebd3846661bc10d9
Victor Okpon <vikzy2rhyme@yahoo.com>
2022-09-07 08:34:18 +0100

    test Button

---
+++ CommonUI/src/Tests/Button.test.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { create } from 'react-test-renderer';
+import Button from '../Components/Button/Button';
+
+describe('Button', () => {
+    const button = create(<Button />);
+
+    test('should match snapshot', () => {
+        expect(button.toJSON()).toMatchInlineSnapshot(`
+            <button
+              className="btn no-border-on-hover btn waves-effect waves-light no-border-on-hover"
+              onClick={[Function]}
+              type="button"
+            >
+              <div>
+                <div>
+                  <div />
+                </div>
+                <span
+                  className="justify-center"
+                >
+                  <span />
+                  <span
+                    style={
+                      Object {
+                        "marginLeft": "5px",
+                      }
+                    }
+                  />
+                </span>
+              </div>
+            </button>
+        `);
+    });    
+});
