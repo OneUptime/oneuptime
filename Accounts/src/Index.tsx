9b6d71546e3a39c0a811320b40b04a57880e01e7
Simon Larsen <simonlarsen@oneuptime.com>
2022-09-28 12:48:58 +0100

    add css reset

--- Accounts/src/Index.tsx
+++ Accounts/src/Index.tsx
@@ -3,6 +3,7 @@ import 'CommonUI/src/Styles/Bootstrap';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import "the-new-css-reset/css/reset.css"
 
 const root: any = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
