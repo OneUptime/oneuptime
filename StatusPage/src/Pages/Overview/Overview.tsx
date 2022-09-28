7d047d28b11f4c80a577512a30e47b0ec754c8dc
Simon Larsen <simonlarsen@oneuptime.com>
2022-09-28 14:52:51 +0100

    add 404 to accounts.

--- StatusPage/src/Pages/Overview/Overview.tsx
+++ StatusPage/src/Pages/Overview/Overview.tsx
@@ -6,8 +6,8 @@ const Overview: FunctionComponent<PageComponentProps> = (
     _props: PageComponentProps
 ): ReactElement => {
     return (
-        <Page title={'Page Not Found'}>
-            <p>Page you are looking for does not exist.</p>
+        <Page title={'Overview'}>
+            <p>Overview</p>
         </Page>
     );
 };
