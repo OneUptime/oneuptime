238d017b9ce2958ad17053f2f5049faa5fe0ea5f
Munyaneza Armel <munyaarmel61@gmail.com>
2022-10-01 11:46:34 +0000

    fix-lint

--- CommonUI/src/Tests/alert.test.tsx
+++ CommonUI/src/Tests/alert.test.tsx
@@ -1,67 +1,66 @@
 import React from 'react';
-import { render,screen, fireEvent} from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import Alert,{AlertType} from '../Components/Alerts/Alert'
-import  Icon from '../Components/Icon/Icon';
+import Alert, { AlertType } from '../Components/Alerts/Alert'
+import Icon from '../Components/Icon/Icon';
 
 describe('alert tests', () => {
-  test('it should render all props passed', () => {
-    const handleClick:  undefined | (() => void) = jest.fn();
-    const handleClose: (() => void) | undefined = jest.fn();
+   test('it should render all props passed', () => {
+        const handleClick:  undefined | (() => void) = jest.fn();
+        const handleClose: (() => void) | undefined = jest.fn();
     render(
-        <Alert
-            title=' sample-title'
-            strongTitle='sample-strong'
-            type={AlertType.SUCCESS}
-            onClick= {handleClick}
-            onClose={handleClose}
-        />
+    <Alert
+                title= 'title'
+                strongTitle= 'strong'
+                type= {AlertType.SUCCESS}
+                onClick= {handleClick}
+                onClose= {handleClose}
+            />
     );
-    expect(Icon).toBeInTheDocument;
-    expect(handleClick).toBeCalled;
-    expect(handleClose).toBeCalled;
-
+        expect(Icon).toBeInTheDocument;
+        expect(handleClick).toBeCalled;
+        expect(handleClose).toBeCalled;
 });
     test('it should show icon when alert type is equal to success', () => {
-      render(<Alert  type={AlertType.SUCCESS}/>)
-      expect(Icon).toBeInTheDocument;
+        render(<Alert type={AlertType.SUCCESS} />)
+        expect(Icon).toBeInTheDocument;
     });
     test('it should show icon when alert type is equal to info', () => {
-      render(<Alert  type={AlertType.INFO}/>)
-      expect(Icon).toBeInTheDocument;
+       render(<Alert type={AlertType.INFO} />)
+       expect(Icon).toBeInTheDocument;
     });
     test('it should show icon when alert type is equal to warning', () => {
-      render(<Alert  type={AlertType.WARNING}/>)
-      expect(Icon).toBeInTheDocument;
+       render(<Alert type={AlertType.WARNING} />)
+       expect(Icon).toBeInTheDocument;
     });
     test('it should show icon when alert type is equal to danger', () => {
-      render(<Alert  type={AlertType.DANGER}/>)
-      expect(Icon).toBeInTheDocument;
+       render(<Alert type={AlertType.DANGER} />)
+       expect(Icon).toBeInTheDocument;
     });
     test('it should have a title displayed in document', () => {
-      render(<Alert title='title'/>);
+      render(<Alert title='title' />);
       expect(screen.getByText('title')).toBeInTheDocument;
   });
   test('it should have a strong text displayed in document ', () => {
-    render(<Alert strongTitle='strong'/>);
+    render(<Alert strongTitle='strong' />);
     expect(screen.getByText('strong')).toBeInTheDocument;
 });
-test('it should handle onClick event', () => {
-  const handleClick: (() => void) | undefined = jest.fn();
-  render(<Alert title='title' onClick={handleClick} />);
-  fireEvent.click( screen.getByText('title'));
-  expect(handleClick).toBeCalled;
+  test('it should handle onClick event', () => {
+       const handleClick: (() => void) | undefined = jest.fn();
+    render(<Alert title='title' onClick={handleClick} />);
+        fireEvent.click(screen.getByText('title'));
+    expect(handleClick).toBeCalled;
 });
-test('it should handle onClose event', () => {
-  const handleClose: undefined | (() => void) = jest.fn();
-  render(<Alert title='title' onClose={handleClose} />);
-  fireEvent.click( screen.getByText('title'));
+ test('it should handle onClose event', () => {
+       const handleClose: undefined | (() => void) = jest.fn();
+   render(<Alert title='title' onClose={handleClose} />);
+        fireEvent.click( screen.getByText('title'));
   expect(handleClose).toBeCalled;
 });
 test('it should  display button  onClose event', () => {
-  const handleClose: undefined | (() => void) = jest.fn();
-  render(<Alert onClose={handleClose} />);
-  fireEvent.click( screen.getByRole('button'));
-  expect(screen.getByRole('button')).toBeVisible();
+      const handleClose: undefined | (() => void) = jest.fn();
+   render(<Alert onClose={handleClose} />);
+        fireEvent.click( screen.getByRole('button'));
+   expect(screen.getByRole('button')).toBeVisible();
 });
 });
