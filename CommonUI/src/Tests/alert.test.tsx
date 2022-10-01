d6e0ff1228df5e278bbaefef6d82e8da1b3a3c67
Munyaneza Armel <munyaarmel61@gmail.com>
2022-10-01 12:56:42 +0000

    fix-lint

--- CommonUI/src/Tests/alert.test.tsx
+++ CommonUI/src/Tests/alert.test.tsx
@@ -9,13 +9,13 @@ describe('alert tests', () => {
         const handleClick: undefined | (() => void) = jest.fn();
         const handleClose: (() => void) | undefined = jest.fn();
         render(
-         <Alert
-             title="title"
-             strongTitle="strong"
-             type={AlertType.SUCCESS}
-             onClick={handleClick}
-             onClose={handleClose}
-           />
+            <Alert
+                title="title"
+                strongTitle="strong"
+                type={AlertType.SUCCESS}
+                onClick={handleClick}
+                onClose={handleClose}
+            />
         );
         expect(Icon).toBeInTheDocument;
         expect(handleClick).toBeCalled;
@@ -30,36 +30,36 @@ describe('alert tests', () => {
         expect(Icon).toBeInTheDocument;
     });
     test('it should show icon when alert type is equal to warning', () => {
-        render(<Alert type={AlertType.WARNING} />)
+        render(<Alert type={AlertType.WARNING} />);
         expect(Icon).toBeInTheDocument;
-    }); 
+  }); 
     test('it should show icon when alert type is equal to danger', () => {
-        render(<Alert type={AlertType.DANGER} />)
+        render(<Alert type={AlertType.DANGER} />);
         expect(Icon).toBeInTheDocument;
     });
     test('it should have a title displayed in document', () => {
         render(<Alert title="title" />);
         expect(screen.getByText('title')).toBeInTheDocument;
     });
-  test('it should have a strong text displayed in document ', () => {
+    test('it should have a strong text displayed in document ', () => {
         render(<Alert strongTitle="strong" />);
         expect(screen.getByText('strong')).toBeInTheDocument;
-  });
-  test('it should handle onClick event', () => {
-       const handleClick: (() => void) | undefined = jest.fn();
-      render(<Alert title="title" onClick={handleClick} />);
+    });
+    test('it should handle onClick event', () => {
+        const handleClick: (() => void) | undefined = jest.fn();
+        render(<Alert title="title" onClick={handleClick} />);
         fireEvent.click(screen.getByText('title'));
         expect(handleClick).toBeCalled;
-});
+    });
     test('it should handle onClose event', () => {
         const handleClose: undefined | (() => void) = jest.fn();
-       render(<Alert title="title" onClose={handleClose} />);
+        render(<Alert title="title" onClose={handleClose} />);
         fireEvent.click(screen.getByText('title'));
         expect(handleClose).toBeCalled;
     });
     test('it should  display button  onClose event', () => {
         const handleClose: undefined | (() => void) = jest.fn();
-       render(<Alert onClose={handleClose} />);
+        render(<Alert onClose={handleClose} />);
         fireEvent.click(screen.getByRole('button'));
         expect(screen.getByRole('button')).toBeVisible();
     });
