526963b69468a2bb02619c50d291d82a26306e59
Simon Larsen <simonlarsen@oneuptime.com>
2022-10-19 15:32:10 +0100

    add worker files

---
+++ Workers/Index.ts
@@ -0,0 +1,25 @@
+import { PostgresAppInstance } from 'CommonServer/Infrastructure/PostgresDatabase';
+import Redis from 'CommonServer/Infrastructure/Redis';
+import logger from 'CommonServer/Utils/Logger';
+import App from 'CommonServer/Utils/StartServer';
+
+const APP_NAME: string = 'workers';
+
+const init: Function = async (): Promise<void> => {
+    try {
+        // init the app
+        await App(APP_NAME);
+        // connect to the database.
+        await PostgresAppInstance.connect(
+            PostgresAppInstance.getDatasourceOptions()
+        );
+
+        // connect redis
+        await Redis.connect();
+    } catch (err) {
+        logger.error('App Init Failed:');
+        logger.error(err);
+    }
+};
+
+init();
