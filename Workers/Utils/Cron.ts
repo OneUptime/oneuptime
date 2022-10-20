5a305ef7c077deed1c4a1f285e486e2af2a3f1f1
Simon Larsen <simonlarsen@oneuptime.com>
2022-10-20 12:32:19 +0100

    fix worker

---
+++ Workers/Utils/Cron.ts
@@ -0,0 +1,18 @@
+import logger from 'CommonServer/Utils/Logger';
+import cron from 'node-cron';
+
+
+const RunCron = (jobName: string, schedule: string, runFunction: Function) => {
+    cron.schedule(schedule, async () => {
+        try {
+            logger.info(`Job ${jobName} Start`);
+            await runFunction();
+            logger.info(`Job ${jobName} End`);
+        } catch (e) {
+            logger.info(`Job ${jobName} Error`);
+            logger.error(e);
+        }
+    });
+}
+
+export default RunCron;
