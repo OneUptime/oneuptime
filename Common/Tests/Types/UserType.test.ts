b30eb49be60692c52862a52950dce646db4c6531
Cato Perez <82598461+yawMD@users.noreply.github.com>
2022-10-24 10:47:24 +0000

    init

--- Common/Tests/Types/UserType.test.ts
+++ Common/Tests/Types/UserType.test.ts
@@ -1,10 +1,10 @@
-import UserType from "../../Types/UserType";
+import UserType from '../../Types/UserType';
 
-describe('enum UserType',() =>{
-    test('each user type should have a corresponding type',() =>{
-        expect(UserType['API']).toEqual('API')
-        expect(UserType['User']).toEqual('User')
-        expect(UserType['MasterAdmin']).toEqual('MasterAdmin')
-        expect(UserType['Public']).toEqual('Public')
-    })
-})
+describe('enum UserType', () => {
+    test('each user type should have a corresponding type', () => {
+        expect(UserType['API']).toEqual('API');
+        expect(UserType['User']).toEqual('User');
+        expect(UserType['MasterAdmin']).toEqual('MasterAdmin');
+        expect(UserType['Public']).toEqual('Public');
+    });
+});
