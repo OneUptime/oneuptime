646dddb3c812c1d050540758a50864d623103b1c
Simon Larsen <simonlarsen@oneuptime.com>
2022-11-09 18:35:13 +0000

    fix gomplate version

--- preinstall.sh
+++ preinstall.sh
@@ -126,7 +126,7 @@ if [[ ! $(which docker-compose) && ! $(docker-compose --version) ]]; then
 fi
 
 if [[ ! $(which gomplate) ]]; then
-    sudo curl -o /usr/local/bin/gomplate -sSL https://github.com/hairyhenderson/gomplate/releases/download/<version>/gomplate_$(uname -s)-$(uname -m)
+    sudo curl -o /usr/local/bin/gomplate -sSL https://github.com/hairyhenderson/gomplate/releases/download/3.11.3/gomplate_$(uname -s)-$(uname -m)
     chmod 755 /usr/local/bin/gomplate
     gomplate --help
 fi
