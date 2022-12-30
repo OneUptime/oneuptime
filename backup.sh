6c3d17632caf8123c3217b9cc3b5f621a3db3b5f
Simon Larsen <simonlarsen@oneuptime.com>
2022-12-30 14:03:59 +0000

    fix script

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ export $(grep -v '^#' config.env | xargs)
 
 echo "Starting backup...."
 
-sudo docker run -it --net=host --rm \
+docker run -it --net=host --rm \
 --env-file config.env \
 --volume=$(pwd)$DATABASE_BACKUP_DIRECTORY:/var/lib/postgresql/data \
 postgres:latest /usr/bin/pg_dump -Fc --dbname=postgresql://$DATABASE_BACKUP_USERNAME:$DATABASE_BACKUP_PASSWORD@$DATABASE_BACKUP_HOST:$DATABASE_BACKUP_PORT/$DATABASE_BACKUP_NAME --file=/var/lib/postgresql/data/db-$(date +%d).backup
