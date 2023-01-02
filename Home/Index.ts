379478a3d2930b2a659ce2d559588c1dee0764ea
Simon Larsen <simonlarsen@oneuptime.com>
2023-01-02 14:18:59 +0000

    fix lint

--- Home/Index.ts
+++ Home/Index.ts
@@ -76,12 +76,9 @@ app.get(
     }
 );
 
-app.get(
-    '/status',
-    (_req: ExpressRequest, res: ExpressResponse) => {
-        res.status(200).send({"status": "ok"})
-    }
-);
+app.get('/status', (_req: ExpressRequest, res: ExpressResponse) => {
+    res.status(200).send({ status: 'ok' });
+});
 
 app.get('/status-page', (_req: ExpressRequest, res: ExpressResponse) => {
     res.redirect('/product/public-status-page');
