4ac717bd3da38acce9081b5bfd318cf60623fe29
Simon Larsen <simonlarsen@oneuptime.com>
2023-01-06 10:21:08 +0000

    fix err

--- Common/Types/API/HTTPErrorResponse.ts
+++ Common/Types/API/HTTPErrorResponse.ts
@@ -8,16 +8,16 @@ export default class HTTPErrorResponse extends HTTPResponse<JSONObject> {
             return '';
         }
 
-        if (!this.data['error']) {
-            return '';
-        }
-
         if (this.data['data'] && Typeof.String === this.data['data']) {
             return this.data['data'] as string;
         }
 
         if (this.data['message'] && Typeof.String === this.data['message']) {
             return this.data['message'] as string;
+        }
+
+        if (!this.data['error']) {
+            return '';
         }
 
         return this.data['error'] as string;
