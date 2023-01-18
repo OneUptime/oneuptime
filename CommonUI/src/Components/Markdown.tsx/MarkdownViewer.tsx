3f5e61fecbd0353e4c64e981f80c0e7b147f2808
Simon Larsen <simonlarsen@oneuptime.com>
2023-01-18 15:56:59 +0000

    fix markdown view

--- CommonUI/src/Components/Markdown.tsx/MarkdownViewer.tsx
+++ CommonUI/src/Components/Markdown.tsx/MarkdownViewer.tsx
@@ -15,9 +15,18 @@ const MarkdownViewer: FunctionComponent<ComponentProps> = (
 ): ReactElement => {
     return (
         <div>
-            <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                {props.text}
-            </ReactMarkdown>
+            <ReactMarkdown
+                components={{
+                    // Map `h1` (`# heading`) to use `h2`s.
+                    h1: ({ node, ...props }) => <h1 className='text-4xl font-bold' {...props} />,
+                    h2: ({node, ...props}) => <h1 className='text-3xl font-bold' {...props} />,
+                    h3: ({node, ...props}) => <h1 className='text-2xl font-bold' {...props} />,
+                    h4: ({ node, ...props }) => <h1 className='text-xl font-bold' {...props} />,
+                    h5: ({node, ...props}) => <h1 className='text-lg font-bold' {...props} />,
+                    h6: ({node, ...props}) => <h1 className='text-base font-bold' {...props} />,
+                  }}
+                children={props.text} remarkPlugins={[remarkGfm]} />
+            
         </div>
     );
 };
