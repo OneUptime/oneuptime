7d5642860d26efa2f589a4591faa20818fc01ef3
Simon Larsen <simonlarsen@oneuptime.com>
2023-01-31 09:40:57 +0000

    make db backups options work

--- backup.sh
+++ backup.sh
@@ -8,9 +8,17 @@ echo "Starting backup...."
 
 git pull
 
+# Backup as SQL, CANNOT be used with pg_reestore.
 sudo docker run --net=host --rm \
 --env-file config.env \
 --volume=$(pwd)$DATABASE_BACKUP_DIRECTORY:/var/lib/postgresql/data \
-postgres:latest /usr/bin/pg_dump -Fc --dbname=postgresql://$DATABASE_BACKUP_USERNAME:$DATABASE_BACKUP_PASSWORD@$DATABASE_BACKUP_HOST:$DATABASE_BACKUP_PORT/$DATABASE_BACKUP_NAME --file=/var/lib/postgresql/data/db-$(date +%d).backup
+postgres:latest /usr/bin/pg_dump --format=plain --clean --create --dbname=postgresql://$DATABASE_BACKUP_USERNAME:$DATABASE_BACKUP_PASSWORD@$DATABASE_BACKUP_HOST:$DATABASE_BACKUP_PORT/$DATABASE_BACKUP_NAME --file=/var/lib/postgresql/data/db-$(date +%d).sql
+
+# Backup as Tar can be used with pg_restore
+sudo docker run --net=host --rm \
+--env-file config.env \
+--volume=$(pwd)$DATABASE_BACKUP_DIRECTORY:/var/lib/postgresql/data \
+postgres:latest /usr/bin/pg_dump --format=tar --clean --create --dbname=postgresql://$DATABASE_BACKUP_USERNAME:$DATABASE_BACKUP_PASSWORD@$DATABASE_BACKUP_HOST:$DATABASE_BACKUP_PORT/$DATABASE_BACKUP_NAME --file=/var/lib/postgresql/data/db-$(date +%d).tar
+
 
 echo "Backup completed successfully!"
