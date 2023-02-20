38aa085eb1a4a0576fdf58c7c1a2cd4f4479c935
Simon Larsen <simonlarsen@oneuptime.com>
2023-02-20 19:43:29 +0000

    add trigger types.

---
+++ CommonServer/Types/Workflow/Components/OnTriggerBaseModel.ts
@@ -0,0 +1,90 @@
+import BaseModel from 'Common/Models/BaseModel';
+import BadDataException from 'Common/Types/Exception/BadDataException';
+import ObjectID from 'Common/Types/ObjectID';
+import ComponentMetadata from 'Common/Types/Workflow/Component';
+import DatabaseService from '../../../Services/DatabaseService';
+import { ExpressRequest, ExpressResponse } from '../../../Utils/Express';
+import Response from '../../../Utils/Response';
+import ComponentCode, {
+    ExecuteWorkflowType,
+    InitProps,
+} from '../ComponentCode';
+import BaseModelComponents from "Common/Types/Workflow/Components/BaseModel";
+import Text from 'Common/Types/Text';
+import WorkflowService from '../../../Services/WorkflowService';
+import LIMIT_MAX from 'Common/Types/Database/LimitMax';
+import Workflow from 'Model/Models/Workflow';
+import ClusterKeyAuthorization from '../../../Middleware/ClusterKeyAuthorization';
+
+export default class OnTriggerBaseModel<TBaseModel extends BaseModel> extends ComponentCode {
+    public constructor(modelService: DatabaseService<TBaseModel>, type: string) {
+        super();
+
+        const BaseModelComponent: ComponentMetadata | undefined = BaseModelComponents.getComponents(modelService.getModel()).find((i: ComponentMetadata) => {
+            return i.id === `${Text.pascalCaseToDashes(modelService.getModel().tableName!)}-${type}`;
+        });
+
+        if (!BaseModelComponent) {
+            throw new BadDataException('On Create trigger component for ' + modelService.getModel().tableName + " not found.");
+        }
+        this.setMetadata(BaseModelComponent);
+    }
+
+    public override async init(props: InitProps): Promise<void> {
+        props.router.get(
+            `/model/:projectId/${this.getMetadata().id}`,
+            ClusterKeyAuthorization.isAuthorizedServiceMiddleware, async (req: ExpressRequest, res: ExpressResponse) => {
+                await this.initTrigger(req, res, props);
+            }
+        );
+
+        props.router.post(
+            `/model/:projectId/${this.getMetadata().id}`,
+            ClusterKeyAuthorization.isAuthorizedServiceMiddleware, async (req: ExpressRequest, res: ExpressResponse) => {
+                await this.initTrigger(req, res, props);
+            }
+        );
+    }
+
+    public async initTrigger(
+        req: ExpressRequest,
+        res: ExpressResponse,
+        props: InitProps
+    ): Promise<void> {
+
+        // get all the enabled workflows with this trigger. 
+
+        const workflows: Array<Workflow> = await WorkflowService.findBy({
+            query: {
+                triggerId: this.getMetadata().id,
+                projectId: new ObjectID(req.params['projectId'] as string),
+                isEnabled: true
+            },
+            props: {
+                isRoot: true,
+            },
+            limit: LIMIT_MAX,
+            skip: 0,
+            select: {
+                _id: true
+            }
+        });
+
+        const promises = []; 
+
+        for (const workflow of workflows) {
+            /// Run Graph.
+
+            const executeWorkflow: ExecuteWorkflowType = {
+                workflowId: workflow.id!,
+                returnValues: req.body,
+            };
+
+            promises.push(props.executeWorkflow(executeWorkflow));
+        }
+
+        await Promise.all(promises);
+
+        Response.sendJsonObjectResponse(req, res, { status: 'Triggered' });
+    }
+}
