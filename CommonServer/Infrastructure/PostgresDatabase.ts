300d455af0cf6165744e0a711c7ff581b8817d30
Simon Larsen <simonlarsen@oneuptime.com>
2023-03-02 14:48:44 +0000

    postgres

--- CommonServer/Infrastructure/PostgresDatabase.ts
+++ CommonServer/Infrastructure/PostgresDatabase.ts
@@ -1,6 +1,7 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import logger from '../Utils/Logger';
 import { dataSourceOptions, testDataSourceOptions } from './PostgresConfig';
+import Sleep from "Common/Types/Sleep"
 
 export default class Database {
     private dataSource!: DataSource | null;
@@ -24,16 +25,41 @@ export default class Database {
     public async connect(
         dataSourceOptions: DataSourceOptions
     ): Promise<DataSource> {
+
+        let retry = 0;
+
         try {
-            const PostgresDataSource: DataSource = new DataSource(
-                dataSourceOptions
-            );
-            const dataSource: DataSource =
-                await PostgresDataSource.initialize();
-            logger.info('Posgres Database Connected');
-            this.dataSource = dataSource;
-            return dataSource;
+
+            const connectToDatabase: Function = async (): Promise<DataSource> => {
+
+                try {
+                    const PostgresDataSource: DataSource = new DataSource(
+                        dataSourceOptions
+                    );
+                    const dataSource: DataSource =
+                        await PostgresDataSource.initialize();
+                    logger.info('Posgres Database Connected');
+                    this.dataSource = dataSource;
+                    return dataSource;
+                } catch (err) {
+                    if(retry<3){
+                        logger.info("Cannot connect to Postgres. Retrying again in 5 second");
+                        // sleep for 5 seconds. 
+
+                        await Sleep.sleep(5000);
+
+                        retry++; 
+                        return await connectToDatabase();
+                    }else{
+                        throw err; 
+                    }
+                }
+            }
+
+            return await connectToDatabase();
+
         } catch (err) {
+
             logger.error('Posgres Database Connection Failed');
             logger.error(err);
             throw err;
