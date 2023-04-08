f0dfb711702927a056c8c74787fe3881d7c7d2a3
aphilas <31406314+aphilas@users.noreply.github.com>
2023-04-08 15:32:28 +0300

    Add test Common/Types/ListData

---
+++ Common/Tests/Types/ListData.test.ts
@@ -0,0 +1,42 @@
+import { JSONArray, JSONObject } from '../../Types/JSON';
+import ListData from '../../Types/ListData';
+import PositiveNumber from '../../Types/PositiveNumber';
+
+describe('ListData', () => {
+    it('should create a ListData instance', () => {
+        const data: JSONArray = [{ foo: 1 }, { foo: 2 }];
+        const skip: PositiveNumber = new PositiveNumber(0);
+        const count: PositiveNumber = new PositiveNumber(0);
+        const limit: PositiveNumber = new PositiveNumber(0);
+
+        const listData: ListData = new ListData({
+            data,
+            count,
+            skip,
+            limit,
+        });
+        expect(listData).toBeInstanceOf(ListData);
+
+        expect(listData.count.toNumber()).toBe(count.toNumber());
+        expect(listData.skip.toNumber()).toBe(skip.toNumber());
+        expect(listData.limit.toNumber()).toBe(limit.toNumber());
+    });
+
+    describe('toJSON', () => {
+        it('converts ListData to JSONObject', () => {
+            const listData: ListData = new ListData({
+                data: [{ foo: 'bar' }],
+                count: new PositiveNumber(0),
+                skip: new PositiveNumber(0),
+                limit: new PositiveNumber(0),
+            });
+
+            const jsonObject: JSONObject = listData.toJSON();
+
+            expect(jsonObject['data']).toEqual([{ foo: 'bar' }]);
+            expect(jsonObject['count']).toEqual(0);
+            expect(jsonObject['skip']).toEqual(0);
+            expect(jsonObject['limit']).toEqual(0);
+        });
+    });
+});
