1d3641c74b4a6951f23d203e0a18cfd07a6aa5a4
Simon Larsen <simonlarsen@oneuptime.com>
2023-04-27 21:04:20 +0100

    fix side menu syle

--- CommonUI/src/Components/SideMenu/SideMenuItem.tsx
+++ CommonUI/src/Components/SideMenu/SideMenuItem.tsx
@@ -21,12 +21,79 @@ const SideMenuItem: FunctionComponent<ComponentProps> = (
 ) => {
     const badgeClasName: string = `p-1 rounded-full pr-3 pl-3 text-white  text-sm`;
 
+    let linkClassName: string = `text-gray-500 hover:text-gray-900 hover:bg-gray-100 group rounded-md px-3 py-2 flex items-center text-sm font-medium`;
+
+    if(Navigation.isOnThisPage(props.link.to)){
+        linkClassName = `bg-gray-100 text-indigo-600 hover:bg-white group rounded-md px-3 py-2 flex items-center text-sm font-medium`;
+    }
+
+    if(props.badge && props.badge > 0){
+        if(props.badgeType === BadgeType.DANGER){
+            linkClassName = `text-red-400 hover:text-red-600 hover:bg-gray-100 group rounded-md px-3 py-2 flex items-center text-sm font-medium`;
+
+            if(Navigation.isOnThisPage(props.link.to)){
+                linkClassName = `bg-gray-100 text-red-600 hover:bg-white group rounded-md px-3 py-2 flex items-center text-sm font-medium`;
+            }
+
+        }
+
+        if(props.badgeType === BadgeType.WARNING){
+            linkClassName = `text-yellow-400 hover:text-yellow-600 hover:bg-gray-100 group rounded-md px-3 py-2 flex items-center text-sm font-medium`;
+
+            if(Navigation.isOnThisPage(props.link.to)){
+                linkClassName = `bg-gray-100 text-yellow-600 hover:bg-white group rounded-md px-3 py-2 flex items-center text-sm font-medium`;
+            }
+        }
+
+        if(props.badgeType === BadgeType.SUCCESS){
+            linkClassName = `text-emerald-400 hover:text-emerald-600 hover:bg-gray-100 group rounded-md px-3 py-2 flex items-center text-sm font-medium`;
+
+            if(Navigation.isOnThisPage(props.link.to)){
+                linkClassName = `bg-gray-100 text-emerald-600 hover:bg-white group rounded-md px-3 py-2 flex items-center text-sm font-medium`;
+            }
+        }
+    }
+
+    let iconClassName = "text-gray-400 group-hover:text-gray-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6";
+
+    if(Navigation.isOnThisPage(props.link.to)){
+        iconClassName = "text-indigo-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6";
+    }
+
+
+    if(props.badge && props.badge > 0){
+        if(props.badgeType === BadgeType.DANGER){
+            iconClassName = `text-red-400 group-hover:text-red-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6`;
+
+            if(Navigation.isOnThisPage(props.link.to)){
+                iconClassName = `text-red-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6`;
+            }
+
+        }
+
+        if(props.badgeType === BadgeType.WARNING){
+            iconClassName = `text-yellow-400 group-hover:text-yellow-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6`;
+
+            if(Navigation.isOnThisPage(props.link.to)){
+                iconClassName = `text-yellow-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6`;
+            }
+        }
+
+        if(props.badgeType === BadgeType.SUCCESS){
+            iconClassName = `text-emerald-400 group-hover:text-emerald-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6`;
+
+            if(Navigation.isOnThisPage(props.link.to)){
+                iconClassName = `text-emerald-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6`;
+            }
+        }
+    }
+
+
+
     return (
         <UILink
             className={`${props.className ? props.className : ''}  ${
-                Navigation.isOnThisPage(props.link.to)
-                    ? 'bg-gray-100 text-indigo-600 hover:bg-white group rounded-md px-3 py-2 flex items-center text-sm font-medium'
-                    : 'text-gray-500 hover:text-gray-900 hover:bg-gray-100 group rounded-md px-3 py-2 flex items-center text-sm font-medium'
+                linkClassName
             } flex justify-between`}
             to={props.link.to}
         >
@@ -35,9 +102,7 @@ const SideMenuItem: FunctionComponent<ComponentProps> = (
                     <>
                         <Icon
                             className={
-                                Navigation.isOnThisPage(props.link.to)
-                                    ? 'text-indigo-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6'
-                                    : 'text-gray-400 group-hover:text-gray-500 flex-shrink-0 -ml-1 mr-3 h-6 w-6'
+                                iconClassName
                             }
                             icon={props.icon}
                         />
