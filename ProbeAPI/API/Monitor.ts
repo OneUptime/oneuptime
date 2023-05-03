c959b0845f69c20121e77fe7e7867238b5d1a727
Simon Larsen <simonlarsen@oneuptime.com>
2023-05-03 21:13:00 +0100

    add probe auth

---
+++ ProbeAPI/API/Monitor.ts
@@ -0,0 +1,77 @@
+import Express, {
+    ExpressRequest,
+    ExpressResponse,
+    ExpressRouter,
+    NextFunction,
+} from 'CommonServer/Utils/Express';
+import ObjectID from 'Common/Types/ObjectID';
+import Response from 'CommonServer/Utils/Response';
+import BadDataException from 'Common/Types/Exception/BadDataException';
+import ProbeService from 'CommonServer/Services/ProbeService';
+import OneUptimeDate from 'Common/Types/Date';
+import { JSONObject } from 'Common/Types/JSON';
+import Probe from 'Model/Models/Probe';
+
+const router: ExpressRouter = Express.getRouter();
+
+router.post(
+    '/monitor/list',
+    async (
+        req: ExpressRequest,
+        res: ExpressResponse,
+        next: NextFunction
+    ): Promise<void> => {
+        try {
+            const data: JSONObject = req.body;
+
+            if (!data['probeId'] || !data['probeKey']) {
+                return Response.sendErrorResponse(
+                    req,
+                    res,
+                    new BadDataException('ProbeId or ProbeKey is missing')
+                );
+            }
+
+            const probeId: ObjectID = new ObjectID(data['probeId'] as string);
+
+            const probeKey: string = data['probeKey'] as string;
+
+            const probe: Probe | null = await ProbeService.findOneBy({
+                query: {
+                    _id: probeId.toString(),
+                    key: probeKey,
+                },
+                select: {
+                    _id: true,
+                },
+                props: {
+                    isRoot: true,
+                },
+            });
+
+            if (!probe) {
+                return Response.sendErrorResponse(
+                    req,
+                    res,
+                    new BadDataException('Invalid Probe ID or Probe Key')
+                );
+            }
+
+            await ProbeService.updateOneById({
+                id: probeId,
+                data: {
+                    lastAlive: OneUptimeDate.getCurrentDate(),
+                },
+                props: {
+                    isRoot: true,
+                },
+            });
+
+            return Response.sendEmptyResponse(req, res);
+        } catch (err) {
+            return next(err);
+        }
+    }
+);
+
+export default router;
