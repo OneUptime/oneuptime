4fe92a1096aea6fb27beffaa9d5cdd8c8028bfc7
Simon Larsen <simonlarsen@oneuptime.com>
2023-05-03 17:09:20 +0100

    fix monitor probe service

--- CommonServer/Services/MonitorProbeService.ts
+++ CommonServer/Services/MonitorProbeService.ts
@@ -10,11 +10,12 @@ export class Service extends DatabaseService<Model> {
     }
 
     protected override async onBeforeCreate(createBy: CreateBy<Model>): Promise<OnCreate<Model>> {
-        if(createBy.data.monitorId && createBy.data.probeId){
+
+        if((createBy.data.monitorId || createBy.data.monitor) && (createBy.data.probeId || createBy.data.probe)){
             const monitorProbe = await this.findOneBy({
                 query: {
-                    monitorId: createBy.data.monitorId,
-                    probeId: createBy.data.probeId
+                    monitorId: createBy.data.monitorId! || createBy.data.monitor?.id!,
+                    probeId: createBy.data.probeId! || createBy.data.probe?.id!
                 },
                 select: {
                     _id: true
