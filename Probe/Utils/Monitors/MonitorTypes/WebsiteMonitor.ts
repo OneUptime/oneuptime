2555e22bdfe4a48cc4901a43df788dff1c03b6e5
Simon Larsen <simonlarsen@oneuptime.com>
2023-05-08 22:08:51 +0100

    add website monitor

--- Probe/Utils/Monitors/MonitorTypes/WebsiteMonitor.ts
+++ Probe/Utils/Monitors/MonitorTypes/WebsiteMonitor.ts
@@ -1,28 +1,58 @@
 import URL from 'Common/Types/API/URL';
 import Headers from 'Common/Types/API/Headers';
-import NotImplementedException from 'Common/Types/Exception/NotImplementedException';
 import PositiveNumber from 'Common/Types/PositiveNumber';
 import StatusCode from 'Common/Types/API/StatusCode';
 import HTML from 'Common/Types/Html';
-import { SslResponse } from './SslMonitor';
+import API from 'Common/Utils/API';
+import { HttpProtocol } from 'CommonServer/Config';
+import Protocol from 'Common/Types/API/Protocol';
 
 export interface WebsiteResponse {
     url: URL;
     requestHeaders: Headers;
     isSecure: boolean;
-    reponseTimeInMS: PositiveNumber;
-    statusCode: StatusCode;
-    responseBody: HTML;
+    responseTimeInMS: PositiveNumber;
+    statusCode: number;
+    responseBody: string;
     responseHeaders: Headers;
     isOnline: boolean;
-    SSL: SslResponse;
 }
 
-export default class Website {
-    public static async fetch(
-        _url: URL,
-        _requestHeaders: Headers
+export default class PingMonitor {
+    public static async ping(
+        url: URL
     ): Promise<WebsiteResponse> {
-        throw new NotImplementedException();
+        try {
+            const startTime: [number, number] = process.hrtime();
+            const result = await API.get(url, {}, {});
+            const endTime: [number, number] = process.hrtime(startTime);
+            const responseTimeInMS: PositiveNumber = new PositiveNumber(
+                (endTime[0] * 1000000000 + endTime[1]) / 1000000
+            );
+
+            return {
+                url: url,
+                requestHeaders: {},
+                isOnline: true,
+                isSecure: url.protocol === Protocol.HTTPS,
+                responseTimeInMS: responseTimeInMS,
+                statusCode: result.statusCode,
+                responseBody: result.data.toString(),
+                responseHeaders: result.headers
+
+            }
+        } catch (err) {
+            return {
+                url: url,
+                isOnline: false,
+                requestHeaders: {},
+                isSecure: url.protocol === Protocol.HTTPS,
+                responseTimeInMS: new PositiveNumber(0),
+                statusCode: 0,
+                responseBody: '',
+                responseHeaders: {}
+            }
+        }
     }
 }
+
