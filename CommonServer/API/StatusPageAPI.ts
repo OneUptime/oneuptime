73bf0a26c74b9ecdab748d3e6666593909f512e9
Simon Larsen <simonlarsen@oneuptime.com>
2023-05-16 13:38:43 +0100

    add more fields

--- CommonServer/API/StatusPageAPI.ts
+++ CommonServer/API/StatusPageAPI.ts
@@ -686,6 +686,8 @@ export default class StatusPageAPI extends BaseAPI<
                                 title: true,
                                 description: true,
                                 _id: true,
+                                showAnnouncementAt: true, 
+                                endAnnouncementAt: true
                             },
                             skip: 0,
                             limit: LIMIT_PER_PROJECT,
