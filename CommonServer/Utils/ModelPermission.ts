38000fdbec01efefaf0d16833049cf2683693121
Simon Larsen <simonlarsen@oneuptime.com>
2023-06-09 16:56:26 +0100

    make api error message more descriptive.

--- CommonServer/Utils/ModelPermission.ts
+++ CommonServer/Utils/ModelPermission.ts
@@ -902,7 +902,11 @@ export default class ModelPermission {
             if (!canReadOnTheseColumns.columns.includes(key)) {
                 if (!tableColumns.includes(key)) {
                     throw new BadDataException(
-                        `Invalid select clause. Cannnot select on "${key}". This column does not exist on ${model.singularName}. Here are the columns you can select on: ${tableColumns.join(", ")}`
+                        `Invalid select clause. Cannnot select on "${key}". This column does not exist on ${
+                            model.singularName
+                        }. Here are the columns you can select on: ${tableColumns.join(
+                            ', '
+                        )}`
                     );
                 }
 
