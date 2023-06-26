908c108116c0919efec7fa7e952daa3e388eb2fd
Simon Larsen <simonlarsen@oneuptime.com>
2023-06-26 11:59:03 +0100

    add log statements to domain verification

--- CommonServer/Types/Domain.ts
+++ CommonServer/Types/Domain.ts
@@ -1,5 +1,6 @@
 import DomainCommon from 'Common/Types/Domain';
 import dns from 'dns';
+import logger from '../Utils/Logger';
 
 export default class Domain extends DomainCommon {
     public static verifyTxtRecord(
@@ -16,6 +17,11 @@ export default class Domain extends DomainCommon {
                     if (err) {
                         return reject(err);
                     }
+
+                    logger.info("Verify TXT Record");
+                    logger.info("Domain "+domain.toString());
+                    logger.info("Data: ")
+                    logger.info(data);
 
                     let isVerified: boolean = false;
                     for (const item of data) {
