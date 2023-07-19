788e8363efc54340e2529c768719dc6a0ee040c3
Simon Larsen <simonlarsen@oneuptime.com>
2023-07-19 20:29:29 +0100

    remove self signed certs generation

--- configure.sh
+++ configure.sh
@@ -146,16 +146,6 @@ fi
 
 cd oneuptime
 
-# Generate Self Signed SSL certificate. 
-
-CERT=./Certs/ServerCerts/Cert.crt
-if test -f "$CERT"; then
-    echo "SSL Certificate exists. Skipping generating a new one."
-else
-    echo "SSL Certificate not found. Generating a new certificate."
-    openssl req -new -x509 -nodes -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=example.com" -out ./Certs/ServerCerts/Cert.crt -keyout ./Certs/ServerCerts/Key.key -days 99999
-fi
-
 # Create .env file if it does not exist. 
 touch config.env
 
