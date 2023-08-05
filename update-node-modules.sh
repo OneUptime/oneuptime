fe44b36d6abd71a06ad2ae4240a11d7afb0f743d
Simon Larsen <simonlarsen@oneuptime.com>
2023-08-05 22:01:37 +0100

    update packages

--- update-node-modules.sh
+++ update-node-modules.sh
@@ -5,7 +5,7 @@ npm install -g npm-check-updates
 # Loop through all the directories. 
 for d in */ ; do
     cd $d || echo "Cannot cd into $d"
-    ncu -u || echo "Cannot update $d"
-    npm install || echo "Cannot npm install $d"
+    rm package-lock.json && ncu -u || echo "Cannot update $d"
+    npm install || npm install --force || echo "Cannot npm install $d"
     cd .. || echo "Cannot cd out"
 done
