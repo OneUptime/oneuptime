bba2fa2ed33388d3f007dffe002e5d08845a8825
Simon Larsen <simonlarsen@oneuptime.com>
2023-08-08 14:06:08 +0100

    add release

--- Scripts/PaymentProvider/CouponCodeGenerator.ts
+++ Scripts/PaymentProvider/CouponCodeGenerator.ts
@@ -1,4 +1,4 @@
-// To run this script: 
+// To run this script:
 // export $(grep -v '^#' config.env | xargs) && ts-node ./Scripts/PaymentProvider/CouponCodeGenerator.ts > coupons.csv
 
 import BillingService from 'CommonServer/Services/BillingService';
@@ -7,10 +7,10 @@ import Sleep from 'Common/Types/Sleep';
 const main: Function = async () => {
     for (let i = 0; i < 2000; i++) {
         const code = await BillingService.generateCouponCode({
-            name: "Name",
+            name: 'Name',
             percentOff: 100,
             durationInMonths: 12,
-            maxRedemptions: 1
+            maxRedemptions: 1,
         });
         console.log(code);
         await Sleep.sleep(50);
