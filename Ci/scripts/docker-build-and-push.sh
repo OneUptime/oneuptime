26aaee83b8581e10439bf1e6eb228e4edb218f5b
Simon Larsen <simonlarsen@oneuptime.com>
2023-09-29 10:10:57 +0100

    remove helm chart

--- Ci/scripts/docker-build-and-push.sh
+++
@@ -1,6 +0,0 @@
-cd $1
-echo "Building $1"
-sudo docker build -t oneuptime/$1:$2 .
-echo "Pushing $1"
-sudo docker push oneuptime/$1:$2
-cd ..
--- Ci/scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-# This will download, build and package docker containers.
-echo "RUNNING COMMAND: chmod +x ./ci/docker-build-all-and-push.sh"
-chmod +x ./ci/scripts/docker-build-all-and-push.sh
-./ci/scripts/docker-build-all-and-push.sh test
-# Setup Kubernetes Cluster
-chmod +x ./ci/scripts/setup-cluster.sh
-echo "RUNNING COMMAND: ./ci/setup-cluster.sh"
-./ci/scripts/setup-cluster.sh
