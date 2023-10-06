9fec234b078c76b195f9dfe3144c513ccdf8e6f2
yashug <gosulayaswanth2@gmail.com>
2023-10-06 19:45:05 +0530

    Rewrites ErrorBoundary as class Component to catch errors

--- CommonUI/src/Components/ErrorBoundary.tsx
+++ CommonUI/src/Components/ErrorBoundary.tsx
@@ -1,46 +1,54 @@
-import Exception from 'Common/Types/Exception/Exception';
-import React, { FunctionComponent, ReactElement, useState } from 'react';
+import React, { Component, ReactNode } from 'react';
 
 export interface ComponentProps {
-    children: ReactElement;
+    children?: ReactNode;
 }
 
-const ErrorBoundary: FunctionComponent<ComponentProps> = (
-    props: ComponentProps
-): ReactElement => {
-    const [error, setError] = useState<string | null>(null);
+interface ComponentState {
+    hasError: boolean;
+}
 
-    try {
-        if (!error) {
-            return props.children;
-        }
-    } catch (e) {
-        const exception: Exception = e as Exception;
-        setError(exception.message);
+class ErrorBoundary extends Component<ComponentProps, ComponentState> {
+    public constructor(props: ComponentProps) {
+        super(props);
+        this.state = { hasError: false };
     }
 
-    return (
-        <div
-            id="app-loading"
-            style={{
-                position: 'fixed',
-                top: '0',
-                bottom: '0',
-                left: '0',
-                right: '0',
-                backgroundColor: '#fdfdfd',
-                zIndex: '999',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <div>
-                An unexpected error has occurred. Please reload the page to
-                continue
-            </div>
-        </div>
-    );
-};
+    public static getDerivedStateFromError(_: Error): ComponentState {
+        return { hasError: true };
+    }
+
+    // TODO: log the error to an error reporting service
+    // public componentDidCatch?(error: Error, errorInfo: ErrorInfo) {}
+
+    public override render(): ReactNode {
+        if (this.state.hasError) {
+            return (
+                <div
+                    id="app-loading"
+                    style={{
+                        position: 'fixed',
+                        top: '0',
+                        bottom: '0',
+                        left: '0',
+                        right: '0',
+                        backgroundColor: '#fdfdfd',
+                        zIndex: '999',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div>
+                        An unexpected error has occurred. Please reload the page
+                        to continue
+                    </div>
+                </div>
+            );
+        }
+
+        return this.props.children;
+    }
+}
 
 export default ErrorBoundary;
