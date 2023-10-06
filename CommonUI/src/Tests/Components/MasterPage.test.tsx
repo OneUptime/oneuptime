9ff18d6df52c4f1b002c7a07e056e39f9f5c0a84
yashug <gosulayaswanth2@gmail.com>
2023-10-06 21:14:34 +0530

    Adds test cases for MasterPage & TopSection

---
+++ CommonUI/src/Tests/Components/MasterPage.test.tsx
@@ -0,0 +1,62 @@
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MasterPage, {
+    ComponentProps,
+} from '../../Components/MasterPage/MasterPage';
+
+describe('MasterPage', () => {
+    const defaultProps: ComponentProps = {
+        children: <div>Children</div>,
+        isLoading: false,
+        error: '',
+    };
+
+    it('should render correctly', () => {
+        render(<MasterPage {...defaultProps} />);
+
+        const children: HTMLElement = screen.getByText('Children');
+        expect(children).toBeInTheDocument();
+    });
+
+    it('should render correctly with isLoading', () => {
+        render(<MasterPage {...defaultProps} isLoading />);
+
+        const loader: HTMLElement = screen.getByRole('bar-loader');
+        expect(loader).toBeInTheDocument();
+    });
+
+    it('should render correctly with error', () => {
+        const error: string = 'error';
+        render(<MasterPage {...defaultProps} error={error} />);
+
+        const errorElement: HTMLElement = screen.getByText(error);
+        expect(errorElement).toBeInTheDocument();
+    });
+
+    it('should render correctly with server error', () => {
+        const error: string = 'Server Error';
+        render(<MasterPage {...defaultProps} error={error} />);
+
+        const errorElement: HTMLElement = screen.getByText(
+            'Network Error: Please reload the page and try again.'
+        );
+        expect(errorElement).toBeInTheDocument();
+    });
+
+    it('should render correctly with footer', () => {
+        const footer: string = 'footer';
+        render(<MasterPage {...defaultProps} footer={<div>footer</div>} />);
+
+        const footerElement: HTMLElement = screen.getByText(footer);
+        expect(footerElement).toBeInTheDocument();
+    });
+
+    it('should render correctly with makeTopSectionUnstick', () => {
+        render(<MasterPage {...defaultProps} makeTopSectionUnstick />);
+
+        const topSection: HTMLElement =
+            screen.getByRole('banner').parentElement!;
+        expect(topSection).not.toHaveClass('sticky');
+    });
+});
