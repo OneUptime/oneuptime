43d9046eab317bfd8dedd14cec71518cf6de8ebd
Jordan Jones <jordpjones@gmail.com>
2023-10-11 11:29:03 -0700

    chore(utils): add method to verify if string is uuid

--- Common/Utils/UUID.ts
+++ Common/Utils/UUID.ts
@@ -1,7 +1,15 @@
-import { v1 as uuidv1 } from 'uuid';
+import {
+    v1 as uuidv1,
+    validate as uuidValidate,
+    version as uuidVersion,
+} from 'uuid';
 
 export default class UUID {
     public static generate(): string {
         return uuidv1();
+    }
+
+    public static validate(uuid: string, version: number = 1): boolean {
+        return uuidValidate(uuid) && uuidVersion(uuid) === version;
     }
 }
