1945bbfd456171d5c83e1da8ade47cdfdc0bc2d3
Simon Larsen <simonlarsen@oneuptime.com>
2023-10-14 16:45:17 +0100

    fix llama compile err

--- Llama/Index.ts
+++ Llama/Index.ts
@@ -1,8 +1,5 @@
-import { fileURLToPath } from "url";
 import path from "path";
 import { LlamaModel, LlamaContext, LlamaChatSession } from "node-llama-cpp";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const model = new LlamaModel({
     modelPath: path.join(__dirname, "ggml-model-q4_0.gguf")
@@ -11,16 +8,20 @@ const model = new LlamaModel({
 const context = new LlamaContext({ model });
 const session = new LlamaChatSession({ context });
 
+const init: Function = async () => {
+    const q1 = "Hi there, how are you?";
+    console.log("User: " + q1);
 
-const q1 = "Hi there, how are you?";
-console.log("User: " + q1);
-
-const a1 = await session.prompt(q1);
-console.log("AI: " + a1);
+    const a1 = await session.prompt(q1);
+    console.log("AI: " + a1);
 
 
-const q2 = "Summerize what you said";
-console.log("User: " + q2);
+    const q2 = "Summerize what you said";
+    console.log("User: " + q2);
 
-const a2 = await session.prompt(q2);
-console.log("AI: " + a2);
+    const a2 = await session.prompt(q2);
+    console.log("AI: " + a2);
+};
+
+
+init();
