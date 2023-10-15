805139055a4b286d897d7c476988ef27727f86ad
Simon Larsen <simonlarsen@oneuptime.com>
2023-10-15 21:04:58 +0100

    make llama work

--- Llama/app.py
+++ Llama/app.py
@@ -1,31 +1,24 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
+import transformers
+import torch
 
-def load_model(model_name):
-    """
-    Function to load the model and tokenizer.
-    """
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
-    return tokenizer, model
+model = "meta-llama/Llama-2-7b-chat-hf"
 
-def generate_text(input_text, tokenizer, model):
-    """
-    Function to generate text using the model.
-    """
-    inputs = tokenizer.encode(input_text, return_tensors='pt')
-    outputs = model.generate(inputs, max_length=250, num_return_sequences=3)
-    generated_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
-    return generated_text
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
 
-def main():
-    model_name = "allenai/longformer-base-4096"  # replace with your model name
-    tokenizer, model = load_model(model_name)
-    
-    input_text = "Hello, world!"  # replace with your input text
-    generated_text = generate_text(input_text, tokenizer, model)
-    
-    for i, text in enumerate(generated_text):
-        print(f"Generated text {i+1}: {text}")
-
-if __name__ == "__main__":
-    main()
+sequences = pipeline(
+    'Weather in London today?\n',
+    do_sample=True,
+    top_k=10,
+    num_return_sequences=1,
+    eos_token_id=tokenizer.eos_token_id,
+    max_length=200,
+)
+for seq in sequences:
+    print(f"Result: {seq['generated_text']}")
