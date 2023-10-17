262fffd9ffe5543208ec05d0ae2546367644f8c7
Simon Larsen <simonlarsen@oneuptime.com>
2023-10-17 12:48:46 +0100

    fix url

--- StatusPage/src/Utils/Config.ts
+++ StatusPage/src/Utils/Config.ts
@@ -1,21 +1,25 @@
-import { HOST, HTTP_PROTOCOL } from 'CommonUI/src/Config';
 import URL from 'Common/Types/API/URL';
 import Route from 'Common/Types/API/Route';
+import Protocol from 'Common/Types/API/Protocol';
+
+const PROTOCOL: Protocol = window.location.protocol.includes('https')
+    ? Protocol.HTTPS
+    : Protocol.HTTP;
 
 export const STATUS_PAGE_API_URL: URL = new URL(
-    HTTP_PROTOCOL,
-    HOST,
+    PROTOCOL,
+    window.location.host,
     new Route('/status-page-api')
 );
 
 export const STATUS_PAGE_SSO_API_URL: URL = new URL(
-    HTTP_PROTOCOL,
-    HOST,
+    PROTOCOL,
+    window.location.host,
     new Route('/status-page-sso-api')
 );
 
 export const STATUS_PAGE_IDENTITY_API_URL: URL = new URL(
-    HTTP_PROTOCOL,
-    HOST,
+    PROTOCOL,
+    window.location.host,
     new Route('/status-page-identity-api')
 );
