a1c6121bee48e9766f7ffd41924f35ac7aa55805
Hammad <hammad@spekit.co>
2023-10-23 19:15:10 +0500

    tests UI by querying key elements and removes snapshots

--- CommonUI/src/Tests/Components/DuplicateModel.test.tsx
+++ CommonUI/src/Tests/Components/DuplicateModel.test.tsx
@@ -29,7 +29,7 @@ class TestModel extends BaseModel {
     public changeThis?: string = 'original';
 }
 
-jest.mock('../Utils/ModelAPI/ModelAPI', () => {
+jest.mock('../../Utils/ModelAPI/ModelAPI', () => {
     return {
         getItem: jest
             .fn()
@@ -60,7 +60,7 @@ jest.mock('../Utils/ModelAPI/ModelAPI', () => {
     };
 });
 
-jest.mock('../Utils/Navigation', () => {
+jest.mock('../../Utils/Navigation', () => {
     return {
         navigate: jest.fn(),
     };
@@ -87,7 +87,18 @@ describe('DuplicateModel', () => {
                 fieldsToChange={fieldsToChange}
             />
         );
-        expect(document.body).toMatchSnapshot();
+        expect(
+            document.querySelector('#payment-details-heading')?.textContent
+        ).toBe('Duplicate Foo');
+        expect(document.querySelector('h2')?.textContent).toBe('Duplicate Foo');
+        expect(
+            document.querySelector('p.mt-1.text-sm.text-gray-500')?.textContent
+        ).toBe(
+            'Duplicating this foo will create another foo exactly like this one.'
+        );
+        expect(document.querySelector('button')?.textContent).toBe(
+            'Duplicate Foo'
+        );
     });
     it('shows confirmation modal when duplicate button is clicked', () => {
         render(
@@ -102,7 +113,21 @@ describe('DuplicateModel', () => {
             name: 'Duplicate Foo',
         });
         fireEvent.click(button);
-        expect(screen.getByRole('dialog')).toMatchSnapshot();
+        expect(screen.getByRole('dialog')).toBeDefined();
+        expect(
+            screen.getByRole('dialog').querySelectorAll('h3')[0]?.textContent
+        ).toBe('Duplicate Foo');
+        expect(
+            screen.getByRole('dialog').querySelectorAll('h3')[1]?.textContent
+        ).toBe('Are you sure you want to duplicate this foo?');
+        expect(
+            screen.getByRole('dialog').querySelectorAll('button')[1]
+                ?.textContent
+        ).toBe('Duplicate Foo');
+        expect(
+            screen.getByRole('dialog').querySelectorAll('button')[2]
+                ?.textContent
+        ).toBe('Close');
     });
     it('duplicates item when confirmation button is clicked', async () => {
         const onDuplicateSuccess: (item: TestModel) => void = jest.fn();
@@ -140,7 +165,7 @@ describe('DuplicateModel', () => {
         });
         await waitFor(() => {
             return expect(
-                require('../Utils/Navigation').navigate
+                require('../../Utils/Navigation').navigate
             ).toBeCalledWith(new Route('/done/foobar'));
         });
     });
@@ -200,7 +225,18 @@ describe('DuplicateModel', () => {
             fireEvent.click(confirmationButton);
         });
         await screen.findByText('Duplicate Error');
-        expect(screen.getByRole('dialog')).toMatchSnapshot();
+        expect(
+            screen.getByRole('dialog').querySelector('h3')?.textContent
+        ).toBe('Duplicate Error');
+        expect(
+            screen
+                .getByRole('dialog')
+                .querySelector('.modal-body>div.text-gray-500.mt-5.text-sm')
+                ?.textContent
+        ).toBe('Error: Could not create Foo');
+        expect(
+            screen.getByRole('dialog').querySelector('button')?.textContent
+        ).toBe('Close');
     });
     it('handles item not found error correctly', async () => {
         const onDuplicateSuccess: (item: TestModel) => void = jest.fn();
@@ -231,7 +267,18 @@ describe('DuplicateModel', () => {
             fireEvent.click(confirmationButton);
         });
         await screen.findByText('Duplicate Error');
-        expect(screen.getByRole('dialog')).toMatchSnapshot();
+        expect(
+            screen.getByRole('dialog').querySelector('h3')?.textContent
+        ).toBe('Duplicate Error');
+        expect(
+            screen
+                .getByRole('dialog')
+                .querySelector('.modal-body>div.text-gray-500.mt-5.text-sm')
+                ?.textContent
+        ).toBe('Error: Could not find Foo with id foo');
+        expect(
+            screen.getByRole('dialog').querySelector('button')?.textContent
+        ).toBe('Close');
     });
     it('closes error dialog when close button is clicked', async () => {
         const onDuplicateSuccess: (item: TestModel) => void = jest.fn();
