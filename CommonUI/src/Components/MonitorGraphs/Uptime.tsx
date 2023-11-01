4b71a81f7c1bc13f7747694aff6574467b3163fc
Simon Larsen <simonlarsen@oneuptime.com>
2023-11-01 10:40:03 +0000

    fix fmt on uptime

--- CommonUI/src/Components/MonitorGraphs/Uptime.tsx
+++ CommonUI/src/Components/MonitorGraphs/Uptime.tsx
@@ -38,23 +38,28 @@ const MonitorUptimeGraph: FunctionComponent<ComponentProps> = (
      * @param {ObjectID} monitorId - The ID of the monitor for which events are to be fetched.
      * @returns {Array<MonitorEvent>} - An array of `MonitorEvent` objects.
      */
-    const getMonitorEventsForId: (monitorId: ObjectID) => Array<MonitorEvent> = (monitorId: ObjectID): Array<MonitorEvent> => {
+    const getMonitorEventsForId: (
+        monitorId: ObjectID
+    ) => Array<MonitorEvent> = (monitorId: ObjectID): Array<MonitorEvent> => {
         // Initialize an empty array to store the monitor events.
         const eventList: Array<MonitorEvent> = [];
 
-        const monitorEvents = props.items.filter((item) => item.monitorId?.toString() === monitorId.toString());
+        const monitorEvents: Array<MonitorStatusTimeline> = props.items.filter(
+            (item: MonitorStatusTimeline) => {
+                return item.monitorId?.toString() === monitorId.toString();
+            }
+        );
 
         // Loop through the items in the props object.
         for (let i: number = 0; i < monitorEvents.length; i++) {
-
             // If the current item is null or undefined, skip to the next iteration.
             if (!monitorEvents[i]) {
                 continue;
             }
 
-
             // Set the start date of the event to the creation date of the current item. If it doesn't exist, use the current date.
-            const startDate: Date = monitorEvents[i]!.createdAt || OneUptimeDate.getCurrentDate();
+            const startDate: Date =
+                monitorEvents[i]!.createdAt || OneUptimeDate.getCurrentDate();
 
             // Initialize the end date as the current date.
             let endDate: Date = OneUptimeDate.getCurrentDate();
@@ -71,90 +76,116 @@ const MonitorUptimeGraph: FunctionComponent<ComponentProps> = (
                 label: monitorEvents[i]?.monitorStatus?.name || 'Operational',
                 priority: monitorEvents[i]?.monitorStatus?.priority || 0,
                 color: monitorEvents[i]?.monitorStatus?.color || Green,
-                monitorId: monitorEvents[i]?.monitorId!,
+                monitorId: monitorEvents[i]!.monitorId!,
             });
         }
 
         // Return the populated eventList array.
         return eventList;
-    }
+    };
 
+    const getMonitorEvents: () => Array<MonitorEvent> =
+        (): Array<MonitorEvent> => {
+            // get all distinct monitor ids.
+            const monitorIds: Array<ObjectID> = [];
 
+            for (let i: number = 0; i < props.items.length; i++) {
+                if (!props.items[i]) {
+                    continue;
+                }
 
-    const getMonitorEvents: () => Array<MonitorEvent> = (): Array<MonitorEvent> => {
+                const monitorId: string | undefined =
+                    props.items[i]!.monitorId?.toString();
 
+                if (!monitorId) {
+                    continue;
+                }
 
-        // get all distinct monitor ids.
-        const monitorIds: Array<ObjectID> = [];
-
-        for (let i: number = 0; i < props.items.length; i++) {
-            if (!props.items[i]) {
-                continue;
+                if (
+                    !monitorIds.find((item: ObjectID) => {
+                        return item.toString() === monitorId;
+                    })
+                ) {
+                    monitorIds.push(new ObjectID(monitorId));
+                }
             }
 
-            const monitorId: string | undefined = props.items[i]!.monitorId?.toString();
+            const eventList: Array<MonitorEvent> = [];
+            // convert data to events.
 
-            if (!monitorId) {
-                continue;
+            for (const monitorId of monitorIds) {
+                const monitorEvents: Array<MonitorEvent> =
+                    getMonitorEventsForId(monitorId);
+                eventList.push(...monitorEvents);
             }
 
-            if (!monitorIds.find((item) => item.toString() === monitorId)) {
-                monitorIds.push(new ObjectID(monitorId));
-            }
-        }
+            // sort event list by start date.
+            eventList.sort((a: MonitorEvent, b: MonitorEvent) => {
+                if (OneUptimeDate.isAfter(a.startDate, b.startDate)) {
+                    return 1;
+                }
 
-        const eventList: Array<MonitorEvent> = [];
-        // convert data to events.
+                if (OneUptimeDate.isAfter(b.startDate, a.startDate)) {
+                    return -1;
+                }
 
-        for (const monitorId of monitorIds) {
-            const monitorEvents: Array<MonitorEvent> = getMonitorEventsForId(monitorId);
-            eventList.push(...monitorEvents);
-        }
+                return 0;
+            });
 
-        // sort event list by start date.
-        eventList.sort((a: MonitorEvent, b: MonitorEvent) => {
-            if (OneUptimeDate.isAfter(a.startDate, b.startDate)) {
-                return 1;
-            }
-
-            if (OneUptimeDate.isAfter(b.startDate, a.startDate)) {
-                return -1;
-            }
-
-            return 0;
-        });
-
-        return [...eventList];
-    }
-
+            return [...eventList];
+        };
 
     useEffect(() => {
-        let monitorEventList: Array<Event> = getMonitorEvents();
+        const monitorEventList: Array<Event> = getMonitorEvents();
 
-        let eventList: Array<Event> = [];
+        const eventList: Array<Event> = [];
 
-        for(const monitorEvent of monitorEventList) {
-
-            // if this event starts after the last event, then add it to the list directly. 
-            if(eventList.length === 0 || OneUptimeDate.isAfter(monitorEvent.startDate, eventList[eventList.length - 1]!.endDate) || OneUptimeDate.isEqualBySeconds(monitorEvent.startDate, eventList[eventList.length - 1]!.endDate)) {
+        for (const monitorEvent of monitorEventList) {
+            // if this event starts after the last event, then add it to the list directly.
+            if (
+                eventList.length === 0 ||
+                OneUptimeDate.isAfter(
+                    monitorEvent.startDate,
+                    eventList[eventList.length - 1]!.endDate
+                ) ||
+                OneUptimeDate.isEqualBySeconds(
+                    monitorEvent.startDate,
+                    eventList[eventList.length - 1]!.endDate
+                )
+            ) {
                 eventList.push(monitorEvent);
                 continue;
             }
 
             // if this event starts before the last event, then we need to check if it ends before the last event. If it does, then we can skip this event if the monitrEvent is of lower priority than the last event. If it is of higher priority, then we need to add it to the list and remove the last event from the list.
-            if(OneUptimeDate.isBefore(monitorEvent.startDate, eventList[eventList.length - 1]!.endDate)) {
-                
-                if(monitorEvent.priority > eventList[eventList.length - 1]!.priority) {
+            if (
+                OneUptimeDate.isBefore(
+                    monitorEvent.startDate,
+                    eventList[eventList.length - 1]!.endDate
+                )
+            ) {
+                if (
+                    monitorEvent.priority >
+                    eventList[eventList.length - 1]!.priority
+                ) {
                     // end the last event at the start of this event.
 
-                    const tempLastEvent: Event = {...eventList[eventList.length - 1]} as any;
+                    const tempLastEvent: Event = {
+                        ...eventList[eventList.length - 1],
+                    } as Event;
 
-                    eventList[eventList.length - 1]!.endDate = monitorEvent.startDate;
+                    eventList[eventList.length - 1]!.endDate =
+                        monitorEvent.startDate;
                     eventList.push(monitorEvent);
 
                     // if the monitorEvent endDate is before the end of the last event, then we need to add the end of the last event to the list.
 
-                    if(OneUptimeDate.isBefore(monitorEvent.endDate, tempLastEvent.endDate)) {
+                    if (
+                        OneUptimeDate.isBefore(
+                            monitorEvent.endDate,
+                            tempLastEvent.endDate
+                        )
+                    ) {
                         eventList.push({
                             startDate: monitorEvent.endDate,
                             endDate: tempLastEvent.endDate,
@@ -167,9 +198,7 @@ const MonitorUptimeGraph: FunctionComponent<ComponentProps> = (
 
                 continue;
             }
-
         }
-
 
         setEvents(eventList);
     }, [props.items]);
