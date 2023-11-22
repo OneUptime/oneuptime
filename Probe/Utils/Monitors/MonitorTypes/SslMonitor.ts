ca52106c23715e95bff08f7acdf50975292312f9
Simon Larsen <simonlarsen@oneuptime.com>
2023-11-22 17:32:58 +0000

    Add SSL certificate check function

--- Probe/Utils/Monitors/MonitorTypes/SslMonitor.ts
+++ Probe/Utils/Monitors/MonitorTypes/SslMonitor.ts
@@ -1,5 +1,7 @@
 import Hostname from 'Common/Types/API/Hostname';
 import NotImplementedException from 'Common/Types/Exception/NotImplementedException';
+// import https from 'https';
+// import { parse } from 'url';
 
 export interface SslResponse {
     isSelfSigned: boolean;
@@ -22,3 +24,34 @@ export default class SSL {
         throw new NotImplementedException();
     }
 }
+
+
+// function checkCertificate(url) {
+//     const options = {
+//         host: parse(url).hostname,
+//         port: 443,
+//         method: 'GET',
+//         agent: new https.Agent({  
+//             rejectUnauthorized: false
+//         })
+//     };
+
+//     const req = https.request(options, res => {
+//         const certificate = res.connection.getPeerCertificate();
+        
+//         if (certificate.valid_to) {
+//             const validTo = new Date(certificate.valid_to);
+//             const diffDays = Math.ceil(Math.abs(validTo - new Date()) / (1000 * 60 * 60 * 24));
+//             if (diffDays < 45) {
+//                 console.log(`The SSL certificate for ${url} expires in less than 45 days.`);
+//             }
+//         }
+//     });
+
+//     req.on('error', error => {
+//         console.error(error);
+//     });
+
+//     req.end();
+// }
+
