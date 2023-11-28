656ed8153dd7428eb9c30d1b55bdf4e9ac20b190
Simon Larsen <simonlarsen@oneuptime.com>
2023-11-28 15:25:20 +0000

    Add @types/react-big-calendar package

---
+++ CommonUI/src/Components/Calendar/Calendar.tsx
@@ -0,0 +1,28 @@
+import "react-big-calendar/lib/css/react-big-calendar.css"
+import { Calendar, momentLocalizer } from "react-big-calendar"
+import React, { FunctionComponent, ReactElement } from "react"
+import moment from 'moment'
+
+const localizer = momentLocalizer(moment)
+
+export interface ComponentProps {
+    id?: string | undefined;
+}
+
+const CalendarElement: FunctionComponent<ComponentProps> = (
+    props: ComponentProps
+): ReactElement => {
+    return (
+        <div id={props.id}>
+            <Calendar 
+                events={[]}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+                localizer={localizer}
+            />
+        </div>
+    );
+};
+
+export default CalendarElement;
