02e8c547252b8e02637bc84df091e69d6f6e7cee
Simon Larsen <simonlarsen@oneuptime.com>
2023-12-01 21:17:21 +0000

    Delete unnecessary files and configurations

--- Integration/Index.ts
+++
@@ -1,8 +0,0 @@
-import { ExpressApplication } from 'CommonServer/Utils/Express';
-import App from 'CommonServer/Utils/StartServer';
-
-export const APP_NAME: string = 'integration';
-
-const app: ExpressApplication = App(APP_NAME);
-
-export default app;
