dab7f81073037c6760b2bafc2d916fb965d2baeb
Simon Larsen <simonlarsen@oneuptime.com>
2023-12-12 16:13:22 +0000

    Add devDependency @types/node to package.json and update playwright.config.ts

--- E2E/playwright.config.ts
+++ E2E/playwright.config.ts
@@ -24,11 +24,11 @@ export default defineConfig({
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: Boolean(process.env.CI),
+    forbidOnly: Boolean(process.env['CI']),
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env['CI'] ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: 1,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
