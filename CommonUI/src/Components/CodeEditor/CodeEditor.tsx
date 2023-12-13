6285d031ebbf1d34520a73780b8c53a4c873afce
Sokna Ly <sokna.ly@iserp.cloud>
2023-12-13 23:26:14 +0700

    fix: revert type change for editor component (error on my local)

--- CommonUI/src/Components/CodeEditor/CodeEditor.tsx
+++ CommonUI/src/Components/CodeEditor/CodeEditor.tsx
@@ -160,7 +160,7 @@ const CodeEditor: FunctionComponent<ComponentProps> = (
                     showFoldingControls: 'mouseover',
                     smoothScrolling: false,
                     suggestOnTriggerCharacters: true,
-                    wordBasedSuggestions: false,
+                    wordBasedSuggestions: 'off',
                     wordSeparators: '~!@#$%^&*()-=+[{]}|;:\'",.<>/?',
                     wordWrap: props.type === CodeType.Markdown ? 'on' : 'off',
                     wordWrapBreakAfterCharacters: '\t})]?|&,;',
