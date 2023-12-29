3111e78eeba8d7fd449da287988a4a3e96681bc8
Simon Larsen <simonlarsen@oneuptime.com>
2023-12-29 21:27:05 +0000

    Update function to handle edge cases
    This commit updates the function to handle edge cases, as shown in the git diff.

---
+++ App/FeatureSet/Docs/Index.ts
@@ -0,0 +1,25 @@
+import 'ejs';
+import Express, {
+    ExpressRequest,
+    ExpressResponse,
+    ExpressStatic,
+    ExpressApplication,
+} from 'CommonServer/Utils/Express';
+import { StaticPath, ViewsPath } from './Utils/Config';
+
+const app: ExpressApplication = Express.getExpressApp();
+
+
+app.get('/docs', (_req: ExpressRequest, res: ExpressResponse) => {
+    res.render(`${ViewsPath}/index`, {
+        support: false,
+        footerCards: true,
+        cta: true,
+        blackLogo: false,
+        requestDemoCta: false,
+    });
+});
+
+app.use('/docs/static',
+    ExpressStatic(StaticPath)
+);
