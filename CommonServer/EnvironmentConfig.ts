06ec8ab315627bb7093f201003fc16e1a96ee469
Sokna Ly <sokna.ly@iserp.cloud>
2023-12-31 11:49:49 +0700

    fix: update status page html css page breadcrump

--- CommonServer/EnvironmentConfig.ts
+++ CommonServer/EnvironmentConfig.ts
@@ -60,7 +60,6 @@ export const ClusterKey: ObjectID = new ObjectID(
 
 export const HasClusterKey: boolean = Boolean(process.env['ONEUPTIME_SECRET']);
 
-
 export const WorkerHostname: Hostname = Hostname.fromString(
     `${process.env['SERVER_WORKERS_HOSTNAME'] || 'localhost'}:${
         process.env['WORKERS_PORT'] || 80
