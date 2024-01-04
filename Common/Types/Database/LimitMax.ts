095aa484c6d4ba5cfdc88ce200f7dc28784f930d
Simon Larsen <simonlarsen@oneuptime.com>
2024-01-05 01:18:40 +0530

    Update LIMIT_PER_PROJECT constant value

--- Common/Types/Database/LimitMax.ts
+++ Common/Types/Database/LimitMax.ts
@@ -1,4 +1,4 @@
 const LIMIT_MAX: number = 10000;
-export const LIMIT_PER_PROJECT: number = 300;
+export const LIMIT_PER_PROJECT: number = 1000;
 export const DEFAULT_LIMIT: number = 10;
 export default LIMIT_MAX;
