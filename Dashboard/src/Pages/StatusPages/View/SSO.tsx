8e0047e0554b501b27afa756a9f3bcd7ac02680d
Simon Larsen <simonlarsen@oneuptime.com>
2024-01-25 18:15:30 +0000

    Add statusPageId to SSO query

--- Dashboard/src/Pages/StatusPages/View/SSO.tsx
+++ Dashboard/src/Pages/StatusPages/View/SSO.tsx
@@ -49,6 +49,7 @@ const SSOPage: FunctionComponent<PageComponentProps> = (
                     query={{
                         projectId:
                             DashboardNavigation.getProjectId()?.toString(),
+                            statusPageId: modelId.toString()
                     }}
                     onBeforeCreate={(
                         item: StatusPageSSO
