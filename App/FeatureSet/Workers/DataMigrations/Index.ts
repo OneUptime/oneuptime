2e815457aef59514ebc5db904a9d367a97c21f44
Simon Larsen <simonlarsen@oneuptime.com>
2024-01-27 07:38:05 +0000

    Add DowntimeMonitorStatus to StatusPage

--- App/FeatureSet/Workers/DataMigrations/Index.ts
+++ App/FeatureSet/Workers/DataMigrations/Index.ts
@@ -26,7 +26,7 @@ const DataMigrations: Array<DataMigrationBase> = [
     new UpdateGlobalConfigFromEnv(),
     new AddPostedAtToPublicNotes(),
     new MoveEnableSubscribersToEnableEmailSubscribersOnStatusPage(),
-    new AddDowntimeMonitorStatusToStatusPage()
+    new AddDowntimeMonitorStatusToStatusPage(),
 ];
 
 export default DataMigrations;
