26b6533810fe36f3ebecf8f4975141da35110719
Simon Larsen <simonlarsen@oneuptime.com>
2024-02-13 13:31:11 +0000

    Refactor TracesList component to use ModelTable

--- Dashboard/src/Pages/Telemetry/Services/View/Traces/Index.tsx
+++ Dashboard/src/Pages/Telemetry/Services/View/Traces/Index.tsx
@@ -1,76 +1,107 @@
 import React, { Fragment, FunctionComponent, ReactElement } from 'react';
 import PageComponentProps from '../../../../PageComponentProps';
-import GanttChart from 'CommonUI/src/Components/GanttChart/Index';
-import Card from 'CommonUI/src/Components/Card/Card';
-import { Black, White } from 'Common/Types/BrandColors';
+import ModelTable from 'CommonUI/src/Components/ModelTable/ModelTable';
+import Span from 'Model/AnalyticsModels/Span';
 
-const ServiceDelete: FunctionComponent<PageComponentProps> = (
+const TracesList: FunctionComponent<PageComponentProps> = (
     _props: PageComponentProps
 ): ReactElement => {
     return (
         <Fragment>
-            <Card
-                title={'Traces'}
-                description={'Traces for the request operation.'}
-            >
-                <div className="overflow-x-auto">
-                    <GanttChart
-                        chart={{
-                            id: 'chart',
-                            rows: [
-                                {
-                                    id: 'row1',
-                                    title: 'row1',
-                                    description: 'row1 description',
-                                },
-                                {
-                                    id: 'row2',
-                                    title: 'row2',
-                                    description: 'row2 description',
-                                },
-                            ],
-                            bars: [
-                                {
-                                    id: "bar-1",
-                                    title: "Bar 1",
-                                    titleColor: White,
-                                    barColor: Black,
-                                    barTimelineStart: 20,
-                                    barTimelineEnd: 80,
-                                    rowId: "row1"
-                                },
-                                {
-                                    id: "bar-2",
-                                    title: "Bar 2",
-                                    titleColor: White,
-                                    barColor: Black,
-                                    barTimelineStart: 90,
-                                    barTimelineEnd: 100,
-                                    rowId: "row1"
-                                },
-                                {
-                                    id: "bar-3",
-                                    title: "Bar 3",
-                                    titleColor: White,
-                                    barColor: Black,
-                                    barTimelineStart: 45,
-                                    barTimelineEnd: 65,
-                                    rowId: "row2"
-                                }
-                                
-                            ],
-                            timeline: {
-                                start: 0,
-                                end: 100,
-                                interval: 10,
-                                intervalUnit: 'ms',
+            <ModelTable<Span>
+                modelType={Log}
+                id="logs-table"
+                isDeleteable={false}
+                isEditable={false}
+                isCreateable={false}
+                name="Status Pages"
+                isViewable={true}
+                cardProps={{
+                    title: 'Status Pages',
+                    description:
+                        'Here is a list of status page for this project.',
+                }}
+                showViewIdButton={true}
+                noItemsMessage={'No status pages found.'}
+                formFields={[
+                    {
+                        field: {
+                            name: true,
+                        },
+                        title: 'Name',
+                        fieldType: FormFieldSchemaType.Text,
+                        required: true,
+                        placeholder: 'Status Page Name',
+                        validation: {
+                            minLength: 2,
+                        },
+                    },
+                    {
+                        field: {
+                            description: true,
+                        },
+                        title: 'Description',
+                        fieldType: FormFieldSchemaType.LongText,
+                        required: true,
+                        placeholder: 'Description',
+                    },
+                ]}
+                showRefreshButton={true}
+                showFilterButton={true}
+                viewPageRoute={Navigation.getCurrentRoute()}
+                columns={[
+                    {
+                        field: {
+                            name: true,
+                        },
+                        title: 'Name',
+                        type: FieldType.Text,
+                        isFilterable: true,
+                    },
+                    {
+                        field: {
+                            description: true,
+                        },
+                        title: 'Description',
+                        type: FieldType.Text,
+                        isFilterable: true,
+                    },
+                    {
+                        field: {
+                            labels: {
+                                name: true,
+                                color: true,
                             },
-                        }}
-                    />
-                </div>
-            </Card>
+                        },
+                        title: 'Labels',
+                        type: FieldType.EntityArray,
+                        isFilterable: true,
+                        filterEntityType: Label,
+                        filterQuery: {
+                            projectId:
+                                DashboardNavigation.getProjectId()?.toString(),
+                        },
+                        filterDropdownField: {
+                            label: 'name',
+                            value: '_id',
+                        },
+                        getElement: (item: JSONObject): ReactElement => {
+                            return (
+                                <LabelsElement
+                                    labels={
+                                        BaseModel.fromJSON(
+                                            (item['labels'] as JSONArray) || [],
+                                            Label
+                                        ) as Array<Label>
+                                    }
+                                />
+                            );
+                        },
+                    },
+                ]}
+            />
         </Fragment>
     );
 };
 
-export default ServiceDelete;
+export default TracesList;
---
+++ Dashboard/src/Pages/Telemetry/Services/View/Traces/View/Index.tsx
@@ -0,0 +1,76 @@
+import React, { Fragment, FunctionComponent, ReactElement } from 'react';
+import PageComponentProps from '../../../../../PageComponentProps';
+import GanttChart from 'CommonUI/src/Components/GanttChart/Index';
+import Card from 'CommonUI/src/Components/Card/Card';
+import { Black, White } from 'Common/Types/BrandColors';
+
+const ServiceDelete: FunctionComponent<PageComponentProps> = (
+    _props: PageComponentProps
+): ReactElement => {
+    return (
+        <Fragment>
+            <Card
+                title={'Traces'}
+                description={'Traces for the request operation.'}
+            >
+                <div className="overflow-x-auto">
+                    <GanttChart
+                        chart={{
+                            id: 'chart',
+                            rows: [
+                                {
+                                    id: 'row1',
+                                    title: 'row1',
+                                    description: 'row1 description',
+                                },
+                                {
+                                    id: 'row2',
+                                    title: 'row2',
+                                    description: 'row2 description',
+                                },
+                            ],
+                            bars: [
+                                {
+                                    id: "bar-1",
+                                    title: "Bar 1",
+                                    titleColor: White,
+                                    barColor: Black,
+                                    barTimelineStart: 20,
+                                    barTimelineEnd: 80,
+                                    rowId: "row1"
+                                },
+                                {
+                                    id: "bar-2",
+                                    title: "Bar 2",
+                                    titleColor: White,
+                                    barColor: Black,
+                                    barTimelineStart: 90,
+                                    barTimelineEnd: 100,
+                                    rowId: "row1"
+                                },
+                                {
+                                    id: "bar-3",
+                                    title: "Bar 3",
+                                    titleColor: White,
+                                    barColor: Black,
+                                    barTimelineStart: 45,
+                                    barTimelineEnd: 65,
+                                    rowId: "row2"
+                                }
+                                
+                            ],
+                            timeline: {
+                                start: 0,
+                                end: 100,
+                                interval: 10,
+                                intervalUnit: 'ms',
+                            },
+                        }}
+                    />
+                </div>
+            </Card>
+        </Fragment>
+    );
+};
+
+export default ServiceDelete;
