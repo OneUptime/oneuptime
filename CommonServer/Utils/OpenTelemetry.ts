0cf5edc8e97eb4ad9c25b65e9ce0abd6cafd0745
Simon Larsen <simonlarsen@oneuptime.com>
2024-02-19 20:08:51 +0000

    Add OpenTelemetry OTLP exporters and environment variables

--- CommonServer/Utils/OpenTelemetry.ts
+++ CommonServer/Utils/OpenTelemetry.ts
@@ -1,22 +1,20 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node';
+import * as opentelemetry from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import {
-    MetricReader,
-    PeriodicExportingMetricReader,
-    ConsoleMetricExporter,
-} from '@opentelemetry/sdk-metrics';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
+import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
 
 
-// For troubleshooting, set the log level to DiagLogLevel.DEBUG
-const sdk = new NodeSDK({
-    traceExporter: new ConsoleSpanExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
-    }),
-    instrumentations: [getNodeAutoInstrumentations()],
+const sdk = new opentelemetry.NodeSDK({
+  traceExporter: new OTLPTraceExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter(),
+  }) as any,
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
-sdk.start();
+if(process.env['OTEL_EXPORTER_OTLP_ENDPOINT'] && process.env['OTEL_EXPORTER_OTLP_HEADERS']){
+    sdk.start();
+}
 
 export default sdk;
