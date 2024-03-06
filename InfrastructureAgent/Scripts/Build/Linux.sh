eccefe30df62077f3b251ba336df736bd08f05e0
Simon Larsen <simonlarsen@oneuptime.com>
2024-03-06 15:42:06 +0000

    Fix file name typo in .gitignore and update build scripts for InfrastructureAgent

--- InfrastructureAgent/Scripts/Build/Linux.sh
+++ InfrastructureAgent/Scripts/Build/Linux.sh
@@ -1,6 +1,10 @@
+# This is taken from: https://nodejs.org/api/single-executable-applications.html
+
 node --experimental-sea-config sea-config.json
-cp $(command -v node) InfrastrctureAgent
-npx postject hello NODE_SEA_BLOB sea-prep.blob \
-    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
+cp $(command -v node) InfrastructureAgent
+npm i postject -g
+npx postject InfrastructureAgent NODE_SEA_BLOB sea-prep.blob \
+    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
+
 
  
