fab0c18b1f35207288f1364addd79c6b0234b190
Simon Larsen <simonlarsen@oneuptime.com>
2024-03-12 13:20:39 +0000

    Add infrastructure agent binary release workflows***

---
+++ InfrastructureAgent/Scripts/Build/Linux.sh
@@ -0,0 +1,7 @@
+# This is taken from: https://nodejs.org/api/single-executable-applications.html
+
+node --experimental-sea-config sea-config.json
+cp $(command -v node) InfrastructureAgent
+npm i postject -g
+npx postject InfrastructureAgent NODE_SEA_BLOB sea-prep.blob \
+    --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
