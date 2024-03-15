b482a2fd6ce78a77150141ae5b894b1f0daf56d5
Simon Larsen <simonlarsen@oneuptime.com>
2024-03-15 17:51:46 +0000

    Add NVM installation and Node.js setup to Linux.sh script

--- InfrastructureAgent/Scripts/Install/Linux.sh
+++ InfrastructureAgent/Scripts/Install/Linux.sh
@@ -3,6 +3,14 @@
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
+# Export to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Refresh bash 
+source ~/.bashrc
+
 # Install latest Node.js via NVM
 nvm install node
 
