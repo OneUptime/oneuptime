7477aeca945314e6ebf88b1e4269ddff3316799f
Simon Larsen <simonlarsen@oneuptime.com>
2024-03-27 13:51:01 +0000

    Remove react-google-charts dependency and update LineChart component

--- CommonUI/src/Components/Charts/Line/LineChart.tsx
+++ CommonUI/src/Components/Charts/Line/LineChart.tsx
@@ -3,12 +3,74 @@ import { ResponsiveLine } from "@nivo/line";
 import OneUptimeDate from "Common/Types/Date";
 import { Indigo500 } from "Common/Types/BrandColors";
 
-export interface ComponentProps {
+export interface LineChartDataItem { 
+    x: string | number | Date;
+    y: number;
+}
+
+export enum ChartCurve {
+    LINEAR = "linear",
+    MONOTONE_X = "monotoneX",
+    STEP = "step",
+    STEP_BEFORE = "stepBefore",
+    STEP_AFTER = "stepAfter",
+}
+
+export enum XScaleType { 
+    TIME = 'time',
+}
+
+export enum XScalePrecision {
+    SECOND = 'second',
+    MINUTE = 'minute',
+    HOUR = 'hour',
+    DAY = 'day',
+    MONTH = 'month',
+    YEAR = 'year',
 
 }
 
+export interface XScale { 
+    type: XScaleType;
+    min: string | Date;
+    max: string | Date;
+    precision: XScalePrecision;
+}
+
+export interface LineChartData { 
+    id: string;
+    data: Array<LineChartDataItem>;
+}
+
+export enum YScaleType {
+    LINEAR = 'linear',
+}
+
+export interface YScale {
+    type: YScaleType;
+    min: 'auto' | number;
+    max: 'auto' | number;
+}
+
+export interface AxisBottom {
+    legend: string;
+}
+
+export interface AxisLeft {
+    legend: string;
+}
+
+export interface ComponentProps {
+    data: LineChartData;
+    curve?: ChartCurve;
+    xScale: XScale;
+    yScale: YScale;
+    axisBottom: AxisBottom;
+    axisLeft: AxisLeft;
+}
+
 const LineChart: FunctionComponent<ComponentProps> = (
-    _props: ComponentProps
+    props: ComponentProps
 ): ReactElement => {
     const data = [
         {
@@ -32,41 +94,38 @@ const LineChart: FunctionComponent<ComponentProps> = (
                 onMouseEnter={(data) => console.log(data)}
                 onMouseLeave={(data) => console.log(data)}
                 margin={{ bottom: 30, left: 30 }}
+                curve={props.curve || ChartCurve.LINEAR}
+                markers={[
+                    {
+                      axis: 'x',
+                      legend: 'x marker',
+                      lineStyle: {
+                        stroke: '#b0413e',
+                        strokeWidth: 2
+                      },
+                      value: OneUptimeDate.getSomeMinutesAfter(2)
+                    }
+                  ]}
                 xScale={{
-                    type: 'time',
-                    min: OneUptimeDate.getCurrentDate(),
-                    max: 'auto',
-                    precision: 'minute',
-                    nice: true,
-                    format: "%Y-%m-%d %H:%M:%S",
+                    type: props.xScale.type,
+                    max: props.xScale.max,
+                    min: props.xScale.min,
+                    precision: props.xScale.precision,
                 }}
                 yScale={{
-                    type: 'linear',
-                    min: 0,
-                    max: 'auto',
-                    stacked: true,
-                    reverse: false
+                    type: props.yScale.type,
+                    min: props.yScale.min,
+                    max: props.yScale.max,
                 }}
                 xFormat="time:%Y-%m-%d %H:%M:%S"
                 axisTop={null}
                 axisRight={null}
                 axisBottom={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'X axis',
-                    legendOffset: 36,
-                    legendPosition: 'middle',
-                    format: "%H:%M:%S",
+                    legend: props.axisBottom.legend,
                 }}
                 useMesh={true}
                 axisLeft={{
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'Y axis',
-                    legendOffset: -40,
-                    legendPosition: 'middle'
+                    legend: props.axisLeft.legend,
                 }}
                 enableSlices="x"
                 sliceTooltip={({ slice }) => (
