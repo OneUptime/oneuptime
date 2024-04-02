d99f5310e745bd9b782815d4e77d991ad0e2422b
Simon Larsen <simonlarsen@oneuptime.com>
2024-04-02 20:09:08 +0100

    Add server process criteria filters to monitor

--- Common/Types/Monitor/CriteriaFilter.ts
+++ Common/Types/Monitor/CriteriaFilter.ts
@@ -8,6 +8,9 @@ export enum CheckOn {
     ResponseBody = 'Response Body',
     IsOnline = 'Is Online',
     IncomingRequest = 'Incoming Request',
+    ServerProcessName = 'Server Process Name',
+    ServerProcessCommand = 'Server Process Command',
+    ServerProcessPID = 'Server Process PID',
     RequestBody = 'Request Body',
     RequestHeader = 'Request Header',
     RequestHeaderValue = 'Request Header Value',
@@ -74,6 +77,8 @@ export enum FilterType {
     NotRecievedInMinutes = 'Not Recieved In Minutes',
     RecievedInMinutes = 'Recieved In Minutes',
     EvaluatesToTrue = 'Evaluates To True',
+    IsExecuting = 'Is Executing',
+    IsNotExecuting = 'Is Not Executing',
 }
 
 export enum FilterCondition {
