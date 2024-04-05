1991977af12f6db0291b36b4c4c3e4e4b77415d1
Simon Larsen <simonlarsen@oneuptime.com>
2024-04-05 18:59:43 +0100

    Fix dependencies in IsolatedVM package.json and package-lock.json

--- IsolatedVM/Index.ts
+++ IsolatedVM/Index.ts
@@ -1,21 +1,17 @@
-
 import Express, { ExpressApplication } from 'CommonServer/Utils/Express';
 import logger from 'CommonServer/Utils/Logger';
 import App from 'CommonServer/Utils/StartServer';
 import process from 'process';
 import { PromiseVoidFunction } from 'Common/Types/FunctionTypes';
 
-
 const app: ExpressApplication = Express.getExpressApp();
 
 const APP_NAME: string = 'isolated-vm';
-
 
 const init: PromiseVoidFunction = async (): Promise<void> => {
     try {
         // init the app
         await App(APP_NAME);
-
     } catch (err) {
         logger.error('App Init Failed:');
         logger.error(err);
