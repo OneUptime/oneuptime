5d3d19d210f8ae46ba6bdf0f2950f9cf45f7e649
Simon Larsen <simonlarsen@oneuptime.com>
2024-04-06 13:30:01 +0100

    Update OneUptime app deployment and add isolated-vm upstream in Nginx configuration

--- IsolatedVM/Utils/VM.ts
+++ IsolatedVM/Utils/VM.ts
@@ -2,29 +2,32 @@ import { JSONObject, JSONValue } from "Common/Types/JSON";
 import http from "http";
 import https from "https";
 import axios from "axios";
-import vm from "node:vm";
+import vm, { Context } from "node:vm";
 
 export default class VMUtil {
     public static async runCodeInSandbox(
         code: string,
         options: {
             timeout?: number;
-            allowAsync?: boolean;
-            includeHttpPackage: boolean;
-            consoleLog?: (logValue: JSONValue) => void | undefined;
             args?: JSONObject | undefined;
         }
-    ): Promise<any> {
-        let sandbox: any = {};
+    ): Promise<{
+        returnValue: any;
+        logMessages: string[];
+    }> {
 
-        if (options.includeHttpPackage) {
-            sandbox = {
-                ...sandbox,
-                http: http,
-                https: https,
-                axios: axios,
-            };
-        }
+        let logMessages: string[] = [];
+
+        let sandbox: Context = {
+            console: {
+                log: (...args: JSONValue[]) => {
+                    logMessages.push(args.join(' '));
+                },
+            },
+            http: http,
+            https: https,
+            axios: axios,
+        };
 
         if (options.args) {
             sandbox = {
@@ -33,21 +36,17 @@ export default class VMUtil {
             };
         }
 
-        if (options.consoleLog) {
-            sandbox = {
-                ...sandbox,
-                console: {
-                    log: options.consoleLog,
-                },
-            };
-        }
-
         vm.createContext(sandbox); // Contextify the object.
 
         const script: string = `module.exports = async function(args) { ${(code as string) || ''} }`;
 
-        const returnVal: any = vm.runInContext(script, sandbox); // run the script 
+        const returnVal: any = vm.runInContext(script, sandbox, {
+            timeout: options.timeout || 5000,
+        }); // run the script 
 
-        return returnVal;
+        return {
+            returnValue: returnVal,
+            logMessages,
+        };
     }
 }
