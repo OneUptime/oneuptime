1516f811808c1409a60c7f0165c40528ef95da5d
Simon Larsen <simonlarsen@oneuptime.com>
2024-04-25 15:07:33 +0100

    Refactor E2E configuration and Docker Compose file

--- E2E/playwright.config.ts
+++ E2E/playwright.config.ts
@@ -53,12 +53,6 @@ export default defineConfig({
             name: 'firefox',
             use: { ...devices['Desktop Firefox'] },
         },
-
-        {
-            name: 'webkit',
-            use: { ...devices['Desktop Safari'] },
-        },
-
         /* Test against mobile viewports. */
         // {
         //   name: 'Mobile Chrome',
