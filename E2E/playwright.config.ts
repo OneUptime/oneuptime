ecfc47ee595efea792ec1a8ab4028feabdbe430b
Simon Larsen <simonlarsen@oneuptime.com>
2024-04-25 13:50:33 +0100

    Update dependencies and test directory in E2E package files

--- E2E/playwright.config.ts
+++ E2E/playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-    testDir: './tests',
+    testDir: './Tests',
     /* Maximum time one test can run for. */
     timeout: 30 * 1000,
     //  globalTimeout: 600 * 1000,
