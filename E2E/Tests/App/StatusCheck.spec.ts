3bc2e639d9a4d2dc744614fc2663cb329f4e6290
Simon Larsen <simonlarsen@oneuptime.com>
2024-04-25 19:15:14 +0100

    Add Jest and Babel ESLint parser to devDependencies

---
+++ E2E/Tests/App/StatusCheck.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { BASE_URL } from '../../Config';
+
+
+test.describe('check live and health check of the app', () => {
+    
+    test('check if app status is ok', async ({ page }) => {
+        await page.goto(`${BASE_URL.addRoute("/status").toString()}`);
+        const content = await page.content();
+        expect(content).toContain('{"status":"ok"}');
+    });
+
+    test('check if app is ready', async ({ page }) => {
+        await page.goto(`${BASE_URL.addRoute("/status/ready").toString()}`);
+        const content = await page.content();
+        expect(content).toContain('{"status":"ok"}');
+    });
+
+    test('check if app is live', async ({ page }) => {
+        await page.goto(`${BASE_URL.addRoute("/status/live").toString()}`);
+        const content = await page.content();
+        expect(content).toContain('{"status":"ok"}');
+    });
+});
