d51e818d10c36462688852a33ae5276d76e1a779
Simon Larsen <simonlarsen@oneuptime.com>
2024-04-27 09:42:41 +0100

    Update E2E Config.ts to use E2E_TEST_STATUS_PAGE_URL instead of E2E_TEST_REGISTERED_USER_PASSWORD

--- E2E/Config.ts
+++ E2E/Config.ts
@@ -24,7 +24,7 @@ export const REGISTERED_USER_PASSWORD: string =
 export const IS_BILLING_ENABLED: boolean = env('BILLING_ENABLED') === 'true';
 
 export const STATUS_PAGE_URL: URL | null = env(
-    'E2E_TEST_REGISTERED_USER_PASSWORD'
+    'E2E_TEST_STATUS_PAGE_URL'
 )
-    ? URL.fromString(env('E2E_TEST_REGISTERED_USER_PASSWORD'))
+    ? URL.fromString(env('E2E_TEST_STATUS_PAGE_URL'))
     : null;
