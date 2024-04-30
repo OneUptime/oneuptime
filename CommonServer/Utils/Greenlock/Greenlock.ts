d783eaea9d18f4d0ebb9553e71c6fe6cb03a788b
Simon Larsen <simonlarsen@oneuptime.com>
2024-04-30 21:24:04 +0100

    Refactor table name in PostgresDatabase.ts, remove unused volume in app.yaml, fix typos in Domains.tsx and Index.ts, update nodemon.json files, remove greenlock dependency, and add validation for cname in StatusPageDomainService.ts

--- CommonServer/Utils/Greenlock/Greenlock.ts
+++ CommonServer/Utils/Greenlock/Greenlock.ts
@@ -11,6 +11,7 @@ import SortOrder from 'Common/Types/BaseDatabase/SortOrder';
 import QueryHelper from '../../Types/Database/QueryHelper';
 import BadDataException from 'Common/Types/Exception/BadDataException';
 import { Challenge } from 'acme-client/types/rfc8555';
+import ServerException from 'Common/Types/Exception/ServerException';
 
 export default class GreenlockUtil {
     public static async renewAllCertsWhichAreExpiringSoon(data: {
@@ -93,134 +94,144 @@ export default class GreenlockUtil {
         domain: string;
         validateCname: (domain: string) => Promise<boolean>;
     }): Promise<void> {
-        let { domain } = data;
+        try {
+            let { domain } = data;
 
-        domain = domain.trim().toLowerCase();
+            domain = domain.trim().toLowerCase();
 
-        //validate cname
+            //validate cname
 
-        const isValidCname: boolean = await data.validateCname(domain);
+            const isValidCname: boolean = await data.validateCname(domain);
 
-        if (!isValidCname) {
-            await GreenlockUtil.removeDomain(domain);
-            logger.error(`Cname is not valid for domain: ${domain}`);
-            throw new BadDataException(
-                'Cname is not valid for domain ' + domain
+            if (!isValidCname) {
+                await GreenlockUtil.removeDomain(domain);
+                logger.error(`Cname is not valid for domain: ${domain}`);
+                throw new BadDataException(
+                    'Cname is not valid for domain ' + domain
+                );
+            }
+
+            const client: acme.Client = new acme.Client({
+                directoryUrl: acme.directory.letsencrypt.production,
+                accountKey: await acme.crypto.createPrivateKey(),
+            });
+
+            const [certificateKey, certificateRequest] =
+                await acme.crypto.createCsr({
+                    commonName: domain,
+                });
+
+            const certificate: string = await client.auto({
+                csr: certificateRequest,
+                email: LetsEncryptNotificationEmail.toString(),
+                termsOfServiceAgreed: true,
+                challengePriority: ['http-01'], // only http-01 challenge is supported by oneuptime
+                challengeCreateFn: async (
+                    authz: acme.Authorization,
+                    challenge: Challenge,
+                    keyAuthorization: string
+                ) => {
+                    // Satisfy challenge here
+                    /* http-01 */
+                    if (challenge.type === 'http-01') {
+                        const acmeChallenge: AcmeChallenge =
+                            new AcmeChallenge();
+                        acmeChallenge.challenge = keyAuthorization;
+                        acmeChallenge.token = challenge.token;
+                        acmeChallenge.domain = authz.identifier.value;
+
+                        await AcmeChallengeService.create({
+                            data: acmeChallenge,
+                            props: {
+                                isRoot: true,
+                            },
+                        });
+                    }
+                },
+                challengeRemoveFn: async (
+                    authz: acme.Authorization,
+                    challenge: Challenge
+                ) => {
+                    // Clean up challenge here
+
+                    if (challenge.type === 'http-01') {
+                        await AcmeChallengeService.deleteBy({
+                            query: {
+                                domain: authz.identifier.value,
+                            },
+                            limit: 1,
+                            skip: 0,
+                            props: {
+                                isRoot: true,
+                            },
+                        });
+                    }
+                },
+            });
+
+            // get expires at date from certificate
+            const cert: acme.CertificateInfo =
+                await acme.forge.readCertificateInfo(certificate);
+            const issuedAt: Date = cert.notBefore;
+            const expiresAt: Date = cert.notAfter;
+
+            // check if the certificate is already in the database.
+            const existingCertificate: AcmeCertificate | null =
+                await AcmeCertificateService.findOneBy({
+                    query: {
+                        domain: domain,
+                    },
+                    select: {
+                        _id: true,
+                    },
+                    props: {
+                        isRoot: true,
+                    },
+                });
+
+            if (existingCertificate) {
+                // update the certificate
+                await AcmeCertificateService.updateBy({
+                    query: {
+                        domain: domain,
+                    },
+                    limit: 1,
+                    skip: 0,
+                    data: {
+                        certificate: certificate.toString(),
+                        certificateKey: certificateKey.toString(),
+                        issuedAt: issuedAt,
+                        expiresAt: expiresAt,
+                    },
+                    props: {
+                        isRoot: true,
+                    },
+                });
+            } else {
+                // create the certificate
+                const acmeCertificate: AcmeCertificate = new AcmeCertificate();
+
+                acmeCertificate.domain = domain;
+                acmeCertificate.certificate = certificate.toString();
+                acmeCertificate.certificateKey = certificateKey.toString();
+                acmeCertificate.issuedAt = issuedAt;
+                acmeCertificate.expiresAt = expiresAt;
+
+                await AcmeCertificateService.create({
+                    data: acmeCertificate,
+                    props: {
+                        isRoot: true,
+                    },
+                });
+            }
+        } catch (e) {
+            logger.error(
+                `Error ordering certificate for domain: ${data.domain}`
             );
-        }
-
-        const client: acme.Client = new acme.Client({
-            directoryUrl: acme.directory.letsencrypt.production,
-            accountKey: await acme.crypto.createPrivateKey(),
-        });
-
-        const [certificateKey, certificateRequest] =
-            await acme.crypto.createCsr({
-                commonName: domain,
-            });
-
-        const certificate: string = await client.auto({
-            csr: certificateRequest,
-            email: LetsEncryptNotificationEmail.toString(),
-            termsOfServiceAgreed: true,
-            challengePriority: ['http-01'], // only http-01 challenge is supported by oneuptime
-            challengeCreateFn: async (
-                authz: acme.Authorization,
-                challenge: Challenge,
-                keyAuthorization: string
-            ) => {
-                // Satisfy challenge here
-                /* http-01 */
-                if (challenge.type === 'http-01') {
-                    const acmeChallenge: AcmeChallenge = new AcmeChallenge();
-                    acmeChallenge.challenge = keyAuthorization;
-                    acmeChallenge.token = challenge.token;
-                    acmeChallenge.domain = authz.identifier.value;
-
-                    await AcmeChallengeService.create({
-                        data: acmeChallenge,
-                        props: {
-                            isRoot: true,
-                        },
-                    });
-                }
-            },
-            challengeRemoveFn: async (
-                authz: acme.Authorization,
-                challenge: Challenge
-            ) => {
-                // Clean up challenge here
-
-                if (challenge.type === 'http-01') {
-                    await AcmeChallengeService.deleteBy({
-                        query: {
-                            domain: authz.identifier.value,
-                        },
-                        limit: 1,
-                        skip: 0,
-                        props: {
-                            isRoot: true,
-                        },
-                    });
-                }
-            },
-        });
-
-        // get expires at date from certificate
-        const cert: acme.CertificateInfo = await acme.forge.readCertificateInfo(
-            certificate
-        );
-        const issuedAt: Date = cert.notBefore;
-        const expiresAt: Date = cert.notAfter;
-
-        // check if the certificate is already in the database.
-        const existingCertificate: AcmeCertificate | null =
-            await AcmeCertificateService.findOneBy({
-                query: {
-                    domain: domain,
-                },
-                select: {
-                    _id: true,
-                },
-                props: {
-                    isRoot: true,
-                },
-            });
-
-        if (existingCertificate) {
-            // update the certificate
-            await AcmeCertificateService.updateBy({
-                query: {
-                    domain: domain,
-                },
-                limit: 1,
-                skip: 0,
-                data: {
-                    certificate: certificate.toString(),
-                    certificateKey: certificateKey.toString(),
-                    issuedAt: issuedAt,
-                    expiresAt: expiresAt,
-                },
-                props: {
-                    isRoot: true,
-                },
-            });
-        } else {
-            // create the certificate
-            const acmeCertificate: AcmeCertificate = new AcmeCertificate();
-
-            acmeCertificate.domain = domain;
-            acmeCertificate.certificate = certificate.toString();
-            acmeCertificate.certificateKey = certificateKey.toString();
-            acmeCertificate.issuedAt = issuedAt;
-            acmeCertificate.expiresAt = expiresAt;
-
-            await AcmeCertificateService.create({
-                data: acmeCertificate,
-                props: {
-                    isRoot: true,
-                },
-            });
+            logger.error(e);
+            throw new ServerException(
+                `Unable to order certificate for ${data.domain}. Please contact support at support@oneuptime.com for more information.`
+            );
         }
     }
 }
