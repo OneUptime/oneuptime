86f262583c7d5a82550cefc1d139ae54e5ab5a24
Simon Larsen <simonlarsen@oneuptime.com>
2024-05-03 13:07:45 +0100

    feat: Generate new certificates for status page domains
    This commit adds a new data migration script, GenerateNewCertsForStatusPage.ts, which generates new certificates for status page domains. It retrieves all domains in the greenlock certs, orders them, and updates the certificates accordingly. This will ensure that the status page domains have up-to-date and valid certificates.
    
    Refactor and delete unused Metric services and models
    Refactor Queue class to use explicit type annotation for queue variable
    Add Queue caching to improve performance and reduce redundant connections
    Refactor test-e2e job in release.yml workflow
    Fix error handling in StatusPageDomainService.ts
    Update select field in MoveGreenlockCertsToAcmeCerts.ts to use _id instead of id
    Update ingress configuration for OneUptime Helm chart
    Refactor StatusPageAPI to improve code readability and maintainability
    Refactor StatusPageAPI and UpdateByID to improve code readability and maintainability
    Refactor Let's Encrypt configuration and update GreenlockUtil class

---
+++ App/FeatureSet/Workers/DataMigrations/GenerateNewCertsForStatusPage.ts
@@ -0,0 +1,47 @@
+import DataMigrationBase from './DataMigrationBase';
+import LIMIT_MAX from 'Common/Types/Database/LimitMax';
+import StatusPageDomainService from 'CommonServer/Services/StatusPageDomainService';
+import logger from 'CommonServer/Utils/Logger';
+import StatusPageDomain from 'Model/Models/StatusPageDomain';
+
+export default class GenerateNewCertsForStatusPage extends DataMigrationBase {
+    public constructor() {
+        super('GenerateNewCertsForStatusPage');
+    }
+
+    public override async migrate(): Promise<void> {
+
+        // get all domains in greenlock certs.
+        const statusPageDomains: Array<StatusPageDomain> =
+            await StatusPageDomainService.findBy({
+                query: {
+
+                },
+                props: {
+                    isRoot: true,
+                },
+                limit: LIMIT_MAX,
+                skip: 0,
+                select: {
+                    _id: true,
+                },
+            });
+
+
+        // now order these domains
+
+        for (const statusPageDomain of statusPageDomains) {
+            // get status page domain.
+
+            try {
+                await StatusPageDomainService.orderCert(statusPageDomain);
+            } catch (e) {
+                logger.error(e);
+            }
+        }
+    }
+
+    public override async rollback(): Promise<void> {
+        return;
+    }
+}
