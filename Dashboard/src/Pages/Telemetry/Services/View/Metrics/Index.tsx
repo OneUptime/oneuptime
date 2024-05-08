8d66d4f0c2d4e4c3b83e9666c5f556baeab628a9
Simon Larsen <simonlarsen@oneuptime.com>
2024-05-08 14:37:46 +0100

    feat: Add AnalyticsModelTable component to Metrics page

--- Dashboard/src/Pages/Telemetry/Services/View/Metrics/Index.tsx
+++ Dashboard/src/Pages/Telemetry/Services/View/Metrics/Index.tsx
@@ -1,13 +1,54 @@
 import React, { Fragment, FunctionComponent, ReactElement } from 'react';
 import PageComponentProps from '../../../../PageComponentProps';
-import ComingSoon from 'CommonUI/src/Components/ComingSoon/ComingSoon';
+import AnalyticsModelTable from 'CommonUI/src/Components/ModelTable/AnalyticsModelTable';
+import Metric from 'Model/AnalyticsModels/Metric';
+import DashboardNavigation from '../../../../../Utils/Navigation';
+import Navigation from 'CommonUI/src/Utils/Navigation';
+import ObjectID from 'Common/Types/ObjectID';
+import FieldType from 'CommonUI/src/Components/Types/FieldType';
 
 const ServiceDelete: FunctionComponent<PageComponentProps> = (
     _props: PageComponentProps
 ): ReactElement => {
+
+    const modelId: ObjectID = Navigation.getLastParamAsObjectID(1);
+    
     return (
         <Fragment>
-            <ComingSoon />
+            <AnalyticsModelTable<Metric>
+                modelType={Metric}
+                id="metrics-table"
+                isDeleteable={false}
+                isEditable={false}
+                isCreateable={false}
+                singularName="Metric"
+                pluralName="Metrics"
+                name="Metrics"
+                isViewable={true}
+                cardProps={{
+                    title: 'Metrics',
+                    description:
+                        'Metrics are the individual data points that make up a service. They are the building blocks of a service and represent the work done by a single service.',
+                }}
+                query={{
+                    projectId: DashboardNavigation.getProjectId(),
+                    serviceId: modelId,
+                }}
+                showViewIdButton={true}
+                noItemsMessage={'No metrics found for this service.'}
+                showRefreshButton={true}
+                viewPageRoute={Navigation.getCurrentRoute()}
+                filters={[]}
+                columns={[
+                    {
+                        field: {
+                            name: true,
+                        },
+                        title: 'Name',
+                        type: FieldType.Text,
+                    }
+                ]}
+            />
         </Fragment>
     );
 };
