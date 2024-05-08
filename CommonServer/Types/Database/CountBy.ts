d6aafa84934107289146924890ea23f046f7c646
Simon Larsen <simonlarsen@oneuptime.com>
2024-05-08 16:51:10 +0100

    feat: Add support for counting with groupBy in DatabaseService

--- CommonServer/Types/Database/CountBy.ts
+++ CommonServer/Types/Database/CountBy.ts
@@ -2,10 +2,12 @@ import BaseModel from 'Common/Models/BaseModel';
 import PositiveNumber from 'Common/Types/PositiveNumber';
 import DatabaseCommonInteractionProps from 'Common/Types/BaseDatabase/DatabaseCommonInteractionProps';
 import Query from './Query';
+import GroupBy from './GroupBy';
 
 export default interface CountBy<TBaseModel extends BaseModel> {
     query: Query<TBaseModel>;
     skip?: PositiveNumber | number;
+    groupBy?: GroupBy<TBaseModel> | undefined;
     limit?: PositiveNumber | number;
     props: DatabaseCommonInteractionProps;
     distinctOn?: string | undefined;
