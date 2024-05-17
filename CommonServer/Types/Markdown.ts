7edd8f197a1b9021451bcc1a6de170e001c809cd
Simon Larsen <simonlarsen@oneuptime.com>
2024-05-17 13:32:56 +0100

    refactor: Remove unused code in Markdown.ts
    This code change removes the unused code in the Markdown.ts file that was responsible for customizing the rendering of paragraphs, blockquotes, images, and headings. This code is no longer needed and can be safely removed, simplifying the codebase and improving maintainability.

--- CommonServer/Types/Markdown.ts
+++ CommonServer/Types/Markdown.ts
@@ -45,39 +45,6 @@ export default class Markdown {
 
         const renderer: Renderer = new Renderer();
 
-        renderer.paragraph = function (text) {
-            return `<p class="mt-2 mb-2 leading-8 text-gray-600">${text}</p>`;
-        };
-
-        renderer.blockquote = function (quote) {
-            return `<blockquote class="p-4 pt-1 pb-1 my-4 border-s-4 border-indigo-500">
-            <div class="leading-8 text-gray-600">${quote}</div>
-        </blockquote>`;
-        };
-
-        renderer.image = function (href, _title, text) {
-            return `<img src="${href}" alt="${text}" class="rounded-md shadow-md" />`;
-        };
-
-        renderer.code = function (code, language) {
-            return `<pre class="language-${language} rounded-md"><code class="language-${language} rounded-md">${code}</code></pre>`;
-        };
-
-        renderer.heading = function (text, level) {
-            if (level === 1) {
-                return `<h1 class="my-5 mt-8 text-4xl font-bold tracking-tight text-gray-800">${text}</h1>`;
-            } else if (level === 2) {
-                return `<h2 class="my-5  mt-8 text-3xl font-bold tracking-tight text-gray-800">${text}</h2>`;
-            } else if (level === 3) {
-                return `<h3 class="my-5  mt-8 text-2xl font-bold tracking-tight text-gray-800">${text}</h3>`;
-            } else if (level === 4) {
-                return `<h4 class="my-5  mt-8 text-xl font-bold tracking-tight text-gray-800">${text}</h4>`;
-            } else if (level === 5) {
-                return `<h5 class="my-5  mt-8 text-lg font-bold tracking-tight text-gray-800">${text}</h5>`;
-            }
-            return `<h6 class="my-5 tracking-tight font-bold text-gray-800">${text}</h6>`;
-        };
-
         this.emailRenderer = renderer;
 
         return renderer;
