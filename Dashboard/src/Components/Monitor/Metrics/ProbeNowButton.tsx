e94cc8083aecc03f20f133c0ce63ea1351236375
Simon Larsen <simonlarsen@oneuptime.com>
2024-05-23 18:58:43 +0100

    refactor: Improve error messages in MetricsInfo and SSLCertificateMonitorView components

--- Dashboard/src/Components/Monitor/Metrics/ProbeNowButton.tsx
+++ Dashboard/src/Components/Monitor/Metrics/ProbeNowButton.tsx
@@ -3,7 +3,7 @@ import React, { FunctionComponent, ReactElement } from 'react';
 export interface ComponentProps {}
 
 const Metrics: FunctionComponent<ComponentProps> = (
-    props: ComponentProps
+    
 ): ReactElement => {
     return <div></div>;
 };
