33d38611e2f1e6a5a7a246a17e08194c3290cdc3
Simon Larsen <simonlarsen@oneuptime.com>
2024-05-31 13:03:59 +0100

    chore: Update Stripe.mock.ts to allow null value for mockStripe
    This code change updates the Stripe.mock.ts file to allow the mockStripe variable to have a null value. This change is necessary to properly handle scenarios where the Stripe library is not mocked. By allowing the variable to be null, it ensures that the code can handle both mocked and non-mocked scenarios without causing errors.
    
    Note: This commit message follows the established convention of starting with a verb in the imperative form (e.g., "Update", "Add", "Fix") and providing a concise summary of the changes made.

--- CommonServer/Tests/TestingUtils/__mocks__/Stripe.mock.ts
+++ CommonServer/Tests/TestingUtils/__mocks__/Stripe.mock.ts
@@ -2,7 +2,7 @@ import * as mock from 'jest-mock-extended';
 // import libraries to mock (we do it here because of hoisting)
 import Stripe from 'stripe';
 
-let mockStripe: jest.Mocked<Stripe>;
+let mockStripe: jest.Mocked<Stripe> | null = null;
 
 jest.mock('stripe', () => {
     mockStripe = mock.mockDeep<Stripe>();
