ff1195c837680138008acce0dcd9910e22efcdff
Simon Larsen <simonlarsen@oneuptime.com>
2024-06-01 19:37:32 +0100

    refactor: Remove unnecessary code in DatabaseService.ts

--- CommonServer/Services/DatabaseService.ts
+++ CommonServer/Services/DatabaseService.ts
@@ -955,7 +955,6 @@ class DatabaseService<TBaseModel extends BaseModel> extends BaseService {
 
     private async _deleteBy(deleteBy: DeleteBy<TBaseModel>): Promise<number> {
         try {
-
             if (this.doNotAllowDelete && !deleteBy.props.isRoot) {
                 throw new BadDataException('Delete not allowed');
             }
