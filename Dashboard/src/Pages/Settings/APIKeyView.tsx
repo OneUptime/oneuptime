6785e62edd53d4a0615946800afd8942cf034c90
Simon Larsen <simonlarsen@oneuptime.com>
2024-06-02 21:06:10 +0100

    refactor: Update generatePhone method in Faker.ts
    The `generatePhone` method in `Faker.ts` has been updated to remove the country code parameter from the `faker.phone.number` function call. This change improves the consistency and simplicity of generating phone numbers using the Faker library.

--- Dashboard/src/Pages/Settings/APIKeyView.tsx
+++ Dashboard/src/Pages/Settings/APIKeyView.tsx
@@ -28,16 +28,226 @@ import React, {
     ReactElement,
 } from 'react';
 
+export enum PermissionType {
+    AllowPermissions = 'AllowPermissions',
+    BlockPermissions = 'BlockPermissions',
+}
+
+
 const APIKeyView: FunctionComponent<PageComponentProps> = (
     props: PageComponentProps
 ): ReactElement => {
     const modelId: ObjectID = Navigation.getLastParamAsObjectID();
     const [refresher, setRefresher] = React.useState<boolean>(false);
 
-    const formRef: MutableRefObject<FormProps<FormValues<ApiKeyPermission>>> =
-        React.useRef<
-            FormProps<FormValues<ApiKeyPermission>>
-        >() as MutableRefObject<FormProps<FormValues<ApiKeyPermission>>>;
+
+
+
+    const getPermissionTable = (data: {
+        permissionType: PermissionType
+    }): ReactElement => {
+        const { permissionType } = data;
+
+        const formRef: MutableRefObject<FormProps<FormValues<ApiKeyPermission>>> =
+            React.useRef<
+                FormProps<FormValues<ApiKeyPermission>>
+            >() as MutableRefObject<FormProps<FormValues<ApiKeyPermission>>>;
+
+        let tableTitle = 'Allow Permissions';
+
+        if (permissionType === PermissionType.BlockPermissions) {
+            tableTitle = 'Block Permissions';
+        }
+
+        let tableDescription = 'Here you can manage allow permissions for this team.';
+
+        if (permissionType === PermissionType.BlockPermissions) {
+            tableDescription = 'Here you can manage block permissions for this team. This will override any allow permissions set for this team.';
+        }
+
+        {/* API Key Permisison Table */ }
+
+        return (<ModelTable<ApiKeyPermission>
+            modelType={ApiKeyPermission}
+            id="api-key-permission-table"
+            isDeleteable={true}
+            name="Settings > API Key > Permissions"
+            query={{
+                apiKeyId: modelId,
+                projectId: DashboardNavigation.getProjectId()?.toString(),
+                isBlockPermission: permissionType === PermissionType.BlockPermissions,
+            }}
+            onBeforeCreate={(
+                item: ApiKeyPermission
+            ): Promise<ApiKeyPermission> => {
+                if (!props.currentProject || !props.currentProject._id) {
+                    throw new BadDataException('Project ID cannot be null');
+                }
+
+                item.apiKeyId = modelId;
+                item.projectId = new ObjectID(props.currentProject._id);
+                item.isBlockPermission = permissionType === PermissionType.BlockPermissions;
+                return Promise.resolve(item);
+            }}
+            isEditable={true}
+            isCreateable={true}
+            isViewable={false}
+            cardProps={{
+                title: tableTitle,
+                description:
+                    tableDescription,
+            }}
+            noItemsMessage={
+                'No permisisons created for this API Key so far.'
+            }
+            createEditFromRef={formRef}
+            formFields={[
+                {
+                    field: {
+                        permission: true,
+                    },
+                    onChange: async (_value: any) => {
+                        await formRef.current.setFieldValue(
+                            'labels',
+                            [],
+                            true
+                        );
+                    },
+                    title: 'Permission',
+                    fieldType: FormFieldSchemaType.Dropdown,
+                    required: true,
+                    placeholder: 'Permission',
+                    dropdownOptions:
+                        PermissionUtil.projectPermissionsAsDropdownOptions(),
+                },
+                {
+                    field: {
+                        labels: true,
+                    },
+                    title: 'Restrict to Labels',
+                    description:
+                        'If you want to restrict this permission to specific labels, you can select them here. This is an optional and an advanced feature.',
+                    fieldType: FormFieldSchemaType.MultiSelectDropdown,
+                    dropdownModal: {
+                        type: Label,
+                        labelField: 'name',
+                        valueField: '_id',
+                    },
+                    showIf: (
+                        values: FormValues<TeamPermission>
+                    ): boolean => {
+                        if (!values['permission']) {
+                            return false;
+                        }
+
+                        if (
+                            values['permission'] &&
+                            !PermissionHelper.isAccessControlPermission(
+                                values['permission'] as Permission
+                            )
+                        ) {
+                            return false;
+                        }
+
+                        return true;
+                    },
+                    required: false,
+                    placeholder: 'Labels',
+                },
+            ]}
+            showRefreshButton={true}
+            viewPageRoute={Navigation.getCurrentRoute()}
+            filters={[
+                {
+                    field: {
+                        permission: true,
+                    },
+                    title: 'Permission',
+                    type: FieldType.Text,
+                },
+                {
+                    field: {
+                        labels: {
+                            name: true,
+                        },
+                    },
+                    title: 'Restrict to Labels',
+                    type: FieldType.EntityArray,
+                    filterEntityType: Label,
+                    filterQuery: {
+                        projectId:
+                            DashboardNavigation.getProjectId()?.toString(),
+                    },
+                    filterDropdownField: {
+                        label: 'name',
+                        value: '_id',
+                    },
+                },
+            ]}
+            columns={[
+                {
+                    field: {
+                        permission: true,
+                    },
+                    title: 'Permission',
+                    type: FieldType.Text,
+
+                    getElement: (item: ApiKeyPermission): ReactElement => {
+                        return (
+                            <p>
+                                {PermissionHelper.getTitle(
+                                    item['permission'] as Permission
+                                )}
+                            </p>
+                        );
+                    },
+                },
+                {
+                    field: {
+                        labels: {
+                            name: true,
+                            color: true,
+                        },
+                    },
+                    title: 'Restrict to Labels',
+                    type: FieldType.EntityArray,
+
+                    getElement: (item: ApiKeyPermission): ReactElement => {
+                        if (
+                            item &&
+                            item['permission'] &&
+                            !PermissionHelper.isAccessControlPermission(
+                                item['permission'] as Permission
+                            )
+                        ) {
+                            return (
+                                <p>
+                                    Restriction by labels cannot be applied
+                                    to this permission.
+                                </p>
+                            );
+                        }
+
+                        if (
+                            !item['labels'] ||
+                            item['labels'].length === 0
+                        ) {
+                            return (
+                                <p>
+                                    No restrictions has been applied to this
+                                    permission.
+                                </p>
+                            );
+                        }
+
+                        return (
+                            <LabelsElement labels={item['labels'] || []} />
+                        );
+                    },
+                },
+            ]}
+        />)
+    }
 
     return (
         <Fragment>
@@ -123,187 +333,7 @@ const APIKeyView: FunctionComponent<PageComponentProps> = (
                 }}
             />
 
-            {/* API Key Permisison Table */}
 
-            <ModelTable<ApiKeyPermission>
-                modelType={ApiKeyPermission}
-                id="api-key-permission-table"
-                isDeleteable={true}
-                name="Settings > API Key > Permissions"
-                query={{
-                    apiKeyId: modelId,
-                    projectId: DashboardNavigation.getProjectId()?.toString(),
-                    isBlockPermission: false,
-                }}
-                onBeforeCreate={(
-                    item: ApiKeyPermission
-                ): Promise<ApiKeyPermission> => {
-                    if (!props.currentProject || !props.currentProject._id) {
-                        throw new BadDataException('Project ID cannot be null');
-                    }
-
-                    item.apiKeyId = modelId;
-                    item.projectId = new ObjectID(props.currentProject._id);
-                    return Promise.resolve(item);
-                }}
-                isEditable={true}
-                isCreateable={true}
-                isViewable={false}
-                cardProps={{
-                    title: 'Allow Permissions',
-                    description:
-                        'Here you can manage allow permissions for this API Key.',
-                }}
-                noItemsMessage={
-                    'No permisisons created for this API Key so far.'
-                }
-                createEditFromRef={formRef}
-                formFields={[
-                    {
-                        field: {
-                            permission: true,
-                        },
-                        onChange: async (_value: any) => {
-                            await formRef.current.setFieldValue(
-                                'labels',
-                                [],
-                                true
-                            );
-                        },
-                        title: 'Permission',
-                        fieldType: FormFieldSchemaType.Dropdown,
-                        required: true,
-                        placeholder: 'Permission',
-                        dropdownOptions:
-                            PermissionUtil.projectPermissionsAsDropdownOptions(),
-                    },
-                    {
-                        field: {
-                            labels: true,
-                        },
-                        title: 'Restrict to Labels',
-                        description:
-                            'If you want to restrict this permission to specific labels, you can select them here. This is an optional and an advanced feature.',
-                        fieldType: FormFieldSchemaType.MultiSelectDropdown,
-                        dropdownModal: {
-                            type: Label,
-                            labelField: 'name',
-                            valueField: '_id',
-                        },
-                        showIf: (
-                            values: FormValues<TeamPermission>
-                        ): boolean => {
-                            if (!values['permission']) {
-                                return false;
-                            }
-
-                            if (
-                                values['permission'] &&
-                                !PermissionHelper.isAccessControlPermission(
-                                    values['permission'] as Permission
-                                )
-                            ) {
-                                return false;
-                            }
-
-                            return true;
-                        },
-                        required: false,
-                        placeholder: 'Labels',
-                    },
-                ]}
-                showRefreshButton={true}
-                viewPageRoute={Navigation.getCurrentRoute()}
-                filters={[
-                    {
-                        field: {
-                            permission: true,
-                        },
-                        title: 'Permission',
-                        type: FieldType.Text,
-                    },
-                    {
-                        field: {
-                            labels: {
-                                name: true,
-                            },
-                        },
-                        title: 'Restrict to Labels',
-                        type: FieldType.EntityArray,
-                        filterEntityType: Label,
-                        filterQuery: {
-                            projectId:
-                                DashboardNavigation.getProjectId()?.toString(),
-                        },
-                        filterDropdownField: {
-                            label: 'name',
-                            value: '_id',
-                        },
-                    },
-                ]}
-                columns={[
-                    {
-                        field: {
-                            permission: true,
-                        },
-                        title: 'Permission',
-                        type: FieldType.Text,
-
-                        getElement: (item: ApiKeyPermission): ReactElement => {
-                            return (
-                                <p>
-                                    {PermissionHelper.getTitle(
-                                        item['permission'] as Permission
-                                    )}
-                                </p>
-                            );
-                        },
-                    },
-                    {
-                        field: {
-                            labels: {
-                                name: true,
-                                color: true,
-                            },
-                        },
-                        title: 'Restrict to Labels',
-                        type: FieldType.EntityArray,
-
-                        getElement: (item: ApiKeyPermission): ReactElement => {
-                            if (
-                                item &&
-                                item['permission'] &&
-                                !PermissionHelper.isAccessControlPermission(
-                                    item['permission'] as Permission
-                                )
-                            ) {
-                                return (
-                                    <p>
-                                        Restriction by labels cannot be applied
-                                        to this permission.
-                                    </p>
-                                );
-                            }
-
-                            if (
-                                !item['labels'] ||
-                                item['labels'].length === 0
-                            ) {
-                                return (
-                                    <p>
-                                        No restrictions has been applied to this
-                                        permission.
-                                    </p>
-                                );
-                            }
-
-                            return (
-                                <LabelsElement labels={item['labels'] || []} />
-                            );
-                        },
-                    },
-                ]}
-            />
 
             <ResetObjectID<ApiKey>
                 modelType={ApiKey}
