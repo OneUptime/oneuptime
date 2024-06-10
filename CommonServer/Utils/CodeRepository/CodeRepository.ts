64ae5eeb898985f5f0f7226eae545c2419ef232e
Simon Larsen <simonlarsen@oneuptime.com>
2024-06-10 19:40:27 +0100

    add code repository basic functions

--- CommonServer/Utils/CodeRepository/CodeRepository.ts
+++ CommonServer/Utils/CodeRepository/CodeRepository.ts
@@ -1,5 +1,78 @@
+import Execute from '../Execute';
+import CodeRepositoryFile from './CodeRepositoryFile';
+
 export default class CodeRepositoryUtil {
-    public static getGitCommitHashForFile(filePath: string): string {
-        // this is git repository.
+    public static async getGitCommitHashForFile(
+        filePath: string
+    ): Promise<string> {
+        return await Execute.executeCommand(
+            `git log -1 --pretty=format:"%H" ${filePath}`
+        );
+    }
+
+    public static async getFilesInDirectory(directoryPath: string): Promise<{
+        files: Array<CodeRepositoryFile>;
+        subDirectories: Array<string>;
+    }> {
+        const files: Array<CodeRepositoryFile> = [];
+        const output: string = await Execute.executeCommand(
+            `ls ${directoryPath}`
+        );
+
+        const fileNames: Array<string> = output.split('\n');
+
+        const subDirectories: Array<string> = [];
+
+        for (const fileName of fileNames) {
+            if (fileName === '') {
+                continue;
+            }
+
+            const isDirectory: boolean = (
+                await Execute.executeCommand(
+                    `file ${directoryPath}/${fileName}`
+                )
+            ).includes('directory');
+
+            if (isDirectory) {
+                subDirectories.push(`${directoryPath}/${fileName}`);
+                continue;
+            }
+
+            const filePath: string = `${directoryPath}/${fileName}`;
+            const gitCommitHash: string = await this.getGitCommitHashForFile(
+                filePath
+            );
+            const fileExtension: string = fileName.split('.').pop() || '';
+            files.push({
+                filePath,
+                gitCommitHash,
+                fileExtension,
+                fileName,
+            });
+        }
+
+        return {
+            files,
+            subDirectories: subDirectories,
+        };
+    }
+
+    public static async getFilesInDirectoryRecursive(
+        directoryPath: string
+    ): Promise<Array<CodeRepositoryFile>> {
+        const files: Array<CodeRepositoryFile> = [];
+
+        const { files: filesInDirectory, subDirectories } =
+            await this.getFilesInDirectory(directoryPath);
+        files.push(...filesInDirectory);
+
+        for (const subDirectory of subDirectories) {
+            files.push(
+                ...(await this.getFilesInDirectoryRecursive(subDirectory))
+            );
+        }
+
+        return files;
     }
 }
--- Nginx/Utils/SelfSignedSSL.ts
+++ Nginx/Utils/SelfSignedSSL.ts
@@ -1,8 +1,8 @@
-import Exec from 'CommonServer/Utils/Exec';
+import Exec from 'CommonServer/Utils/Execute';
 
 export default class SelfSignedSSL {
     public static async generate(path: string, host: string): Promise<void> {
-        return await Exec.exec(
+        await Exec.executeCommand(
             `mkdir -p ${path} && openssl req -new -x509 -nodes -subj "/C=US/ST=NY/L=NYC/O=Global Security/OU=IT Department/CN=example.com" -out ${path}/${host}.crt -keyout ${path}/${host}.key -days 99999 && chmod -R 777 ${path}`
         );
     }
