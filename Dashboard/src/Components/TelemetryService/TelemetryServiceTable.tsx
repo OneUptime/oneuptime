c6961b9a0bfebbd24ac67a02ec6cf6897c459f63
Simon Larsen <simonlarsen@oneuptime.com>
2024-07-16 16:28:54 -0600

    refactor: Update TelemetryServiceTable component
    This commit refactors the Services page by replacing the ModelTable component with the TelemetryServiceTable component. The TelemetryServiceTable component provides a more specialized and optimized table for displaying telemetry services. This change improves the performance and user experience of the Services page.

---
+++ Dashboard/src/Components/TelemetryService/TelemetryServiceTable.tsx
@@ -0,0 +1,155 @@
+import LabelsElement from "../../Components/Label/Labels";
+import DashboardNavigation from "../../Utils/Navigation";
+import FormFieldSchemaType from "CommonUI/src/Components/Forms/Types/FormFieldSchemaType";
+import ModelTable from "CommonUI/src/Components/ModelTable/ModelTable";
+import FieldType from "CommonUI/src/Components/Types/FieldType";
+import Query from "CommonUI/src/Utils/BaseDatabase/Query";
+import Label from "Model/Models/Label";
+import Monitor from "Model/Models/Monitor";
+import React, { Fragment, FunctionComponent, ReactElement } from "react";
+import ActionButtonSchema from "CommonUI/src/Components/ActionButton/ActionButtonSchema";
+import { CardButtonSchema } from "CommonUI/src/Components/Card/Card";
+import TelemetryService from "Model/Models/TelemetryService";
+import RouteMap, { RouteUtil } from "../../Utils/RouteMap";
+import PageMap from "../../Utils/PageMap";
+import TelemetryServiceElement from "./TelemetryServiceElement";
+
+export interface ComponentProps {
+  query?: Query<Monitor> | undefined;
+  noItemsMessage?: string | undefined;
+  title?: string | undefined;
+  description?: string | undefined;
+  disableCreate?: boolean | undefined;
+  actionButtons?: Array<ActionButtonSchema<Monitor>> | undefined;
+  cardButtons?: Array<CardButtonSchema> | undefined;
+}
+
+const TelemetryServiceTable: FunctionComponent<ComponentProps> = (
+  props: ComponentProps,
+): ReactElement => {
+  return (
+    <ModelTable<TelemetryService>
+      modelType={TelemetryService}
+      id="services-table"
+      isDeleteable={false}
+      isEditable={false}
+      query={props.query || {}}
+      actionButtons={props.actionButtons}
+      isCreateable={!props.disableCreate}
+      name="Services"
+      isViewable={true}
+      cardProps={{
+        title: props.title || "Services",
+        description:
+          props.description || "Here is a list of services for this project.",
+        buttons: props.cardButtons,
+      }}
+      showViewIdButton={true}
+      noItemsMessage={props.noItemsMessage || "No services found."}
+      formFields={[
+        {
+          field: {
+            name: true,
+          },
+          title: "Name",
+          fieldType: FormFieldSchemaType.Text,
+          required: true,
+          placeholder: "Service  Name",
+          validation: {
+            minLength: 2,
+          },
+        },
+        {
+          field: {
+            description: true,
+          },
+          title: "Description",
+          fieldType: FormFieldSchemaType.LongText,
+          required: true,
+          placeholder: "Service Description",
+        },
+      ]}
+      showRefreshButton={true}
+      viewPageRoute={RouteUtil.populateRouteParams(
+        RouteMap[PageMap.TELEMETRY_SERVICES]!,
+      )}
+      filters={[
+        {
+          field: {
+            name: true,
+          },
+          title: "Name",
+          type: FieldType.Text,
+        },
+        {
+          field: {
+            description: true,
+          },
+          title: "Description",
+          type: FieldType.Text,
+        },
+        {
+          field: {
+            labels: {
+              name: true,
+              color: true,
+            },
+          },
+          title: "Labels",
+          type: FieldType.EntityArray,
+
+          filterEntityType: Label,
+          filterQuery: {
+            projectId: DashboardNavigation.getProjectId()?.toString(),
+          },
+          filterDropdownField: {
+            label: "name",
+            value: "_id",
+          },
+        },
+      ]}
+      selectMoreFields={{
+        serviceColor: true,
+      }}
+      columns={[
+        {
+          field: {
+            name: true,
+          },
+          title: "Name",
+          type: FieldType.Element,
+          getElement: (service: TelemetryService): ReactElement => {
+            return (
+              <Fragment>
+                <TelemetryServiceElement telemetryService={service} />
+              </Fragment>
+            );
+          },
+        },
+        {
+          field: {
+            description: true,
+          },
+          title: "Description",
+          type: FieldType.Text,
+        },
+        {
+          field: {
+            labels: {
+              name: true,
+              color: true,
+            },
+          },
+          title: "Labels",
+          type: FieldType.EntityArray,
+
+          getElement: (item: TelemetryService): ReactElement => {
+            return <LabelsElement labels={item["labels"] || []} />;
+          },
+        },
+      ]}
+    />
+  );
+};
+
+export default TelemetryServiceTable;
--- Dashboard/src/Pages/Telemetry/Services.tsx
+++ Dashboard/src/Pages/Telemetry/Services.tsx
@@ -1,20 +1,12 @@
-import LabelsElement from "../../Components/Label/Labels";
-import TelemetryServiceElement from "../../Components/TelemetryService/TelemetryServiceElement";
-import DashboardNavigation from "../../Utils/Navigation";
 import PageMap from "../../Utils/PageMap";
 import RouteMap, { RouteUtil } from "../../Utils/RouteMap";
 import PageComponentProps from "../PageComponentProps";
 import SideMenu from "./SideMenu";
 import Route from "Common/Types/API/Route";
 import ErrorMessage from "CommonUI/src/Components/ErrorMessage/ErrorMessage";
-import FormFieldSchemaType from "CommonUI/src/Components/Forms/Types/FormFieldSchemaType";
-import ModelTable from "CommonUI/src/Components/ModelTable/ModelTable";
 import Page from "CommonUI/src/Components/Page/Page";
-import FieldType from "CommonUI/src/Components/Types/FieldType";
-import Navigation from "CommonUI/src/Utils/Navigation";
-import Label from "Model/Models/Label";
-import TelemetryService from "Model/Models/TelemetryService";
-import React, { Fragment, FunctionComponent, ReactElement } from "react";
+import React, { FunctionComponent, ReactElement } from "react";
+import TelemetryServiceTable from "../../Components/TelemetryService/TelemetryServiceTable";
 
 const Services: FunctionComponent<PageComponentProps> = (
   props: PageComponentProps,
@@ -51,121 +43,7 @@ const Services: FunctionComponent<PageComponentProps> = (
       ]}
       sideMenu={<SideMenu />}
     >
-      <ModelTable<TelemetryService>
-        modelType={TelemetryService}
-        id="services-table"
-        isDeleteable={false}
-        isEditable={false}
-        isCreateable={true}
-        name="Services"
-        isViewable={true}
-        cardProps={{
-          title: "Services",
-          description: "Here is a list of services for this project.",
-        }}
-        showViewIdButton={true}
-        noItemsMessage={"No services found."}
-        formFields={[
-          {
-            field: {
-              name: true,
-            },
-            title: "Name",
-            fieldType: FormFieldSchemaType.Text,
-            required: true,
-            placeholder: "Service  Name",
-            validation: {
-              minLength: 2,
-            },
-          },
-          {
-            field: {
-              description: true,
-            },
-            title: "Description",
-            fieldType: FormFieldSchemaType.LongText,
-            required: true,
-            placeholder: "Service Description",
-          },
-        ]}
-        showRefreshButton={true}
-        viewPageRoute={Navigation.getCurrentRoute()}
-        filters={[
-          {
-            field: {
-              name: true,
-            },
-            title: "Name",
-            type: FieldType.Text,
-          },
-          {
-            field: {
-              description: true,
-            },
-            title: "Description",
-            type: FieldType.Text,
-          },
-          {
-            field: {
-              labels: {
-                name: true,
-                color: true,
-              },
-            },
-            title: "Labels",
-            type: FieldType.EntityArray,
-
-            filterEntityType: Label,
-            filterQuery: {
-              projectId: DashboardNavigation.getProjectId()?.toString(),
-            },
-            filterDropdownField: {
-              label: "name",
-              value: "_id",
-            },
-          },
-        ]}
-        selectMoreFields={{
-          serviceColor: true,
-        }}
-        columns={[
-          {
-            field: {
-              name: true,
-            },
-            title: "Name",
-            type: FieldType.Element,
-            getElement: (service: TelemetryService): ReactElement => {
-              return (
-                <Fragment>
-                  <TelemetryServiceElement telemetryService={service} />
-                </Fragment>
-              );
-            },
-          },
-          {
-            field: {
-              description: true,
-            },
-            title: "Description",
-            type: FieldType.Text,
-          },
-          {
-            field: {
-              labels: {
-                name: true,
-                color: true,
-              },
-            },
-            title: "Labels",
-            type: FieldType.EntityArray,
-
-            getElement: (item: TelemetryService): ReactElement => {
-              return <LabelsElement labels={item["labels"] || []} />;
-            },
-          },
-        ]}
-      />
+      <TelemetryServiceTable />
     </Page>
   );
 };
