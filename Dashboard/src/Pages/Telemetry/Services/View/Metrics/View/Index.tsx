9dae8e17644c795855325b9dda75b8eb360365ff
Simon Larsen <simonlarsen@oneuptime.com>
2024-07-21 10:06:08 -0600

    refactor: Remove commented out code for empty formula configuration

--- Dashboard/src/Pages/Telemetry/Services/View/Metrics/View/Index.tsx
+++ Dashboard/src/Pages/Telemetry/Services/View/Metrics/View/Index.tsx
@@ -1,5 +1,5 @@
 import MetricsAggregationType from "Common/Types/Metrics/MetricsAggregationType";
-import MetricView from "../../../../../../Components/Metrics/MetricViewa";
+import MetricView from "../../../../../../Components/Metrics/MetricView";
 import PageComponentProps from "../../../../../PageComponentProps";
 import Navigation from "CommonUI/src/Utils/Navigation";
 import React, { FunctionComponent, ReactElement } from "react";
