229b5cd772a44f0c5ac61e3b313196fd57661f3b
Simon Larsen <simonlarsen@oneuptime.com>
2024-07-30 09:39:19 -0600

    feat: Add ArrayUtil import to TelemetryAPI
    This commit adds the import statement for the ArrayUtil module to the TelemetryAPI file. The ArrayUtil module is used to remove duplicate attributes fetched from the server. This import ensures that the ArrayUtil functions can be used in the TelemetryAPI module, improving the efficiency and accuracy of attribute handling in the telemetry system.

--- CommonServer/API/TelemetryAPI.ts
+++ CommonServer/API/TelemetryAPI.ts
@@ -11,6 +11,7 @@ import CommonAPI from "./CommonAPI";
 import DatabaseCommonInteractionProps from "Common/Types/BaseDatabase/DatabaseCommonInteractionProps";
 import TelemetryType from "Common/Types/Telemetry/TelemetryType";
 import TelemetryAttributeService from "../Services/TelemetryAttributeService";
+import ArrayUtil from "Common/Types/ArrayUtil";
 
 const router: ExpressRouter = Express.getRouter();
 
@@ -71,11 +72,12 @@ const getAttributes: GetAttributesFunction = async (
       );
     }
 
-    const attributes: string[] =
+    const attributes: string[] = ArrayUtil.removeDuplicates(
       await TelemetryAttributeService.fetchAttributes({
         projectId: databaseProps.tenantId,
         telemetryType,
-      });
+      }),
+    );
 
     return Response.sendJsonObjectResponse(req, res, {
       attributes: attributes,
