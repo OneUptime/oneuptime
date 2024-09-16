5a317858957f978f8026f40d07106010b92bf248
Simon Larsen <simonlarsen@oneuptime.com>
2024-09-16 08:30:48 -0700

    Add initial Worker module with configuration files, Dockerfile, and README

---
+++ Worker/Index.ts
@@ -0,0 +1,52 @@
+import WorkerRoutes from "./Routes";
+import { PromiseVoidFunction } from "Common/Types/FunctionTypes";
+import InfrastructureStatus from "Common/Server/Infrastructure/Status";
+import logger from "Common/Server/Utils/Logger";
+import App from "Common/Server/Utils/StartServer";
+import Telemetry from "Common/Server/Utils/Telemetry";
+import "ejs";
+
+const APP_NAME: string = "worker";
+
+const init: PromiseVoidFunction = async (): Promise<void> => {
+  try {
+    // Initialize telemetry
+    Telemetry.init({
+      serviceName: APP_NAME,
+    });
+
+    const statusCheck: PromiseVoidFunction = async (): Promise<void> => {
+      // Check the status of infrastructure components
+      return await InfrastructureStatus.checkStatus({
+        checkClickhouseStatus: false,
+        checkPostgresStatus: false,
+        checkRedisStatus: false,
+      });
+    };
+
+    // Initialize the app with service name and status checks
+    await App.init({
+      appName: APP_NAME,
+      statusOptions: {
+        liveCheck: statusCheck,
+        readyCheck: statusCheck,
+      },
+    });
+
+    // Initialize home routes at the end since it has a catch-all route
+    await WorkerRoutes.init();
+
+    // Add default routes to the app
+    await App.addDefaultRoutes();
+  } catch (err) {
+    logger.error("App Init Failed:");
+    logger.error(err);
+    throw err;
+  }
+};
+
+init().catch((err: Error) => {
+  logger.error(err);
+  logger.error("Exiting node process");
+  process.exit(1);
+});
