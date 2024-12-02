91b2a6e44f7fb1994e3d91eb02f752deb4f26cad
Simon Larsen <simonlarsen@oneuptime.com>
2024-12-02 18:13:11 +0000

    Update DashboardTextComponent to adjust text height calculation and apply it as font size for better text display

--- Dashboard/src/Components/Dashboard/Components/DashboardTextComponent.tsx
+++ Dashboard/src/Components/Dashboard/Components/DashboardTextComponent.tsx
@@ -10,14 +10,14 @@ const DashboardTextComponentElement: FunctionComponent<ComponentProps> = (
   props: ComponentProps,
 ): ReactElement => {
   const textClassName: string = `truncate ${props.component.arguments.isBold ? "font-medium" : ""} ${props.component.arguments.isItalic ? "italic" : ""} ${props.component.arguments.isUnderline ? "underline" : ""}`;
-  const textHeightInxPx: number = props.dashboardComponentHeightInPx - 20;
+  const textHeightInxPx: number = props.dashboardComponentHeightInPx * 0.50;
 
   return (
     <div>
       <div
         className={textClassName}
         style={{
-          height: textHeightInxPx > 0 ? `${textHeightInxPx}px` : "auto",
+          fontSize: textHeightInxPx > 0 ? `${textHeightInxPx}px` : "",
         }}
       >
         {props.component.arguments.text}
