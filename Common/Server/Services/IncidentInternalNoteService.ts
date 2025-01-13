8568cc0c891b82ecceddf4df4be62b6fa3c3e622
Simon Larsen <simonlarsen@oneuptime.com>
2025-01-13 08:48:58 +0000

    feat: implement logging for internal and public notes creation in IncidentNote services

--- Common/Server/Services/IncidentInternalNoteService.ts
+++ Common/Server/Services/IncidentInternalNoteService.ts
@@ -1,9 +1,57 @@
+import User from "../../Models/DatabaseModels/User";
+import Name from "../../Types/Name";
+import ObjectID from "../../Types/ObjectID";
 import DatabaseService from "./DatabaseService";
 import Model from "Common/Models/DatabaseModels/IncidentInternalNote";
+import UserService from "./UserService";
+import { OnCreate } from "../Types/Database/Hooks";
+import IncidentLogService from "./IncidentLogService";
+import { IncidentLogEventType } from "../../Models/DatabaseModels/IncidentLog";
+import { Blue500 } from "../../Types/BrandColors";
 
 export class Service extends DatabaseService<Model> {
   public constructor() {
     super(Model);
+  }
+
+
+  public override async onCreateSuccess(_onCreate: OnCreate<Model>, createdItem: Model): Promise<Model> {
+
+    const userId: ObjectID | null | undefined = createdItem.createdByUserId || createdItem.createdByUser?.id;
+    let userName: Name | null = null;
+
+    if (userId) {
+      const user: User | null = await UserService.findOneById({
+        id: userId,
+        select: {
+          name: true,
+          email: true
+        },
+        props: {
+          isRoot: true
+        }
+      });
+
+      if (user && user.name) {
+        userName = user.name;
+      }
+    }
+
+
+    await IncidentLogService.createIncidentLog({
+      incidentId: createdItem.id!,
+      projectId: createdItem.projectId!,
+      incidentLogEventType: IncidentLogEventType.PrivateNote,
+      displayColor: Blue500,
+      logInMarkdown: `**Private (Internal) Note Posted${userName ? " by "+userName : ""}**
+
+${createdItem.note}
+          `,
+    });
+
+
+    return createdItem;
+
   }
 }
 
--- Common/Server/Services/IncidentPublicNoteService.ts
+++ Common/Server/Services/IncidentPublicNoteService.ts
@@ -3,6 +3,13 @@ import { OnCreate } from "../Types/Database/Hooks";
 import DatabaseService from "./DatabaseService";
 import OneUptimeDate from "../../Types/Date";
 import Model from "Common/Models/DatabaseModels/IncidentPublicNote";
+import IncidentLogService from "./IncidentLogService";
+import { IncidentLogEventType } from "../../Models/DatabaseModels/IncidentLog";
+import { Blue500 } from "../../Types/BrandColors";
+import ObjectID from "../../Types/ObjectID";
+import User from "../../Models/DatabaseModels/User";
+import UserService from "./UserService";
+import Name from "../../Types/Name";
 
 export class Service extends DatabaseService<Model> {
   public constructor() {
@@ -20,6 +27,46 @@ export class Service extends DatabaseService<Model> {
       createBy: createBy,
       carryForward: null,
     };
+  }
+
+
+  public override async onCreateSuccess(_onCreate: OnCreate<Model>, createdItem: Model): Promise<Model> {
+
+    const userId: ObjectID | null | undefined = createdItem.createdByUserId || createdItem.createdByUser?.id;
+    let userName: Name | null = null;
+
+    if (userId) {
+      const user: User | null = await UserService.findOneById({
+        id: userId,
+        select: {
+          name: true,
+          email: true
+        },
+        props: {
+          isRoot: true
+        }
+      });
+
+      if (user && user.name) {
+        userName = user.name;
+      }
+    }
+
+
+    await IncidentLogService.createIncidentLog({
+      incidentId: createdItem.id!,
+      projectId: createdItem.projectId!,
+      incidentLogEventType: IncidentLogEventType.PublicNote,
+      displayColor: Blue500,
+      logInMarkdown: `**Note Posted on Status Page${userName ? " by "+userName : ""}**
+
+${createdItem.note}
+          `,
+    });
+
+
+    return createdItem;
+
   }
 }
 
