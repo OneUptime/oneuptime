0b69ae195f55616f7acb0c5f22c63de004c3802b
Simon Larsen <simonlarsen@oneuptime.com>
2025-01-14 14:47:28 +0000

    feat: implement onBeforeDelete and onDeleteSuccess methods to handle OwnerTeamRemoved events in IncidentOwnerTeamService

--- Common/Server/Services/IncidentOwnerTeamService.ts
+++ Common/Server/Services/IncidentOwnerTeamService.ts
@@ -1,16 +1,84 @@
 import ObjectID from "../../Types/ObjectID";
-import { OnCreate } from "../Types/Database/Hooks";
+import { OnCreate, OnDelete } from "../Types/Database/Hooks";
 import DatabaseService from "./DatabaseService";
 import Model from "Common/Models/DatabaseModels/IncidentOwnerTeam";
 import IncidentFeedService from "./IncidentFeedService";
 import { IncidentFeedEventType } from "../../Models/DatabaseModels/IncidentFeed";
-import { Gray500 } from "../../Types/BrandColors";
+import { Gray500, Red500 } from "../../Types/BrandColors";
 import TeamService from "./TeamService";
 import Team from "../../Models/DatabaseModels/Team";
+import DeleteBy from "../Types/Database/DeleteBy";
 
 export class Service extends DatabaseService<Model> {
   public constructor() {
     super(Model);
+  }
+
+
+  protected override async onBeforeDelete(deleteBy: DeleteBy<Model>): Promise<OnDelete<Model>> {
+    const itemsToDelete: Model[] = await this.findBy({
+      query: deleteBy.query,
+      limit: deleteBy.limit,
+      skip: deleteBy.skip,
+      props: {
+        isRoot: true
+      },
+      select: {
+        incidentId: true,
+        projectId: true,
+        teamId: true,
+      }
+    })
+
+    return {
+      carryForward: {
+        itemsToDelete: itemsToDelete
+      },
+      deleteBy: deleteBy
+    }
+  }
+
+  protected override async onDeleteSuccess(onDelete: OnDelete<Model>, _itemIdsBeforeDelete: Array<ObjectID>): Promise<OnDelete<Model>> {
+
+    const deleteByUserId: ObjectID | undefined = onDelete.deleteBy.deletedByUser?.id || onDelete.deleteBy.props.userId;
+
+    const itemsToDelete: Model[] = onDelete.carryForward.itemsToDelete;
+
+    for (const item of itemsToDelete) {
+
+      const incidentId: ObjectID | undefined = item.incidentId;
+      const projectId: ObjectID | undefined = item.projectId;
+      const teamId: ObjectID | undefined = item.teamId;
+
+
+      if (incidentId && teamId && projectId) {
+
+        const team: Team | null = await TeamService.findOneById({
+          id: teamId,
+          select: {
+            name: true
+          },
+          props: {
+            isRoot: true
+          }
+        })
+
+        if (team && team.name) {
+
+          await IncidentFeedService.createIncidentFeed({
+            incidentId: incidentId,
+            projectId: projectId,
+            incidentFeedEventType: IncidentFeedEventType.OwnerTeamRemoved,
+            displayColor: Red500,
+            feedInfoInMarkdown: `**Team ${team.name}** was removed from the incident as the owner.`,
+            userId: deleteByUserId || undefined,
+          });
+        }
+      }
+    }
+
+    return onDelete;
+
   }
 
   public override async onCreateSuccess(onCreate: OnCreate<Model>, createdItem: Model): Promise<Model> {
