8f45deee2a62812f063224178153020750097875
Simon Larsen <simonlarsen@oneuptime.com>
2025-03-14 22:26:21 +0000

    feat: add DisableTelemetry check in CaptureSpan decorator to conditionally disable telemetry tracking

--- Common/Server/Utils/Telemetry/CaptureSpan.ts
+++ Common/Server/Utils/Telemetry/CaptureSpan.ts
@@ -3,85 +3,91 @@ import { Span, SpanStatusCode } from "@opentelemetry/api";
 import logger from "../Logger"; // Make sure to import your logger
 import { JSONObject } from "../../../Types/JSON";
 import JSONFunctions from "../../../Types/JSONFunctions";
+import { DisableTelemetry } from "../../EnvironmentConfig";
 
 function CaptureSpan(data?: {
-  name?: string;
-  attributes?: JSONObject;
-  captureArguments?: boolean;
+    name?: string;
+    attributes?: JSONObject;
+    captureArguments?: boolean;
 }): (
-  _target: any,
-  propertyKey: string,
-  descriptor: PropertyDescriptor
-) => PropertyDescriptor {
-  return function (
     _target: any,
     propertyKey: string,
     descriptor: PropertyDescriptor
-  ) {
-    const originalMethod = descriptor.value;
+) => PropertyDescriptor {
+    return function (
+        _target: any,
+        propertyKey: string,
+        descriptor: PropertyDescriptor
+    ) {
+        const originalMethod = descriptor.value;
 
-    const name: string | undefined = data?.name || propertyKey;
+        const name: string | undefined = data?.name || propertyKey;
 
-    logger.debug(`Capturing span for ${name}`);
+        logger.debug(`Capturing span for ${name}`);
 
-    descriptor.value = async function (...args: any[]) {
-      let functionArguments = {};
+        descriptor.value = async function (...args: any[]) {
+            if (DisableTelemetry) {
+                logger.debug(`Telemetry is disabled. Running function ${name} without capturing span.`);
+                return await originalMethod.apply(this, args);
+            }
 
-      if (data?.captureArguments) {
-        functionArguments = args.reduce(
-          (acc: { [key: string]: any }, arg: any, index: number) => {
-            acc[`arg${index}`] = arg;
-            return acc;
-          },
-          {}
-        );
-      }
+            let functionArguments = {};
 
-      logger.debug(`Starting span for ${name} with args:`);
-      logger.debug(functionArguments);
+            if (data?.captureArguments) {
+                functionArguments = args.reduce(
+                    (acc: { [key: string]: any }, arg: any, index: number) => {
+                        acc[`arg${index}`] = arg;
+                        return acc;
+                    },
+                    {}
+                );
+            }
 
-      const spanAttributes = JSONFunctions.flattenObject({
-        ...functionArguments,
-        ...data?.attributes,
-      }) as { [key: string]: any };
+            logger.debug(`Starting span for ${name} with args:`);
+            logger.debug(functionArguments);
 
-      logger.debug(`Span attributes for ${name}:`);
-      logger.debug(spanAttributes);
+            const spanAttributes = JSONFunctions.flattenObject({
+                ...functionArguments,
+                ...data?.attributes,
+            }) as { [key: string]: any };
 
-      return await Telemetry.startActiveSpan<Promise<void>>({
-        name: name,
-        options: {
-          attributes: spanAttributes,
-        },
-        fn: async (span: Span): Promise<void> => {
-          try {
-            await originalMethod.apply(this, args);
-            // mark span as success
-            span.setStatus({
-              code: SpanStatusCode.OK,
+            logger.debug(`Span attributes for ${name}:`);
+            logger.debug(spanAttributes);
+
+            return await Telemetry.startActiveSpan<Promise<void>>({
+                name: name,
+                options: {
+                    attributes: spanAttributes,
+                },
+                fn: async (span: Span): Promise<void> => {
+                    try {
+                        await originalMethod.apply(this, args);
+                        // mark span as success
+                        span.setStatus({
+                            code: SpanStatusCode.OK,
+                        });
+                    } catch (err) {
+                        Telemetry.recordExceptionMarkSpanAsErrorAndEndSpan({
+                            span,
+                            exception: err,
+                        });
+                        // mark span as error
+                        span.setStatus({
+                            code: SpanStatusCode.ERROR,
+                        });
+                        logger.debug(`Error in span for ${name}:`);
+                        logger.debug(err);
+                        throw err;
+                    } finally {
+                        span.end();
+                        logger.debug(`Ended span for ${name}`);
+                    }
+                },
             });
-          } catch (err) {
-            Telemetry.recordExceptionMarkSpanAsErrorAndEndSpan({
-              span,
-              exception: err,
-            });
-            // mark span as error
-            span.setStatus({
-              code: SpanStatusCode.ERROR,
-            });
-            logger.debug(`Error in span for ${name}:`);
-            logger.debug(err);
-            throw err;
-          } finally {
-            span.end();
-            logger.debug(`Ended span for ${name}`);
-          }
-        },
-      });
+        };
+
+        return descriptor;
     };
-
-    return descriptor;
-  };
 }
 
 export default CaptureSpan;
