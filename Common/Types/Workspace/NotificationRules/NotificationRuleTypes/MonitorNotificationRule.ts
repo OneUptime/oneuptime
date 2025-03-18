a0725c5e2e9cfec1ff0bc5d3d5beb4ed1c82efab
Simon Larsen <simonlarsen@oneuptime.com>
2025-03-18 15:41:02 +0000

    refactor: clean up interface definition for MonitorNotificationRule

--- Common/Types/Workspace/NotificationRules/NotificationRuleTypes/MonitorNotificationRule.ts
+++ Common/Types/Workspace/NotificationRules/NotificationRuleTypes/MonitorNotificationRule.ts
@@ -1,7 +1,6 @@
 import BaseNotificationRule from "../BaseNotificationRule";
 
 // This rule is just used to post to existing channels.
-export default interface MonitorNotificationRule
-  extends BaseNotificationRule {
+export default interface MonitorNotificationRule extends BaseNotificationRule {
   _type: "MonitorNotificationRule";
 }
