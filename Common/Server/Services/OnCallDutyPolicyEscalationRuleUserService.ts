18aa3ba615be3e19fd1c464aae865874e19a4296
Simon Larsen <simonlarsen@oneuptime.com>
2025-04-09 19:56:57 +0100

    feat: enhance user notification system for on-call policy changes with email, SMS, and call alerts

--- Common/Server/Services/OnCallDutyPolicyEscalationRuleUserService.ts
+++ Common/Server/Services/OnCallDutyPolicyEscalationRuleUserService.ts
@@ -1,9 +1,218 @@
+import BadDataException from "../../Types/Exception/BadDataException";
+import ObjectID from "../../Types/ObjectID";
+import Timezone from "../../Types/Timezone";
+import { OnCreate, OnDelete } from "../Types/Database/Hooks";
 import DatabaseService from "./DatabaseService";
 import Model from "Common/Models/DatabaseModels/OnCallDutyPolicyEscalationRuleUser";
+import UserService from "./UserService";
+import Dictionary from "../../Types/Dictionary";
+import OneUptimeDate from "../../Types/Date";
+import OnCallDutyPolicyService from "./OnCallDutyPolicyService";
+import EmailTemplateType from "../../Types/Email/EmailTemplateType";
+import { EmailEnvelope } from "../../Types/Email/EmailMessage";
+import { SMSMessage } from "../../Types/SMS/SMS";
+import UserNotificationSettingService from "./UserNotificationSettingService";
+import NotificationSettingEventType from "../../Types/NotificationSetting/NotificationSettingEventType";
+import { CallRequestMessage } from "../../Types/Call/CallRequest";
+import DeleteBy from "../Types/Database/DeleteBy";
+import { LIMIT_PER_PROJECT } from "../../Types/Database/LimitMax";
 
 export class Service extends DatabaseService<Model> {
   public constructor() {
     super(Model);
+  }
+
+  protected override async onCreateSuccess(
+    _onCreate: OnCreate<Model>,
+    createdItem: Model
+  ): Promise<Model> {
+    const createdItemId: ObjectID = createdItem.id!;
+
+    if (!createdItemId) {
+      throw new BadDataException("Created item does not have an ID");
+    }
+
+    const createdModel: Model | null = await this.findOneById({
+      id: createdItemId,
+      select: {
+        projectId: true,
+        user: {
+          timezone: true,
+          _id: true,
+        },
+        onCallDutyPolicyEscalationRule: {
+          name: true,
+          _id: true,
+          order: true,
+        },
+        onCallDutyPolicy: {
+          name: true,
+          _id: true,
+        },
+      },
+      props: {
+        isRoot: true,
+      },
+    });
+
+    // send notification to the new current user.
+
+    const sendEmailToUserId: ObjectID | undefined | null =
+      createdModel?.user?.id;
+
+    if (!sendEmailToUserId) {
+      return createdItem;
+    }
+
+    if (!createdModel) {
+      return createdItem;
+    }
+
+    const vars: Dictionary<string> = {
+      onCallPolicyName:
+        createdModel.onCallDutyPolicy?.name || "No name provided",
+      escalationRuleName:
+        createdModel.onCallDutyPolicyEscalationRule?.name || "No name provided",
+      escalationRuleOrder:
+        createdModel.onCallDutyPolicyEscalationRule?.order?.toString() ||
+        "No order provided",
+      reason: "You have been added to the on-call duty policy escalation rule.",
+      onCallPolicyViewLink: (
+        await OnCallDutyPolicyService.getOnCallPolicyLinkInDashboard(
+          createdModel.onCallDutyPolicy!.projectId!,
+          createdModel.onCallDutyPolicy!.id!
+        )
+      ).toString(),
+    };
+
+    // current user changed, send alert the new current user.
+    const emailMessage: EmailEnvelope = {
+      templateType: EmailTemplateType.UserAddedToOnCallPolicy,
+      vars: vars,
+      subject: `You have been added to the on-call duty policy ${createdModel.onCallDutyPolicy?.name}`,
+    };
+
+    const sms: SMSMessage = {
+      message: `This is a message from OneUptime. You have been added to the on-call duty policy ${createdModel.onCallDutyPolicy?.name} for escalation rule ${createdModel.onCallDutyPolicyEscalationRule?.name} with order ${createdModel.onCallDutyPolicyEscalationRule?.order}. To unsubscribe from this notification go to User Settings in OneUptime Dashboard.`,
+    };
+
+    const callMessage: CallRequestMessage = {
+      data: [
+        {
+          sayMessage: `This is a message from OneUptime. You have been added to the on-call duty policy ${createdModel.onCallDutyPolicy?.name} for escalation rule ${createdModel.onCallDutyPolicyEscalationRule?.name} with order ${createdModel.onCallDutyPolicyEscalationRule?.order}. To unsubscribe from this notification go to User Settings in OneUptime Dashboard. Good Bye`,
+        },
+      ],
+    };
+
+    await UserNotificationSettingService.sendUserNotification({
+      userId: sendEmailToUserId,
+      projectId: createdModel.onCallDutyPolicy?.projectId!,
+      emailEnvelope: emailMessage,
+      smsMessage: sms,
+      callRequestMessage: callMessage,
+      eventType:
+        NotificationSettingEventType.SEND_WHEN_USER_IS_ADDED_TO_ON_CALL_POLICY,
+    });
+
+    return createdItem;
+  }
+
+
+  protected override async onBeforeDelete(deleteBy: DeleteBy<Model>): Promise<OnDelete<Model>> {
+    const itemsToFetchBeforeDelete: Array<Model> = await this.findBy({
+      query: deleteBy.query,
+      props: {
+        isRoot: true,
+      },
+      select: {
+        projectId: true,
+        user: {
+          timezone: true,
+          _id: true,
+        },
+        onCallDutyPolicyEscalationRule: {
+          name: true,
+          _id: true,
+          order: true,
+        },
+        onCallDutyPolicy: {
+          name: true,
+          _id: true,
+        },
+      },
+      limit: LIMIT_PER_PROJECT,
+      skip: 0
+    });
+
+    return {
+      deleteBy, carryForward: {
+        deletedItems: itemsToFetchBeforeDelete,
+      }
+    }
+  }
+
+  protected override async onDeleteSuccess(
+    onDelete: OnDelete<Model>,
+    _itemIdsBeforeDelete: Array<ObjectID>
+  ): Promise<OnDelete<Model>> {
+    const deletedItems: Array<Model> = onDelete.carryForward.deletedItems;
+
+    for(const deletedItem of deletedItems) {
+      const sendEmailToUserId: ObjectID | undefined | null =
+        deletedItem.user?.id;
+
+      if (!sendEmailToUserId) {
+        return onDelete;
+      }
+
+      const vars: Dictionary<string> = {
+        onCallPolicyName:
+          deletedItem.onCallDutyPolicy?.name || "No name provided",
+        escalationRuleName:
+          deletedItem.onCallDutyPolicyEscalationRule?.name || "No name provided",
+        escalationRuleOrder:
+          deletedItem.onCallDutyPolicyEscalationRule?.order?.toString() ||
+          "No order provided",
+        reason: "You have been removed from the on-call duty policy escalation rule.",
+        onCallPolicyViewLink: (
+          await OnCallDutyPolicyService.getOnCallPolicyLinkInDashboard(
+            deletedItem.onCallDutyPolicy!.projectId!,
+            deletedItem.onCallDutyPolicy!.id!
+          )
+        ).toString(),
+      };
+
+      // current user changed, send alert the new current user.
+      const emailMessage: EmailEnvelope = {
+        templateType: EmailTemplateType.UserRemovedFromOnCallPolicy,
+        vars: vars,
+        subject: `You have been removed from the on-call duty policy ${deletedItem.onCallDutyPolicy?.name}`,
+      };
+
+      const sms: SMSMessage = {
+        message: `This is a message from OneUptime. You have been removed from the on-call duty policy ${deletedItem.onCallDutyPolicy?.name} for escalation rule ${deletedItem.onCallDutyPolicyEscalationRule?.name} with order ${deletedItem.onCallDutyPolicyEscalationRule?.order}. To unsubscribe from this notification go to User Settings in OneUptime Dashboard.`,
+      };
+
+      const callMessage: CallRequestMessage = {
+        data: [
+          {
+            sayMessage: `This is a message from OneUptime. You have been removed from the on-call duty policy ${deletedItem.onCallDutyPolicy?.name} for escalation rule ${deletedItem.onCallDutyPolicyEscalationRule?.name} with order ${deletedItem.onCallDutyPolicyEscalationRule?.order}. To unsubscribe from this notification go to User Settings in OneUptime Dashboard. Good Bye`,
+          },
+        ],
+      };
+
+      UserNotificationSettingService.sendUserNotification({
+        userId: sendEmailToUserId,
+        projectId: deletedItem.onCallDutyPolicy?.projectId!,
+        emailEnvelope: emailMessage,
+        smsMessage: sms,
+        callRequestMessage: callMessage,
+        eventType:
+          NotificationSettingEventType.SEND_WHEN_USER_IS_REMOVED_FROM_ON_CALL_POLICY,
+      });
+    }
+
+    return onDelete;
   }
 }
 export default new Service();
