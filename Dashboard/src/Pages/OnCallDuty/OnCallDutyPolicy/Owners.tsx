7b263280634c7fbe6bc78bcf6ae83e520ab6432b
Simon Larsen <simonlarsen@oneuptime.com>
2025-04-16 14:06:48 +0100

    feat: add OnCallDutyPolicyOwners component for managing policy owner teams and users

---
+++ Dashboard/src/Pages/OnCallDuty/OnCallDutyPolicy/Owners.tsx
@@ -0,0 +1,221 @@
+import TeamElement from "../../../Components/Team/Team";
+import UserElement from "../../../Components/User/User";
+import ProjectUser from "../../../Utils/ProjectUser";
+import PageComponentProps from "../../PageComponentProps";
+import BadDataException from "Common/Types/Exception/BadDataException";
+import ObjectID from "Common/Types/ObjectID";
+import FormFieldSchemaType from "Common/UI/Components/Forms/Types/FormFieldSchemaType";
+import ModelTable from "Common/UI/Components/ModelTable/ModelTable";
+import FieldType from "Common/UI/Components/Types/FieldType";
+import Navigation from "Common/UI/Utils/Navigation";
+import OnCallDutyPolicyOwnerTeam from "Common/Models/DatabaseModels/OnCallDutyPolicyOwnerTeam";
+import OnCallDutyPolicyOwnerUser from "Common/Models/DatabaseModels/OnCallDutyPolicyOwnerUser";
+import Team from "Common/Models/DatabaseModels/Team";
+import User from "Common/Models/DatabaseModels/User";
+import React, { Fragment, FunctionComponent, ReactElement } from "react";
+import ProjectUtil from "Common/UI/Utils/Project";
+
+const OnCallDutyPolicyOwners: FunctionComponent<
+  PageComponentProps
+> = (): ReactElement => {
+  const modelId: ObjectID = Navigation.getLastParamAsObjectID(1);
+
+  return (
+    <Fragment>
+      
+      <ModelTable<OnCallDutyPolicyOwnerTeam>
+        modelType={OnCallDutyPolicyOwnerTeam}
+        id="table-OnCallDutyPolicy Team-owner-team"
+        name="OnCallDutyPolicy > Owner Team"
+        singularName="Team"
+        isDeleteable={true}
+        createVerb={"Add"}
+        isCreateable={true}
+        isViewable={false}
+        showViewIdButton={true}
+        query={{
+          onCallDutyPolicyId: modelId,
+          projectId: ProjectUtil.getCurrentProjectId()!,
+        }}
+        onBeforeCreate={(item: OnCallDutyPolicyOwnerTeam): Promise<OnCallDutyPolicyOwnerTeam> => {
+          item.onCallDutyPolicyId = modelId;
+          item.projectId = ProjectUtil.getCurrentProjectId()!;
+          return Promise.resolve(item);
+        }}
+        cardProps={{
+          title: "Owners (Teams)",
+          description:
+            "Here is list of teams that own this on-call policy. They will be alerted when this on-call policy is created or updated.",
+        }}
+        noItemsMessage={"No teams associated with this on-call policy so far."}
+        formFields={[
+          {
+            field: {
+              team: true,
+            },
+            title: "Team",
+            fieldType: FormFieldSchemaType.Dropdown,
+            required: true,
+            placeholder: "Select Team",
+            dropdownModal: {
+              type: Team,
+              labelField: "name",
+              valueField: "_id",
+            },
+          },
+        ]}
+        showRefreshButton={true}
+        viewPageRoute={Navigation.getCurrentRoute()}
+        filters={[
+          {
+            field: {
+              team: true,
+            },
+            type: FieldType.Entity,
+            title: "Team",
+            filterEntityType: Team,
+            filterQuery: {
+              projectId: ProjectUtil.getCurrentProjectId()!,
+            },
+            filterDropdownField: {
+              label: "name",
+              value: "_id",
+            },
+          },
+          {
+            field: {
+              createdAt: true,
+            },
+            title: "Owner since",
+            type: FieldType.Date,
+          },
+        ]}
+        columns={[
+          {
+            field: {
+              team: {
+                name: true,
+              },
+            },
+            title: "Team",
+            type: FieldType.Entity,
+            getElement: (item: OnCallDutyPolicyOwnerTeam): ReactElement => {
+              if (!item["team"]) {
+                throw new BadDataException("Team not found");
+              }
+
+              return <TeamElement team={item["team"] as Team} />;
+            },
+          },
+          {
+            field: {
+              createdAt: true,
+            },
+            title: "Owner since",
+            type: FieldType.DateTime,
+          },
+        ]}
+      />
+
+      <ModelTable<OnCallDutyPolicyOwnerUser>
+        modelType={OnCallDutyPolicyOwnerUser}
+        id="table-on-call policy-owner-team"
+        name="on-call policy > Owner Team"
+        isDeleteable={true}
+        singularName="User"
+        isCreateable={true}
+        isViewable={false}
+        showViewIdButton={true}
+        createVerb={"Add"}
+        query={{
+          onCallDutyPolicyId: modelId,
+          projectId: ProjectUtil.getCurrentProjectId()!,
+        }}
+        onBeforeCreate={(item: OnCallDutyPolicyOwnerUser): Promise<OnCallDutyPolicyOwnerUser> => {
+          item.onCallDutyPolicyId = modelId;
+          item.projectId = ProjectUtil.getCurrentProjectId()!;
+          return Promise.resolve(item);
+        }}
+        cardProps={{
+          title: "Owners (Users)",
+          description:
+            "Here is list of users that own this on-call policy. They will be alerted when this on-call policy is created or updated.",
+        }}
+        noItemsMessage={"No users associated with this on-call policy so far."}
+        formFields={[
+          {
+            field: {
+              user: true,
+            },
+            title: "User",
+            fieldType: FormFieldSchemaType.Dropdown,
+            required: true,
+            placeholder: "Select User",
+            fetchDropdownOptions: async () => {
+              return await ProjectUser.fetchProjectUsersAsDropdownOptions(
+                ProjectUtil.getCurrentProjectId()!,
+              );
+            },
+          },
+        ]}
+        showRefreshButton={true}
+        viewPageRoute={Navigation.getCurrentRoute()}
+        filters={[
+          {
+            field: {
+              user: true,
+            },
+            title: "User",
+            type: FieldType.Entity,
+            filterEntityType: User,
+            fetchFilterDropdownOptions: async () => {
+              return await ProjectUser.fetchProjectUsersAsDropdownOptions(
+                ProjectUtil.getCurrentProjectId()!,
+              );
+            },
+            filterDropdownField: {
+              label: "name",
+              value: "_id",
+            },
+          },
+          {
+            field: {
+              createdAt: true,
+            },
+            title: "Owner since",
+            type: FieldType.Date,
+          },
+        ]}
+        columns={[
+          {
+            field: {
+              user: {
+                name: true,
+                email: true,
+                profilePictureId: true,
+              },
+            },
+            title: "User",
+            type: FieldType.Entity,
+            getElement: (item: OnCallDutyPolicyOwnerUser): ReactElement => {
+              if (!item["user"]) {
+                throw new BadDataException("User not found");
+              }
+
+              return <UserElement user={item["user"] as User} />;
+            },
+          },
+          {
+            field: {
+              createdAt: true,
+            },
+            title: "Owner since",
+            type: FieldType.DateTime,
+          },
+        ]}
+      />
+    </Fragment>
+  );
+};
+
+export default OnCallDutyPolicyOwners;
