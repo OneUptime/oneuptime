3325035cfc70b20758b14f3f9c52239718664cf3
Simon Larsen <simonlarsen@oneuptime.com>
2025-05-15 11:51:03 +0100

    refactor: Remove hard delete call for items older than 180 days in OnCallDutyPolicyTimeLogService constructor

--- Common/Server/Services/OnCallDutyPolicyTimeLogService.ts
+++ Common/Server/Services/OnCallDutyPolicyTimeLogService.ts
@@ -4,7 +4,6 @@ import Model from "Common/Models/DatabaseModels/OnCallDutyPolicyTimeLog";
 export class Service extends DatabaseService<Model> {
   public constructor() {
     super(Model);
-    this.hardDeleteItemsOlderThanInDays("createdAt", 180);
   }
 }
 
