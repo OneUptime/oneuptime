d22199586a210ef1cfc59f5fa34f0434aae64c22
Simon Larsen <simonlarsen@oneuptime.com>
2025-05-15 11:50:44 +0100

    feat: Add hard delete functionality for items older than 180 days in OnCallDutyPolicyTimeLogService

--- Common/Server/Services/OnCallDutyPolicyTimeLogService.ts
+++ Common/Server/Services/OnCallDutyPolicyTimeLogService.ts
@@ -4,6 +4,7 @@ import Model from "Common/Models/DatabaseModels/OnCallDutyPolicyTimeLog";
 export class Service extends DatabaseService<Model> {
   public constructor() {
     super(Model);
+    this.hardDeleteItemsOlderThanInDays("createdAt", 180);
   }
 }
 
