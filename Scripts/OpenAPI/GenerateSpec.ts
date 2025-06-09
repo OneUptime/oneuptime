39d6eac670bbbbc7b92e5903ce1192b1fc9ecab5
Simon Larsen <simonlarsen@oneuptime.com>
2025-06-09 14:13:17 +0100

    refactor: streamline OpenAPI spec validation and error handling

--- Scripts/OpenAPI/GenerateSpec.ts
+++ Scripts/OpenAPI/GenerateSpec.ts
@@ -1,31 +1,32 @@
 import OpenAPI from "Common/Server/Utils/OpenAPI";
 import fs from "fs";
 import { validate, ValidationResult } from "@readme/openapi-parser";
-import JSONFunctions from "Common/Types/JSONFunctions";
-import BadDataException from "Common/Types/Exception/BadDataException";
 
 async function generateOpenAPISpec(): Promise<void> {
   const spec = OpenAPI.generateOpenAPISpec();
 
+  const outputPath = "./openapi.json";
+  fs.writeFileSync(outputPath, JSON.stringify(spec, null, 2), "utf8");
+
   const validationResult: ValidationResult = await validate(
-    JSONFunctions.toString(spec)
+    outputPath
   );
 
   if (validationResult.valid) {
     console.log("OpenAPI spec is valid.");
   } else {
-    throw new BadDataException("OpenAPI spec validation failed.");
+    throw validationResult.errors;
   }
 
   // Write the OpenAPI spec to a file
 
-  const outputPath = "./openapi.json";
-  fs.writeFileSync(outputPath, JSON.stringify(spec, null, 2), "utf8");
+  
 
   console.log(`OpenAPI spec generated and saved to ${outputPath}`);
 }
 
 generateOpenAPISpec().catch((error) => {
   console.error("Error generating OpenAPI spec:");
+  console.error(error);
   process.exit(1);
 });
