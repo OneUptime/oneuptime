b68f25b80ddd100e16e7b380598e6f179c9afbe0
Simon Larsen <simonlarsen@oneuptime.com>
2025-06-16 13:07:29 +0100

    fix: ensure consistent styling for Markdown link in cheatsheet

--- Common/UI/Utils/Markdown.tsx
+++ Common/UI/Utils/Markdown.tsx
@@ -11,6 +11,7 @@ export default class MarkdownUtil {
         <Link
           to={URL.fromString("https://www.markdownguide.org/cheat-sheet/")}
           openInNewTab={true}
+          className="underline text-blue-600 hover:text-blue-800"
         >
           Learn more about Markdown syntax
         </Link>
