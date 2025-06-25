0b3765594f4e0f8b26cc2bdef270e3b193dc14d9
Simon Larsen <simonlarsen@oneuptime.com>
2025-06-25 18:33:50 +0100

    feat: Add EnableDocumentation decorator to File model for improved documentation generation

--- Common/Models/DatabaseModels/File.ts
+++ Common/Models/DatabaseModels/File.ts
@@ -2,11 +2,13 @@ import FileModel from "./DatabaseBaseModel/FileModel";
 import Route from "../../Types/API/Route";
 import TableAccessControl from "../../Types/Database/AccessControl/TableAccessControl";
 import CrudApiEndpoint from "../../Types/Database/CrudApiEndpoint";
+import EnableDocumentation from "../../Types/Database/EnableDocumentation";
 import TableMetadata from "../../Types/Database/TableMetadata";
 import IconProp from "../../Types/Icon/IconProp";
 import Permission from "../../Types/Permission";
 import { Entity } from "typeorm";
 
+@EnableDocumentation()
 @TableMetadata({
   tableName: "File",
   singularName: "File",
