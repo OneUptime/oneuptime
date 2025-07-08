0a65c72b8055a3b9080eae35cfd28dacb75cfab4
Simon Larsen <simonlarsen@oneuptime.com>
2025-07-08 17:29:20 +0100

    feat: Add responsive design to EventHistoryDayList component for better mobile experience

--- Common/UI/Components/EventHistoryList/EventHistoryDayList.tsx
+++ Common/UI/Components/EventHistoryList/EventHistoryDayList.tsx
@@ -2,7 +2,7 @@ import EventHistoryItem, {
   ComponentProps as ItemComponentProps,
 } from "../EventItem/EventItem";
 import OneUptimeDate from "../../../Types/Date";
-import React, { FunctionComponent, ReactElement } from "react";
+import React, { FunctionComponent, ReactElement, useState, useEffect } from "react";
 
 export interface ComponentProps {
   date: Date;
@@ -13,31 +13,53 @@ export interface ComponentProps {
 const EventHistoryDayList: FunctionComponent<ComponentProps> = (
   props: ComponentProps,
 ): ReactElement => {
+  const [windowWidth, setWindowWidth] = useState<number>(
+    typeof window !== "undefined" ? window.innerWidth : 1024
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+    
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const isMobile = windowWidth <= 768;
   return (
     <div
-      className="flex bottom-Gray500-border"
+      className="md:flex bottom-Gray500-border"
       style={{
-        marginLeft: "-10px",
-        marginRight: "-10px",
-        marginBottom: props.isLastItem ? "0px" : "20px",
-        borderBottomWidth: props.isLastItem ? "0px" : "1px",
+      marginLeft: "-10px",
+      marginRight: "-10px",
+      marginBottom: props.isLastItem ? "0px" : "20px",
+      borderBottomWidth: props.isLastItem ? "0px" : "1px",
       }}
     >
       <div
-        className="text-gray-400 mt-2 text-sm"
-        style={{
-          padding: "20px",
-          paddingLeft: "10px",
-          paddingRight: "0px",
-          width: "15%",
-        }}
+      className="text-gray-400 mt-2 text-sm"
+      style={{
+        padding: "20px",
+        paddingLeft: "10px",
+        paddingRight: "0px",
+        width: isMobile ? "100%" : "15%",
+      }}
       >
-        {OneUptimeDate.getDateAsLocalFormattedString(props.date, true)}
+      {OneUptimeDate.getDateAsLocalFormattedString(props.date, true)}
       </div>
-      <div style={{ padding: "10px", paddingTop: "0px", width: "85%" }}>
-        {props.items.map((item: ItemComponentProps, i: number) => {
-          return <EventHistoryItem key={i} {...item} />;
-        })}
+      <div style={{ 
+        padding: "10px", 
+        paddingTop: "0px", 
+        width: isMobile ? "100%" : "85%" 
+      }}>
+      {props.items.map((item: ItemComponentProps, i: number) => {
+        return <EventHistoryItem key={i} {...item} />;
+      })}
       </div>
     </div>
   );
