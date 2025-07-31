9cfc912161a4fc305f232362a26762794f1ed15a
Simon Larsen <simonlarsen@oneuptime.com>
2025-07-31 12:56:06 +0100

    feat: enhance response messages for incoming request checks with time difference

--- Common/Server/Utils/Monitor/MonitorResource.ts
+++ Common/Server/Utils/Monitor/MonitorResource.ts
@@ -228,6 +228,7 @@ export default class MonitorResourceUtil {
       await MonitorService.updateOneById({
         id: monitor.id!,
         data: {
+          incomingRequestMonitorHeartbeatCheckedAt: OneUptimeDate.getCurrentDate(),
           incomingMonitorRequest: {
             ...dataToProcess,
           } as any,
