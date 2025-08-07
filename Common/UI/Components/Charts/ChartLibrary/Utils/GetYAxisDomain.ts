675a031ee62d566e21a07c563e7f1fd0ca8b5b66
Nawaz Dhandala <hello@nawazdhandala.com>
2025-08-07 21:02:59 +0100

    fix: Correct return type of getYAxisDomain function to match expected output

--- Common/UI/Components/Charts/ChartLibrary/Utils/GetYAxisDomain.ts
+++ Common/UI/Components/Charts/ChartLibrary/Utils/GetYAxisDomain.ts
@@ -4,11 +4,11 @@ export const getYAxisDomain: (
   autoMinValue: boolean,
   minValue: number | undefined,
   maxValue: number | undefined,
-) => (number | "auto")[] = (
+) => [number | "auto", number | "auto"] = (
   autoMinValue: boolean,
   minValue: number | undefined,
   maxValue: number | undefined,
-): (number | "auto")[] => {
+): [number | "auto", number | "auto"] => {
   const minDomain: number | "auto" = autoMinValue ? "auto" : minValue ?? 0;
   const maxDomain: number | "auto" = maxValue ?? "auto";
   return [minDomain, maxDomain];
