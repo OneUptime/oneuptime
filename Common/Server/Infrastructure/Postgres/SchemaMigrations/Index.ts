9625f1381cc38b4f8854d68a500fa372ea5f8be0
Simon Larsen <simonlarsen@oneuptime.com>
2025-09-09 14:12:33 +0100

    feat: Add migration for AnnouncementMonitor and AnnouncementTemplateMonitor tables with foreign key constraints

--- Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
@@ -166,6 +166,7 @@ import { MigrationName1756296282627 } from "./1756296282627-MigrationName";
 import { MigrationName1756300358095 } from "./1756300358095-MigrationName";
 import { MigrationName1756821449686 } from "./1756821449686-MigrationName";
 import { MigrationName1757416939595 } from "./1757416939595-MigrationName";
+import { MigrationName1757423505855 } from "./1757423505855-MigrationName";
 
 export default [
   InitialMigration,
@@ -336,4 +337,5 @@ export default [
   MigrationName1756300358095,
   MigrationName1756821449686,
   MigrationName1757416939595,
+  MigrationName1757423505855
 ];
