73beb80056d2d62bdd4f6d54534059b316db8a42
Simon Larsen <simonlarsen@oneuptime.com>
2025-09-25 12:12:48 +0100

    feat: Add enablePushGroups column to ProjectSCIM for SCIM provisioning

---
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/1758798730753-MigrationName.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MigrationName1758798730753 implements MigrationInterface {
+    public name = 'MigrationName1758798730753'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "ProjectSCIM" ADD "enablePushGroups" boolean NOT NULL DEFAULT false`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "ProjectSCIM" DROP COLUMN "enablePushGroups"`);
+    }
+
+}
