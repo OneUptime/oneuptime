10e344dad5ae0b18e3ad1e1f094f107cc72c6503
Simon Larsen <simonlarsen@oneuptime.com>
2025-10-12 10:08:23 +0100

    feat: add react-native-get-random-values dependency and import in App component

--- MobileApp/App.tsx
+++ MobileApp/App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-get-random-values';
 import React, { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
