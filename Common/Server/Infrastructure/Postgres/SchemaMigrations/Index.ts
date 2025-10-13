513e4146ed2870e66b978702fdc2d63841d442ad
Simon Larsen <simonlarsen@oneuptime.com>
2025-10-13 09:59:33 +0100

    feat: add migration for WhatsAppMessageId field in WhatsAppLog table with index

--- Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
@@ -175,6 +175,7 @@ import { MigrationName1759175457008 } from "./1759175457008-MigrationName";
 import { MigrationName1759232954703 } from "./1759232954703-MigrationName";
 import { RenameUserTwoFactorAuthToUserTotpAuth1759234532998 } from "./1759234532998-MigrationName";
 import { MigrationName1759943124812 } from "./1759943124812-MigrationName";
+import { MigrationName1760345757975 } from "./1760345757975-MigrationName";
 
 export default [
   InitialMigration,
@@ -354,4 +355,5 @@ export default [
   MigrationName1759232954703,
   RenameUserTwoFactorAuthToUserTotpAuth1759234532998,
   MigrationName1759943124812,
+  MigrationName1760345757975
 ];
