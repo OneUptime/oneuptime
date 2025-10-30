0bff616ca91e77a874cb138b0f8b1902b9b92f2b
Nawaz Dhandala <hello@nawazdhandala.com>
2025-10-30 11:59:07 +0000

    chore(jobs): comment out KeepCurrentStateConsistent implementations and unused imports for Alert, Incident, Monitor, and ScheduledMaintenance

--- Worker/Jobs/ScheduledMaintenance/KeepCurrentStateConsistent.ts
+++ Worker/Jobs/ScheduledMaintenance/KeepCurrentStateConsistent.ts
@@ -1,83 +1,83 @@
 import { EVERY_DAY } from "Common/Utils/CronTime";
 import RunCron from "../../Utils/Cron";
-import logger from "Common/Server/Utils/Logger";
-import ProjectService from "Common/Server/Services/ProjectService";
-import ScheduledMaintenanceService from "Common/Server/Services/ScheduledMaintenanceService";
-import Project from "Common/Models/DatabaseModels/Project";
-import ScheduledMaintenance from "Common/Models/DatabaseModels/ScheduledMaintenance";
+// import logger from "Common/Server/Utils/Logger";
+// import ProjectService from "Common/Server/Services/ProjectService";
+// import ScheduledMaintenanceService from "Common/Server/Services/ScheduledMaintenanceService";
+// import Project from "Common/Models/DatabaseModels/Project";
+// import ScheduledMaintenance from "Common/Models/DatabaseModels/ScheduledMaintenance";
 
 RunCron(
   "ScheduledMaintenance:KeepCurrentStateConsistent",
   { schedule: EVERY_DAY, runOnStartup: true },
   async () => {
-    try {
-      /*
-       * get all projects, then get all scheduled maintenances for each project, then get the last state of each scheduled maintenance and check with the current state of each scheduled maintenance.
-       * if they are different, then update the current state of the scheduled maintenance.
-       */
+    // try {
+    //   /*
+    //    * get all projects, then get all scheduled maintenances for each project, then get the last state of each scheduled maintenance and check with the current state of each scheduled maintenance.
+    //    * if they are different, then update the current state of the scheduled maintenance.
+    //    */
 
-      const projects: Array<Project> =
-        await ProjectService.getAllActiveProjects({
-          select: {
-            _id: true,
-          },
-        });
+    //   const projects: Array<Project> =
+    //     await ProjectService.getAllActiveProjects({
+    //       select: {
+    //         _id: true,
+    //       },
+    //     });
 
-      for (const project of projects) {
-        try {
-          if (!project) {
-            continue;
-          }
+    //   for (const project of projects) {
+    //     try {
+    //       if (!project) {
+    //         continue;
+    //       }
 
-          if (!project.id) {
-            continue;
-          }
+    //       if (!project.id) {
+    //         continue;
+    //       }
 
-          const scheduledMaintenances: Array<ScheduledMaintenance> =
-            await ScheduledMaintenanceService.findAllBy({
-              query: {
-                projectId: project.id,
-              },
-              select: {
-                _id: true,
-              },
-              props: {
-                isRoot: true,
-              },
-            });
+    //       const scheduledMaintenances: Array<ScheduledMaintenance> =
+    //         await ScheduledMaintenanceService.findAllBy({
+    //           query: {
+    //             projectId: project.id,
+    //           },
+    //           select: {
+    //             _id: true,
+    //           },
+    //           props: {
+    //             isRoot: true,
+    //           },
+    //         });
 
-          for (const scheduledMaintenance of scheduledMaintenances) {
-            try {
-              if (!scheduledMaintenance) {
-                continue;
-              }
-              if (!scheduledMaintenance.id) {
-                continue;
-              }
-              await ScheduledMaintenanceService.refreshScheduledMaintenanceCurrentStatus(
-                scheduledMaintenance.id!,
-              );
-            } catch (err) {
-              logger.error(
-                "Error in ScheduledMaintenance:KeepCurrentStateConsistent job",
-              );
-              logger.error(err);
-              continue;
-            }
-          }
-        } catch (err) {
-          logger.error(
-            "Error in ScheduledMaintenance:KeepCurrentStateConsistent job",
-          );
-          logger.error(err);
-          continue;
-        }
-      }
-    } catch (err) {
-      logger.error(
-        "Error in ScheduledMaintenance:KeepCurrentStateConsistent job",
-      );
-      logger.error(err);
-    }
+    //       for (const scheduledMaintenance of scheduledMaintenances) {
+    //         try {
+    //           if (!scheduledMaintenance) {
+    //             continue;
+    //           }
+    //           if (!scheduledMaintenance.id) {
+    //             continue;
+    //           }
+    //           await ScheduledMaintenanceService.refreshScheduledMaintenanceCurrentStatus(
+    //             scheduledMaintenance.id!,
+    //           );
+    //         } catch (err) {
+    //           logger.error(
+    //             "Error in ScheduledMaintenance:KeepCurrentStateConsistent job",
+    //           );
+    //           logger.error(err);
+    //           continue;
+    //         }
+    //       }
+    //     } catch (err) {
+    //       logger.error(
+    //         "Error in ScheduledMaintenance:KeepCurrentStateConsistent job",
+    //       );
+    //       logger.error(err);
+    //       continue;
+    //     }
+    //   }
+    // } catch (err) {
+    //   logger.error(
+    //     "Error in ScheduledMaintenance:KeepCurrentStateConsistent job",
+    //   );
+    //   logger.error(err);
+    // }
   },
 );
