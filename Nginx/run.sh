d8206e12de38e37d186a2b03529fe96aea82e08e
Nawaz Dhandala <hello@nawazdhandala.com>
2025-11-03 21:48:15 +0000

    feat(nginx): add ServerCerts volume/mount and enable conditional primary-domain SSL provisioning
    - add server-certs emptyDir volume and mount to nginx Deployment
    - update default.conf.template to conditionally listen on 7850 and use /etc/nginx/certs/ServerCerts/${PRIMARY_DOMAIN}.crt/.key when PROVISION_SSL is set
    - enhance run.sh to export PRIMARY_DOMAIN, temporarily adjust PROVISION_SSL for envsubst, and restore original PROVISION_SSL afterwards

--- Nginx/run.sh
+++ Nginx/run.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
+ORIGINAL_PROVISION_SSL="$PROVISION_SSL"
+
+PRIMARY_DOMAIN=""
+if [ -n "$HOST" ]; then
+  PRIMARY_DOMAIN=$(printf '%s' "$HOST" | cut -d: -f1 | tr '[:upper:]' '[:lower:]')
+fi
+
+if [ -n "$PRIMARY_DOMAIN" ]; then
+  export PRIMARY_DOMAIN
+fi
+
+if [ "$PROVISION_SSL" = "true" ]; then
+  export PROVISION_SSL
+else
+  export PROVISION_SSL=""
+fi
+
 # Run envsubst on template
 /etc/nginx/envsubst-on-templates.sh
+
+# Restore environment variables for subsequent processes
+if [ -n "$PRIMARY_DOMAIN" ]; then
+  unset PRIMARY_DOMAIN
+fi
+
+if [ -n "$ORIGINAL_PROVISION_SSL" ]; then
+  export PROVISION_SSL="$ORIGINAL_PROVISION_SSL"
+else
+  unset PROVISION_SSL
+fi
 
 # Start the first process
 nginx -c /etc/nginx/nginx.conf -g "daemon off;" &
