847bac5c6a022a99ae706ecbd4f4ac2e8c2dbd89
Nawaz Dhandala <hello@nawazdhandala.com>
2025-11-26 16:42:10 +0000

    refactor: enhance Chrome and Firefox executable path retrieval with additional candidates

--- Common/Server/Utils/Browser.ts
+++ Common/Server/Utils/Browser.ts
@@ -169,6 +169,8 @@ export default class BrowserUtil {
     const chromeExecutableCandidates: Array<string> = [
       `/root/.cache/ms-playwright/${chromeInstallationName}/chrome-linux/chrome`,
       `/root/.cache/ms-playwright/${chromeInstallationName}/chrome-linux64/chrome`,
+      `/root/.cache/ms-playwright/${chromeInstallationName}/chrome64/chrome`,
+      `/root/.cache/ms-playwright/${chromeInstallationName}/chrome/chrome`,
     ];
 
     for (const executablePath of chromeExecutableCandidates) {
@@ -208,6 +210,19 @@ export default class BrowserUtil {
       throw new BadDataException("Firefox executable path not found.");
     }
 
-    return `/root/.cache/ms-playwright/${firefoxInstallationName}/firefox/firefox`;
+    const firefoxExecutableCandidates: Array<string> = [
+      `/root/.cache/ms-playwright/${firefoxInstallationName}/firefox/firefox`,
+      `/root/.cache/ms-playwright/${firefoxInstallationName}/firefox-linux64/firefox`,
+      `/root/.cache/ms-playwright/${firefoxInstallationName}/firefox64/firefox`,
+      `/root/.cache/ms-playwright/${firefoxInstallationName}/firefox-64/firefox`,
+    ];
+
+    for (const executablePath of firefoxExecutableCandidates) {
+      if (await LocalFile.doesFileExist(executablePath)) {
+        return executablePath;
+      }
+    }
+
+    throw new BadDataException("Firefox executable path not found.");
   }
 }
