0dc3bb4f3306a089252b235309f1cffb11a312ed
Nawaz Dhandala <hello@nawazdhandala.com>
2025-12-03 13:10:12 +0000

    feat: add MigrationName1764767371788 for updating OnCallDutyPolicyScheduleLayer defaults

---
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/1764767371788-MigrationName.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MigrationName1764767371788 implements MigrationInterface {
+    public name = 'MigrationName1764767371788'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "rotation" SET DEFAULT '{"_type":"Recurring","value":{"intervalType":"Day","intervalCount":{"_type":"PositiveNumber","value":1}}}'`);
+        await queryRunner.query(`ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "restrictionTimes" SET DEFAULT '{"_type":"RestrictionTimes","value":{"restictionType":"None","dayRestrictionTimes":null,"weeklyRestrictionTimes":[]}}'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "restrictionTimes" SET DEFAULT '{"_type": "RestrictionTimes", "value": {"restictionType": "None", "dayRestrictionTimes": null, "weeklyRestrictionTimes": []}}'`);
+        await queryRunner.query(`ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "rotation" SET DEFAULT '{"_type": "Recurring", "value": {"intervalType": "Day", "intervalCount": {"_type": "PositiveNumber", "value": 1}}}'`);
+    }
+
+}
--- Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
@@ -188,6 +188,7 @@ import { MigrationName1763480947474 } from "./1763480947474-MigrationName";
 import { MigrationName1763643080445 } from "./1763643080445-MigrationName";
 import { MigrationName1764324618043 } from "./1764324618043-MigrationName";
 import { MigrationName1764762146063 } from "./1764762146063-MigrationName";
+import { MigrationName1764767371788 } from "./1764767371788-MigrationName";
 
 export default [
   InitialMigration,
@@ -380,4 +381,5 @@ export default [
   MigrationName1763643080445,
   MigrationName1764324618043,
   MigrationName1764762146063,
+  MigrationName1764767371788
 ];
