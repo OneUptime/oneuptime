a4a56bf2c7ebad6a050f024adae20269a77f77f9
Nawaz Dhandala <hello@nawazdhandala.com>
2026-01-21 21:04:36 +0000

    chore(GoModuleGenerator): update go.mod to pin additional indirect dependencies for compatibility with Terraform 1.5+

--- Scripts/TerraformProvider/Core/GoModuleGenerator.ts
+++ Scripts/TerraformProvider/Core/GoModuleGenerator.ts
@@ -18,13 +18,39 @@ export class GoModuleGenerator {
   }
 
   private async generateGoMod(): Promise<void> {
+    // Pin all dependencies to specific versions that are known to work with Terraform 1.5+
+    // This prevents go mod tidy from upgrading to incompatible versions
     const goModContent: string = `module ${this.config.goModuleName}
 
 go 1.21
 
 require (
-    github.com/hashicorp/terraform-plugin-framework v1.8.0
-    github.com/hashicorp/terraform-plugin-log v0.9.0
+	github.com/hashicorp/terraform-plugin-framework v1.8.0
+	github.com/hashicorp/terraform-plugin-log v0.9.0
+)
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/hashicorp/go-hclog v1.5.0 // indirect
+	github.com/hashicorp/go-plugin v1.6.0 // indirect
+	github.com/hashicorp/go-uuid v1.0.3 // indirect
+	github.com/hashicorp/terraform-plugin-go v0.22.2 // indirect
+	github.com/hashicorp/terraform-registry-address v0.2.3 // indirect
+	github.com/hashicorp/terraform-svchost v0.1.1 // indirect
+	github.com/hashicorp/yamux v0.1.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
+	github.com/oklog/run v1.0.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240227224415-6ceb2ff114de // indirect
+	google.golang.org/grpc v1.63.2 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 )
 `;
 
