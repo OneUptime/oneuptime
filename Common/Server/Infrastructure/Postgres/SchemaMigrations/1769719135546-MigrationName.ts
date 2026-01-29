699c1d434118881b90ff645a5b7dd6a95f468f52
Nawaz Dhandala <hello@nawazdhandala.com>
2026-01-29 20:42:13 +0000

    feat: Update MigrationName1769719135546 to include down method and fix formatting

--- Common/Server/Infrastructure/Postgres/SchemaMigrations/1769719135546-MigrationName.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/1769719135546-MigrationName.ts
@@ -1,20 +1,35 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class MigrationName1769719135546 implements MigrationInterface {
-    name = 'MigrationName1769719135546'
+  public name = "MigrationName1769719135546";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "IncidentMember" DROP COLUMN "startsAt"`);
-        await queryRunner.query(`ALTER TABLE "IncidentMember" DROP COLUMN "endsAt"`);
-        await queryRunner.query(`ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "rotation" SET DEFAULT '{"_type":"Recurring","value":{"intervalType":"Day","intervalCount":{"_type":"PositiveNumber","value":1}}}'`);
-        await queryRunner.query(`ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "restrictionTimes" SET DEFAULT '{"_type":"RestrictionTimes","value":{"restictionType":"None","dayRestrictionTimes":null,"weeklyRestrictionTimes":[]}}'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "IncidentMember" DROP COLUMN "startsAt"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "IncidentMember" DROP COLUMN "endsAt"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "rotation" SET DEFAULT '{"_type":"Recurring","value":{"intervalType":"Day","intervalCount":{"_type":"PositiveNumber","value":1}}}'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "restrictionTimes" SET DEFAULT '{"_type":"RestrictionTimes","value":{"restictionType":"None","dayRestrictionTimes":null,"weeklyRestrictionTimes":[]}}'`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "restrictionTimes" SET DEFAULT '{"_type": "RestrictionTimes", "value": {"restictionType": "None", "dayRestrictionTimes": null, "weeklyRestrictionTimes": []}}'`);
-        await queryRunner.query(`ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "rotation" SET DEFAULT '{"_type": "Recurring", "value": {"intervalType": "Day", "intervalCount": {"_type": "PositiveNumber", "value": 1}}}'`);
-        await queryRunner.query(`ALTER TABLE "IncidentMember" ADD "endsAt" TIMESTAMP WITH TIME ZONE`);
-        await queryRunner.query(`ALTER TABLE "IncidentMember" ADD "startsAt" TIMESTAMP WITH TIME ZONE`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "restrictionTimes" SET DEFAULT '{"_type": "RestrictionTimes", "value": {"restictionType": "None", "dayRestrictionTimes": null, "weeklyRestrictionTimes": []}}'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "rotation" SET DEFAULT '{"_type": "Recurring", "value": {"intervalType": "Day", "intervalCount": {"_type": "PositiveNumber", "value": 1}}}'`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "IncidentMember" ADD "endsAt" TIMESTAMP WITH TIME ZONE`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "IncidentMember" ADD "startsAt" TIMESTAMP WITH TIME ZONE`,
+    );
+  }
 }
--- Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
@@ -487,5 +487,5 @@ export default [
   MigrationName1769629928240,
   RenameEvaluateOverTimeInCriteriaFilter1769676117342,
   MigrationName1769712834410,
-  MigrationName1769719135546
+  MigrationName1769719135546,
 ];
