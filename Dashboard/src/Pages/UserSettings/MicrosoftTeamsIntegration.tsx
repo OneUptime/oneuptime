8f63e93eba04b43204e84ab28cd3eee9f1c6b972
Nawaz Dhandala <hello@nawazdhandala.com>
2026-02-02 18:26:52 +0000

    feat: Replace ComingSoon component with MicrosoftTeamsIntegration for immediate functionality

--- Dashboard/src/Pages/UserSettings/MicrosoftTeamsIntegration.tsx
+++ Dashboard/src/Pages/UserSettings/MicrosoftTeamsIntegration.tsx
@@ -1,14 +1,9 @@
 import PageComponentProps from "../PageComponentProps";
 import React, { FunctionComponent, ReactElement } from "react";
-import ComingSoon from "Common/UI/Components/ComingSoon/ComingSoon";
+import MicrosoftTeamsIntegration from "../../Components/MicrosoftTeams/MicrosoftTeamsIntegration";
 
 const Settings: FunctionComponent<PageComponentProps> = (): ReactElement => {
-  return (
-    <ComingSoon
-      title="Microsoft Teams Integration is coming soon, but you can still integrate Teams with Workflows!"
-      description="We are working hard to bring you the Microsoft Teams integration. In the meantime, you can still integrate with Workflows to receive alerts in Microsoft Teams. Please click on Workflows in the top navigation to get started."
-    />
-  );
+  return <MicrosoftTeamsIntegration onConnected={() => {}} onDisconnected={() => {}} />;
 };
 
 export default Settings;
