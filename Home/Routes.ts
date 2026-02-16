8e91a786f92b872aa91dc26d8b3809272682b131
Nawaz Dhandala <hello@nawazdhandala.com>
2026-02-16 10:39:43 +0000

    refactor: streamline route definition for CLI endpoint

--- Home/Routes.ts
+++ Home/Routes.ts
@@ -1271,19 +1271,16 @@ const HomeFeatureSet: FeatureSet = {
       },
     );
 
-    app.get(
-      "/tool/cli",
-      (_req: ExpressRequest, res: ExpressResponse) => {
-        const seo: PageSEOData & { fullCanonicalUrl: string } = getSEOForPath(
-          "/tool/cli",
-          res.locals["homeUrl"] as string,
-        );
-        res.render(`${ViewsPath}/cli`, {
-          enableGoogleTagManager: IsBillingEnabled,
-          seo,
-        });
-      },
-    );
+    app.get("/tool/cli", (_req: ExpressRequest, res: ExpressResponse) => {
+      const seo: PageSEOData & { fullCanonicalUrl: string } = getSEOForPath(
+        "/tool/cli",
+        res.locals["homeUrl"] as string,
+      );
+      res.render(`${ViewsPath}/cli`, {
+        enableGoogleTagManager: IsBillingEnabled,
+        seo,
+      });
+    });
 
     app.get(
       "/enterprise/overview",
