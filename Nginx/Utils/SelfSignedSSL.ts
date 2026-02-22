8ba6d5d058b3101bd4353df85fa6f762722d3134
Nawaz Dhandala <hello@nawazdhandala.com>
2026-02-22 09:18:16 +0000

    refactor: remove SelfSignedSSL utility class

--- Nginx/Utils/SelfSignedSSL.ts
+++
@@ -1,9 +0,0 @@
-import Exec from "Common/Server/Utils/Execute";
-
-export default class SelfSignedSSL {
-  public static async generate(path: string, host: string): Promise<void> {
-    await Exec.executeCommand(
-      `mkdir -p ${path} && openssl req -new -x509 -nodes -subj "/C=US/ST=NY/L=NYC/O=Global Security/OU=IT Department/CN=example.com" -out ${path}/${host}.crt -keyout ${path}/${host}.key -days 99999 && chmod -R 777 ${path}`,
-    );
-  }
-}
