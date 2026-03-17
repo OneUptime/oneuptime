af7a3a9286273b31b1ab2e69bfef480f338a1d05
Nawaz Dhandala <hello@nawazdhandala.com>
2026-03-17 15:31:07 +0000

    feat: Add migration for KubernetesCluster and KubernetesClusterLabel constraints and indexes

--- Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
@@ -267,6 +267,7 @@ import { MigrationName1773344537755 } from "./1773344537755-MigrationName";
 import { MigrationName1773402621107 } from "./1773402621107-MigrationName";
 import { MigrationName1773676206197 } from "./1773676206197-MigrationName";
 import { MigrationName1774000000000 } from "./1774000000000-MigrationName";
+import { MigrationName1773761409952 } from "./1773761409952-MigrationName";
 
 export default [
   InitialMigration,
@@ -538,4 +539,5 @@ export default [
   MigrationName1773402621107,
   MigrationName1773676206197,
   MigrationName1774000000000,
+  MigrationName1773761409952
 ];
