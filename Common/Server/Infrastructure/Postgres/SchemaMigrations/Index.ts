342028fb9599640ba38bded7abbb48d3474b3d03
Nawaz Dhandala <hello@nawazdhandala.com>
2026-04-09 12:30:56 +0100

    Add MigrationName1774559064921 to schema migrations

--- Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/Index.ts
@@ -275,6 +275,7 @@ import { MigrationName1774524742178 } from "./1774524742178-MigrationName";
 import { MigrationName1774524742179 } from "./1774524742179-MigrationName";
 import { MigrationName1774559064919 } from "./1774559064919-MigrationName";
 import { MigrationName1774559064920 } from "./1774559064920-MigrationName";
+import { MigrationName1774559064921 } from "./1774559064921-MigrationName";
 
 export default [
   InitialMigration,
@@ -554,4 +555,5 @@ export default [
   MigrationName1774524742179,
   MigrationName1774559064919,
   MigrationName1774559064920,
+  MigrationName1774559064921
 ];
