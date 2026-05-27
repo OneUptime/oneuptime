4245bb2d7a819a2871461b514fb350511d0a7eac
Nawaz Dhandala <hello@nawazdhandala.com>
2026-05-27 15:22:07 +0100

    feat(migrations): add IF EXISTS clause to index drop and create statements in MigrationName1779879993421

--- Common/Server/Infrastructure/Postgres/SchemaMigrations/1779879993421-MigrationName.ts
+++ Common/Server/Infrastructure/Postgres/SchemaMigrations/1779879993421-MigrationName.ts
@@ -5,7 +5,7 @@ export class MigrationName1779879993421 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DROP INDEX "public"."IDX_telemetry_exception_project_service_fingerprint"`,
+      `DROP INDEX IF EXISTS "public"."IDX_telemetry_exception_project_service_fingerprint"`,
     );
     await queryRunner.query(
       `ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "rotation" SET DEFAULT '{"_type":"Recurring","value":{"intervalType":"Day","intervalCount":{"_type":"PositiveNumber","value":1}}}'`,
@@ -14,13 +14,13 @@ export class MigrationName1779879993421 implements MigrationInterface {
       `ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "restrictionTimes" SET DEFAULT '{"_type":"RestrictionTimes","value":{"restictionType":"None","dayRestrictionTimes":null,"weeklyRestrictionTimes":[]}}'`,
     );
     await queryRunner.query(
-      `CREATE UNIQUE INDEX "IDX_1f55d43a0b73e883bb226158c7" ON "TelemetryException" ("projectId", "serviceId", "fingerprint") `,
+      `CREATE UNIQUE INDEX IF NOT EXISTS "IDX_1f55d43a0b73e883bb226158c7" ON "TelemetryException" ("projectId", "serviceId", "fingerprint") `,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DROP INDEX "public"."IDX_1f55d43a0b73e883bb226158c7"`,
+      `DROP INDEX IF EXISTS "public"."IDX_1f55d43a0b73e883bb226158c7"`,
     );
     await queryRunner.query(
       `ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "restrictionTimes" SET DEFAULT '{"_type": "RestrictionTimes", "value": {"restictionType": "None", "dayRestrictionTimes": null, "weeklyRestrictionTimes": []}}'`,
@@ -29,7 +29,7 @@ export class MigrationName1779879993421 implements MigrationInterface {
       `ALTER TABLE "OnCallDutyPolicyScheduleLayer" ALTER COLUMN "rotation" SET DEFAULT '{"_type": "Recurring", "value": {"intervalType": "Day", "intervalCount": {"_type": "PositiveNumber", "value": 1}}}'`,
     );
     await queryRunner.query(
-      `CREATE UNIQUE INDEX "IDX_telemetry_exception_project_service_fingerprint" ON "TelemetryException" ("projectId", "serviceId", "fingerprint") `,
+      `CREATE UNIQUE INDEX IF NOT EXISTS "IDX_telemetry_exception_project_service_fingerprint" ON "TelemetryException" ("projectId", "serviceId", "fingerprint") `,
     );
   }
 }
