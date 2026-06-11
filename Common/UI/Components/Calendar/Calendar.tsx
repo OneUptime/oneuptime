4ddf573b3b366a4f0fcdcfdc7471cc8096e6ceb4
mahdi dolati <mahdi.dlt13830510@gmail.com>
2026-06-11 16:05:24 +0330

    fix(calendar): fire onRangeChange on initial render

--- Common/UI/Components/Calendar/Calendar.tsx
+++ Common/UI/Components/Calendar/Calendar.tsx
@@ -4,7 +4,7 @@ import Color from "../../../Types/Color";
 import OneUptimeDate from "../../../Types/Date";
 import StartAndEndTime from "../../../Types/Time/StartAndEndTime";
 import moment from "moment-timezone";
-import React, { FunctionComponent, ReactElement, useMemo } from "react";
+import React, { FunctionComponent, ReactElement, useEffect, useMemo } from "react";
 import {
   Calendar,
   DateLocalizer,
@@ -40,6 +40,35 @@ const CalendarElement: FunctionComponent<ComponentProps> = (
     return {
       defaultDate: OneUptimeDate.getCurrentDate(),
     };
+  }, []);
+
+  // react-big-calendar does not fire onRangeChange on the initial render.
+  // Compute the initial range here so callers can fetch events immediately.
+  useEffect(() => {
+    const view: DefaultCalendarView =
+      props.defaultCalendarView || DefaultCalendarView.Week;
+    const now: Date = defaultDate;
+
+    let startTime: Date;
+    let endTime: Date;
+
+    if (view === DefaultCalendarView.Week) {
+      startTime = moment(now).startOf("week").toDate();
+      endTime = moment(now).endOf("week").toDate();
+    } else if (view === DefaultCalendarView.Month) {
+      startTime = moment(now).startOf("month").toDate();
+      endTime = moment(now).endOf("month").toDate();
+    } else if (view === DefaultCalendarView.Day) {
+      startTime = moment(now).startOf("day").toDate();
+      endTime = moment(now).endOf("day").toDate();
+    } else {
+      // Agenda: 30-day window
+      startTime = moment(now).startOf("day").toDate();
+      endTime = moment(now).add(30, "days").endOf("day").toDate();
+    }
+
+    props.onRangeChange({ startTime, endTime });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const eventStyleGetter: EventPropGetter<any> = (
