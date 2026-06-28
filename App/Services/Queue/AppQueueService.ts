680b250f890c46d17545be8299754b105eb7554a
Nawaz Dhandala <hello@nawazdhandala.com>
2026-06-28 12:26:00 -0700

    fix queue size for runbooks

--- App/Services/Queue/AppQueueService.ts
+++ App/Services/Queue/AppQueueService.ts
@@ -2,12 +2,17 @@ import Queue, { QueueName } from "Common/Server/Infrastructure/Queue";
 
 export default class AppQueueService {
   public static async getQueueSize(): Promise<number> {
-    const [workerSize, workflowSize, telemetrySize]: [number, number, number] =
-      await Promise.all([
+    const [workerSize, workflowSize, telemetrySize, runbookSize]: [
+      number,
+      number,
+      number,
+      number,
+    ] = await Promise.all([
         Queue.getQueueSize(QueueName.Worker),
         Queue.getQueueSize(QueueName.Workflow),
         Queue.getQueueSize(QueueName.Telemetry),
+        Queue.getQueueSize(QueueName.Runbook),
       ]);
-    return workerSize + workflowSize + telemetrySize;
+    return workerSize + workflowSize + telemetrySize + runbookSize;
   }
 }
